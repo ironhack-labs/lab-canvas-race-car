77467ebb6b675e306f1ed24c9a75ad9d6042d3f3
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-10-26 19:48:26 +0200

    Primera Iteracion Terminada, a falta de la linea central discontinua

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,36 @@
 window.onload = function() {
+//DECLARAR GLOBALMENTE LAS VARIABLES
+  var canvas = document.getElementById('example');
+  var ctx = canvas.getContext('2d');
+
+// DIBUJAR EL TABLERO DEL JUEGO
+  function draw() {
+      ctx.fillStyle = "green";
+      ctx.fillRect(0, 0, 400, 700);
+
+      ctx.fillStyle = "grey";
+      ctx.fillRect(50, 0, 300, 700);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(195, 0, 10, 700);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(70, 0, 20, 700);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(310, 0, 20, 700);
+
+  }
+
+
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
-
   function startGame() {
-
+  draw();
   }
+
+
 };
