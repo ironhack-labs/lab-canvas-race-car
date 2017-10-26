4c6fbf04b633c69c2e6069e197507498ba202fd7
acampos91 <acampos91@hotmail.com>
2017-10-26 21:19:41 +0200

    primera iteración

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
@@ -5,5 +6,30 @@ window.onload = function() {
 
   function startGame() {
 
-  }
+    function draw () {
+    var canvas = document.getElementById('startGameb');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 500, 550);
+
+    ctx.fillStyle = "grey";
+    ctx.fillRect(30, 0, 440, 550);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(50, 0, 10, 550);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(440, 0, 10, 550);
+
+    ctx.setLineDash([20, 30]);
+    ctx.moveTo(250, 0);
+    ctx.lineTo(250, 550);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+    ctx.stroke ();
+   }
+   draw ()
+}
+
 };
