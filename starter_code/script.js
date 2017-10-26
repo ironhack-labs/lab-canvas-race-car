70230a09ac9c0619068f98d49db8c306c14e180b
pablohk <pablusmmid@gmail.com>
2017-10-26 21:46:09 +0200

    Iteration 21 finished

--- starter_code/script.js
+++ starter_code/script.js
@@ -6,15 +6,15 @@ window.onload = function() {
   function startGame() {
     var canvas = document.getElementById('road-board');
     var ctx = canvas.getContext('2d');
-
-    drawRoad(ctx);
-
-  }
-
-  function drawRoad(ctx) {
     var road = new Road([300, 600], [20, 10, 30, 5, 10], ["gray", "white", "green"]);
     console.log(road);
-    ctx.beginPath();
+    drawRoad(ctx,road);
+    drawCar(ctx,road);
+  }
+
+  function drawRoad(ctx,road) {
+
+
     // Draw road
     ctx.fillStyle = road.colors[0];
     ctx.fillRect(0, 0, road.size[0], road.size[1]);
@@ -32,17 +32,25 @@ window.onload = function() {
     // Draw center line
     ctx.fillRect((road.size[0] - road.elements[3]) / 2, 0, road.elements[3], road.size[1]);
 
-
     // Complete center line
-       ctx.setLineDash([10, 5]);
-       ctx.moveTo(road.size[0]/2,0);
-       ctx.lineTo(road.size[0]/2, road.size[1]);
-       ctx.lineWidth = road.elements[3]+5;
-       ctx.strokeStyle=road.colors[0];
-       ctx.stroke();
+    ctx.setLineDash([10, 5]);
+    ctx.moveTo(road.size[0] / 2, 0);
+    ctx.lineTo(road.size[0] / 2, road.size[1]);
+    ctx.lineWidth = road.elements[3] + 5;
+    ctx.strokeStyle = road.colors[0];
+    ctx.stroke();
 
-  ctx.fill();
-}
+  }
+
+  function drawCar(ctx,road) {
+    var carImage = new Image();
+
+    carImage.onload = function() {
+      ctx.drawImage(carImage, (road.size[0]/2)-40, (road.size[1]-510), 80, 50);
+    };
+    carImage.src = "./images/car.png";
+  }
+
 };
 
 
