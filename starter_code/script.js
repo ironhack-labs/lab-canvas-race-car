00ae55e2d32c175e4ae9a83b9c5ab898a2db2d70
Sara <saraeme20@gmail.com>
2017-10-26 22:44:18 +0200

    on the way to 3rd iteration. Road is there and car moves according to arrows but cant make it appear on the DOM. One obstacle painted.

--- starter_code/script.js
+++ starter_code/script.js
@@ -9,27 +9,58 @@ window.onload = function() {
     var canvas = document.getElementById('road');
     var ctx = canvas.getContext('2d');
     drawRoad();
-    drawCar();
+    draw(car);
 
 
+
+    document.onkeydown = function(e) {
+    switch (e.keyCode) {
+    case 37: car.moveLeft();  console.log('left',  car); break;
+    case 39: car.moveRight(); console.log('right', car); break;
   }
+  updateCanvas();
+};
 
+function updateCanvas() {
+  ctx.clearRect(0,0,1500,1700);
+  ctx.fillText("car_x: " + car.x, 580,40);
+  ctx.fillText("car_y: " + car.y, 580,60);
+  draw(car);
+}
 
+updateCanvas();
+
+  };
 
 
 };
 
-function drawCar(){
-  var canvas = document.getElementById('road');
-  var ctx = canvas.getContext('2d');
-  var img = new Image();
-  imgScale = 640/480;
-    img.onload = function() {
-    ctx.drawImage(img,130,350,30*imgScale,80);
-    };
-    img.src = "images/car.png";
 
+
+    function draw(car) {
+      var car = {
+        x: 200,
+        y: 200,
+        moveLeft:  function() { this.x -= 25 ;},
+        moveRight: function() { this.x += 25 ;},
+  };
+
+    var img = new Image();
+    imgScale = 640/480;
+    img.onload = function() {
+     ctx.drawImage(img, car.x, car.y, 30*imgScale,80);
+      };
+    img.src = "images/car.png";
 };
+
+
+function drawOb() {
+    var canvas = document.getElementById('road');
+    var ctx = canvas.getContext('2d');
+   ctx.fillStyle = "#61380B";
+    ctx.fillRect(20, 20, 280, 20);}
+
+  drawOb();
 
 function drawRoad() {
 var canvas = document.getElementById('road');
@@ -61,5 +92,9 @@ ctx.fillRect(150, 350, 10, 30);
 ctx.fillStyle = "#ffffff";
 ctx.fillRect(150, 400, 10, 30);
 
+drawOb();
 
-        }
+
+
+
+        };
