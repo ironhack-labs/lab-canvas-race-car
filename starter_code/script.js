9063921fbc75c5d6315c5e9d2d827a0b0df39a72
gemaderus <hi@gemaderus.com>
2017-10-27 09:56:25 +0200

    Third iteration

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,22 +1,21 @@
+var car;
+
 window.onload = function() {
 
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
 
   function draw() {
-      // ctx.fillRect(25, 25, 100, 100);
-      // ctx.clearRect(45, 45, 60, 60);
-      //ctx.strokeRect(50, 50, 50, 50);
       ctx.fillStyle = "green";
       ctx.fillRect(5, 0, 500, 600);
-      //ctx.clearRect(5, 5, 400, 500);
-      // ctx.strokeRect(5, 0, 400, 500);
       ctx.fillStyle = "grey";
       ctx.fillRect(50, 0, 400, 600);
       ctx.fillStyle = "white";
       ctx.fillRect(60, 0, 20, 600);
       ctx.fillRect(420, 0, 20, 600);
 
+      ctx.strokeStyle = "white";
       ctx.beginPath();
       ctx.setLineDash([15, 6]);
       ctx.moveTo(250, 0);
@@ -24,36 +23,54 @@ window.onload = function() {
 
       ctx.stroke();
       ctx.fillStyle = "white";
-      var img = new Image();   // Create new img element
-      img.src = './images/car.png';
-      imgScale = 158/319;
-      img.onload = function() {
-      ctx.drawImage(img, 225, 500, 100*imgScale, 100);
-    }
-
-
-
-//     imgScale = 640/480;
-//   img.onload = function() {
-//     ctx.drawImage(img, 0, 0,150*imgScale,150);
-//   };
-
   }
-
-  draw();
-
-
 
   document.getElementById("start-button").onclick = function() {
     var buttonStart = $("#start-button");
-    //$("#game-board").addClass("is-visible");
     startGame();
   };
 
   function startGame() {
+    car = {
+      x: 225,
+      y: 500,
 
-    // var buttonStart = $("#start-button");
-    // $("#game-board").addClass(".is-visible");
+      moveLeft:  function() {
+        if(this.x > 40){
+          this.x -= 25
+        }
+      },
 
+      moveRight: function() {
+        if(this.x < 420){
+          this.x += 25;
+        }
+      }
     }
+
+    function drawCar(car) {
+      var img = new Image();   // Create new img element
+        img.src = './images/car.png';
+        imgScale = 158/319;
+        img.onload = function() {
+          ctx.drawImage(img, car.x, car.y, 100*imgScale, 100);
+        }
+    }
+
+    document.onkeydown = function(e) {
+      switch (e.keyCode) {
+        case 37: car.moveLeft();  console.log('left',  car); break;
+        case 39: car.moveRight(); console.log('right', car); break;
+      }
+      updateCanvas();
+    }
+
+    function updateCanvas() {
+      ctx.clearRect(0,0,1500,1700);
+      draw();
+      drawCar(car);
+    }
+
+    updateCanvas();
+  }
 };
