e0953324e07398557f268a6148face6577e74fa4
Camila Maya <camilamaya21@gmail.com>
2017-10-27 18:13:15 +0200

    3. Third Iteration: Make Player´s Car move right and left

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,55 +1,38 @@
 window.onload = function() {
+
+  //Start Button
   document.getElementById('start-button').onclick = function() {
-
-
     startGame();
-
-
-  };
-
-  function startGame() {
-
-    function drawGameBoard() {
-      var ctx = document.getElementById('race-game').getContext('2d');
-
-      ctx.fillStyle = 'rgb(0, 129, 0)';
-      ctx.fillRect(0, 0, 120, 900);
-      ctx.fillStyle = 'rgb(128, 128, 128)';
-      ctx.fillRect(120, 0, 50, 900);
-      ctx.fillStyle = 'rgb(255, 255, 255)';
-      ctx.fillRect(145, 0, 50, 900);
-      ctx.fillStyle = 'rgb(128, 128, 128)';
-      ctx.fillRect(170, 0, 460, 900);
-      ctx.fillStyle = 'rgb(255, 255, 255)';
-      ctx.fillRect(630, 0, 50, 900);
-      ctx.fillStyle = 'rgb(128, 128, 128)';
-      ctx.fillRect(655, 0, 50, 900);
-      ctx.fillStyle = 'rgb(0, 129, 0)';
-      ctx.fillRect(680, 0, 120, 900);
-      ctx.beginPath();
-      ctx.setLineDash([30, 30]);
-      ctx.moveTo(400, 1000);
-      ctx.lineTo(400, 10);
-      ctx.lineWidth = 10;
-      ctx.strokeStyle = 'rgb(255, 255, 255)';
-      ctx.stroke();
-    }
-
-    function drawCar() {
-      var ctx = document.getElementById('race-game').getContext('2d');
-      var img = new Image();
-      imgScale = 640/480;
-      img.onload = function () {
-        ctx.drawImage (img, 350, 550, 100, 190);
-      };
-      img.src = './images/car.png';
-    }
-
-    drawCar();
+    draw(car);
     drawGameBoard();
-
   }
 
+//Keydown
+  function startGame() {
+    window.addEventListener('keydown', doKeyDown, true);
 
+    function doKeyDown(e) {
+      switch (e.keyCode) {
+        case 37:
+          car.moveLeft();
+          console.log('left', car);
+          break;
+        case 39:
+          car.moveRight();
+          console.log('right', car);
+          break;
+      }
+      updateCanvas();
+    }
+  }
+
+  function updateCanvas() {
+    ctx.clearRect(0, 0, 1000, 1000);
+    ctx.fillText("Car_x: " + car.x, 580, 40);
+    ctx.fillText("Car_y: " + car.y, 580, 60);
+    draw(car);
+    drawGameBoard();
+  }
+  doKeyDown(car);
 
 };
