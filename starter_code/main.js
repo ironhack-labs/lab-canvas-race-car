bcb81b59d6f75c4b0c94202d31a7b29b59ddfb38
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-27 09:36:52 +0200

    First iteration done

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,6 +1,23 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
+
+    var canvas = document.getElementById('road');
+    var ctx = canvas.getContext('2d');
+      ctx.fillStyle = "green";
+      ctx.fillRect(0, 0, 400, 700);
+
+      ctx.fillStyle = "grey";
+      ctx.fillRect(50, 0, 300, 700);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(195, 0, 10, 800);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(70, 0, 20, 800);
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(310, 0, 20, 700);
   };
 
   function startGame() {
