a42c9774cc8084c3ca93b52cf28da16e437ed8b1
Ernesmh <ernesmh@gmail.com>
2017-10-27 17:37:48 +0200

    el coche se mueve. intento obstaculos

--- starter_code/main.js
+++ starter_code/main.js
@@ -2,12 +2,20 @@
   this.x = 148;
   this.y = 90;
 
-}
+};
 
-Car.prototype.moveLeft(){
-  return this.x -=1;
-}
+Car.prototype.moveLeft = function(){
+  this.x = this.x - 5;
+};
 
-Car.prototype.moveRight(){
-  return this.x +=1;
-}
+Car.prototype.moveRight = function(){
+  this.x +=5;
+};
+
+Car.prototype.moveUp = function(){
+  this.y = this.y - 5;
+};
+
+Car.prototype.moveDown = function(){
+  this.y +=5;
+};
--- starter_code/script.js
+++ starter_code/script.js
@@ -3,88 +3,99 @@ window.onload = function() {
   var ctx = canvas.getContext('2d');
   var car = new Car();
   var img = new Image();
-  var vimScale = 640/480;
-  img.src = 'images/car.png';
+  var myObstacles = [];
+  //var vimScale = 640 / 480; 
+  img.src = 'images/car.png';
+
   document.getElementById("start-button").onclick = function() {
     startGame();
-
-  };
-
-  document.onkeydown(e){
-    if(){
-
-    }
   }
 
-  function paintCar(){
-       ctx.drawImage(img, car.x, car.y,31,63);
+  document.onkeydown = function(e) {
+    if (e.keyCode == 37 && (car.x >= 75 && car.x <= 190)) {
+      car.moveLeft();
+    }
+    if (e.keyCode == 39 && (car.x >= 75 && car.x <= 190)) {
+      car.moveRight();
+    }
+    if (e.keyCode == 38 && (car.y >= 0 && car.y <= 170)) {
+      car.moveUp();
+    }
+    if (e.keyCode == 40 && (car.y >= 0 && car.y <= 170)) {
+      car.moveDown();
+    }
+    startGame();
   }
 
-    function road(){
-      ctx.fillStyle = 'rgb(54, 122, 44)';
-      ctx.fillRect(50, 0, 200, 150);
+  function paintCar() {  
+    ctx.drawImage(img, car.x, car.y, 30, 30);
+  }
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(70, 0, 160, 150);
+  function road() {
+    ctx.fillStyle = 'rgb(54, 122, 44)';
+    ctx.fillRect(50, 0, 200, 150);
 
-      ctx.fillStyle = 'rgb(252, 252, 252)';
-      ctx.fillRect(75, 0, 5, 150);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(70, 0, 160, 150);
 
-      ctx.fillStyle = 'rgb(252, 252, 252)';
-      ctx.fillRect(220, 0, 5, 150);
+    ctx.fillStyle = 'rgb(252, 252, 252)';
+    ctx.fillRect(75, 0, 5, 150);
 
-      ctx.fillStyle = 'rgb(252, 252, 252)';
-      ctx.fillRect(148, 0, 5, 150);
+    ctx.fillStyle = 'rgb(252, 252, 252)';
+    ctx.fillRect(220, 0, 5, 150);
 
-      // LINEAS
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 2, 5, 5);
+    ctx.fillStyle = 'rgb(252, 252, 252)';
+    ctx.fillRect(148, 0, 5, 150);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 12, 5, 5);
+    // LINEAS
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 2, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 22, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 12, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 32, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 22, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 42, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 32, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 52, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 42, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 62, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 52, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 72, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 62, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 82, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 72, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 92, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 82, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 102, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 92, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 112, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 102, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 122, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 112, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 132, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 122, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 142, 5, 5);
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 132, 5, 5);
 
-      ctx.fillStyle = 'rgb(177, 187, 209)';
-      ctx.fillRect(148, 152, 5, 5);
-    }
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 142, 5, 5);
+
+    ctx.fillStyle = 'rgb(177, 187, 209)';
+    ctx.fillRect(148, 152, 5, 5);
+  }
 
   function startGame() {
     road();
@@ -93,40 +104,40 @@ window.onload = function() {
 
 
 
-// var car= {
-//   x: 131,
-//   y:90,
-//   moveLeft : function() {this.y -= 5},
-//   moveLeft : function() {this.y -= 5},
-// }
-//
-// document.onkeydown = function (e){
-//   switch (e.keyCode){
-//     case 16: car.moveLeft();
-//     break;
-//     case 17: car.moveRight();
-//     break;
-//   }
-//   updateCanvas();
-// }
-//
-// function draw(car){
-//   var img = new Image();
-//   img_onload = function(){
-//     ctx.drawImage(img, car.x, car.y, 200, 200);
-//   }
-//   img.src="images/car.png";
-// }
-//
-// function updateCanvas(){
-//   ctx.clearRect(0,0,1500,1700);
-//   draw(car);
-// }
-//
-//
+    // var car= {
+    //   x: 131,
+    //   y:90,
+    //   moveLeft : function() {this.y -= 5},
+    //   moveLeft : function() {this.y -= 5},
+    // }
+    //
+    // document.onkeydown = function (e){
+    //   switch (e.keyCode){
+    //     case 16: car.moveLeft();
+    //     break;
+    //     case 17: car.moveRight();
+    //     break;
+    //   }
+    //   updateCanvas();
+    // }
+    //
+    // function draw(car){
+    //   var img = new Image();
+    //   img_onload = function(){
+    //     ctx.drawImage(img, car.x, car.y, 200, 200);
+    //   }
+    //   img.src="images/car.png";
+    // }
+    //
+    // function updateCanvas(){
+    //   ctx.clearRect(0,0,1500,1700);
+    //   draw(car);
+    // }
+    //
+    //
 
 
-}
+  }
 
 
 
