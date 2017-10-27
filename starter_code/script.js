2d48811017b874edbf6321ffc4dafd3e0056511f
Rocio Garcia <gooblyn@gmail.com>
2017-10-27 11:11:42 +0200

    Third iteration done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,59 +1,66 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+  var canvas = document.getElementById("board");
+  var ctx = canvas.getContext('2d');
+  var car = new Car();
 
   // Function that draws the board in the canvas
-  function createBoard (){
-    var canvas = document.getElementById("board");
-    var ctx = canvas.getContext('2d');
+  function drawRoad() {
     // Fill the background color - grey
     ctx.fillStyle = "#BFC9CA";
-    ctx.fillRect(0, 0, 700, 650);
+    ctx.fillRect(0, 0, 600, 550);
     // Fill the green parts
     ctx.fillStyle = "#15B405";
-    ctx.fillRect(0, 0, 40, 650);
-    ctx.fillRect(660 ,0, 40, 650);
+    ctx.fillRect(0, 0, 40, 550);
+    ctx.fillRect(560, 0, 40, 550);
     // Fill the white parts
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(60, 0, 20, 650);
-    ctx.fillRect(620, 0, 20, 650);
-    // Create the dashed line
+    ctx.fillRect(60, 0, 20, 550);
+    ctx.fillRect(520, 0, 20, 550);
+  }
+
+  function drawCar() {
+    var img = new Image();
+    img.src = "./images/car.png";
+    img.onload = function() {
+      ctx.drawImage(img, car.x, car.y, car.width, car.height);
+    };
+  };
+
+  function drawLine() {
     ctx.lineWidth = 10;
     ctx.strokeStyle = "#FFFFFF";
     ctx.beginPath();
-    ctx.setLineDash([30,50]);
-    ctx.moveTo(350, 0);
-    ctx.lineTo(350, 650);
+    ctx.setLineDash([30, 50]);
+    ctx.moveTo(300, 0);
+    ctx.lineTo(300, 550);
     ctx.stroke();
-    var car = {
-      x: 320,
-      y: 550,
-      moveLeft: function(){this.x-30;},
-      moveRigth: function(){this.x+30;},
-    };
-    drawCar(car, ctx);
-  }
-
-  function drawCar (car, board){
-    var img = new Image();
-    img.src = "./images/car.png";
-    img.onload = function(){
-      board.drawImage(img, 320, 550, 60, 80);
-    };
   };
 
-  document.onkeydown = function(e, car) {
-  switch (e.keyCode) {
-    case 37: car.moveLeft();  console.log('left',  car); break;
-    case 39: car.moveRight(); console.log('right', car); break;
+  function createBoard() {
+    drawRoad();
+    drawLine();
+    drawCar();
   }
-  drawCar(car);
-};
-
 
   function startGame() {
     createBoard();
+  };
 
+  document.onkeydown = function(e) {
+    if (e.keyCode == 37) { // Move to the left
+      if (car.x > 80) {
+        car.moveLeft();
+      }
+    }
+    if (e.keyCode == 39) { // Move to the rigth
+      if (car.x < 500){
+        car.moveRigth();
+      }
+    }
+    createBoard();
+  };
+
+  document.getElementById("start-button").onclick = function() {
+    startGame();
   };
 };
