a0886471f4815757cf3bd773610a48ec804a167d
pablohk <pablusmmid@gmail.com>
2017-10-27 16:43:38 +0200

    Iteration 3 finsihed. Remove css for canvas tag in style.css, It make error in heigth px. Change definition the center line dashed

--- starter_code/main.js
+++ starter_code/main.js
@@ -2,9 +2,9 @@ window.onload = function() {
 
   var canvas = document.getElementById('road-board');
   var ctx = canvas.getContext('2d');
-  var road = new Road([300, 600], [20, 10, 30, 5, 10], ["gray", "white", "green"]);
-  var car = new Car((road.size[0] / 2) - 20, 120, 40, 20);
-  console.log(road);
+  var road = new Road([300, 600], [20, 10, 30, 7, 10], ["gray", "white", "green"]);
+  var car = new Car((road.size[0] / 2) - 30, road.size[1]-120, 60, 100);
+  console.log(road.size[1]);
 
   document.getElementById("start-button").onclick = function() {
     startGame();
@@ -13,10 +13,14 @@ window.onload = function() {
   window.onkeydown = function(e) {
     switch (e.keyCode) {
       case 37:
-        car.moveLeft();
+        if (car.x > 0) {
+          car.moveLeft();
+        }
         break;
       case 39:
-        car.moveRigth();
+        if ((car.x + car.width) < road.size[0]) {
+          car.moveRigth();
+        }
         break;
       default:
     }
@@ -34,7 +38,8 @@ window.onload = function() {
     ctx.fillStyle = road.colors[0];
     ctx.fillRect(0, 0, road.size[0], road.size[1]);
 
-    // Draw off road
+
+    // Draw off-road
     ctx.fillStyle = road.colors[2];
     ctx.fillRect(0, 0, road.elements[0], road.size[1]);
     ctx.fillRect(road.size[0] - road.elements[0], 0, road.elements[0], road.size[1]);
@@ -45,16 +50,12 @@ window.onload = function() {
     ctx.fillRect(road.size[0] - road.elements[2] - road.elements[1], 0, road.elements[1], road.size[1]);
 
     // Draw center line
-    ctx.fillRect((road.size[0] - road.elements[3]) / 2, 0, road.elements[3], road.size[1]);
-
-    // Complete center line
-    ctx.setLineDash([10, 5]);
+    ctx.setLineDash([40, 30]);
     ctx.moveTo(road.size[0] / 2, 0);
     ctx.lineTo(road.size[0] / 2, road.size[1]);
-    ctx.lineWidth = road.elements[3] + 5;
-    ctx.strokeStyle = road.colors[0];
+    ctx.lineWidth = road.elements[3];
+    ctx.strokeStyle = road.colors[1];
     ctx.stroke();
-
   }
 
   function drawCar() {
