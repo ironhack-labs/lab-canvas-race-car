d7e04e90fda9aea5ec3dd9d98dfdc86e700f43b6
sdediego <sergio.dediego@outlook.com>
2017-10-27 17:38:31 +0200

    Iteracion 4

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,17 +3,19 @@ window.onload = function() {
     function startGame() {
 
     }
-    
+
     var board = new Board();
+    board.start();
     var car = new Car();
     car.setCarImage(board);
 
     document.getElementById("start-button").onclick = function() {
-        startGame();
+        //startGame();
     };
 
     document.onkeydown = function(event) {
         car.move(event.keyCode);
+        board.update();
         car.setCarImage(board);
     };
 };
