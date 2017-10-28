518f4f33db173e8de0c95701d8d501400da44253
gemaderus <hi@gemaderus.com>
2017-10-28 17:47:59 +0200

    Wip.

---
+++ starter_code/main.js
@@ -0,0 +1,95 @@
+//
+//
+//
+//
+// window.onload = function() {
+//
+//
+//   // var Obstacle = function() {
+//   //   ctx.fillStyle = "#950000";
+//   //   ctx.fillRect(this.x, this.y, 120, 40);
+//   //
+//   //   this.x = Math.floor(Math.random(450 - 50) * 50);
+//   //   this.y = 0;
+//   // }
+//
+//   document.getElementById("start-button").onclick = function() {
+//
+//     var car = new Car();
+//     this.moveRigth();
+//     this.moveLeft();
+//     this.drawCar(car);
+//     // startGame();
+//
+//     // var obstacle = new Obstacle ();
+//   };
+//
+// var Car = function() {
+//   x =  225,
+//   y = 500
+// }
+//
+// Car.prototype.moveLeft = function() {
+//   if(this.x > 40){
+//     this.x -= 25
+//   };
+// }
+//
+// Car.prototype.moveRigth = function() {
+//   if(this.x < 420){
+//     this.x += 25;
+//   };
+// }
+//
+// function startGame() {
+//   var car = new Car();
+//   // moveRigth();
+//   // moveLeft();
+//   // this.drawCar(car);
+// }
+//
+// Car.protoype.drawCar = function(car) {
+//   var img = new Image();   // Create new img element
+//   img.src = './images/car.png';
+//   imgScale = 158/319;
+//   img.onload = function() {
+//     ctx.drawImage(img, car.x, car.y, 100*imgScale, 100);
+//   }
+// }
+//
+//   // function startGame() {
+//   //   car = {
+//   //     x: 225,
+//   //     y: 500,
+//   //
+//   //     moveLeft:  function() {
+//   //       if(this.x > 40){
+//   //         this.x -= 25
+//   //       }
+//   //     },
+//   //
+//   //     moveRight: function() {
+//   //       if(this.x < 420){
+//   //         this.x += 25;
+//   //       }
+//   //     }
+//   //   }
+//
+//
+//   //   document.onkeydown = function(e) {
+//   //     switch (e.keyCode) {
+//   //       case 37: car.moveLeft();  console.log('left',  car); break;
+//   //       case 39: car.moveRight(); console.log('right', car); break;
+//   //     }
+//   //     updateCanvas();
+//   //   }
+//   //
+//   //   function updateCanvas() {
+//   //     ctx.clearRect(0,0,1500,1700);
+//   //     draw();
+//   //     // drawCar(car);
+//   //   }
+//   //
+//   //   updateCanvas();
+//   // }
+// // };
