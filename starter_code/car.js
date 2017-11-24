f82bc4e418773372c93f12e5acafb049065b8c3a
mouyaq <mouyaq@gmail.com>
2017-11-24 21:37:51 +0100

    car movement done

--- starter_code/car.js
+++ starter_code/car.js
@@ -1,6 +1,14 @@
+var DOWN_KEY = 40;
+var LEFT_KEY = 37;
+var RIGHT_KEY = 39;
+var UP_KEY = 38;
+
 function Car(canvas, sprite) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
+    // Initial position
+    this.x = 125;
+    this.y = 450;
     this.sprite = new Image();
     this.sprite.src = sprite;
     this.sprite.onload = (function() {
@@ -11,7 +19,7 @@ function Car(canvas, sprite) {
 Car.prototype.draw = function() {
     if (this.isReady()) {
         this.ctx.save();
-        this.ctx.drawImage(this.sprite, 125, 450, 50, 100);
+        this.ctx.drawImage(this.sprite, this.x, this.y, 50, 100);
         this.ctx.save();
     }
 }
@@ -19,3 +27,42 @@ Car.prototype.draw = function() {
 Car.prototype.isReady = function() {
     return this.sprite.isReady;
   }
+
+Car.prototype.onKeyDown = function(event) {
+    console.log(event.keyCode);
+    switch(event.keyCode) {
+        case DOWN_KEY:
+            this.moveDown();
+            break;
+        case LEFT_KEY:
+            this.moveLeft();
+            break;
+        case RIGHT_KEY:
+            this.moveRight();
+            break;
+        case UP_KEY:
+            this.moveUp();
+            break;
+        default:
+    }
+}
+
+Car.prototype.onKeyUp = function(event) {
+
+}
+
+Car.prototype.moveLeft = function() {
+    this.x -= 10;
+}
+
+Car.prototype.moveRight = function() {
+    this.x += 10;
+}
+
+Car.prototype.moveUp = function() {
+    this.y -= 5;
+}
+
+Car.prototype.moveDown = function() {
+    this.y += 5;
+}
--- starter_code/game.js
+++ starter_code/game.js
@@ -1,10 +1,20 @@
 function Game(canvas) {
     this.road = new Road(canvas);
     this.car = new Car(canvas, "images/car.png");
+    document.onkeydown = this.onKeyDown.bind(this);
+    document.onkeyup = this.onKeyUp.bind(this);
 }
 
 Game.prototype.draw = function() {
     this.road.draw();
     this.car.draw();
     window.requestAnimationFrame(this.draw.bind(this));
+}
+
+Game.prototype.onKeyDown = function(event) {
+    this.car.onKeyDown(event);
+}
+
+Game.prototype.onKeyUp = function(event) {
+    this.car.onKeyUp(event);
 }
