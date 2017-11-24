1e36b4445032466e3660de792d244852acdb83c4
mouyaq <mouyaq@gmail.com>
2017-11-24 21:08:07 +0100

    fix: remove comments draw car function

--- starter_code/car.js
+++ starter_code/car.js
@@ -3,17 +3,12 @@ function Car(canvas, sprite) {
     this.ctx = this.canvas.getContext('2d');
     this.sprite = new Image();
     this.sprite.src = sprite;
-
     this.sprite.onload = (function() {
         this.sprite.isReady = true;
       }).bind(this);
 }
 
 Car.prototype.draw = function() {
-    
-    //context.drawImage(img,x,y);
-    //context.drawImage(img,x,y,width,height);
-    //context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
     if (this.isReady()) {
         this.ctx.save();
         this.ctx.drawImage(this.sprite, 125, 450, 50, 100);
