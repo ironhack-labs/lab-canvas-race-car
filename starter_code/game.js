64b3e89bcea95f769c57933f9034622b051c1139
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-24 11:10:49 +0100

    Feat: Road Paint

--- starter_code/game.js
+++ starter_code/game.js
@@ -1,9 +1,31 @@
-function Game(canvasId){
+function Game(canvasId, width, height){
   this.canvas = document.getElementById(canvasId);
   this.ctx = this.canvas.getContext('2d');
 // debugger
   this.car=new Car(this.canvas,"./images/car.png");
+
+  this.x=0;
+  this.y=0;
+  this.width=450;
+  this.height=550;
 }
+
+Game.prototype.paintRoad = function () {
+
+  this.ctx.fillStyle = "rgb(62, 162, 1)";
+  this.ctx.fillRect(0, 0, 25, this.height);
+  this.ctx.fillRect(this.width-25, 0, 25, this.height);
+  this.ctx.fillStyle = "rgb(177, 177, 177)";
+  this.ctx.fillRect(25, 0, 15, this.height);
+  this.ctx.fillRect(this.width-40, 0, 15, this.height);
+  this.ctx.fillRect(50, 0, this.width-100, this.height);
+  this.ctx.fillStyle = "white";
+  for(var i=0;i<this.height;i+=120){
+    this.ctx.fillRect(225, i, 15, 60);
+  }
+
+};
+
 
 Game.prototype.isReady = function() {
   return this.car.isReady();
@@ -19,6 +41,7 @@ Game.prototype.draw = function() {
     // for (var i = 0; i < this.bars.length; i++) {
     //   this.bars[i].draw();
     // }
+    this.paintRoad();
     this.car.draw();
   }
 
