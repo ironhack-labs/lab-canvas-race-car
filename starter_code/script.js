8866a4088d72401d79fe3fe268f9513bae241e7d
asalria <ameliasalria@gmail.com>
2017-11-25 14:24:59 +0100

    feat: Car drawn

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,23 +3,42 @@ window.onload = function() {
     startGame();
   };
 
-  function startGame() {
+  function getCanvas(){
     var canvas = document.getElementById('example');
     var ctx = canvas.getContext('2d');
+    return ctx;
+  }
+
+  function startGame() {
+
+    ctx = getCanvas();
     ctx.fillStyle = 'green';
-    ctx.fillRect(25, 125, 500, 1000);
+    ctx.fillRect(0, 10, 500, 800);
     ctx.fillStyle = 'grey';
-    ctx.fillRect(65, 125, 420, 1000);
+    ctx.fillRect(40, 10, 420, 800);
     ctx.fillStyle = 'white';
-    ctx.fillRect(85, 125, 20, 1000);
+    ctx.fillRect(60, 10, 20, 800);
     ctx.fillStyle = 'white';
-    ctx.fillRect(canvas.width-80, 125, 20, 1000);
+    ctx.fillRect(ctx.canvas.clientWidth-80, 10, 20, 800);
     ctx.setLineDash([18, 17]);/*dashes are 5px and spaces are 3px*/
     ctx.beginPath();
-    ctx.moveTo(275,125);
-    ctx.lineTo(275, 1000);
+    ctx.moveTo(250,10);
+    ctx.lineTo(250, 800);
     ctx.lineWidth = 8;
     ctx.strokeStyle = 'white';
     ctx.stroke();
+    ctx.closePath();
+
+    drawCar(ctx.canvas.clientWidth/2,100);
   }
+
+  function drawCar(position) {
+    var ctx = getCanvas();
+    var img = new Image();
+    img.onload = function() { 
+       ctx.drawImage(img, position-img.width/4, 300, img.width/2, img.height/2); 
+    }
+    img.src = 'images/car.png';
+  }
+  
 };
