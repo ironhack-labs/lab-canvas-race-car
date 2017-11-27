ffcfa108827d284a428e5930f3016e6ca66b9ba2
Cristina Bertrand <cristina@11870.com>
2017-11-27 13:55:55 +0100

    FEAT: painted the road, but I think I can reduce my code, because I use too functions

--- starter_code/js/game.js
+++ starter_code/js/game.js
@@ -1,7 +1,5 @@
 function Game(canvasId) {
   this.canvas = document.getElementById(canvasId);
-  this.canvas.width = 1000;
-  this.canvas.height = 400;
   this.ctx = this.canvas.getContext('2d');
 
   this.road = new Road();
@@ -13,3 +11,5 @@ Game.prototype.draw = function() {
   this.road.draw();
 
 };
+
+var game = new Game("canvas");
--- starter_code/js/road.js
+++ starter_code/js/road.js
@@ -4,17 +4,48 @@ function Road(canvasId) {
 
 }
 
-Road.prototype.drawRect = function(x, y, width, height) {
+Road.prototype.drawGarden = function(x, y, width, height) {
 this.ctx.beginPath();
 this.ctx.rect(x, y, width, height);
 this.ctx.fill();
-this.ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+this.ctx.fillRect(0, 0, 500, 700);
+this.ctx.fillStyle = "rgb(0, 153, 51)";
+this.ctx.fill();
+this.ctx.fillStyle = "rgb(102, 102, 102)";
+this.ctx.fillRect(50, 0, 400, 700);
 this.ctx.closePath();
 };
 
-Road.prototype.draw = function() {
-this.drawRect(0, 0, 500, 700);
+Road.prototype.drawRoad = function(x, y, width, height) {
+this.ctx.beginPath();
+this.ctx.rect(x, y, width, height);
+this.ctx.closePath();
+};
 
+Road.prototype.drawLines = function(){
+this.ctx.save();
+this.ctx.fillStyle = "#FFFFFF";
+this.ctx.fillRect(62, 0, 10, 700);
+this.ctx.fillStyle = "#FFFFFF";
+this.ctx.fillRect(427, 0, 10, 700);
+this.ctx.restore();
+};
+
+Road.prototype.drawDashes = function(){
+  this.ctx.lineWidth = 6;
+  this.ctx.strokeStyle = "#FFFFFF";
+  this.ctx.beginPath();
+  this.ctx.setLineDash([30, 30]);
+  this.ctx.moveTo(250, 20);
+  this.ctx.lineTo(250, 700);
+  this.ctx.stroke();
+};
+
+Road.prototype.draw = function() {
+this.drawGarden(0, 0, 500, 700);
+this.drawRoad(50, 0, 400, 700);
+this.drawLines(70, 0, 100, 700);
+this.drawDashes();
 };
 
 var road = new Road("canvas");
