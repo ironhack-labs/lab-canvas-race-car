becf31e4a31593a8a8da025849d74896447c3763
Felix Heras <heras.felix@gmail.com>
2017-11-27 12:39:07 +0100

    feat:First iteration

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,25 @@ window.onload = function() {
   };
 
   function startGame() {
-
+var game = new Game();
+game.canvasInit();
+game.roadInit();
   }
+};
+
+function Game() {
+var ctx ;
+}
+
+Game.prototype.canvasInit = function () {
+
+      var canvas = document.getElementById("canvas");
+    canvas.width = 500;
+    canvas.height = 800;
+    this.ctx = canvas.getContext("2d");
+};
+
+Game.prototype.roadInit = function () {
+  var road = new Road();
+  road.draw(this.ctx);
 };
