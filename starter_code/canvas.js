62aee05801522a6a9dcd92656b2153ad11dcb26b
100fue <malvacien@gmail.com>
2018-01-18 19:53:05 +0100

    iteration 1 done

---
+++ starter_code/canvas.js
@@ -0,0 +1,10 @@
+/*
+$(document).ready(function(){
+
+	
+
+	var cvSize = {width:500, height:800};
+	$("#myCv").attr(cvSize);
+	var ctx = cv.getContext('2d');
+
+});*/
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,64 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+	document.getElementById("start-button").onclick = function() {
+		startGame();
+	};
 
-  function startGame() {
 
-  }
+function draw(){
+	
+}
+
+
+function startGame() {
+	var cv = document.getElementById('myCv');
+	var ctx = cv.getContext('2d');
+
+	
+	ctx.fillStyle = "#1c8100"; //dar color verde
+	ctx.fillRect(0, 0, 500, 800); //posX, posY, tam W, tam Y
+	ctx.fillStyle = "#808080"; //dar color 
+	ctx.fillRect(30, 0, 440, 800); //posX, posY, tam X, tam Y
+	ctx.clearRect(40, 0, 10, 800); //Borrar posX, posY, grosor de borrado, altura 
+	ctx.clearRect(450, 0, 10, 800);
+	ctx.beginPath(); //donde empieza la linea discontinua
+	ctx.strokeStyle = "#ffffff"; //dar color linea discontinua
+	ctx.lineWidth = 5; //grosor linea discontinua
+	ctx.setLineDash([30, 30]);
+	ctx.moveTo(250, 800); //donde se mueve X, Y
+	ctx.lineTo(250, 30); //donde se linea X, Y
+	ctx.stroke(); //Cerra linea
+
+	var img = new Image();
+    img.onload = function () {
+      ctx.drawImage(img, 225, 650, 50, 100); // X, Y, Ancho, Alto
+    };
+
+    img.src = 'images/car.png'
+}
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
