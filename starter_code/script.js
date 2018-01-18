77872ce3ae4032d969dec3e75c560cc34284adaf
Kathia Rieser <kath.medulla@gmail.com>
2018-01-18 22:43:30 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,14 @@ window.onload = function() {
   };
 
   function startGame() {
+    this.ctx = document.getElementById('cargame').getContext('2d');
+    ctx.fillRect(0, 0, 300, 600);
+    ctx.fillStyle="#00FF00";
+    ctx.fillRect(0, 0, 30, 600);
+    ctx.moveTo(150,0);
+    //ctx.lineTo(150,150);
+    ctx.fillRect(0, 0, 30, 600);
+    ctx.stroke();
 
   }
 };
