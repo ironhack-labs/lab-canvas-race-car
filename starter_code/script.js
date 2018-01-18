7da41f3a81b872e58f92664fdce77b32a4af27f3
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-18 20:15:49 +0100

    Iteración 2 (mejora botón

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,15 +1,5 @@
-// window.onload = function() {
-//   document.getElementById("start-button").onclick = function() {
-//     startGame();
-//   };
-
-//   function startGame() {
-//     canvas.start();
-
-//   }
-// };
 $(document).ready(function(){
   $(".start-button").click(function() {
-      $(".ocultar").remove();
+      $(".ocultar").toggle();
    });
 });
