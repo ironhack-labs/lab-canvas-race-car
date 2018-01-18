d90610d6952e79e76a338dffa21b76395d6d3fdc
Kathia Rieser <kath.medulla@gmail.com>
2018-01-19 00:53:44 +0100

    done

---
+++ starter_code/canvas.js
@@ -0,0 +1,44 @@
+function GameBoard(){
+  this.ctx = document.getElementById('cargame').getContext('2d');
+
+}
+
+GameBoard.prototype.drawGameBoard = function(){
+
+  this.ctx.fillStyle = "#00AA00";
+  this.ctx.fillRect(0, 0, 330, 700);
+  this.ctx.fillStyle = "#808080";
+  this.ctx.fillRect(15, 0, 300, 700); //x,y
+  this.ctx.fillStyle = "#FFF";
+  this.ctx.fillRect(20, 0, 5, 700);
+  this.ctx.fillRect(305, 0, 5, 700);
+  this.ctx.moveTo(160, 0);// x, Y
+  this.ctx.strokeStyle = "#FFF";
+  this.ctx.setLineDash([10, 20]); //líneas cortadas
+  this.ctx.lineWidth=5;
+  this.ctx.lineTo(160, 700);
+  this.ctx.stroke();
+
+}
+
+GameBoard.prototype.cleanGameBoard = function(){
+
+  this.ctx.clearRect(0,0,330,700);
+}
+
+function Car(){
+  this.speed = 10;
+  this.imgUrl = "images/car.png";
+  this.x = 155;
+  this.y = 640;
+  this.scale = 158 / 319;
+}
+
+Car.prototype.drawCar = function(ctx){
+
+  var img = new Image();
+  img.src = this.imgUrl;
+  imgScale = 640/480;
+  ctx.drawImage(img, 0, 0,90*this.scale,90)
+
+}
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,5 +1,8 @@
 window.onload = function() {
+
+
   document.getElementById("start-button").onclick = function() {
+  
 
 
     startGame();
@@ -7,11 +10,19 @@ window.onload = function() {
 
   function startGame() {
     
-
     var gameBoard = new GameBoard();
-    gameBoard.drawGameBoard();
+    //var car = new Car();
     
+    //car.drawCar(gameBoard.ctx);
+    gameBoard.drawGameBoard(); 
+
+    var img = new imgen();
+    img.src = "images/car.png";
+    gameBoard.ctx.drawImage(img,10,10,150,180);
+
+  
 
 
+   
   }
 };
