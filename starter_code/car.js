c6230a8584bd7f2ef33c29c19a8c37448a469d4c
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-01-18 22:16:41 +0100

    a

--- starter_code/car.js
+++ starter_code/car.js
@@ -1,19 +1,29 @@
-function CreateCarObject(ctx) {
+function CreateCarObject() {
     this.x = 220;
     this.speed = 10;
+    this.img = new Image();
+    this.scale = 158 / 319;
 }
 
-CreateCarObject.prototype.move = function(lado){
-     this.speed = this.speed * lado;
+CreateCarObject.prototype.move = function(key){
+    switch (key) {
+        case 37: // left
+          this.x -= 5;
+          if (this.x < 20) {
+            this.x = 20;
+          }
+          break;
+        case 39: // right
+          this.x += 5;
+          if (this.x > 265) {
+            this.x = 265;
+          }
+          break;
+        default:
+          return; // exit this handler for other keys
+      }
   }
 
-CreateCarObject.prototype.render = function(delta,ctx){
-    this.x += this.speed/1000*delta;
-    var img = new Image();
-    imgScale = 158 / 319;
-    img.onload = function () {
-        ctx.drawImage(img, this.x, 650, 90 * imgScale, 90);
-    }
-    img.src = "images/car.png" 
-    console.log("creo el coche");
+CreateCarObject.prototype.render = function(ctx){
+        ctx.drawImage(this.img, this.x, 650, 90 * this.scale, 90);
 }
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,66 +1,22 @@
 $(document).ready(() => {
-  var ctx = document.getElementById('myCanvas').getContext('2d');
-  var car = new CreateCarObject(ctx);
-  var now = Date.now();
-  var delta = 0;
-  var i = 0;
+  var canvas = new GameBoard();
+  var car = new CreateCarObject();
 
-  function createBoard() {
-    ctx.fillStyle = "#00b200";
-    ctx.fillRect(0, 0, 330, 750);
-    ctx.fillStyle = "#808080";
-    ctx.fillRect(10, 0, 310, 750);
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(15, 0, 5, 750);
-    ctx.fillRect(310, 0, 5, 750);
-    ctx.strokeStyle = "#FFF";
-    ctx.setLineDash([10, 20]);
-    ctx.moveTo(165, 0);
-    ctx.lineTo(165, 750);
-    ctx.stroke();
-  }
-
-  function step() {
-    var img = new Image();
-    img.src = "images/car.png"
-    imgScale = 158 / 319;
-    img.onload = function () {
-      ctx.clearRect(0, 0, 330, 750)
-      createBoard();
-      ctx.drawImage(img, car.x, 650, 90 * imgScale, 90);
-      requestAnimationFrame(step)
+  function gameStart() {
+    car.img.src = "images/car.png";
+    car.img.onload = function () {
+      canvas.clean();
+      canvas.drawRoad();
+      car.render(canvas.ctx);
+      window.requestAnimationFrame(gameStart)
     }
   }
 
-  function startGame() {cod
-    requestAnimationFrame(step);
-  }
-
-  $(document)
-    .keydown(function (e) {
-      switch (e.which) {
-        case 37: // left
-          car.x -= 5;
-          if (car.x < 20) {
-            car.x = 20;
-          }
-          break;
-        case 39: // right
-          car.x += 5;
-          if (car.x > 265) {
-            car.x = 265;
-          }
-          break;
-        default:
-          return; // exit this handler for other keys
-      }
-      e.preventDefault(); // prevent the default action (scroll / move caret)
+  $(document).keydown(function (e) {
+      car.move(e.which);
     });
 
-  $("#start-button")
-    .click(function () {
-      startGame();
-    });
-
+  $("#start-button").click(function () {
+    gameStart();
+  });
 })
-
