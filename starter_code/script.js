b369695b7767aec66177519445446fd757b63a75
Kathia Rieser <kath.medulla@gmail.com>
2018-01-18 23:53:17 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,17 +1,17 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+
+
     startGame();
   };
 
   function startGame() {
-    this.ctx = document.getElementById('cargame').getContext('2d');
-    ctx.fillRect(0, 0, 300, 600);
-    ctx.fillStyle="#00FF00";
-    ctx.fillRect(0, 0, 30, 600);
-    ctx.moveTo(150,0);
-    //ctx.lineTo(150,150);
-    ctx.fillRect(0, 0, 30, 600);
-    ctx.stroke();
+    
+
+    var gameBoard = new GameBoard();
+    gameBoard.drawGameBoard();
+    
+
 
   }
 };
