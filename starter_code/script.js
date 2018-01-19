faaf79ca5b0db50892fe139d0e83d397bf74736e
Carlos San Martin <carlosanmartin@gmail.com>
2018-01-19 09:48:04 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,7 +3,26 @@ window.onload = function() {
     startGame();
   };
 
-  function startGame() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
 
+  
+
+  function startGame() {   
+  
+  ctx.fillStyle = '#00CA0D';
+  ctx.fillRect(0, 0, 520, 750);
+
+  ctx.fillStyle = '#A1A1A1';
+  ctx.fillRect(30, 0, 460, 750);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(255, 0, 10, 750);
+
+  var img = new Image();
+  img.src = 'images/car.png';
+  img.onload = function() {
+    ctx.drawImage(img, 235, 600, 60, 110)
+  }
   }
 };
