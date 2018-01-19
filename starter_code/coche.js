e16de152acf8c7a56c60ffd9249ea53f5f22767b
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-01-19 09:52:15 +0100

    gracias avello

--- starter_code/coche.js
+++ starter_code/coche.js
@@ -1,22 +1,25 @@
 function CarToGo(){
-    this.posX= 250;
-    this.speed= 10;
+    this.posX= 225;
+    this.posY= 625;
+    this.moveLeft = function(){
+        if(this.posX > 50)
+        this.posX -= 15;
+    };
+    this.moveRight = function(){
+        if(this.posX < 245)
+        thisposX += 15;
+    };
 }
 
-CarToGo.prototype.moving = function(keyPressed){
-    switch(keyPressed){
-        case 37:
-        this.x -= 5;
-        break;
-
-        case 39:
-        this.x +=5;
-        break;
-
-        default:
-        return;
-    }
+CarToGo.prototype.moving = function(canvas){
+    var img = new Image();
+    img.src = 'images/car.png';
+    img.onload = function() {
+    canvas.ctx.drawImage(img,this.posX,this.posY,40,80);
+    };bind(this);
+    
 }
+
 
 // CarToGo.prototype.draw = function(){
 //     var img = new Image();
