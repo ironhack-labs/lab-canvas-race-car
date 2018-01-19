3bee2168096021f059e651ff35996115acd34a01
jesusdelrioc <jesusdelrioc@gmail.com>
2018-01-19 09:09:01 +0100

    final

---
+++ starter_code/canvas.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+function draw() {
+    var canvas = document.getElementById('carretera');
+      var ctx = canvas.getContext('2d');
+      
+      ctx.fillstyle= "#008100";
+      ctx.fillRect(0,0,380,720);
