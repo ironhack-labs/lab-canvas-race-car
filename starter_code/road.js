4a2b91c9b71dcf16b73bf46c42ab8f9952f6352a
Luna Fidalgo <lunafidalgo@gmail.com>
2018-03-29 13:57:28 +0200

    Primera iteración completada

---
+++ starter_code/road.js
@@ -0,0 +1,35 @@
+function Road (canvasId){
+  var canvas = document.getElementById(canvasId)
+  this.w = canvas.getAttribute("width");
+  this.h = canvas.getAttribute("height");
+  this.ctx = canvas.getContext("2d");
+}
+
+Road.prototype.drawRect = function (x,y,width, height){
+  this.ctx.fillRect(x,y,width,height);
+}
+
+Road.prototype.draw = function (){
+  var margen = 20;
+  var margenInterior = 5;
+  var linea = 8;
+  
+  this.ctx.fillStyle = "rgb(0,146,0)"
+  this.drawRect(0,0,this.w,this.h);
+
+  this.ctx.fillStyle = "rgb(127,127,127)";
+  this.drawRect(margen, 0, this.w-margen*2, this.h);
+
+  this.ctx.fillStyle = "rgb(255,255,255)";
+  this.drawRect(margen + margenInterior, 0, linea,this.h)
+
+  this.drawRect( this.w-margen-margenInterior-linea , 0, linea,this.h)
+
+  var lineaIntermitente = 10;
+  var anchoLineaIntermitente = 5;
+  var altoLineaIntermitente = 30;
+  for(var i = 0; i < this.h; i +=50 ){
+    this.drawRect( this.w/2 , i, anchoLineaIntermitente, altoLineaIntermitente)
+  }
+}
+
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,22 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
+    dibujarCarretera();
+    
   };
 
-  function startGame() {
+ 
 
+  function startGame() {
+    var canvas = document.createElement("canvas");
+    document.getElementById("game-board").appendChild(canvas);
+    canvas.id = "road";
+    canvas.setAttribute("width", 400);
+    canvas.setAttribute("height", 600);
+  }
+
+  function dibujarCarretera() {
+    var road = new Road("road");
+    road.draw();
   }
 };
