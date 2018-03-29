481483128cdf683b90a31b15e5b921fbce19b7a5
luisnueda <nueda.luis@gmail.com>
2018-03-29 20:01:23 +0200

    coche en movimiento

--- starter_code/game.js
+++ starter_code/game.js
@@ -1,18 +1,32 @@
 function Game(canvasId) {
+ 
   this.canvas = document.getElementById(canvasId);
   this.ctx = this.canvas.getContext('2d');
 
   this.carretera = [new Carretera(this.canvas, this.ctx)
                   , new Carretera(this.canvas, this.ctx)];
-
+  this.coche = new Coche(this.canvas, this.ctx);
+ 
 }
 
 Game.prototype.start = function() {
 
   this.carretera[1].y = -510;
   setInterval(function(){
-    this.clear();
-    this.carretera[0].draw();
+    this.creaCarretera();
+    this.coche.draw();
+  }.bind(this),10)
+  
+}
+
+Game.prototype.clear = function(){
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+Game.prototype.creaCarretera = function(){
+
+  this.clear();
+  this.carretera[0].draw();
     this.carretera[1].draw();
     if(this.carretera[0].y >= 510){
       this.carretera[0].y = -510;
@@ -22,10 +36,4 @@ Game.prototype.start = function() {
     }
     this.carretera[0].y ++;
     this.carretera[1].y ++;
-  }.bind(this),5)
-}
-
-
-Game.prototype.clear = function(){
-  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
--- starter_code/script.js
+++ starter_code/script.js
@@ -6,5 +6,25 @@ window.onload = function() {
   function startGame() {
     var game = new Game("canvas");
     game.start();
+
+    document.onkeydown = function(event) {
+      var RIGHT_KEY = 39;
+      var TOP_KEY = 38;
+      var BOTTOM_KEY = 40;
+      var LEFT_KEY = 37;
+  
+      switch(event.keyCode) {
+        case RIGHT_KEY:
+            if(game.coche.x < game.canvas.width - game.coche.width){
+              game.coche.x +=10;
+            }
+          break;
+        case LEFT_KEY:
+            if(game.coche.x > 0){
+              game.coche.x  -=10;
+            }
+          break;
+      }
+    }
   }
 };
