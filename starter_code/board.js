cc762d7e73a8b96a48217d7964762bafe5602321
Tanis Pan <e.pan.gar@gmail.com>
2018-03-29 15:22:21 +0200

    Iteracion 3

--- starter_code/board.js
+++ starter_code/board.js
@@ -8,6 +8,13 @@ function Canvas(id) {
 Canvas.prototype.drawRect = function(x, y, width, height) {
   this.ctx.fillStyle='green';
   this.ctx.fillRect(0, 0, this.w, this.h)
+  this.ctx.fillStyle='grey';
+  this.ctx.fillRect(50, 0,this.w-100, this.h)
+  this.ctx.fillStyle='white';
+  this.ctx.fillRect(60, 0, 10, this.h)
+  this.ctx.fillStyle='white';
+  this.ctx.fillRect(this.w-70, 0, 10, this.h)
+  
   
 }
 
