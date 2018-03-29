ce04d0474ff96a1901e2227b53b48f56a635d1e6
Alvaro Reguera <alvaro.reguera@gmail.com>
2018-03-29 20:52:25 +0200

    BOOOOM

--- starter_code/car.js
+++ starter_code/car.js
@@ -1,22 +1,49 @@
-function Car(canvas,ctx,url) {
-    this.canvas = canvas;
-    this.ctx = ctx;
-    this.image = new Image();
-    this.url = url;
-    
-  }
+function Car(canvas, ctx, url) {
+  this.canvas = canvas;
+  this.ctx = ctx;
+  this.image = new Image();
+  this.url = url;
+  var RIGHT_KEY = 39;
+  var LEFT_KEY = 37;
+  var SPACE = 32;
+  this.x = 210.5;
 
-  Car.prototype.drawImage = function () {
+  // this.audio = new Audio('sound.mp3');
+  this.image.src = this.url;
 
-    this.image.src = this.url;
+  this.image.onload = function () {
+    this.width = this.image.width * 0.5;
+    this.height = this.image.height * 0.5;
+    this.y = this.canvas.height - this.height
+  }.bind(this)
 
-    this.image.onload = function() {
-      console.log(this.image.width, this.image.height );
-      console.log(this.canvas.width, this.canvas.height);
-      var width = this.image.width * 0.5;
-      var height = this.image.height * 0.5;
-      console.log(this.canvas.width/2)
-        this.ctx.drawImage(this.image,0,0,this.image.width, this.image.height, this.canvas.width/2 - width/2, this.canvas.height-height,width,height);
-    }.bind(this)
-  }
+
+  document.onkeydown = function (event) {
+    var d = 10;
+
+    switch (event.keyCode) {
+      case RIGHT_KEY:
+        this.x += d;
+        break;
+      case LEFT_KEY:
+        this.x -= d;
+        break;
+      // case SPACE:
+      //   console.log("da");
+      //   this.audio.play();
+      //   break;
+
+    }
+
+  }.bind(this);
+};
+
+
+Car.prototype.drawImage = function () {
+
+  this.ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, this.x, this.canvas.height - this.height, this.width, this.height);
+
+}
+
+
 
--- starter_code/script.js
+++ starter_code/script.js
@@ -2,18 +2,10 @@ window.onload = function () {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
   document.getElementById("start-button").onclick = function () {
-    startGame();
+   var game = new Game(canvas,ctx);
+   game.start();
   };
   
-  function startGame() {
-    var road = new Road (canvas, ctx);
-    var car = new Car (canvas, ctx, 'images/car.png');
-    road.draw();
- 
-      car.drawImage();
-  
-    
-  }
 
 
 }
