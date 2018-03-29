62f7de72abcde22840d4af734f1182b9512266d3
Borja Arana <barana1977@hotmail.com>
2018-03-29 20:07:16 +0200

    Formateado

--- starter_code/car.js
+++ starter_code/car.js
@@ -6,17 +6,17 @@ function Car(canvas, ctx){
     this.ctx = ctx;   
     this.x = 10;
 };
-Car.prototype.drawCar = function () {
+Car.prototype.drawCar = function() {
     this.img = new Image();
     this.img.src = 'images/car.png';
-    this.img.onload = function () {
-        this.ctx.drawImage(this.img, this.x, 700, 60, 120);
+    this.img.onload = function() {
+    this.ctx.drawImage(this.img, this.x, 700, 60, 120);
     }.bind(this);
 };
 
 
-Car.prototype.moveCar = function () {
-    document.onkeydown = function (event) {
+Car.prototype.moveCar = function() {
+    document.onkeydown = function(event) {
         var d = 15;
         switch (event.keyCode) {
             case RIGHT_KEY:
