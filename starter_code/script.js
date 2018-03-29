fd5e261c70ad2d0888b3b44d341f03d6d9b40b40
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-29 19:47:58 +0200

    MUERTE

--- starter_code/script.js
+++ starter_code/script.js
@@ -5,13 +5,16 @@ window.onload = function() {
 
   function startGame() {
     var canvas = document.getElementById("canvas");
-    this.ctx = canvas.getContext("2d");    
+    this.ctx = canvas.getContext("2d");
     this.img = new Image();
     this.img.src = "images/car.png";
-    this.obstaculoArray = []; 
+    this.obstaculoArray = [];
+    this.obstaculoPx = [];
+    this.obstaculoPy = [];
+    this.obstaculoW = [];
     this.draw();
     this.imgcar();
-    
+    //this.obstaculos();
     this.y = 157;
     document.onkeydown = function(e) {
       switch (e.keyCode) {
@@ -25,9 +28,12 @@ window.onload = function() {
           break;
       }
     }.bind(this);
-     setInterval( function(){
-      this.obstaculos();
-     }.bind(this),50)
+    setInterval(
+      function() {
+        this.obstaculos();
+      }.bind(this),
+      500
+    );
   }
 
   startGame.prototype.drawGreen = function() {
@@ -35,7 +41,7 @@ window.onload = function() {
     this.ctx.fillRect(0, 0, 35, 500);
     this.ctx.fillRect(315, 0, 35, 500);
   };
-  
+
   startGame.prototype.drawRoad = function() {
     this.ctx.fillStyle = "grey";
     this.ctx.fillRect(35, 0, 290, 500);
@@ -55,59 +61,84 @@ window.onload = function() {
     this.img.onload = function() {
       this.ctx.drawImage(this.img, 157.5, 410, 35, 65);
     }.bind(this);
-  }
+  };
 
-  startGame.prototype.drawObstaculos = function () {
-    setInterval( function(){for (var i = 0; i < this.obstaculoArray.length; i++) {
-        this.clear();
-        this.draw();
-        this.player(this.y);
-        this.ctx.fillStyle = "green";
-        this.ctx.fillRect(this.obstaculoArray[i][0], this.obstaculoArray[i][2] += 0.5, this.obstaculoArray[i][1], 30);
-        if (this.obstaculoArray[i][2] > 500)
-          this.obstaculoArray.shift();    
-    }debugger;
-    i = 0;}.bind(this),10)
-    
-    
-  }
+  startGame.prototype.newObstaculos = function() {
+    setInterval(
+      function() {
+        for (var i = 0; i < this.obstaculoArray.length; i++) {
+          //this.clear();
+          //this.draw();
+          //this.player(this.y);
+          //this.ctx.fillStyle = "green";
+          this.obstaculoArray[i](this.obstaculoPx[i],i ,this.obstaculoW[i]);
+          if (this.obstaculoArray[i].length > 5) this.obstaculoArray.shift();
+        
+        debugger;
+        i = 0;
+      }.bind(this),
+      10
+    );
+  };
+  startGame.prototype.drawObstaculos = function(px, py, w, h) {
+
+    // this.clear();
+    this.draw();
+    this.player(this.y);
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(px, py, w, h);
+  };
   startGame.prototype.clear = function() {
     this.ctx.clearRect(0, 0, 350, 500);
-  }
+  };
   startGame.prototype.draw = function() {
     this.drawGreen();
     this.drawRoad();
     this.drawWhiteBorder();
     this.drawWhiteLines();
-    
   };
-  
+
   startGame.prototype.player = function(y) {
-    
-      
-        this.clear();
-        this.draw();
-        for(let i = 0; i < this.obstaculoArray; i++){
-        this.ctx.fillStyle = "green";
-        this.ctx.fillRect(this.obstaculoArray[i][0], ++this.obstaculoArray[i][2], this.obstaculoArray[i][1], 30)}
-        this.ctx.drawImage(this.img, y, 410, 35, 65);
-      
-    
+    this.clear();
+    this.draw();
+    // for (let i = 0; i < this.obstaculoArray; i++) {
+    //   this.ctx.fillStyle = "green";
+    //   this.ctx.fillRect(
+    //     this.obstaculoArray[i][0],
+    //     ++this.obstaculoArray[i][2],
+    //     this.obstaculoArray[i][1],
+    //     30
+    //   );
+    // }
+    this.ctx.drawImage(this.img, y, 410, 35, 65);
   };
-  startGame.prototype.moveLeft = function () {
+  startGame.prototype.moveLeft = function() {
     if (this.y <= 35) return;
-    this.player(this.y -= 5)
-  }
-  startGame.prototype.moveRight = function () {
-    if (this.y >= 295)
-      return;
-    this.player(this.y += 5)
-  }
-  startGame.prototype.obstaculos = function () {
-    var randomW = Math.floor(Math.random()* 150)+50;
-    var randomP = Math.floor(Math.random()* 220);
+    this.player((this.y -= 5));
+  };
+  startGame.prototype.moveRight = function() {
+    if (this.y >= 295) return;
+    this.player((this.y += 5));
+  };
+  startGame.prototype.obstaculos = function() {
+    var randomW = Math.floor(Math.random() * 150) + 50;
+    var randomP = Math.floor(Math.random() * 220);
     this.ctx.fillRect(randomP, 0, randomW, 30);
-    this.obstaculoArray.push([randomP, randomW, 0]);
-    this.drawObstaculos();
-  }
+    this.obstaculoPx.push(randomP);
+    this.obstaculoPy.push(0);
+    this.obstaculoW.push(randomW);
+                  this.obstaculoArray.push(
+                    function (px, i, w){
+                      this.draw();
+                      this.player(this.y);
+                      this.ctx.fillStyle = "green";
+                      this.ctx.fillRect(px, ++this.obstaculoPy[i], w, 30);
+                    
+                  }.bind(this)
+                );
+    this.newObstaculos();
+  //  this.obstaculoArray[0](this.obstaculoPx[0],this.obstaculoPy[0],this.obstaculoW[0]);
+   // debugger;
+   // this.newObstaculos();
+  };
 };
