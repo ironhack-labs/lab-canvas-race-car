9ff1b28419af65f7e839aafd75404ae2ce77a7ae
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-29 16:04:46 +0200

    Movimiento del coche con limites

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,42 +1,83 @@
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
-    new startGame().draw();
-    
+    new startGame();
   };
 
   function startGame() {
-    this.canvas = document.getElementById("canvas");
+    var canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
-    
+    document.getElementsBy;
+    this.img = new Image();
+    this.img.src = "images/car.png";
+    this.y = 157;
+    this.draw();
+    document.onkeydown = function(e) {
+      switch (e.keyCode) {
+        case 37:
+          this.moveLeft();
+          break;
+        case 39:
+          this.moveRight();
+          break;
+        default:
+          break;
+      }
+    }.bind(this);
   }
 
   startGame.prototype.drawGreen = function() {
     this.ctx.fillStyle = "green";
     this.ctx.fillRect(0, 0, 35, 500);
     this.ctx.fillRect(315, 0, 35, 500);
-  }
+  };
   
-  startGame.prototype.drawRoad = function () {
+  startGame.prototype.drawRoad = function() {
     this.ctx.fillStyle = "grey";
     this.ctx.fillRect(35, 0, 290, 500);
-  }
+  };
 
   startGame.prototype.drawWhiteBorder = function() {
-  this.ctx.fillStyle = "white";
-  this.ctx.fillRect(45, 0, 10, 500);
-  this.ctx.fillRect(305, 0, 10, 500);
- }
- startGame.prototype.drawWhiteLines = function () {
-   for (let i = 5; i < 500; i+=30) {
-     this.ctx.fillRect(172.5, i, 5, 25);    
-   }
- }
-  
-  startGame.prototype.draw = function () {
+    this.ctx.fillStyle = "white";
+    this.ctx.fillRect(45, 0, 10, 500);
+    this.ctx.fillRect(305, 0, 10, 500);
+  };
+  startGame.prototype.drawWhiteLines = function() {
+    for (let i = 5; i < 500; i += 30) {
+      this.ctx.fillRect(172.5, i, 5, 25);
+    }
+  };
+  startGame.prototype.image = function(width, height, x, y) {
+    this.img.onload = function() {
+      this.ctx.drawImage(this.img, 157.5, 410, 35, 65);
+    }.bind(this);
+  };
+
+  startGame.prototype.player = function(y) {
+    
+      
+        this.ctx.clearRect(0, 0, 350, 500);
+        this.drawGreen();
+        this.drawRoad();
+        this.drawWhiteBorder();
+        this.drawWhiteLines();
+        this.ctx.drawImage(this.img, y, 410, 35, 65);
+      
+    
+  };
+  startGame.prototype.moveLeft = function () {
+    if (this.y <= 35) return;
+    this.player(this.y -= 5)
+  }
+  startGame.prototype.moveRight = function () {
+    if (this.y >= 295)
+      return;
+    this.player(this.y += 5)
+  }
+  startGame.prototype.draw = function() {
     this.drawGreen();
     this.drawRoad();
     this.drawWhiteBorder();
     this.drawWhiteLines();
-  }
-
+    this.image();
+  };
 };
