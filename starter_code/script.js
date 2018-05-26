50bd8b219d173a9db9b5a63bdc2177b68e2fcf16
Alekz7 <ing.aragon.ipn@gmail.com>
2018-05-26 00:25:56 -0500

    MEXPT-ALEJANDRO_ARAGON_ALVAREZ

--- starter_code/script.js
+++ starter_code/script.js
@@ -6,6 +6,7 @@ window.onload = function() {
   let fondo = new Image();
   let car_image = new Image();
   let alco = new Image();
+  let gameOver = false;
 
   fondo.src = 'images/carretera.png';
   car_image.src = 'images/car.png';
@@ -26,49 +27,55 @@ window.onload = function() {
   };
 
   let tiempo = 0;  
+  let alcoArray = [];  
 
-  let alcoAbajo = 10;
-  let alcoAbajo2 = 10;
-  let alcoAbajo3 = 10;
-  function draw(){  	
+  alcoArray[0] = {
+  	x:48,
+  	y:10
+  }
 
-  	console.log(alcoAbajo);
-  	console.log(alcoAbajo2);
-  	console.log(alcoAbajo3);
+  function draw(){    	
 
   	ctx.drawImage(fondo, 0, 0, 500, 500);
 	ctx.drawImage(car_image, car.x, car.y, 50, 100);
 
-	// for(let k = 0 ; k <= alcoY.length) working on the random
-	// I need to work on the collision, but first I need to save all the obstacles
+	for(let k = 0 ; k < alcoArray.length; k ++){
+		ctx.drawImage(alco, alcoArray[k].x, alcoArray[k].y, 200, 40);
+		alcoArray[k].y += 1;
 
-	if (tiempo>0){
-		ctx.drawImage(alco, 10, alcoAbajo, 200, 40);	
-		alcoAbajo += 1;	
-	}			
+		// PARA GENERAR UN NUEVO ALCOHOLIMETRO CADA CIERTO TIEMPO
+		if (alcoArray[k].y == canvas.height - 250 ){
+			xAleatorio = Math.floor(	(Math.random() * 206) + 48	); //entre 48 y 254
+			alcoArray.push({
+				x:xAleatorio,
+				y:10
+			});
+		}		
 
-	if (tiempo>200){
-		ctx.drawImage(alco, 140, alcoAbajo2, 200, 40);
-		alcoAbajo2 += 1;
-	}		
-
-	if (tiempo>600){
-		ctx.drawImage(alco, 140, alcoAbajo3, 200, 40);
-		alcoAbajo3 += 1;
-	}		
-
-	tiempo ++;
-
-	if (tiempo > 1200) {
-		tiempo = 0;
-		alcoAbajo = 10;
-		alcoAbajo2 = 10;
-		alcoAbajo3 = 10;
+		// VERIFICANDO COLISION
+		if ((car.x >= alcoArray[k].x) && (car.x <= (alcoArray[k].x + 200) )) {
+			if ((alcoArray[k].y >= 400) && (alcoArray[k].y <= 500)){
+				ctx.fillStyle = "red";
+				ctx.font = "20px Verdana";
+				ctx.fillText("Fin del juego, al torito ", 140,250);
+				gameOver = true;				
+			}			
+		}
 	}
-	requestAnimationFrame(draw);
+	tiempo ++;	
+	ctx.fillStyle = "#000";
+	ctx.font = "20px Verdana";
+	ctx.fillText("Puntos: " + tiempo, 50,50);
+	if (!gameOver) requestAnimationFrame(draw);
   }
 
   function startGame() {  	
+  	if (car.x == undefined){
+		car.x = 250;
+	};
+	if (car.y == undefined){
+		car.y = 400;
+	};
   	inicioGame(canvas); //dibujar las condiciones iniciales
   	tiempo = 0;
   }
@@ -85,8 +92,7 @@ window.onload = function() {
 	div.appendChild(canvas);
   }
 
-  function inicioGame(canvas){  	
-  	console.log(ctx);	  	
+  function inicioGame(canvas){  	  	
 	fondo.onload = function(){
 	    ctx.drawImage(fondo, 0, 0, 500, 500);
 	}
@@ -112,15 +118,6 @@ window.onload = function() {
 	      break;
 	  }
 	}
-	// document.onkeyup = function(e) {
-	//   stopMove();
-	// }
-
-	// function stopMove() {
-	//     car.x = 0;
-	//     car.y = 0;
-	// }	
-
 	function moveLeft() {		
 	    car.x -= 5;
 	}
@@ -128,5 +125,4 @@ window.onload = function() {
 	function moveRight() {
 	    car.x += 5;
 	}
-
 };
