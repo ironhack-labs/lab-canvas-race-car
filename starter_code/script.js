9ccdce073c96629f832879fc1e1763b3262f6112
Abel Alonso <abel.alonso@gmail.com>
2018-06-22 00:25:51 +0200

    The car can move

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,31 +1,98 @@
+var car = new Car();
+
+function Car (){
+  this.source = "images/car.png";
+  this.x = 265;
+  this.y = 550;
+  this.maxSpeed = 2;
+  this.direction = 0;
+  this.img = new Image();
+  this.maxRigth = 480;
+  this.maxLeft = 50;
+}
+
+Car.prototype.drawCar = function(){
+  that=this;
+  this.img.onload = function() {ctx.drawImage(that.img, that.x, that.y, 70, 100);}
+  this.img.src = car.source;
+};
+
+Car.prototype.moveLeft = function() {
+  this.direction = -this.maxSpeed;
+  this.move();
+}
+
+Car.prototype.moveRight = function() {
+  this.direction = this.maxSpeed;
+  this.move();
+}
+
+Car.prototype.move =function() {
+  if ((this.x>this.maxLeft) && (this.x<this.maxRigth)){
+    this.x += this.direction;
+  } else {
+    this.x = 300;
+    this.direction = 0;
+  }
+  this.draw();
+}
+
+Car.prototype.draw = function() { 
+  ctx.drawImage(this.img, this.x, this.y, 70, 100);
+  console.log(this.x, this.y);
+}
+
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
+    car.drawCar();
   };
-
-  initPosition = [300, 600];
   function startGame() {
-
     //draw the path
     ctx = document.getElementById("game-canvas").getContext("2d");
-    ctx.fillStyle = "rgb(0, 193, 49)";
-    ctx.fillRect(0 ,0 ,600 , 700);
-    ctx.fillStyle = "rgb(198, 198, 198)";
-    ctx.fillRect(50 ,0 ,500 , 700);
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    ctx.fillRect(70 ,0 ,20 , 700);
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    ctx.fillRect(510 ,0 ,20 , 700);
-    ctx.strokeStyle = "rgb(255, 255, 255)";
-    ctx.setLineDash([40, 30]);
-    ctx.lineWidth = 10;
-    ctx.moveTo(300, 0);
-    ctx.lineTo(300, 700);
-    ctx.stroke();
+    drawThePath();
 
-    //draw the car
-    var img = new Image();
-    img.onload = function() {ctx.drawImage(img, initPosition[0], initPosition[1], 70, 70);}
-    img.src = "images/car.png";
+    function updateCanvas() {
+    ctx.clearRect(0,0,600,700);
+    car.move(ctx);
+    drawThePath(ctx);
+    car.draw(ctx);
+    window.requestAnimationFrame(updateCanvas);
   }
-};
+
+window.requestAnimationFrame(updateCanvas);  
+
+}
+
+document.onkeydown = function(e) {
+  switch (e.keyCode) {
+    case 37:
+      car.moveLeft();
+      break;
+    case 39:
+      car.moveRight();
+      break;
+  }
+}
+
+function drawThePath(){
+  ctx.fillStyle = "rgb(0, 193, 49)";
+  ctx.fillRect(0 ,0 ,600 , 700);
+  ctx.fillStyle = "rgb(198, 198, 198)";
+  ctx.fillRect(50 ,0 ,500 , 700);
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillRect(70 ,0 ,20 , 700);
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillRect(510 ,0 ,20 , 700);
+  ctx.strokeStyle = "rgb(255, 255, 255)";
+  ctx.setLineDash([40, 30]);
+  ctx.lineWidth = 10;
+  ctx.moveTo(300, 0);
+  ctx.lineTo(300, 700);
+  ctx.stroke();
+}
+
+
+}
+
