2b5e9b061e8648889443416188b7f7f6b5f2daa9
Sandra Najera <snajera@ucm.es>
2018-06-21 23:23:43 +0200

    Empezado

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,12 @@ window.onload = function() {
   };
 
   function startGame() {
-
+    ctx = document.getElementById('game').getContext('2d');
+    ctx.lineWidth = 20;
+    ctx.strokeStyle =rgb(040,114,051);
+    ctx.beginPath();
+    ctx.lineTo(0, 620);
+    ctx.moveTo(620, 0);
+    ctx.lineTo(620,620);
   }
 };
