4a9fb5bdb096c06be064c3c1f2a64e4969244834
Abel Alonso <abel.alonso@gmail.com>
2018-06-22 09:14:30 +0200

    Some fixed

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,6 +1,6 @@
 var car = new Car();
 
-function Car (){
+function Car() {
   this.source = "images/car.png";
   this.x = 265;
   this.y = 550;
@@ -11,60 +11,41 @@ function Car (){
   this.maxLeft = 50;
 }
 
-Car.prototype.drawCar = function(){
-  that=this;
-  this.img.onload = function() {ctx.drawImage(that.img, that.x, that.y, 70, 100);}
+Car.prototype.drawCar = function() {
+  that = this;
+  this.img.onload = function() {
+    ctx.drawImage(that.img, that.x, that.y, 70, 100);
+  };
   this.img.src = car.source;
 };
 
 Car.prototype.moveLeft = function() {
   this.direction = -this.maxSpeed;
   this.move();
-}
+};
 
 Car.prototype.moveRight = function() {
   this.direction = this.maxSpeed;
   this.move();
-}
+};
 
-Car.prototype.move =function() {
-  if ((this.x>this.maxLeft) && (this.x<this.maxRigth)){
+Car.prototype.move = function() {
+  if (this.x > this.maxLeft && this.x < this.maxRigth) {
     this.x += this.direction;
   } else {
     this.x = 300;
     this.direction = 0;
   }
   this.draw();
-}
+};
 
-Car.prototype.draw = function() { 
+Car.prototype.dontMove = function() {
+  this.direction = 0;
+};
+
+Car.prototype.draw = function() {
   ctx.drawImage(this.img, this.x, this.y, 70, 100);
-  console.log(this.x, this.y);
-}
-
-
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-    car.drawCar();
-  };
-  function startGame() {
-    //draw the path
-    ctx = document.getElementById("game-canvas").getContext("2d");
-    drawThePath();
-    var count=1;
-    function updateCanvas() {
-      count++;
-    ctx.clearRect(0,0,600,700);
-    car.move(ctx);
-    drawThePath(count);
-    car.draw(ctx);
-    window.requestAnimationFrame(updateCanvas);
-  }
-
-window.requestAnimationFrame(updateCanvas);  
-
-}
+};
 
 document.onkeydown = function(e) {
   switch (e.keyCode) {
@@ -75,26 +56,54 @@ document.onkeydown = function(e) {
       car.moveRight();
       break;
   }
-}
+};
 
-function drawThePath(i){
+  /* document.onkeyup = function(e) {
+  switch (e.keyCode) {
+    case 37:
+    case 39:
+      car.dontMove();
+      break;
+  }
+}
+ */
+function drawThePath(i) {
   ctx.fillStyle = "rgb(0, 193, 49)";
-  ctx.fillRect(0 ,0 ,600 , 700);
+  ctx.fillRect(0, 0, 600, 700);
   ctx.fillStyle = "rgb(198, 198, 198)";
-  ctx.fillRect(50 ,0 ,500 , 700);
+  ctx.fillRect(50, 0, 500, 700);
   ctx.fillStyle = "rgb(255, 255, 255)";
-  ctx.fillRect(70 ,0 ,20 , 700);
+  ctx.fillRect(70, 0, 20, 700);
   ctx.fillStyle = "rgb(255, 255, 255)";
-  ctx.fillRect(510 ,0 ,20 , 700);
+  ctx.fillRect(510, 0, 20, 700);
   ctx.strokeStyle = "rgb(255, 255, 255)";
   ctx.setLineDash([40, 30]);
-  ctx.lineDashOffset = -2*(car.direction**2*i);;
+  ctx.lineDashOffset = -2 * (car.direction ** 2 * i);
   ctx.lineWidth = 10;
   ctx.moveTo(300, 0);
   ctx.lineTo(300, 700);
   ctx.stroke();
 }
 
+window.onload = function() {
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+    car.drawCar();
+  };
+  function startGame() {
+    //draw the path
+    ctx = document.getElementById("game-canvas").getContext("2d");
+    drawThePath();
+    var count = 1;
 
-}
-
+    function updateCanvas() {
+      count++;
+      ctx.clearRect(0, 0, 600, 700);
+      car.move(ctx);
+      drawThePath(count);
+      car.draw(ctx);
+      window.requestAnimationFrame(updateCanvas);
+    }
+    window.requestAnimationFrame(updateCanvas);
+  }
+};
