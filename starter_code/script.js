4946570f1c1c8f45809da310e92fc8671e62c21b
dmarinsa <dmarinsanchez@gmail.com>
2018-06-22 09:20:26 +0200

    organize

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,46 +1,11 @@
 window.onload = function() {
-var redColor = 'rgb(136, 0, 0)';
 var greyColor = 'rgb(128, 128, 128)';
 var greenColor = 'rgb(0, 129, 0)';
 var whiteColor = '#fff';
-var carRoute = './images/car.png';
-var carRatio = 158/310;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var car = new Car(187.5, 500);
 var obstacles = [];
-
-function Car(x, y){
-  this.x = x;
-  this.y = y;
-  this.image = new Image();
-  this.maxX = 330;
-  this.minX = 40;
-  this.image.src = carRoute;
-}
-
-Car.prototype.moveRight = function (){if(this.x<this.maxX) this.x+=10;};
-Car.prototype.moveLeft = function(){if(this.x>this.minX) this.x-=10;};
-Car.prototype.draw = function(context){ context.drawImage(this.image, this.x, this.y, 150*carRatio, 150);};
-
-function Obstacle(width, position){
-  this.obstacles = [];
-  this.x=position;
-  this.y=0;
-  this.height = 40;
-  this.width = width;
-  this.maxWith = 300;
-  this.speed = 3;
-}
-
-Obstacle.prototype.draw = function(context){
-  context.fillStyle = redColor;
-  context.fillRect(this.x, this.y, this.width, this.height);
-};
-
-Obstacle.prototype.move = function(){
-  this.y += this.speed;
-};
 
 
   document.getElementById("start-button").onclick = function() {
@@ -101,7 +66,7 @@ Obstacle.prototype.move = function(){
       obstacles.forEach(function(e){
         if(e.y > 700){
           obstacles.shift();
-          delete e;
+          console.log(e);
         } else{
           e.draw(ctx);
         }
@@ -111,8 +76,8 @@ Obstacle.prototype.move = function(){
 
   setInterval(function(){
     console.log(obstacles);
-    obstacles.push(new Obstacle(Math.floor(Math.random()*300+40), Math.floor(Math.random()*300)));
-  }, 2000);
+    obstacles.push(new Obstacle(Math.floor(Math.random()*250+40), Math.floor(Math.random()*300)));
+  }, 3000);
 
   function updateCanvas() {  
     clearStage();
