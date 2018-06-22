1d7bbcf2f75509bdbb92945311bfbc8b2abec9d9
LauCF <lauracanosaf@hotmail.com>
2018-06-22 08:57:29 +0200

    Primer commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,43 @@ window.onload = function() {
   };
 
   function startGame() {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 45, 700);
+    ctx.fillRect(405, 0, 45, 700);
+    ctx.fillStyle = "gray";
+    ctx.fillRect(45, 0, 10, 700);
+    ctx.fillRect(395, 0, 10, 700);
+    ctx.strokeStyle = "white";
+    ctx.fillRect(65, 0, 20, 700);
+    ctx.fillRect(375, 0, 20, 700);
+    ctx.fillStyle = "gray";
+    ctx.fillRect(75, 0, 180, 700);
+    ctx.fillRect(225, 0, 180, 700);
+    ctx.setLineDash = "white";
+    ctx.lineWidth = 5;
+    ctx.moveTo(224, 0);
+    ctx.lineTo(226, 700);
+    ctx.stroke();
 
+    function draw(car) {
+      var img = new Image();
+      img.onload = function () {
+        ctx.drawImage(img, 200, 480, 50, 100);
+      }
+      img.src = "./images/car.png";
+    }
+
+    draw()
   }
 };
+
+
+
+
+
+
+
+
