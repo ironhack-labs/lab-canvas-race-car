a4c15ff2ceb81250f11b5ac9b0bac3ae59ca021c
CPBCPB <christinebeck2010@gmail.com>
2018-07-02 15:31:46 -0400

    MIA christine-beck

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,47 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+// window.onload = function() {
+var myCanvas= document.getElementById('theCanvas');
+var ctx = myCanvas.getContext('2d');
+var currentGame;
+var theCar;
 
-  function startGame() {
+var Car = function(){
+  this.x=240;
+  this.y=600;
+  this.width=50;
+  this.height=85;
+  this.img='images/car.png';
+}
 
+Car.prototype.drawCar = function(){
+  var that=this
+  var theImage = new Image();
+  theImg.src=that.img;
+  theImage.onload = function(){
+    ctx.drawImage(theImage, that.x, that.y, that.width, that.height)
   }
-};
+}
+Car.prototype.move = function(whichKey){
+  switch(whichKey{
+    case 'ArrowLeft':
+    console.log('moving left');
+    break;
+    case 'ArrowRight';
+    console.log('moving right')
+  })
+}
+
+
+document.getElementById("start-button").onclick = function() {
+var theCar = new Car();
+theCar.drawCar();
+},
+document.onkeydown=function(){
+
+}
+// }
+
+//     // document
+//   };
+  // document.onkeydown
+
+  //left arrow is 37 right event.key ArrowLeft ArrowRight ArrowUp ArrowDown
