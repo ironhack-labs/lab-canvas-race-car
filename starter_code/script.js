f4923f340427150370358029eba9b808969654a0
Daniel <paseiro101@yahoo.com>
2018-07-02 21:25:43 -0400

    mia Daniel Paseiro

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,6 +1,26 @@
 window.onload = function() {
+  var currentGame:
+
+  var Car = function(){
+    this.x = 240;
+    this.y = 600;
+    this.width = 50;
+    this.height = 85;
+    this.img = 'images/car.png';
+  }
+
+  Car.prototype.drawCar = (){
+    var that = this;
+    var theImage = new Image();
+    theImage.onload = function(){
+      theImage.src = that.img;
+      ctx.drawImage(theImage)
+    }
+  }
+
   document.getElementById("start-button").onclick = function() {
-    startGame();
+    // startGame();
+    var theCar = new Car;
   };
 
   function startGame() {
