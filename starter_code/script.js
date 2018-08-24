586804988ae2128a3e4b3b0c648c76cdb5df0dd5
Adolfo Vela <vela@tekplanner.com>
2018-08-23 22:08:43 -0500

    Dibujada la carretera

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,47 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+// window.onload = function() {
 
-  function startGame() {
+// Llamada al canvas del html
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 
-  }
+// Primer linea verde
+context.beginPath();
+context.rect(0, 0, 25, 500);
+context.fillStyle = 'green';
+context.fill();
+
+// Primer linea blanca
+context.beginPath();
+context.rect(35, 0, 10, 500);
+context.fillStyle = 'white';
+context.fill();
+
+//  linea central
+context.beginPath();
+context.setLineDash([5,15,25]);
+context.moveTo(145, 0);
+context.lineTo(145, 500);
+context.strokeStyle='white';
+context.lineWidth="5";
+context.stroke();
+
+// Segund linea blanca
+context.beginPath();
+context.rect(255, 0, 10, 500);
+context.fillStyle = 'white';
+context.fill();
+
+// Segunda linea verde
+context.beginPath();
+context.rect(275, 0, 25, 500);
+context.fillStyle = 'green';
+context.fill();
+
+document.getElementById("start-button").onclick = function () {
+  startGame();
 };
+
+function startGame() {
+
+}
+// };
