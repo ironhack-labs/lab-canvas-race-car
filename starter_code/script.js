809e6c6db67ba92bace51dc37ebe0c665b85fe17
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-08-30 20:09:30 +0200

    Second & third iteration

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,16 +1,64 @@
 window.onload = function() {
   var game=document.getElementById('game-board');
+  var canvas=document.createElement('canvas');
+  var ctx=canvas.getContext("2d");
   var GREY = "#808080";
   var GREEN = "#3a8200";
+  var ARROW_LEFT=37;
+  var ARROW_RIGHT=39;
+  var car = new Image();   // Create new img element
+  var carPosition={
+        x:0,
+        y:0
+      }
+  var speed = 5;
+  var pressed=false;
+  car.src   = './images/car.png';
+  canvas.width=400;
+  canvas.height=500;
+  carPosition.x=canvas.width/2-25/2;
+  carPosition.y=canvas.height-55;
   startGame();
   document.getElementById("start-button").onclick = function() {
   };
   function startGame() {
-    var canvas=document.createElement('canvas');
-    var ctx=canvas.getContext("2d");
     game.appendChild(canvas)
-    canvas.width=400;
-    canvas.height=500;
+    drawRoad()
+    drawCar(carPosition.x,carPosition.y)
+    document.onkeyup=function(){
+      pressed=false;
+    }
+    document.onkeydown=function(event){
+      switch (event.keyCode) {
+        case ARROW_LEFT:
+          drawRoad();
+          carPosition.x-=speed
+          drawCar(carPosition.x,carPosition.y)
+          break;
+          case ARROW_RIGHT:
+          drawRoad();
+          carPosition.x+=speed
+          drawCar(carPosition.x,carPosition.y)
+          break;
+        default:{
+        }
+          break;
+      }
+    }
+     // Set source path
+    // ctx.strokeRect(35,0,canvas.width-25*2,canvas.height)
+    // ctx.fillStyle=white;
+  }
+  function drawCar(x,y){
+
+    car.onload=function(){
+      ctx.drawImage(car,x,y,25,50)
+    }
+    ctx.drawImage(car,x,y,25,50)
+  }
+  function drawRoad(){
+    // ctx.restore();
+    ctx.setLineDash([])
     ctx.fillStyle=GREEN;
     ctx.fillRect(0,0,canvas.width,canvas.height)
     ctx.fillStyle=GREY;
@@ -21,7 +69,7 @@ window.onload = function() {
     ctx.moveTo(40,0)
     ctx.lineTo(40,canvas.height)
     ctx.stroke()
-    
+
     ctx.beginPath()
     ctx.moveTo(canvas.width-40,0)
     ctx.lineTo(canvas.width-40,canvas.height)
@@ -33,7 +81,5 @@ window.onload = function() {
     ctx.setLineDash([20,10]);
     ctx.lineWidth=5;
     ctx.stroke()
-    // ctx.strokeRect(35,0,canvas.width-25*2,canvas.height)
-    // ctx.fillStyle=white;
   }
 };
