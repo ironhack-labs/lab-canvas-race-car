e0a85678ab636043c4c1daab734296b5d333cba8
Teo Diaz <tdiazsan@gmail.com>
2018-08-30 19:32:21 +0200

    printed

--- starter_code/script.js
+++ starter_code/script.js
@@ -2,8 +2,30 @@ window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
-
+  var canvas = document.getElementById('car-board');
+  var ctx = canvas.getContext("2d");
   function startGame() {
+    
+    ctx.fillStyle = '#3e8317';
+    ctx.fillRect(0,0,40,795);
+    ctx.fillRect(460,0,40,795);
+    ctx.fillStyle = '#808080';
+    ctx.fillRect(40,0,10,795);
+    ctx.fillRect(460,0,10,795);
+    ctx.fillRect(60,0,390,795);
+    ctx.strokeStyle = '#ffffff';
+    ctx.setLineDash([25, 35]);
+    ctx.beginPath();
+    ctx.moveTo(250, 50);
+    ctx.lineWidth = 7;
+    ctx.lineTo(250, 800);
+    ctx.stroke();
 
+    
+   
+    
   }
+
+
+
 };
