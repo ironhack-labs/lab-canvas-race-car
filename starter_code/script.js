8c811493b55a9436d44e9d91e4776ffebf244081
David Sirit <davidsirit@yahoo.com>
2018-08-30 19:36:04 +0200

    First iteration Draw the Game Board, Done

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,26 @@ window.onload = function() {
   };
 
   function startGame() {
+    var canvasSpace = document.getElementById("canvas-field");
+    var ctx = canvasSpace.getContext("2d");
 
+    ctx.fillStyle = "rgb(65, 130, 0)";
+    ctx.fillRect(5, 5, 25, 490);
+    ctx.fillRect(375, 5, 25, 490);
+
+    ctx.fillStyle = "rgb(128, 128, 128)";
+    ctx.fillRect(30, 5, 10, 490);
+    ctx.fillRect(365, 5, 10, 490);
+    ctx.fillRect(55, 5, 295, 490);
+
+    
+    ctx.strokeStyle = 'white'; 
+    ctx.lineWidth = 5;
+    ctx.setLineDash([20,20]);
+    ctx.beginPath();     
+    ctx.moveTo (200, 5); 
+    ctx.lineTo (200, 495);
+    ctx.stroke();
+    //ctx.fill();
   }
 };
