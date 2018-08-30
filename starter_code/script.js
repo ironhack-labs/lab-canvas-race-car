5ed131cc0c0ab1aa9ab09976b0319df280a60b2a
Mapno <mariorvinas@gmail.com>
2018-08-30 18:59:27 +0200

    improved code

--- starter_code/script.js
+++ starter_code/script.js
@@ -23,22 +23,21 @@ window.onload = function() {
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#008100";
     ctx.fillRect(0, 0, 20, c.height);
+    ctx.fillRect(380, 0, 20, c.height);
     ctx.fillStyle = "#808080";
-    ctx.fillRect(20, 0, 30, c.height);
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(30, 0, 40, c.height);
-    ctx.fillStyle = "#808080";
-    ctx.fillRect(40, 0, 360, c.height);
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(360, 0, 370, c.height);
-    ctx.fillStyle = "#808080";
-    ctx.fillRect(370, 0, 380, c.height);
-    ctx.fillStyle = "#008100";
-    ctx.fillRect(380, 0, c.width, c.height);
+    ctx.fillRect(20, 0, 10, c.height);
+    ctx.fillRect(40, 0, 320, c.height);
+    ctx.fillRect(370, 0, 10, c.height);
+    // ctx.fillStyle = "#FFF";
+    // ctx.fillRect(30, 0, 40, c.height);
+    // ctx.fillStyle = "#808080";
+    // ctx.fillStyle = "#FFF";
+    // ctx.fillRect(360, 0, 370, c.height);
+    // ctx.fillStyle = "#808080";
+    // ctx.fillStyle = "#008100";
     for (var i = 0; i < 15; i++){
       ctx.fillStyle = "#FFF";
       ctx.fillRect(196, 13 + 33 * i, 8, 20);
-      // ctx.fillRect(13 + 33 * i, 196, 33 + 33 * i, 204);
     }
   }
 };
