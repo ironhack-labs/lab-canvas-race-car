759b8784ea5009a545c3bbbfe6ec3a01833d3217
agdwm <almudenaguerraswm@gmail.com>
2018-08-31 00:09:09 +0200

    Adde function init and create function to checkKeyCode

--- starter_code/js/canvas.js
+++ starter_code/js/canvas.js
@@ -11,16 +11,16 @@ roadCanvas = new RoadCanvas();
 
 car = new Image();
 car.src = '../starter_code/images/car.png';
-car.width = 158*0.5;
-car.height = 319*0.5;
+car.width = 158 * 0.5;
+car.height = 319 * 0.5;
 
 RoadCanvas.prototype.drawLines = function () {
 	var lineThickness = 10;
 
 	this.ctx.setLineDash([45, 40]); /*dashes are 20px and spaces are 15px*/
 	this.ctx.beginPath();
-	this.ctx.moveTo((this.ROADWIDTH/2)-(lineThickness/2)+5, 0);
-	this.ctx.lineTo((this.ROADWIDTH/2)-(lineThickness/2)+5, this.ROADHEIGHT);
+	this.ctx.moveTo((this.ROADWIDTH / 2) - (lineThickness / 2) + 5, 0);
+	this.ctx.lineTo((this.ROADWIDTH / 2) - (lineThickness / 2) + 5, this.ROADHEIGHT);
 	this.ctx.strokeStyle = '#FFF';
 	this.ctx.lineWidth = lineThickness;
 	this.ctx.stroke();
@@ -32,9 +32,9 @@ RoadCanvas.prototype.createBoard = function () {
 	car.onload = this.drawImageOnCanvas();
 }
 
-RoadCanvas.prototype.drawImageOnCanvas = function() {
-	var posX = (this.ROADWIDTH/2)-(car.width/2);
-	var posY = (this.ROADHEIGHT)-(car.height);
-	
-	this.ctx.drawImage(car, posX, posY, car.width, car.height );
+RoadCanvas.prototype.drawImageOnCanvas = function () {
+	var posX = (this.ROADWIDTH / 2) - (car.width / 2);
+	var posY = (this.ROADHEIGHT) - (car.height);
+
+	this.ctx.drawImage(car, posX, posY, car.width, car.height);
 }
