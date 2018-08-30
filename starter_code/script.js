4d2244a50ba814c52d30a6e7be7c03e018e12b36
omar-nav <omar.navarro@g.austincc.edu>
2018-08-30 18:22:36 -0500

    el canvas ta listo

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,7 +3,34 @@ window.onload = function() {
     startGame();
   };
 
-  function startGame() {
+  function startGame() {}
 
-  }
+  var canvas = document.getElementById("game-board");
+  var ctx = canvas.getContext("2d");
+
+  // place car
+  // var img = document.getElementById("/images/logo.png");
+  // ctx.drawImage(img, 10, 10);
+
+  var imagen = new Image();
+  imagen.src = "./images/car.png";
+
+  imagen.onload = function() {
+    ctx.drawImage(imagen, 70, 490, 50, 100);
+  };
+
+  ctx.beginPath();
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, 600, 800);
+
+  ctx.fillStyle = "grey";
+  ctx.fillRect(25, 0, 540, 600);
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 40;
+  ctx.moveTo(50, 0);
+  ctx.lineTo(50, 600);
+  ctx.moveTo(530, 0);
+  ctx.lineTo(530, 600);
+  ctx.stroke();
 };
