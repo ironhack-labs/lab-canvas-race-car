c7af342d166a740841689388dff2ede6ca0bdb4d
Chris Lopez <pagarizabal2@gmail.com>
2018-08-31 09:01:35 +0200

    basic scenario

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,46 @@ window.onload = function() {
   };
 
   function startGame() {
-
+    board.start();
+    board.car();
+    //player = new Car(130, 500, 40, 80);
   }
+
+
+  var board = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+              this.canvas.width = 300;
+              this.canvas.height = 600;
+              this.context = this.canvas.getContext("2d");
+              this.draw();
+              document.getElementById("game-board").insertBefore(this.canvas, document.getElementById("game-board").childNodes[0]);
+            },
+    frames: 0,
+    clear : function() {
+              this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            },
+    draw : function(){
+              this.context.fillStyle = "green"
+              this.context.fillRect(0,0,300,600);
+              this.context.fillStyle = "grey"
+              this.context.fillRect(40,0,240,600);
+              this.context.strokeStyle= "white";
+              for (i = 0 ; i < 40; i++){
+                this.context.clearRect(147,5+30*i,6,15);
+              }
+              this.context.clearRect(40,0,6,600);
+              this.context.clearRect(254,0,6,600);
+            },
+    car : function() {
+
+      car = new Image();
+      this.context.drawImage(car, 0, 600, 50, 100)
+      console.log("dibuja coche");
+      this.car.src= "images/car.png";
+      console.log("entraaa");
+     }
+
+    }
+  
 };
