a3bd11575dc8a2f8dc821f8ce89a63b0accd51db
omar-nav <omar.navarro@g.austincc.edu>
2018-08-30 20:14:40 -0500

    crear setInterval

--- starter_code/script.js
+++ starter_code/script.js
@@ -23,45 +23,50 @@ window.onload = function() {
     }
   }
 
-  ctx.beginPath();
-  ctx.fillStyle = "green";
-  ctx.fillRect(0, 0, 600, 800);
+  function background() {
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, 600, 800);
 
-  ctx.fillStyle = "grey";
-  ctx.fillRect(25, 0, 540, 600);
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 30;
-  ctx.moveTo(50, 0);
-  ctx.lineTo(50, 600);
-  ctx.moveTo(530, 0);
-  ctx.lineTo(530, 600);
-  ctx.stroke();
-  ctx.lineWidth = 10;
+    ctx.fillStyle = "grey";
+    ctx.fillRect(25, 0, 540, 600);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 30;
+    ctx.moveTo(50, 0);
+    ctx.lineTo(50, 600);
+    ctx.moveTo(530, 0);
+    ctx.lineTo(530, 600);
+    ctx.stroke();
+    ctx.lineWidth = 10;
 
-  ctx.moveTo(300, 0);
-  ctx.lineTo(300, 600);
-  ctx.setLineDash([15, 15]);
-  ctx.stroke();
-
+    ctx.moveTo(300, 0);
+    ctx.lineTo(300, 600);
+    ctx.setLineDash([15, 15]);
+    ctx.stroke();
+  }
   let deLorean = new Carro();
   deLorean.draw();
 
-  // find example with
-  // moving square
-  // function right() {
-  //   this.x++;
-  // }
-
-  // // move car left and right
-  // document.onkeydown = function(e) {
-  //   switch (e.keyCode) {
-  //     // flecha derecha
-  //     case 39:
-  //       right();
-  //       break;
-  //     // flecha izquierda
-  //     // case 37:
-  //     //   left();
-  //     //   break;
-  //   }
+  var interval = setInterval(function() {
+    background();
+    deLorean.draw();
+  }, 1000 / 60);
 };
+// find example with
+// moving square
+// function right() {
+//   this.x++;
+// }
+
+// // move car left and right
+// document.onkeydown = function(e) {
+//   switch (e.keyCode) {
+//     // flecha derecha
+//     case 39:
+//       right();
+//       break;
+//     // flecha izquierda
+//     // case 37:
+//     //   left();
+//     //   break;
+//   }
