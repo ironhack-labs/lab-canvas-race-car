542e613b6a4d518cbb77dfdef89355207cc56a5f
omar-nav <omar.navarro@g.austincc.edu>
2018-08-30 19:20:52 -0500

    el canvas ta listo

--- starter_code/script.js
+++ starter_code/script.js
@@ -13,21 +13,35 @@ window.onload = function() {
   // ctx.drawImage(img, 10, 10);
 
   var imagen = new Image();
+
   imagen.src = "./images/car.png";
 
+  function Carro() {
+    this.x = 70;
+    this.y = 490;
+    this.ssdf;
+  }
+  // class Carro {
+  //   constructor {
+  //     this.x = 70;
+  //     this.y = 490;
+
+  //   }
+  // }
+
+  Carro();
   imagen.onload = function() {
-    ctx.drawImage(imagen, 70, 490, 50, 100);
+    ctx.drawImage(imagen, x, y, 50, 100);
   };
 
   ctx.beginPath();
-
   ctx.fillStyle = "green";
   ctx.fillRect(0, 0, 600, 800);
 
   ctx.fillStyle = "grey";
   ctx.fillRect(25, 0, 540, 600);
   ctx.strokeStyle = "white";
-  ctx.lineWidth = 40;
+  ctx.lineWidth = 30;
   ctx.moveTo(50, 0);
   ctx.lineTo(50, 600);
   ctx.moveTo(530, 0);
@@ -39,4 +53,23 @@ window.onload = function() {
   ctx.lineTo(300, 600);
   ctx.setLineDash([15, 15]);
   ctx.stroke();
+  // find example with
+  // moving square
+  function right() {
+    this.x++;
+  }
+
+  // move car left and right
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      // flecha derecha
+      case 39:
+        right();
+        break;
+      // flecha izquierda
+      // case 37:
+      //   left();
+      //   break;
+    }
+  };
 };
