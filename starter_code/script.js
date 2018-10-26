0b088aa9c41be62beba6ac18968f436fc90dea12
Daniel Ferrari <danielferrari@live.com>
2018-10-26 07:49:25 -0300

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,16 +3,71 @@ window.onload = function() {
     startGame();
   };
 
+  //Create the board
+  $("#game-board").append(
+    '<canvas id="my-canvas" width="400px" height="600px"></canvas>'
+  );
+  // Getting the DOM element.
+  const canvas = document.getElementById("my-canvas");
+
+  // Getting the 2d context.
+  const ctx = canvas.getContext("2d");
+
+  $("#my-canvas").toggle();
+
+  //Start the game
   function startGame() {
-    $("#game-board").append(
-      '<canvas id="canvas-board" width="400px" height="600px"></canvas>'
-    );
-    let ctx = document.getElementById("canvas-board").getContext("2d");
+    if ($("#my-canvas:hidden").length > 0) {
+      $("#my-canvas").toggle();
+      resetCanvas();
+    }
+  }
+  
+  // Our car class.
+  class Hero {
+    constructor() {
+      this.x = 150;
+      this.y = canvas.height - 200;
+      this.width = 100;
+      this.height = 150;
+    }
+
+    draw() {
+      // Prevent our hero from going beyond the available area.
+      if (this.x < 0) this.x = 0;
+      if (this.x > canvas.width - 100) this.x = canvas.width - 100;
+
+      // Drawing the hero itself.
+      var img = new Image();
+      var self = this;
+      img.onload = function() {
+        // We clean everything in the canvas.
+        resetCanvas();
+
+        // Draw the board
+        drawBoard();
+
+        // Draw the car
+        ctx.drawImage(img, self.x, self.y, 100, 150);
+      };
+      img.src = "images/car.png";
+    }
+  }
+
+  // We just need one hero, so let's instantiate it.
+  const ourHero = new Hero();
+
+  // Canvas cleaner.
+  function resetCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function drawBoard() {
     ctx.fillStyle = "grey";
     ctx.fillRect(50, 0, 300, 600);
 
     ctx.beginPath();
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = "white";
     ctx.lineWidth = 10;
     ctx.moveTo(70, 0);
     ctx.lineTo(70, 600);
@@ -20,7 +75,7 @@ window.onload = function() {
     ctx.closePath();
 
     ctx.beginPath();
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = "white";
     ctx.lineWidth = 10;
     ctx.moveTo(330, 0);
     ctx.lineTo(330, 600);
@@ -28,7 +83,7 @@ window.onload = function() {
     ctx.closePath();
 
     ctx.beginPath();
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = "white";
     ctx.lineWidth = 4;
     ctx.moveTo(200, 0);
     ctx.lineTo(200, 30);
@@ -52,11 +107,30 @@ window.onload = function() {
     ctx.lineTo(200, 570);
     ctx.stroke();
     ctx.closePath();
-    
-    var img = new Image();
-    img.onload = function () {
-        ctx.drawImage(img, 150, 400, 100, 150);
-    }
-    img.src = "images/car.png";
   }
+
+  // Keyboard listener to check if the user press arrows keys.
+  window.addEventListener("keydown", e => {
+    // Left arrow key.
+    if (e.keyCode === 37) {
+      if (ourHero.x <= 0) return;
+      ourHero.x -= 30;
+    }
+    // Right arrow key.
+    if (e.keyCode === 39) {
+      if (ourHero.x >= canvas.width - 100) return;
+      ourHero.x += 30;
+    }
+  });
+
+  // Each loop we call render function.
+  function updateCanvas() {
+    // Drawing our hero in the current position.
+    ourHero.draw();
+
+    window.requestAnimationFrame(updateCanvas);
+  }
+
+  // Starting looper.
+  window.requestAnimationFrame(updateCanvas);
 };
