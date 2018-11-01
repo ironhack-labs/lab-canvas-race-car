1de0dfe9e10b42fbc4dfa51696c5dbb2abe57a33
Zeswen <zeswentyrael@gmail.com>
2018-11-01 18:26:41 +0100

    Iteration 5

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,6 +3,10 @@ window.onload = function () {
     startGame();
   };
 
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   function Canvas() {
     this.canvas = document.querySelector("#my-canvas");
     this.ctx = this.canvas.getContext('2d');
@@ -10,6 +14,40 @@ window.onload = function () {
     this.y = 0;
     this.width = 240;
     this.height = 450;
+    this.offset = 0;
+    this.counter = 0;
+    this.obstacles = [];
+    this.car = new Car(this.canvas);
+  }
+
+
+
+  Canvas.prototype.moveAll = function () {
+    setInterval(function () {
+      this.clearAll();
+      this.checkCollision();
+      this.obstacles.forEach(function (obstacle) {
+        obstacle.move();
+      })
+      this.drawAll();
+
+
+      this.counter++
+
+      if (this.counter % 100 === 0) {
+        this.obstacles.push(new Obstacle(this.canvas));
+      }
+    }.bind(this), 1000 / this.car.fps)
+  }
+
+  Canvas.prototype.drawAll = function () {
+    this.drawRoad();
+    this.drawDashed();
+    this.drawLines();
+    this.obstacles.forEach(function (obstacle) {
+      obstacle.draw();
+    })
+    this.car.draw(this.ctx);
   }
 
   Canvas.prototype.drawRoad = function () {
@@ -40,28 +78,98 @@ window.onload = function () {
     this.ctx.lineWidth = 4;
     this.ctx.beginPath();
     this.ctx.setLineDash([6, 10]);
-    this.ctx.moveTo(this.canvas.width / 2, this.y);
-    this.ctx.lineTo(this.canvas.width / 2, this.height);
+    this.ctx.moveTo(this.canvas.width / 2, this.y - 20);
+    this.ctx.lineTo(this.canvas.width / 2, this.height + 100);
+    this.ctx.lineDashOffset = -this.offset;
+    this.offset++;
+    if (this.offset === 100) {
+      this.offset = 0;
+    }
     this.ctx.stroke();
     this.ctx.closePath();
+
   }
 
-  Canvas.prototype.setCar = function() {
-    var img = new Image();
-    img.src = "./images/car.png";
-
-    this.ctx.drawImage(img, this.canvas.width / 2, this.height)
+  function Car(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = this.canvas.width / 2;
+    this.y = this.canvas.height - 30;
+    this.width = 30;
+    this.height= 22;
+    this.fps = 60;
+    this.img = new Image();
+    this.img.src = "./images/car.png";
+    this.setListeners();
   }
+
+  Car.prototype.draw = function (ctx) {
+    ctx.drawImage(this.img, this.x - 15, this.y, this.width, this.height);
+  }
+
+  Car.prototype.setListeners = function () {
+    document.onkeydown = function (e) {
+      switch (e.keyCode) {
+        case 37:
+          this.x -= 5;
+          break;
+
+        case 39:
+          this.x += 5;
+          break;
+      }
+    }.bind(this);
+  }
+
+  Canvas.prototype.checkCollision = function () {
+    this.obstacles.forEach(function (obstacle) {
+      if (this.car.x + this.car.width >= obstacle.x &&
+        obstacle.x + obstacle.width >= this.car.x &&
+        this.car.y + this.car.height >= obstacle.y &&
+        obstacle.height + obstacle.y >= this.car.y) {
+        location.reload()
+      }
+    }.bind(this))
+    if (this.car.x <= 50) {
+      this.car.x = 50;
+    } else if (this.car.x >= 250) {
+      this.car.x = 250;
+    } else {
+      return true;
+    }
+  }
+  Canvas.prototype.clearAll = function () {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  function Obstacle(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.x = getRndInteger(50, 150);
+    this.y = 0;
+    this.width = getRndInteger(50, 100);
+    this.height = 5;
+  }
+
+  Obstacle.prototype.draw = function () {
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  Obstacle.prototype.move = function () {
+    this.y++;
+  }
+
 
   var canvas = new Canvas();
 
+  //var car = new Car(canvas.canvas);
+
   function startGame() {
-    canvas.drawRoad();
-    canvas.drawDashed();
-    canvas.drawLines();
-    canvas.setCar();
-  }
+    canvas.drawAll();
+    canvas.moveAll();
 
 
-};
+  };
 
+}
