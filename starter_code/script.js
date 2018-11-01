da487a62aa6312bd428023e199ec3df91779c307
Yared Santiago <yaredmyers@gmail.com>
2018-11-01 16:52:35 +0100

    iteration 3

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,91 +1,96 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+window.onload = function() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
-// window.onload = function() {
-//   document.getElementById("start-button").onclick = function() {
-//     startGame();
-//   };
+  document.getElementById("start-button").onclick = function() {
+    startGame();
+  };
 
-//   function startGame() {
+  function startGame() {}
 
-//   }
-
-//   function Canvas() {
-
-
-//   }
-// };
-
-//fondo
-ctx.fillStyle="grey";
-ctx.fillRect(0, 0, 400, 600);
-
-// cesped
-ctx.fillStyle="green";
-ctx.fillRect(0, 0, 30, 600);
-ctx.fillStyle="green";
-ctx.fillRect(370, 0, 30, 600);
-
-// arcen
-ctx.fillStyle="white";
-ctx.fillRect(40, 0, 10, 600);
-ctx.fillStyle="white";
-ctx.fillRect(350, 0, 10, 600);
-
-// linea medio
-ctx.beginPath();
-ctx.lineWidth= 4;
-ctx.strokeStyle="white";
-ctx.setLineDash([25,20])
-ctx.moveTo(200, 0);
-ctx.lineTo(200,800)
-ctx.stroke();
-ctx.closePath();
-
-//coche
-
-function Canvas(id) {
-this.canvas = document.getElementById("canvas");
-this.ctx = this.canvas.getContext("2d");
-this.fps = 60;
-this.car = new Car();
-}
-
-function Car()
-{
-  imgCar = new Image();
-  imgCar.src = 'https://github.com/YaredMyers/lab-canvas-race-car/blob/master/starter_code/images/car.png?raw=true';
-  imgCar.onload = function(){
-    ctx.drawImage(imgCar, 175, 450,50,100);
+  function Canvas() {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.fps = 60;
   }
-}
 
-Car();
+  //coche
 
-function Road(){
-//fondo
-ctx.fillStyle="grey";
-ctx.fillRect(0, 0, 400, 600);
+  function Car(canvas, vx) {
+    this.canvas = canvas;
+    this.ctx = canvas.ctx;
+    this.vx = vx;
+    this.x = 175;
+    this.y = 450;
+    this.imgCar = new Image();
+    this.imgCar.src =
+      "https://github.com/YaredMyers/lab-canvas-race-car/blob/master/starter_code/images/car.png?raw=true";
+  }
 
-// cesped
-ctx.fillStyle="green";
-ctx.fillRect(0, 0, 20, 600);
-ctx.fillStyle="green";
-ctx.fillRect(200, 0, 20, 600);
+  Car.prototype.draw = function() {
+      this.ctx.drawImage(this.imgCar, this.x, this.y, 50, 100);
+      this.setListeners();
+  };
 
-// arcen
-ctx.fillStyle="white";
-ctx.fillRect(55, 0, 15, 800);
-ctx.fillStyle="white";
-ctx.fillRect(430, 0, 15, 800);
+  var KEY_RIGHT = 39;
+  var KEY_LEFT = 37;
 
-// linea medio
-ctx.beginPath();
-ctx.lineWidth= 4;
-ctx.strokeStyle="white";
-ctx.setLineDash([25,20])
-ctx.moveTo(250, 0);
-ctx.lineTo(250,800)
-ctx.stroke();
-ctx.closePath();
-}
+  Car.prototype.setListeners = function() {
+    document.onkeydown = function(e) {
+      e.preventDefault();
+      switch (e.keyCode) {
+        case KEY_LEFT:
+          this.x -= this.vx;
+          break;
+        case KEY_RIGHT:
+          this.x += this.vx;
+          break;
+      }
+    }.bind(this);
+  };
+
+  function Road(canvas) {
+    this.x = 0;
+    this.y = 0;
+    this.height = canvas.canvas.height
+  }
+
+  Road.prototype.draw = function() {
+   //fondo
+   ctx.fillStyle = "grey";
+   ctx.fillRect(this.x, this.y, 400, this.height);
+
+   // cesped
+   ctx.fillStyle = "green";
+   ctx.fillRect(0, 0, 30, 600);
+   ctx.fillStyle = "green";
+   ctx.fillRect(370, 0, 30, 600);
+
+   // arcen
+   ctx.fillStyle = "white";
+   ctx.fillRect(40, 0, 10, 600);
+   ctx.fillStyle = "white";
+   ctx.fillRect(350, 0, 10, 600);
+
+   // linea medio
+   ctx.beginPath();
+   ctx.lineWidth = 4;
+   ctx.strokeStyle = "white";
+   ctx.setLineDash([25, 20]);
+   ctx.moveTo(200, 0);
+   ctx.lineTo(200, 800);
+   ctx.stroke();
+   ctx.closePath();
+  }
+
+  var canvas = new Canvas();
+  var road = new Road(canvas);
+  var car = new Car(canvas, 15);
+
+  this.setInterval(function(){
+    road.draw();
+    car.draw();
+  }.bind(this),1000/canvas.fps)
+  car.setListeners();
+
+};
