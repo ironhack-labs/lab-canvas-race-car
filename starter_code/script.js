627ee7f091fc22b1ce964c23c36a781102561925
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-01 17:50:52 +0100

    Iteration 3 with road limits

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var keyLeft = 37;
   var keyRight = 39;
-
+  // var carretera = "(this.canvas.width-200)"
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
@@ -15,8 +15,9 @@ window.onload = function() {
     constructor(id) {
       this.canvas = document.getElementById(id);
       this.ctx = this.canvas.getContext("2d");
-      this.x = 0;
+      this.x = 100;
       this.y = 0;
+      this.yCalle = 0;
       this.width = 10;
       this.height = 500;
       this.coche = new Coche("pista");
@@ -24,19 +25,27 @@ window.onload = function() {
     }
 
     drawLimit(){
+      this.ctx.fillStyle = "green";
+      this.ctx.fillRect(400,this.y,this.x, this.canvas.height);
+      this.ctx.fillStyle = "green";
+      this.ctx.fillRect(0,this.y,this.x, this.canvas.height);
+      this.ctx.fillStyle = "gray";
+      this.ctx.fillRect(this.x,this.y,this.canvas.width-200, this.canvas.height);
       this.ctx.fillStyle = "white";
       this.ctx.fillRect(this.x + 10, this.y, this.width, this.height);
-
       this.ctx.fillStyle = "white";
-      this.ctx.fillRect(this.canvas.width - this.width * 2, this.y, this.width, this.height);
+      this.ctx.fillRect(this.canvas.width-100 - this.width * 2, this.y, this.width, this.height);
     }
     moveStreet(){      
-      
+
+      if(this.yCalle==25)
+        this.yCalle=-40;
         this.ctx.beginPath();
         this.ctx.strokeStyle = "white";  
         this.ctx.setLineDash([40,25]);
-        this.ctx.moveTo(this.canvas.width/2,this.y++)
-        this.ctx.lineTo(this.canvas.width/2,this.canvas.height)
+        
+        this.ctx.moveTo((this.canvas.width)/2,this.yCalle++)
+        this.ctx.lineTo((this.canvas.width)/2,this.canvas.height)
         this.ctx.lineWidth=8;
         this.ctx.stroke();
         this.ctx.closePath()
@@ -51,7 +60,7 @@ window.onload = function() {
       this.coche.move();
     }
     clear(){
-      this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+      this.ctx.clearRect(0,0,this.canvas.width-100,this.canvas.height);
     }
   }
 
@@ -60,7 +69,7 @@ window.onload = function() {
       this.canvas = document.getElementById(id);
       this.ctx = this.canvas.getContext("2d");
       this.modelo = new Image();
-      this.x=this.canvas.width/2-25;
+      this.x=(this.canvas.width)/2-25;
       this.vx=10;
 
       this.move();
@@ -68,9 +77,9 @@ window.onload = function() {
 
     drawCoche(){
       this.modelo.src = "images/car.png";
-      this.modelo.onload=function(){
+      //this.modelo.onload=function(){
       this.ctx.drawImage(this.modelo ,this.x,this.canvas.height-110 ,50 ,100)
-      }.bind(this);
+      //}.bind(this);
     }
 
     move(){
@@ -80,9 +89,11 @@ window.onload = function() {
         e.preventDefault();
         switch(e.keyCode){
           case keyLeft:
+            if(this.x>=100)
             this.x-=this.vx;
             break;
           case keyRight:
+            if(this.x+50<=this.canvas.width-100)
             this.x+=this.vx;
             break;
         }
@@ -93,10 +104,12 @@ window.onload = function() {
   var street = new Canvas("pista");
   setInterval(function(){
   street.clear();
+  
+
+  street.moveCoche();
   street.drawLimit();
   street.moveStreet();
   street.drawCoche();
-  street.moveCoche();
   },1000/60)
 }
 };
