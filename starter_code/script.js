f46ea3e1c7557d541f9dd8e9b5d4275332355c94
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-01 16:15:42 +0100

    Iteration 2

--- starter_code/script.js
+++ starter_code/script.js
@@ -25,18 +25,38 @@ window.onload = function() {
       this.ctx.fillRect(this.canvas.width - this.width * 2, this.y, this.width, this.height);
     }
     moveStreet(){      
+      
         this.ctx.beginPath();
         this.ctx.strokeStyle = "white";  
         this.ctx.setLineDash([40,25]);
-        this.ctx.moveTo(this.canvas.width/2,this.y)
+        this.ctx.moveTo(this.canvas.width/2,this.y++)
         this.ctx.lineTo(this.canvas.width/2,this.canvas.height)
         this.ctx.lineWidth=8;
         this.ctx.stroke();
         this.ctx.closePath()
+        
     }
 
   }
+
+  class Coche{
+    constructor(id){
+      this.canvas = document.getElementById(id);
+      this.ctx = this.canvas.getContext("2d");
+      this.modelo = new Image();
+
+    }
+    drawCoche(){
+      this.modelo.src = "images/car.png";
+      this.modelo.onload=function(){
+      this.ctx.drawImage(this.modelo ,this.canvas.width/2-25,this.canvas.height-110 ,50 ,100)
+      }.bind(this);
+    }
+  }
+  
   var street = new Canvas("pista");
   street.drawLimit();
   street.moveStreet();
+  var coche = new Coche("pista");
+  coche.drawCoche();
 };
