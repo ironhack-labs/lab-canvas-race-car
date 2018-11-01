80ebcdabfc8a72ff719bf47a1c9074dcf6ea7cbc
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-01 16:49:33 +0100

    Iteration 3

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,10 +1,15 @@
 window.onload = function() {
+  var keyLeft = 37;
+  var keyRight = 39;
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-  }
+  
+  
+  
 
   class Canvas {
     constructor(id) {
@@ -14,7 +19,8 @@ window.onload = function() {
       this.y = 0;
       this.width = 10;
       this.height = 500;
-
+      this.coche = new Coche("pista");
+  
     }
 
     drawLimit(){
@@ -37,6 +43,16 @@ window.onload = function() {
         
     }
 
+    drawCoche(){
+      this.coche.drawCoche();
+    }
+
+    moveCoche(){
+      this.coche.move();
+    }
+    clear(){
+      this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    }
   }
 
   class Coche{
@@ -44,19 +60,43 @@ window.onload = function() {
       this.canvas = document.getElementById(id);
       this.ctx = this.canvas.getContext("2d");
       this.modelo = new Image();
+      this.x=this.canvas.width/2-25;
+      this.vx=10;
 
+      this.move();
     }
+
     drawCoche(){
       this.modelo.src = "images/car.png";
       this.modelo.onload=function(){
-      this.ctx.drawImage(this.modelo ,this.canvas.width/2-25,this.canvas.height-110 ,50 ,100)
+      this.ctx.drawImage(this.modelo ,this.x,this.canvas.height-110 ,50 ,100)
+      }.bind(this);
+    }
+
+    move(){
+        
+        document.onkeydown= function(e){
+
+        e.preventDefault();
+        switch(e.keyCode){
+          case keyLeft:
+            this.x-=this.vx;
+            break;
+          case keyRight:
+            this.x+=this.vx;
+            break;
+        }
       }.bind(this);
     }
   }
   
   var street = new Canvas("pista");
+  setInterval(function(){
+  street.clear();
   street.drawLimit();
   street.moveStreet();
-  var coche = new Coche("pista");
-  coche.drawCoche();
+  street.drawCoche();
+  street.moveCoche();
+  },1000/60)
+}
 };
