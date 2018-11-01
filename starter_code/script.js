80cd9d8b9ef9fda2ff999267887780e5fe92fe84
David Diez <ddiezr@gmail.com>
2018-11-01 18:14:31 +0100

    Car moves but doesn't check the limits

--- starter_code/script.js
+++ starter_code/script.js
@@ -2,6 +2,8 @@ window.onload = function () {
     document.getElementById("start-button").onclick = function () {
         startGame();
     };
+    var KEY_RIGHT = 39;
+    var KEY_LEFT = 37;
 
     function Canvas(id) {
         this.canvas = document.querySelector(id);
@@ -13,19 +15,19 @@ window.onload = function () {
         this.car = new Car(this.canvas);
         this.road = new Road(this.canvas);
         this.counter = 0;
-        
+
     }
     Canvas.prototype.draw = function () {
         this.road.draw();
         this.car.draw();
     }
 
-    Canvas.prototype.start = function(){
-        setInterval(function(){
+    Canvas.prototype.start = function () {
+        setInterval(function () {
             this.draw();
             this.road.lineOffset = (-this.counter % 38); //line longitud +  space length
             this.counter++;
-        }.bind(this),1000/this.fps); // Interval 60 times per second
+        }.bind(this), 1000 / this.fps); // Interval 60 times per second
     }
 
     function startGame() {
@@ -64,16 +66,16 @@ window.onload = function () {
         this.ctx.fillRect(this.greenLineWidth + this.greyMarginWidth, 0, this.whiteLineWidth,
             this.canvas.height);
         //2nd white line
-        this.ctx.fillRect(this.greenLineWidth+this.roadWidth-
-            this.whiteLineWidth-this.greyMarginWidth,0,this.whiteLineWidth,this.canvas.height)
+        this.ctx.fillRect(this.greenLineWidth + this.roadWidth -
+            this.whiteLineWidth - this.greyMarginWidth, 0, this.whiteLineWidth, this.canvas.height)
         //DrawCenterLine;    
         this.ctx.beginPath();
         this.ctx.strokeStyle = this.whiteColor;
         this.ctx.lineWidth = this.centerLineWidth;
         this.ctx.lineDashOffset = this.lineOffset;
         this.ctx.setLineDash([15, 23]);
-        this.ctx.moveTo(this.greenLineWidth+(this.roadWidth/2), 0);
-        this.ctx.lineTo(this.greenLineWidth+(this.roadWidth/2),this.canvas.height);
+        this.ctx.moveTo(this.greenLineWidth + (this.roadWidth / 2), 0);
+        this.ctx.lineTo(this.greenLineWidth + (this.roadWidth / 2), this.canvas.height);
         this.ctx.stroke();
         this.ctx.closePath();
     }
@@ -82,14 +84,31 @@ window.onload = function () {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
         this.x = 155;
-        this.y = this.canvas.height -150;
+        this.y = this.canvas.height - 150;
         this.img = new Image();
         this.img.src = "./images/car.png";
+        this.speed = 5;
+        this.setListeners();
     }
-    
+
     Car.prototype.draw = function () {
-        this.ctx.drawImage(this.img,this.x,this.y,40,70);
+        this.ctx.drawImage(this.img, this.x, this.y, 40, 70);
     }
+    Car.prototype.setListeners = function () {
+        
+        document.onkeydown = function (e) {
+            e.preventDefault();
+            switch (e.keyCode) {
+                case KEY_RIGHT:
+                    this.x += this.speed;
+                    break;
+                case KEY_LEFT:
+                    this.x -= this.speed;
+                    break;
+            }
+        }.bind(this); // function inside a method loose the scope this
+    }
+
 
     // Instacniamos nuevo objeto del tipo Canvas
     var myCanvas = new Canvas("#canvas-game");
