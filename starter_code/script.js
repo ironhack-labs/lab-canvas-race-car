59b78b5483ca23bb3e83894974c3103991bed3c1
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-11-01 19:31:17 +0100

    ejercicio terminado!

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,5 +1,13 @@
 window.onload = function(){
 
+  var KEY_UP = 38;
+  var KEY_RIGHT = 39;
+  var KEY_DOWN = 40;
+  var KEY_LEFT = 37;
+
+  document.getElementById('start-button').onclick = function() {
+    startGame();
+  }
 
   function Car(canvas, src, x, y, w, h) {
 
@@ -10,17 +18,37 @@ window.onload = function(){
     this.y = y;
     this.w = w;
     this.h = h;
+    this.vx = 15,
 
     this.image.src = 'images/car.png';
+
+    this.setListeners();
 
   }
   
   Car.prototype.draw = function() {
-    this.image.onload = function() {
       this.ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
-    }.bind(this);
-   
   }
+
+
+  Car.prototype.setListeners = function() {
+    document.onkeydown = function(e) {
+      e.preventDefault();
+      switch(e.keyCode) {
+        case KEY_LEFT: 
+          if (this.x > 75) {
+            this.x -= this.vx;
+          }
+          break; 
+        case KEY_RIGHT: 
+          if (this.x < 360) {
+            this.x += this.vx;
+          }
+          break; 
+      }
+    }.bind(this);
+  }
+
 
   function Rectangle(canvas, x, y, w, h, color) {
     this.canvas = canvas;
@@ -37,6 +65,30 @@ window.onload = function(){
     this.ctx.fillRect(this.x, this.y, this.w, this.h);
  
   };
+
+  Rectangle.prototype.move = function(type) {
+
+    if (type === 'line') {
+      if (this.y < 800) {
+        this.y += 5;
+      } else {
+        this.y = 0
+      }
+  
+    } else {
+
+      if (this.y < 800) {
+        this.y += 5;
+      } else {
+        canvas.obstacles.shift();
+        console.log(canvas.obstacles);
+        this.y = 0
+      }
+  
+    }
+
+    
+  }
   
   function Canvas(id) {
     this.canvas = document.getElementById(id);
@@ -48,7 +100,10 @@ window.onload = function(){
       new Rectangle(this.canvas, 425, 0, 15, this.canvas.height, '#FFFFFF')];
     this.road = []
     this.car = new Car(this.canvas, 'images/car.png', 215, 600, 75, 150);
+    this.obstacles = [];
     this.fps = 60;
+    this.counter = 0;
+    this.id;
   }
 
   Canvas.prototype.generateLines = function() {
@@ -58,11 +113,51 @@ window.onload = function(){
       margin += 65;
     }
   }
-  
+
+  Canvas.prototype.generateObstacles = function() {
+    
+      var x = Math.floor(Math.random() * (250 - 70 + 1) + 70);
+      this.obstacles.push(new Rectangle(this.canvas,x,0,Math.floor(Math.random() * ((360 - x) - 50 + 1) + 50),50, '#950013'));
+      // this.obstacles.push(new Rectangle(this.canvas,Math.floor( Math.random () * (max - min + 1)) + min), 0, x, y, '#950013'));
+      console.log(this.obstacles);
+    }
+    
   Canvas.prototype.start = function() {
+
+    this.obstacles = [];
+    this.road = [];
+
     this.generateLines();
-    this.draw();
+
+    this.id = setInterval(function() {
+      this.clear();
+      this.move();
+      this.draw();
+      
+      this.counter++;
+      
+      if(this.counter % 110 == 0) {
+        this.generateObstacles();
+      }
+
+    }.bind(this), 1000/this.fps);
+
+
   };
+
+  Canvas.prototype.clear = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  Canvas.prototype.move = function() {
+    this.road.forEach(function(lineRoad) {
+      lineRoad.move('line');
+    })
+
+    this.obstacles.forEach(function(obstacle) {
+      obstacle.move('obstacle');
+    })
+  }
   
   Canvas.prototype.draw = function() {
 
@@ -76,13 +171,34 @@ window.onload = function(){
 
     this.car.draw();
 
-    // image.onload = function() {
+    this.obstacles.forEach(function(obstacle) {
+      
+      obstacle.draw();
 
-    // }
-    // this.ctx.drawImage(image, 250, 300, 100, 100);
+      if( canvas.car.x+canvas.car.w >= obstacle.x && obstacle.x+obstacle.w >= canvas.car.x &&
+        canvas.car.y+canvas.car.h >= obstacle.y && obstacle.y+obstacle.h >= canvas.car.y
+      ){
+        stopGame();
+      }
+    })
+
+  
+
   };
   
   var canvas = new Canvas("carGame");
-  canvas.start();
+  
+
+
+  function startGame() {
+     canvas.start();
+  }
+
+  function stopGame() {
+    this.obstacles = [];
+    this.road = [];
+    clearInterval(canvas.id);
+    alert('You lose!!'); 
+  }
   
 }
