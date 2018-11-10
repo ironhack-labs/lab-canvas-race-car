8429f1a193f00798f2428314b0bd64370dacf01f
Gerardo <gera@MacBook-de-Gerardo.local>
2018-11-09 21:49:23 -0600

    Añadiendo esqueleto

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,57 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+//canvas
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+//variables
+var interval
+var frames = 0
+var images = {
+}
+//clases
+function Board(){
+    this.x = 0
+    this.y = 0
+    this.width = canvas.width
+    this.height = canvas.height
+    this.image = new Image()
+    this.image.src = images.bg
+    //this.image.onload = ()=>this.draw()
+    this.draw = function(){
+        this.x--
+        if(this.x < -this.width) this.x = 0
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height)
+        ctx.drawImage(this.image,this.x + this.width,this.y,this.width,this.height)
+    }
+}
 
-  function startGame() {
+//instances
 
-  }
-};
+//main functions
+function start(){
+    if(!interval) interval = setInterval(update,1000/60)
+    
+}
+function update(){
+    frames++
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    bg.draw()
+}
+function gameOver(){
+    clearInterval(interval)
+    interval = null
+}
+
+//aux functions
+
+//listeners
+addEventListener('keyup',function(e){
+    switch(e.keyCode){
+        case 13:{
+            start()
+        }
+        return
+        default:
+            return
+    }
+} )
+
+drawCover()
