96ec813c96b0d5cff4164ce4f68e7605d7398328
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-10 18:16:23 -0600

    Iteration 1 done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,53 @@
+// canvas
+var canvas = document.getElementById("canvas")
+var ctx  = canvas.getContext('2d')
+
+// variables
+
+
+// classes
+function Board(){
+  this.x = 0
+  this.y = 0
+  this.width = canvas.width
+  this.height = canvas.height
+
+  this.draw = function(){
+    ctx.fillStyle = "gray"
+    ctx.fillRect(0,0,400,800)
+    ctx.fillStyle = "green"
+    ctx.fillRect(0,0,30,800)
+    ctx.fillRect(370,0,30,800)
+    ctx.fillStyle = "white"
+    ctx.fillRect(40,0,10,800)
+    ctx.fillRect(350,0,10,800)
+
+    // lines
+    var lineSpace = 80
+    var lineY = 0
+    ctx.fillStyle = "white"
+    for(var i = 0; i < 10; i++){
+      ctx.fillRect(197,lineY,6,40)
+      lineY += lineSpace
+    }
+  }
+}
+
+// instances
+var board = new Board()
+
+// main functions
+
+// aux functions
+
+// listeners
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-
+    board.draw()
   }
 };
