7a70723abd76a13c082d636a781c0479fe76a9f4
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-10 19:55:38 -0600

    Iteration 4 and 5 done

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,9 +3,15 @@ var canvas = document.getElementById("canvas")
 var ctx  = canvas.getContext('2d')
 
 // variables
+var interval
+var frames = 0
 var images = {
-  car: "./images/car.png"
+  car: "images/car.png",
+  obstacle_left: "images/obstacle_left.png",
+  obstacle_right:"images/obstacle_right.png"
 }
+var road = []
+var obstacles = []
 
 // classes
 function Board(){
@@ -25,25 +31,46 @@ function Board(){
     ctx.fillRect(350,0,10,800)
 
     // lines
-    var lineSpace = 80
-    var lineY = 0
-    ctx.fillStyle = "white"
-    for(var i = 0; i < 10; i++){
-      ctx.fillRect(197,lineY,6,40)
-      lineY += lineSpace
-    }
+    // var lineSpace = 80
+    // var lineY = 0
+    // ctx.fillStyle = "white"
+    // for(var i = 0; i < 10; i++){
+    //   ctx.fillRect(197,lineY,6,40)
+    //   lineY += lineSpace
+    // }
+  }
+  this.drawLines = function() {
+    this.y++
+    ctx.fillStyle = 'white'
+    ctx.fillRect(197, this.y, 6, 40)
   }
 }
 
 function Car(){
   this.x = canvas.width / 2 - 40
-  this.y = canvas.height / 2
+  this.y = canvas.height / 2 + 100
   this.width = 79
   this.height = 159
   this.image = new Image()
   this.image.src = images.car
 
   this.draw = function(){
+    if(this.x < 30) this.x = 30
+    if(this.x > 290) this.x = 290
+    ctx.drawImage(this.image, this.x, this.y,this.width,this.height)
+  }
+}
+
+function Obstacle(width,position){
+  // this.x = position === "left" ? 0 : canvas.width - width
+  this.x = position === "left" ? 0 : width
+  this.y = 0
+  this.width = width
+  this.height = 60
+  this.image = new Image()
+  this.image.src = position === "left" ? images.obstacle_left : images.obstacle_right
+  this.draw = function(){
+    this.y+=2
     ctx.drawImage(this.image, this.x, this.y,this.width,this.height)
   }
 }
@@ -53,10 +80,59 @@ var board = new Board()
 var car = new Car()
 
 // main functions
+function update(){
+  frames++
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+  board.draw()
+  drawRoad()
+  drawObstacles()
+  car.draw()
+}
 
 // aux functions
+function generateRoad() {
+  if (frames % 100 === 0) {
+    road.push(new Board())
+  }
+}
+
+function drawRoad() {
+  generateRoad()
+  road.forEach(board => {
+    board.drawLines()
+  })
+}
+
+function generateObstacles() {
+  if(frames % 250 === 0){
+    var width = Math.floor(Math.random()*250)
+    if(width % Math.floor(Math.random()*3) === 0){
+      var position = "left"
+    }
+    obstacles.push(new Obstacle(width,position))
+  }
+}
+
+function drawObstacles(){
+  generateObstacles()
+  obstacles.forEach(function(obstacle){
+    obstacle.draw()
+  })
+}
 
 // listeners
+addEventListener('keydown', function(e){
+  switch(e.keyCode){
+    //ArrowLeft
+    case 37:
+      car.x -= 30
+      break;
+    //ArrowRight
+    case 39:
+      car.x += 30
+      break;
+  }
+})
 
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
@@ -64,7 +140,6 @@ window.onload = function() {
   };
 
   function startGame() {
-    board.draw()
-    car.draw()
+    if(!interval) interval = setInterval(update,100/60)
   }
 };
