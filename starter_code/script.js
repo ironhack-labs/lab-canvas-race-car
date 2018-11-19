76b7c1739bf23043235eb324ccba8a91f56c55f3
StephGidde <steph.gidde@gmx.de>
2018-11-19 20:17:12 +0100

    final

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   var canvas = $("#game")[0];
   var ctx = canvas.getContext("2d");
+  var frameCounter = 0;
+  var obstacleArray = [];
   var img = new Image(); // Create new img element
   img.src = "images/car.png"; // Set source path
   var car = {
@@ -19,28 +21,15 @@ window.onload = function() {
   };
 
   class Obstacle {
-    constructor(x, y, height, width) {
-      this.x = x;
-      this.y = y;
-      this.height = height;
-      this.width = width;
+    constructor() {
+      this.x = Math.floor(Math.random() * 500);
+      this.y = Math.floor(Math.random() * -100);
+      this.height = Math.floor(Math.random() * 100);
+      this.width = Math.floor(Math.random() * 100);
     }
   }
 
-  var obstacleOne = new Obstacle(100, 300, 80, 120);
-  var obstacleTwo = new Obstacle(200, 200, 50, 100);
-  var obstacleThree = new Obstacle(350, 10, 50, 150);
-  var obstacleFour = new Obstacle(280, 100, 50, 150);
-  var obstacleFive = new Obstacle(10, 10, 50, 150);
-  // Test to see how to create a randomly shaped obstacle
-  // function createNewObstacle() {
-  //   var x = Math.floor(Math.random() * 100);
-  //   var y = Math.floor(Math.random() * 100);
-  //   var height = Math.floor(Math.random() * 100);
-  //   var width = Math.floor(Math.random() * 100);
-
-  //   return new Obstacle(x, y, height, width);
-  // }
+  // var obstacle = new Obstacle();
 
   document.getElementById("start-button").onclick = function() {
     ctx.drawImage(img, car.x, car.y, 80, 160);
@@ -61,45 +50,25 @@ window.onload = function() {
           console.log("right", car);
           break;
       }
-      //updateCanvas();
     };
   }
   function updateCanvas() {
     ctx.clearRect(0, 0, 500, 600);
     ctx.drawImage(img, car.x, car.y, 80, 160);
-    drawObstacle();
+    frameCounter++;
+    if (frameCounter % 120 == 0) {
+      var obstacle = new Obstacle();
+      obstacleArray.push(obstacle);
+      console.log(obstacleArray);
+    }
+    for (var i = 0; i < obstacleArray.length; i++) {
+      ctx.fillRect(
+        obstacleArray[i].x,
+        (obstacleArray[i].y += 1),
+        obstacleArray[i].width,
+        obstacleArray[i].height
+      );
+    }
     window.requestAnimationFrame(updateCanvas);
-  }
-  function drawObstacle() {
-    ctx.fillRect(
-      obstacleOne.x,
-      (obstacleOne.y += 1),
-      obstacleOne.width,
-      obstacleOne.height
-    );
-    ctx.fillRect(
-      obstacleTwo.x,
-      (obstacleTwo.y += 1),
-      obstacleTwo.width,
-      obstacleTwo.height
-    );
-    ctx.fillRect(
-      obstacleThree.x,
-      (obstacleThree.y += 1),
-      obstacleThree.width,
-      obstacleThree.height
-    );
-    ctx.fillRect(
-      obstacleFour.x,
-      (obstacleFour.y += 1),
-      obstacleFour.width,
-      obstacleFour.height
-    );
-    ctx.fillRect(
-      obstacleFive.x,
-      (obstacleFive.y += 1),
-      obstacleFive.width,
-      obstacleFive.height
-    );
   }
 };
