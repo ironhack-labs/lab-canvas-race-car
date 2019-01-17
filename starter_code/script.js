137d49f350cdecba644471fe428bb903c982ef55
Marianna Dobal <mddobal@gmail.com>
2019-01-17 21:16:05 -0200

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -29,7 +29,82 @@ window.onload = function() {
 
   function startGame() {
 
+    function random(max, min) {
+      return Math.random() * (max-min) + min ;
+    }
+
+    // function obstacle(ctx) {
+    //   ctx.fillStyle = 'red';
+    //   this.rectWidth = 70//random(150, 30);
+    //   this.obsX = random(240 - this.rectWidth, 40);
+    //   this.obsY = 0
+    //   this.rect = ctx.fillRect(this.obsX, this.obsY, this.rectWidth, 30);
+    // }
+
+    // var obstacles = [];
+    
+
+    // function obstacle(x){
+    //   // let x = random(240 - 100, 40)
+    //   this.x = x;
+    //   this.y = 0;
+    // }
+
+    // //setinterval cria um objeto 
+    // setInterval(function(){
+    //   const myObstacle = new obstacle();
+    //   obstacles.push(myObstacle);
+    // }, 2000);
+
+    // setInterval(function(){
+    //   ctx.clearRect(0, 0, 300, 500);
+    //   drawMap(ctx);
+    //   drawCar();
+
+    //   for(let i = 0; i < obstacles.length; i += 1){
+    //     obstacles[i].y += 1;
+    //     ctx.rect(obstacles[i].x, obstacles[i].y, 40, 20);
+    //     ctx.fillStyle = 'red';
+    //     ctx.fill();
+    //   }
+    // }, 1000);
+
+    var obstaclesArr = [];
+
+    function Obstacle(x){
+      this.y = 0;
+      this.x = x;
+    }
+
+    function xObstacle(){
+      // return 50 + Math.floor(Math.random() * 6) * 30;
+      return random(210, 40);
+    }
+
+    setInterval(function(){
+      const myObstacle = new Obstacle(xObstacle());
+      obstaclesArr.push(myObstacle);
+    }, 2000)
+
+    setInterval(function(){
+      const canvas = document.getElementById('my-canvas');
+      var ctx = canvas.getContext('2d'); 
+      
+      ctx.clearRect(0, 0, 300, 500);
+      drawMap(ctx)
+      drawCar();
+
+      for(let i = 0; i < obstaclesArr.length; i++){
+        obstaclesArr[i].y += 1;
+        ctx.rect(obstaclesArr[i].x, obstaclesArr[i].y, 50, 20);
+        ctx.fillStyle = "#800000";
+        ctx.fill();
+      }
+    }, 10)
+
+
     let playerCar = {
+      carX: 125,
       carX: 125,
       carY: 380,
       step: 10,
