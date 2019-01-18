18ac4e0bc68e5a6ce1f6fa489482187e479c4c47
Mr.X <seniorito.x@icloud.com>
2019-01-18 09:11:29 +0100

    First commit ಥ_ಥ

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,48 @@
 window.onload = function() {
+  //Cuando cargue el DOM me cargas el evento de escucha onclick que lanza la función startGame
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
-
+  //¿Qué funciones quieres que se lancen cuando pulsas sobre el botón Start Game? 
   function startGame() {
-
+    game.drawBackground()
+    game.drawCar();
   }
+
+  var game = new Game;
+
+//Estas declarando las propiedades de Game (función constructora en este caso del canvas) entonces tienes que apuntar al canvas al que quieres dibujar, decirle el contexto, y añadirle el tamaño del canvas 
+  function Game (){
+    this.canvas = document.querySelector("#canvasExp")
+    this.ctx = this.canvas.getContext('2d');
+    this.height = this.canvas.height;
+    this.width = this.canvas.width;
+
+    this.img = new Image();
+    this.img.src = 'images/car.png'
+    
+    
+    
+  }
+
+  Game.prototype.drawBackground = function (){
+    this.ctx.fillStyle = ("#008200")
+    this.ctx.fillRect (0,0,this.width,this.height)
+    this.ctx.fillStyle = ("#808080")
+    this.ctx.fillRect(30,0,this.width*0.88, this.height)
+    this.ctx.fillStyle = ("#FFFFFF")
+    this.ctx.fillRect (36, 0, 9, this.height)
+    this.ctx.fillStyle = ("#FFFFFF")
+    this.ctx.fillRect (427, 0, 9, this.height)
+
+    
+    }
+
+    Game.prototype.drawCar = function (){
+      this.ctx.drawImage(this.img, 100, 100)
+    }
+
+  
+
 };
+
