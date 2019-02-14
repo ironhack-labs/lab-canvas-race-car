503e8348676d2225d1de1ee3e86d9ff439db84d7
Daniel Herrero Hernando <danielherrerohernando@gmail.com>
2019-02-14 22:11:18 +0100

    several improvements

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,3 +1,5 @@
+var accelerator = 0
+
 function DrawApp(){
     this.canvasDom = undefined
     this.ctx = undefined
@@ -9,6 +11,7 @@ function DrawApp(){
     this.carheight = 100
     this.obstacles = []
     this.time = 0
+    this.interval = undefined
 }
 
 DrawApp.prototype.init = function(id){
@@ -35,24 +38,17 @@ DrawApp.prototype.drawBackground = function(){
     
 
 }
-var accelerator = 0
 
 DrawApp.prototype.animateApp = function(){
 
-    setInterval(function(){
+    this.interval = setInterval(function(){
         this.ctx.clearRect(0,0,400,800)
         this.drawBackground()
-        this.ctx.beginPath()
-        this.ctx.setLineDash([100,100])
-        this.ctx.lineWidth = 10
-        this.ctx.strokeStyle = 'white'
-        this.ctx.moveTo(195,this.linepos-100)
-        this.ctx.lineTo(195,this.linepos+800)
-        this.ctx.stroke()
-        this.linepos+=2+accelerator
+        this.drawLine()
         this.drawCar()
         this.drawObstacles()
         this.detectColisions()
+        score.innerText = this.time
         this.time++
         if (this.linepos>=200) this.linepos=0
         if (this.time%400===0) accelerator++
@@ -61,6 +57,17 @@ DrawApp.prototype.animateApp = function(){
         this.obstacles.push(new Obstacle())
     }.bind(this),2000)
 
+}
+
+DrawApp.prototype.drawLine = function(){
+    this.ctx.beginPath()
+    this.ctx.setLineDash([100,100])
+    this.ctx.lineWidth = 10
+    this.ctx.strokeStyle = 'white'
+    this.ctx.moveTo(195,this.linepos-100)
+    this.ctx.lineTo(195,this.linepos+800)
+    this.ctx.stroke()
+    this.linepos+=2+accelerator
 }
 
 
@@ -73,20 +80,20 @@ DrawApp.prototype.drawCar = function(){
 
 DrawApp.prototype.moveCarRight = function(){
     if (this.carpos<320)
-    this.carpos += 6
+    this.carpos += 8
 }
 
 DrawApp.prototype.moveCarLeft = function(){
     if(this.carpos>20)
-    this.carpos -= 6
+    this.carpos -= 8
 }
 
 
 function Obstacle (){
     this.posX = Math.random()*350
-    this.posY = -50
-    this.width = Math.random()*150
-    this.height = 50
+    this.posY = -30
+    this.width = Math.random()*100 + 50
+    this.height = 30
 }
 
 DrawApp.prototype.drawObstacles = function(){
@@ -106,15 +113,25 @@ DrawApp.prototype.detectColisions = function(){
     this.obstacles.forEach(function(elm){
         if ((elm.posY+elm.height>600)&&(elm.posY<600+this.carheight)){
             if((elm.posX<this.carpos)&&(elm.posX+elm.width>this.carpos)){
-                alert("You crashed")
+                finishdiv.style.visibility = "visible"
+                clearInterval(this.interval)
+                this.showFinishScore()
             }
             if((elm.posX<this.carpos+this.carwidth)&&(elm.posX+elm.width>this.carpos+this.carwidth)){
-                alert("You crashed")
+                finishdiv.style.visibility = "visible"
+                clearInterval(this.interval)
+                this.showFinishScore()
             }
             if((elm.posX>this.carpos)&&(elm.posX+elm.width<this.carpos+this.carwidth)){
-                alert("You crashed")
+                finishdiv.style.visibility = "visible"
+                clearInterval(this.interval)
+                this.showFinishScore()
             }
         }
     }.bind(this))
 }
 
+
+DrawApp.prototype.showFinishScore = function(){
+    finishscore.innerText += " " + this.time
+}
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,8 +1,14 @@
 var app = new DrawApp()
 
+var score = undefined
+var finishscore = undefined
+
+var finishdiv = undefined
+
 
 window.onload = function() {
   var gameboard = document.querySelector("#game-board")
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
@@ -18,8 +24,17 @@ window.onload = function() {
   }
 };
 
+document.addEventListener("DOMContentLoaded", function() { 
+  score = document.getElementById("score")
+  finishscore = document.getElementById("finishscore")
+  finishdiv = document.querySelector(".finish")
+}
+)
+
 
 window.onkeydown = function(e){
   if (e.keyCode===37) app.moveCarLeft()
   if (e.keyCode===39) app.moveCarRight()
 }
+
+
