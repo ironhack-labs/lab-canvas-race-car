0847d0cc54a79f148dfe44210d7a0c99083552d8
baltasar torres garcia <baltasartorresgarcia@MacBook-Pro-de-amelia.local>
2019-02-15 16:23:04 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,72 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+window.onload = function () {
+  document.getElementById("start-button").onclick = function () {
+    var app = new StartGame()
+    app.fondo()
+    app.lineaDiscontinua()
+    app.coche()
+    app._setListeners()
+  }
 
-  function startGame() {
+  function StartGame() {
+    this.app = document.getElementById('canvasId');
+    this.ctx = this.app.getContext('2d')
+    this.x = 150
 
   }
-};
+
+  StartGame.prototype.fondo = function () {
+    this.ctx.fillStyle = "green"
+    this.ctx.fillRect(0, 0, 400, 700)
+    this.ctx.fillStyle = "gray"
+    this.ctx.fillRect(50, 0, 300, 700)
+    this.ctx.fillStyle = "white"
+    this.ctx.fillRect(60, 0, 10, 700)
+    this.ctx.fillStyle = "white"
+    this.ctx.fillRect(330, 0, 10, 700)
+    this.ctx.fillStyle = "white"
+  }
+
+  StartGame.prototype.lineaDiscontinua = function () {
+    this.ctx.beginPath()
+    this.ctx.lineWidth = 8
+    this.ctx.strokeStyle = 'white'
+    this.ctx.setLineDash([30, 20])
+    this.ctx.moveTo(200, 0)
+    this.ctx.lineTo(200, 700)
+    this.ctx.stroke()
+  }
+
+  StartGame.prototype.coche = function (url) {
+    var img = new Image()
+    img.src = "./images/car.png"
+    img.onload = function () {
+      this.ctx.drawImage(img, this.x, 580, 90, 90)
+    }.bind(this)
+  }
+
+StartGame.prototype.moverLeft = function () {
+  this.x -= 5
+}
+StartGame.prototype.moverRight = function () {
+  this.x += 5
+}
+StartGame.prototype._setListeners = function () {
+  document.onkeydown = function (e) {
+    switch (e.keyCode) {
+      case (37):
+      this.moverLeft()
+        break;
+      case (39):
+        this.moverRight()
+        break;
+    }
+    this._refrescar()
+  }.bind(this)
+}
+StartGame.prototype._refrescar = function () {
+  this.ctx.clearRect(0,0,400,700)
+  this.fondo()
+  this.lineaDiscontinua()
+  this.coche()
+}
+}
