3217292e5a0a1fc3fc484f0817c035628abc737b
David Gallego Gomez <davizs@hotmail.com>
2019-02-21 00:53:05 +0100

    Commit inicial

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,16 @@ window.onload = function() {
   };
 
   function startGame() {
-
+    var canvas = document.getElementById('racecar');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 25, 550);
+    ctx.fillRect(375, 0, 25, 550);
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(25, 0, 10, 550);
+    ctx.fillRect(365, 0, 10, 550);
+    ctx.fillRect(45, 0, 310, 550);    
   }
 };
+
+
