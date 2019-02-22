27e4378bbdc06aaaf694fee6d8617c20a0f7ce19
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-02-22 18:42:34 +0100

    1º iteracion

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,21 @@ window.onload = function() {
   };
 
   function startGame() {
-
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0,0,350,600);
+    ctx.clearRect(50,0,10,600);
+    ctx.clearRect(290,0,10,600);
+    ctx.fillStyle = "green";
+    ctx.fillRect(0,0,40,600);
+    ctx.fillRect(310,0,40,600)
+    ctx.beginPath();
+    ctx.setLineDash([20, 15]);/*dashes are 5px and spaces are 3px*/;
+    ctx.lineWidth = "5";
+    ctx.strokeStyle = "white";
+    ctx.moveTo(175,25);
+    ctx.lineTo(175,600);
+    ctx.stroke();
   }
 };
