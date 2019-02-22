084a382337d2838ef896a03ca4516390967cb6ab
Fedde <feddetilma@hotmail.com>
2019-02-22 14:32:31 +0100

    first working version

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,47 +1,154 @@
 "use strict";
 var gameBoard;
+var framerate = 0;
+var imgCar = new Image();
+imgCar.src = "images/car.png";
+var obstacles = [];
 
+// GAME BOARD constructor
 function GameBoardConstr() {
-  this.width = 350;
-  this.heigth = 600;
+  this.width = document.getElementById("mycanvas").getAttribute("width");
+  this.height = document.getElementById("mycanvas").getAttribute("height");
   this.score = 0;
   this.ctx = document.getElementById("mycanvas").getContext("2d");
-  this.clearCanvas = this.ctx.clearRect(0, 0, this.width, this.heigth);
+  this.clearCanvas = this.ctx.clearRect(0, 0, this.width, this.height);
+  this.borderLeft = 45;
+  this.borderRight = this.width - 50 - car.width;
+  this.obstacleInterval = 250;
+  this.obstacleSpeed = 2;
+  // this.withDrivable = this.width - 90;
 };
 
+// CAR object
+var car = {
+  x: 150,
+  y: 500,
+  width: 158 * 0.25,
+  height: 319 * 0.25
+}
+
+// OBSTACLE constructor
+function ObstacleConstr() {
+  // 45 to 175
+  this.x = Math.random() * 130 + 45;
+  this.y = -10;
+  // 45 to 130
+  this.width = Math.random() * 85 + 45;
+  this.xmax = this.x + this.width;
+  this.height = 15;
+  this.draw = function () {
+    gameBoard.ctx.fillStyle = "red";
+    gameBoard.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+
+addEventListener("keydown", function (e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      if (car.x > gameBoard.borderLeft) {
+        car.x -= 5;
+      }
+      console.log("left is pressed" + car.x);
+      break;
+    case "ArrowRight":
+      if (car.x < gameBoard.borderRight) {
+        car.x += 5;
+      }
+      console.log("right is pressed " + car.x)
+      break;
+  }
+})
+
+// Initializing the board
 window.onload = function () {
   document.getElementById("start-button").onclick = function () {
     startGame();
   };
 
   function startGame() {
-    // create board
     gameBoard = new GameBoardConstr();
-    // clear board
-    gameBoard.clearCanvas;
-    // create road
-    gameBoard.createRoad();
+    requestAnimationFrame(updateCanvas); // no function parantheses
+    // gameBoard.createRoad();
+    // gameBoard.createCar();
+
+
   }
 };
 
+function updateCanvas() {
+  framerate++;
+  gameBoard.ctx.clearRect(0, 0, gameBoard.width, gameBoard.height)
+  gameBoard.createRoad();
+  gameBoard.createCar();
+  // console.log(framerate); 
+  if (framerate % gameBoard.obstacleInterval === 0) {
+    var obstacle = new ObstacleConstr;
+    obstacles.push(obstacle);
+    // console.log(obstacles);
+    gameBoard.score++;
+  };
+  for (var i = 0; i < obstacles.length; i++) {
+    obstacles[i].y += gameBoard.obstacleSpeed;
+    obstacles[i].draw();
+    // delete item when out of sight
+    if (obstacles[i].y > gameBoard.height) {
+      obstacles.splice(i, 1);
+    };
+    // collision check here?
+    // console.log(obstacles[i].y);
+    // console.log(car.y);
+    if (obstacles[i].y == car.y) {
+      console.log("yes Y");
+    }
+    // if (car.x + car.width >= obstacles[i].x) {
+    //   console.log("yes x 1");
+    // }
+    // if (car.x <= obstacles[i].xmax) {
+    //   console.log("yes x 2");
+    // }
+    if (obstacles[i].y == car.y && car.x + car.width >= obstacles[i].x && car.x <= obstacles[i].xmax) {
+      alert("game over");
+      return;
+    }
+  };
+  requestAnimationFrame(updateCanvas);
+};
+
+
+
+// draw road
 GameBoardConstr.prototype.createRoad = function () {
   // start grey
   this.ctx.fillStyle = "grey";
-  this.ctx.fillRect(0, 0, this.width, this.heigth);
+  this.ctx.fillRect(0, 0, this.width, this.height);
   // green lanes
   var widthGr = 25;
   this.ctx.fillStyle = "green";
-  this.ctx.fillRect(0, 0, widthGr, this.heigth);
-  this.ctx.fillRect(this.width - widthGr, 0, widthGr, this.heigth);
+  this.ctx.fillRect(0, 0, widthGr, this.height);
+  this.ctx.fillRect(this.width - widthGr, 0, widthGr, this.height);
   // white lines (outer)
-  this.ctx.clearRect(widthGr + 10, 0, 10, this.heigth);
-  this.ctx.clearRect(this.width - widthGr - 10 - 10, 0, 10, this.heigth);
+  var widthWi = 10;
+  this.ctx.clearRect(widthGr + widthWi, 0, widthWi, this.height);
+  this.ctx.clearRect(this.width - widthGr - widthWi * 2, 0, widthWi, this.height);
   // white lines (dashed)
   this.ctx.beginPath();
   this.ctx.setLineDash([15, 15]);
   this.ctx.strokeStyle = "white";
   this.ctx.lineWidth = 4;
-  this.ctx.moveTo(this.width / 2, this.heigth + 15 / 2);
+  this.ctx.moveTo(this.width / 2, this.height + 15 / 2);
   this.ctx.lineTo(this.width / 2, 0);
   this.ctx.stroke();
-}
+};
+
+// draw car
+GameBoardConstr.prototype.createCar = function () {
+  // console.log("create car");
+
+  var that = this;
+  // imgCar.onload = function () { 
+  //   that.ctx.drawImage(imgCar, car.x, car.y, car.width, car.height);
+  // };
+  that.ctx.drawImage(imgCar, car.x, car.y, car.width, car.height);
+};
+
+
