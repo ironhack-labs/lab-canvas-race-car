5110dbf953fce298880a6f1845515d3548aee736
Alberto González <algorinetportfolio@gmail.com>
2019-03-28 17:57:17 +0100

    iteration3

--- starter_code/script.js
+++ starter_code/script.js
@@ -6,14 +6,22 @@ var canvas;
 /** @type {CanvasRenderingContext2D} */
 var ctx;
 var w, h, w2, h2;
-var car = new Image;
-car.src = "./images/car.png";
+var img = new Image;
+img.src = "./images/car.png";
+var pressedLeft = false;
+var pressedRight = false;
+var posX;
+var posY;
 
 function setup() {
   canvas = document.querySelector("#canvas");
   ctx = canvas.getContext("2d");
 
- 
+
+  document.onkeydown = handlerDown;
+
+  document.onkeyup = handlerUp;
+
 
   window.onresize = function () {
 
@@ -23,12 +31,12 @@ function setup() {
   }
 
   startGame();
-/* 
-  document.getElementById("start-button").onclick = function () {
-    startGame();
+  /* 
+    document.getElementById("start-button").onclick = function () {
+      startGame();
 
-  };
- */
+    };
+   */
 }
 
 window.onload = function () {
@@ -52,6 +60,8 @@ function setCanvasDimensions() {
 function startGame() {
   document.querySelector(".game-intro").style.display = "none"
   setCanvasDimensions();
+  posX = w2 - 50 / 2;
+  posY = h - 300 / 2;
   gameLoop();
 }
 
@@ -61,45 +71,78 @@ function gameLoop() {
   drawRoad();
   drawDashLine();
   drawSideLine(120);
-  drawSideLine(w-120)
+  drawSideLine(w - 120)
   drawCar();
-  requestAnimationFrame(startGame);
+  requestAnimationFrame(gameLoop);
 }
 
 function drawBackground() {
   ctx.beginPath()
   ctx.fillStyle = "green"
-  ctx.rect(0,0,w,h)
+  ctx.rect(0, 0, w, h)
   ctx.fill();
   ctx.closePath();
 }
+
 function drawRoad() {
   ctx.beginPath()
   ctx.fillStyle = "gray"
-  ctx.rect(100,0,w-200,h)
+  ctx.rect(100, 0, w - 200, h)
   ctx.fill();
   ctx.closePath()
 }
+
 function drawDashLine() {
   ctx.beginPath();
   ctx.strokeStyle = "white"
   ctx.lineWidth = 10;
   ctx.setLineDash([0, 20, 2])
-  ctx.moveTo(w2,0);
-  ctx.lineTo(w2,h);
+  ctx.moveTo(w2, -20 + Math.random()*50);
+  ctx.lineTo(w2, h - Math.random()*50);
   ctx.stroke()
   ctx.closePath();
 }
+
 function drawSideLine(x) {
   ctx.restore();
   ctx.beginPath();
   ctx.strokeStyle = "white"
   ctx.lineWidth = 10;
-  ctx.moveTo(x,0);
-  ctx.lineTo(x,h);
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, h);
   ctx.stroke()
   ctx.closePath();
 }
+
+function moveCar() {
+  if(pressedLeft && posX-1>100) {
+    posX--;
+  } 
+  if(pressedRight && posX+1<w-150) {
+    posX++;
+  } 
+}
+
 function drawCar() {
-  ctx.drawImage(car, w2-50/2,h-300/2,50,100)
+  moveCar()
+  ctx.drawImage(img, posX, posY, 50, 100)
+
+}
+
+function handlerDown(e) {
+  if (e.keyCode === 37) {
+    pressedLeft = true;
+  }
+   if (e.keyCode === 39) {
+    pressedRight = true;
+  }
+}
+
+function handlerUp(e) {
+  if (e.keyCode === 37) {
+    pressedLeft = false;
+  } 
+  if (e.keyCode === 39) {
+    pressedRight = false;
+  }
 }
