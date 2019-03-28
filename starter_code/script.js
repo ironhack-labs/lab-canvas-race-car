dd72822b78c447496b8336add6d333f256c6c36d
Alberto González <algorinetportfolio@gmail.com>
2019-03-28 16:52:43 +0100

    parcial

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,58 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
+/** @type {HTMLCanvasElement} */
+var canvas;
 
-  function startGame() {
+
+// Contexto del canvas, lo utilizamos para pintar
+/** @type {CanvasRenderingContext2D} */
+var ctx;
+var w, h, w2, h2;
+
+function setup() {
+  canvas = document.querySelector("#canvas");
+  ctx = canvas.getContext("2d");
+
+ 
+
+  window.onresize = function () {
+
+
+    setCanvasDimensions()
 
   }
+
+  document.getElementById("start-button").onclick = function () {
+    startGame();
+
+  };
+
+}
+
+window.onload = function () {
+
+  setup();
+
+
 };
+
+function setCanvasDimensions() {
+  w = window.innerWidth
+  h = window.innerHeight
+  w2 = w / 2
+  h2 = h / 2
+  canvas.setAttribute("height", h);
+  canvas.setAttribute("width", w);
+
+}
+
+
+function startGame() {
+  document.querySelector(".game-intro").style.display = "none"
+  setCanvasDimensions();
+  gameLoop();
+}
+
+function gameLoop() {
+  ctx.fillStyle = "black"
+  requestAnimationFrame(startGame());
+
+}
