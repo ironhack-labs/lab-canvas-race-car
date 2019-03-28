3f8fef1efb0d971e7592b081bfedc76dcc5750f5
Natalia Franquet <n.franquet.lm@gmail.com>
2019-03-28 20:12:00 +0100

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -5,12 +5,18 @@ var w;
 var h;
 var dimensions;
 var color;
+var posX;
+var speed;
+var counter = 0;
+var imgCar = new Image();
+imgCar.src = "./images/car.png";
+
 
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
-
+  
     canvas = document.querySelector("#container")
     ctx = canvas.getContext('2d');
     w = canvas.clientWidth;
@@ -19,7 +25,12 @@ window.onload = function() {
     color ='red';
 
   function startGame() {
-    draw(ctx,w,h,color,dimensions);
+    setInterval(() => {
+      ctx.clearRect(0, 0, w, h);
+      draw(ctx,w,h,color,dimensions);
+      counter++;
+    }, 1000/60);
+
   }
 };
 
@@ -29,37 +40,51 @@ function draw (ctx,w,h,color,dimensions){
   drawRoad(ctx, 35, h, 'white', 330);
   drawRoad(ctx, 45, h, 'gray', 310);
   drawLine(ctx,h);
-  drawCar(ctx,h);
+  drawCar();
 }
 
 function drawRoad(ctx,w,h,color,dimensions) {
-  ctx.beginPath()
-  //ctx.setLineDash([0])
-  ctx.fillStyle = color
-  ctx.rect(0+w, 0, dimensions, h)
-  ctx.fill()
-  ctx.restore()
-  ctx.closePath()
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.rect(0+w, 0, dimensions, h);
+  ctx.fill();
+  ctx.restore();
+  ctx.closePath();
 }
 
 function drawLine(ctx,h) {
-  ctx.beginPath()
-  ctx.strokeStyle = `white`
-  ctx.lineWidth = 5
-  ctx.setLineDash([10, 0, 10])
-  ctx.moveTo(200, 0)
-  ctx.lineTo(200, h)
-  ctx.stroke()
-  ctx.closePath()
+  ctx.beginPath();
+  ctx.strokeStyle = `white`;
+  ctx.lineWidth = 5;
+  ctx.setLineDash([10, 0, 10]);
+  ctx.lineDashOffset = -counter;
+  ctx.moveTo(200, 0);
+  ctx.lineTo(200, h);
+  ctx.stroke();
+  ctx.closePath();
 }
 
-function drawCar(ctx,h) {
-  var imgCar = new Image();
-  imgCar.src = "./images/car.png";
-  imgCar.onload = function () {
-  ctx.drawImage(imgCar,165,600,70,150);
-  };
+function drawCar() {
+    ctx.drawImage(imgCar,posX,600,70,150);
 }
+
+posX=165;
+window.onkeydown = function (e) {
+  if(posX > 25) {
+    if (e.keyCode === 37) {
+      posX-=5;
+      drawCar();
+    }
+  }
+  if(posX < 300) {
+    if (e.keyCode === 39) {
+      posX+=5;
+      drawCar();
+    }
+  }
+};
+
+
 
 
 
