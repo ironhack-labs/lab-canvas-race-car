e0397a1e97ba9e4412728da83a1be3d4612c65f4
Alberto González <algorinetportfolio@gmail.com>
2019-03-28 17:15:04 +0100

    iteration 1

--- starter_code/script.js
+++ starter_code/script.js
@@ -20,11 +20,13 @@ function setup() {
 
   }
 
+  startGame();
+/* 
   document.getElementById("start-button").onclick = function () {
     startGame();
 
   };
-
+ */
 }
 
 window.onload = function () {
@@ -52,7 +54,47 @@ function startGame() {
 }
 
 function gameLoop() {
-  ctx.fillStyle = "black"
-  requestAnimationFrame(startGame());
+  ctx.save();
+  drawBackground();
+  drawRoad();
+  drawDashLine();
+  drawSideLine(120);
+  drawSideLine(w-120)
+  requestAnimationFrame(startGame);
 
+}
+
+function drawBackground() {
+  ctx.beginPath()
+  ctx.fillStyle = "green"
+  ctx.rect(0,0,w,h)
+  ctx.fill();
+  ctx.closePath();
+}
+function drawRoad() {
+  ctx.beginPath()
+  ctx.fillStyle = "gray"
+  ctx.rect(100,0,w-200,h)
+  ctx.fill();
+  ctx.closePath()
+}
+function drawDashLine() {
+  ctx.beginPath();
+  ctx.strokeStyle = "white"
+  ctx.lineWidth = 10;
+  ctx.setLineDash([0, 20, 2])
+  ctx.moveTo(w2,0);
+  ctx.lineTo(w2,h);
+  ctx.stroke()
+  ctx.closePath();
+}
+function drawSideLine(x) {
+  ctx.restore();
+  ctx.beginPath();
+  ctx.strokeStyle = "white"
+  ctx.lineWidth = 10;
+  ctx.moveTo(x,0);
+  ctx.lineTo(x,h);
+  ctx.stroke()
+  ctx.closePath();
 }
