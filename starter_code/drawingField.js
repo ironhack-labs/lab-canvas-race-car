88a1dce10a02f3e50e0a30aceeb1633d9d4cda7e
Carlos Cortés <Ccortsg@gmail.com>
2019-03-28 19:11:05 +0100

    Car Done

--- starter_code/drawingField.js
+++ starter_code/drawingField.js
@@ -3,6 +3,7 @@ h = window.innerHeight;
 w2 = w / 2;
 h2 = h / 2;
 
+
 function drawRoad(ctx) {
     // start the path
     ctx.beginPath();
@@ -31,13 +32,7 @@ function drawRoad(ctx) {
     ctx.moveTo(w-105,0)
     ctx.lineTo(w-105,h)
     ctx.stroke()
-    ctx.beginPath();
-    ctx.setLineDash([50,50]);
-    ctx.strokeStyle = `rgba(255, 255, 255, 1)`
-    ctx.lineWidth = 10;
-    ctx.moveTo(w2,0)
-    ctx.lineTo(w2,h)
-    ctx.stroke()
+
 
     // start a new line from these coordinates: x=250, y=50
     // ctx.moveTo(250, 50);
@@ -45,4 +40,33 @@ function drawRoad(ctx) {
     // ctx.lineTo(250, 100);
     // .stroke() executes the drawing
     // ctx.stroke();
+}
+function drawMiddleLine(ctx,offset) {
+    ctx.beginPath();
+    ctx.setLineDash([50,50]);
+    ctx.lineDashOffset = -offset;
+    ctx.strokeStyle = `rgba(255, 255, 255, 1)`
+    ctx.lineWidth = 10;
+    ctx.moveTo(w2,0)
+    ctx.lineTo(w2,h)
+    ctx.stroke()
+}
+
+// function march(ctx,offset) {
+//     offset++;
+//     if (offset > 16) {
+//       offset = 0;
+//     }
+//     drawMiddleLine(ctx,offset);
+//     setTimeout(march, 20);
+//   }
+  
+
+function drawBugga(ctx){
+    // debugger
+    bugga = new Image();   // Create new <img> element
+    bugga.src = './images/car.png'; // Set source path
+    bugga.onload = ()=>{
+        ctx.drawImage(bugga, w2-25,h-200,50,100);
+    }
 }
--- starter_code/script.js
+++ starter_code/script.js
@@ -1,6 +1,7 @@
 // setup(canvas, draw11)
 window.onload = function() {
   var canvas = document.querySelector("#game");
+  var offset = 0;
 
   /** @type {CanvasRenderingContext2D} */
   var ctx = canvas.getContext('2d');
@@ -8,20 +9,39 @@ window.onload = function() {
   canvas.setAttribute("height", window.innerHeight);
   canvas.setAttribute("width", window.innerWidth/2);
   // setup(canvas,draw);
-  
-  // ctx.beginPath();
-  // ctx.fillRect(25, 25, 100, 100);
-  // ctx.stroke();
-  // var lineHeight = 200;
-  // ctx.strokeStyle = red;
-  // ctx.moveTo(i, h2 - lineHeight / 2)
-  // ctx.lineTo(i, h2 + lineHeight / 2)
-  // ctx.stroke();
+  // drawRoad(ctx);
+  // drawMiddleLine(ctx,offset);
+  // march(ctx,offset);
+  // drawBugga(ctx);
+
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
     drawRoad(ctx);
+    drawMiddleLine(ctx,offset);
+    // march(ctx,offset);
+    drawBugga(ctx);
   }
+  // function drawMiddleLine() {
+  //   ctx.beginPath();
+  //   ctx.setLineDash([50,50]);
+  //   ctx.lineDashOffset = -offset;
+  //   ctx.strokeStyle = `rgba(255, 255, 255, 1)`
+  //   ctx.lineWidth = 10;
+  //   ctx.moveTo(w2,0)
+  //   ctx.lineTo(w2,h)
+  //   ctx.stroke()
+  // }
+
+  // function march() {
+  //   offset++;
+  //   if (offset > 50) {
+  //     offset = 0;
+  // }
+  //   drawMiddleLine(ctx,offset);
+  //   setTimeout(march, 20);
+  // }
 };
+
