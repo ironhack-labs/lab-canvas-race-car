337408b9cedb0a7960239c0fb6944213fd8165e1
Francisco Vaca <fcovacarh@gmail.com>
2019-03-28 16:45:18 +0100

    Now start button creates canvas

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,22 @@
+var canvas, ctx;
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
+    var gameboard = document.querySelector('#game-board');
+    canvas = document.createElement('canvas');
+    canvas.width = this.innerWidth / 2;
+    canvas.height = this.innerHeight;
+    canvas.getContext('2d');
+    gameboard.appendChild(canvas);
+  }
 
+  function paintRoad() {
+    ctx.fillRect(0, 0, 50, h);
   }
 };
