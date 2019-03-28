5c678b08ab4e6e8db0ccaba9b6a0862dba49fbd5
erFedeRuiz <federicoruiz.ruiz@gmail.com>
2019-03-28 23:44:03 +0100

    Iteration 1

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,98 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
 
-  function startGame() {
+/** @type {HTMLCanvasElement} */
+var canvas = document.querySelector("#game");
+/** @type {CanvasRenderingContext2D} */
+var ctx = canvas.getContext('2d');
 
-  }
+var w, h, w2, h2;
+var carImg = new Image;
+carImg.src = "./images/car.png";
+var arrowLeftOn = false;
+var arrowRightOn = false;
+var posX;
+var posY;
+var frameID;
+var score = 0;
+
+window.onload = function () {
+  document.onkeydown = pressingDown;
+  document.onkeyup = freeUp;
 };
+
+window.onresize = function () {
+  setCanvasDimensions()
+}
+
+document.getElementById("start-button").onclick = function() {  
+  startGame();
+};
+
+
+function setCanvasDimensions() {
+  w = window.innerWidth/2;
+  h = window.innerHeight;
+  w2 = w / 2;
+  h2 = h / 2;
+
+  canvas.setAttribute("height", h);
+  canvas.setAttribute("width", w);
+}
+
+
+function startGame() {
+  setCanvasDimensions();
+  posX = (w-50)/2 ;
+  posY = h - 300 / 2;
+  gameProcess();
+}
+
+function gameProcess() {
+  //Iteration 1
+  frameID =requestAnimationFrame(gameProcess);
+  ctx.save();
+  drawRoad();
+  drawMovingLine();
+  drawExternalLines(115);
+  drawExternalLines(w - 115)
+
+}
+
+
+
+function drawRoad() {
+  ctx.beginPath()
+  ctx.fillStyle = "#0E8014"
+  ctx.rect(0, 0, w, h)
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath()
+  ctx.fillStyle = "#808080"
+  ctx.rect(100, 0, w - 200, h)
+  ctx.fill();
+  ctx.closePath()
+}
+
+function drawMovingLine() {
+  var offset = Math.random();
+  ctx.beginPath();
+  ctx.strokeStyle = "#FFFFFF"
+  ctx.lineWidth = 10;
+  ctx.setLineDash([0, 20, 2])
+  ctx.moveTo(w2, -20 + offset * 50);
+  ctx.lineTo(w2, h + offset * 50);
+  ctx.stroke()
+  ctx.closePath();
+}
+
+function drawExternalLines(x) {
+  ctx.restore();
+  ctx.beginPath();
+  ctx.strokeStyle = "#FFFFFF"
+  ctx.lineWidth = 10;
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, h);
+  ctx.stroke()
+  ctx.closePath();
+}
+
