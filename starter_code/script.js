aa2eaffc692c778835442f4794e3d3711ed877f4
pipepico <fpr29.10@gmail.com>
2019-04-04 14:09:55 -0600

    corrections

--- starter_code/script.js
+++ starter_code/script.js
@@ -58,15 +58,39 @@ window.onload = function() {
   }
 
   class Obstacles{
-    constructor(x,y,w,h){
-      this.x = x
-      this.y = y
+    constructor(w){
       this.w = w
-      this.h = h
+    }
+    draw(y,w){
+      ctx.fillStyle = 'orange'
+      ctx.fillRect(75,y,w,20)
+      // ctx.fillStyle = 'orange'
+      // ctx.fillRect(150,y,w,20)
+      // ctx.fillStyle = 'orange'
+      // ctx.fillRect(200,y,w,20)
+      // ctx.fillStyle = 'orange'
+      // ctx.fillRect(250,y,w,20)
+    }
+    draw2(y,w){
+      ctx.fillStyle = 'orange'
+      ctx.fillRect(250,y,w,20)
+    }
+    draw3(y,w){
+      ctx.fillStyle = 'orange'
+      ctx.fillRect(150,y,w,20)
+    }
+    draw4(y,w){
+      ctx.fillStyle = 'orange'
+      ctx.fillRect(200,y,w,20)
     }
   }
   let board = new Board()
   let player = new Player(175,400,imagenes.img1)
+  let obstacle = new Obstacles(randomNum(275))
+  let positiony2 = -100
+  let positiony3 = -190
+  let positiony4 = -250
+  let positiony = 0
   let frames = 0
   let interval
   
@@ -84,11 +108,26 @@ window.onload = function() {
       }
     })
   };
-
+  function randomNum(len,suma, resta){
+    let x = Math.floor(Math.random()*len)
+    return x
+  }
   function update(){
     ctx.clearRect(0,0,canvas.width, canvas.height)
     board.draw()
     player.draw()
+    if (positiony > 500){positiony = 0} 
+    if (positiony2 > 500){positiony2 = 0} 
+    if (positiony3 > 500){positiony3 = 0} 
+    if (positiony4 > 500){positiony4 = 0} 
+    positiony++
+    positiony2++
+    positiony3++
+    positiony4++
+    obstacle.draw(positiony,80)
+    obstacle.draw2(positiony2,80)
+    obstacle.draw3(positiony3,80)
+    obstacle.draw4(positiony4,80)
     frames++
   }
   function startGame() {
