f76ea3fc5f5749fc2b8c946408ea43c30c4d6b5d
pipepico <fpr29.10@gmail.com>
2019-04-05 09:09:17 -0600

    updating

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,7 +1,7 @@
 // PP MEX Gabriela Felipe
 window.onload = function() {
   const imagenes = {
-    img1:'/images/car.png',
+    img1:'./images/car.png',
     img2:''
   }
   const canvas = document.querySelector('canvas')
@@ -40,30 +40,41 @@ window.onload = function() {
       this.y = y
       this.img = new Image()
       this.img.src = img
-      this.img.onload = () =>{
-        this.draw()
-      }
+      // this.img.onload = () =>{
+      //   this.draw()
+      // }
     }
     draw(){
+      if (this.y > 400  ) this.y = 400
       ctx.drawImage(this.img, this.x, this.y, 50,80)
+      this.y++
     }
     moveRight(){
-      if (this.x > 275 ) this.x = 165
+      if (this.x > 275 ) this.x = 265
       this.x += 10
     }
     moveLeft(){
-      if (this.x < 75 ) this.x = 185
+      if (this.x < 75 ) this.x = 85
       this.x -= 10
+    }
+    moveUp(){
+      if (this.y > 500 ) this.y = 490
+      this.y += 10
+    }
+    moveDown(){
+      if (this.y < 80 ) this.y = 90
+      this.y -= 10
     }
   }
 
   class Obstacles{
-    constructor(w){
+    constructor(x,w){
+      this.x = x
       this.w = w
     }
-    draw(y,w){
+    draw(){
       ctx.fillStyle = 'orange'
-      ctx.fillRect(75,y,w,20)
+      ctx.fillRect(this.x,positiony,this.w-75,20)
       // ctx.fillStyle = 'orange'
       // ctx.fillRect(150,y,w,20)
       // ctx.fillStyle = 'orange'
@@ -72,21 +83,23 @@ window.onload = function() {
       // ctx.fillRect(250,y,w,20)
     }
     draw2(y,w){
-      ctx.fillStyle = 'orange'
+      ctx.fillStyle = 'blue'
       ctx.fillRect(250,y,w,20)
     }
     draw3(y,w){
-      ctx.fillStyle = 'orange'
-      ctx.fillRect(150,y,w,20)
+      ctx.fillStyle = 'red'
+      ctx.fillRect(150,y,Math.random()*200,20)
     }
     draw4(y,w){
-      ctx.fillStyle = 'orange'
+      ctx.fillStyle = 'green'
       ctx.fillRect(200,y,w,20)
     }
   }
+  const _POSITION = 75
+  const _WIDTH = 200
   let board = new Board()
   let player = new Player(175,400,imagenes.img1)
-  let obstacle = new Obstacles(randomNum(275))
+  let obstacle = new Obstacles(rnd(200),rnd(200))
   let positiony2 = -100
   let positiony3 = -190
   let positiony4 = -250
@@ -104,18 +117,37 @@ window.onload = function() {
         case 37:
           player.moveLeft()
           break
-
+        case 40:
+          player.moveUp()
+          break
+        case 38:
+          player.moveDown()
+          break
       }
     })
   };
-  function randomNum(len,suma, resta){
-    let x = Math.floor(Math.random()*len)
+  function rndX(){
+    let x = Math.floor(Math.random()*_WIDTH) + _POSITION 
+  }
+  function rndWidth(){
+
+  }
+  function rnd(len){
+    let x = len
+    x = Math.floor(Math.random()*len)
     return x
   }
   function update(){
     ctx.clearRect(0,0,canvas.width, canvas.height)
     board.draw()
     player.draw()
+    if (frames >= 500){
+      frames = 0
+      obstacle = new Obstacles(rnd(_WIDTH)+_POSITION)
+    }
+    if( frames%100 === 0){
+    }
+    
     if (positiony > 500){positiony = 0} 
     if (positiony2 > 500){positiony2 = 0} 
     if (positiony3 > 500){positiony3 = 0} 
@@ -124,8 +156,8 @@ window.onload = function() {
     positiony2++
     positiony3++
     positiony4++
+    obstacle.draw2(positiony2,80) 
     obstacle.draw(positiony,80)
-    obstacle.draw2(positiony2,80)
     obstacle.draw3(positiony3,80)
     obstacle.draw4(positiony4,80)
     frames++
