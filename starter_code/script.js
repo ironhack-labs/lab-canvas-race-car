a163d66f5def26ebb0cb0ca96788a6f36f2dfe20
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-09 17:18:47 -0400

    Initial commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,6 +4,46 @@ window.onload = function() {
   };
 
   function startGame() {
+    var img = new Image();
+      img.src = './images/road.png';
+      
 
-  }
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+
+      var backgroundImage = {
+        img: img,
+        y: 0,
+        speed: 1,
+
+        move: function() {
+          this.y += this.speed;
+          this.y %= canvas.height;
+        },
+
+        draw: function() {
+          ctx.drawImage(this.img, 0, this.y);
+          if (this.speed < 0) {
+            ctx.drawImage(this.img, 0, this.y + canvas.height);
+          } else {
+            ctx.drawImage(this.img, 0, this.y - this.img.height);
+          }
+        },
+      };
+
+      function updateCanvas() {
+        backgroundImage.move();
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        backgroundImage.draw();
+
+        requestAnimationFrame(updateCanvas);
+      }
+
+      // start calling updateCanvas once the image is loaded
+      img.onload = updateCanvas;
+
+        }
 };
+
+
