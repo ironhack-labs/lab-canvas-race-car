e034ceb783bc3e9f6c18079bbdecb89be9085cb8
Marnelas <marnelasbenito@gmail.com>
2019-05-02 18:54:49 +0200

    update RaceCarChallenge

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,98 @@
+const DrawApp = {
+  version: "1.0",
+  name: "Car Race",
+  description: "app to Race Point Challenge",
+  author: "Manuel",
+  canvasDom: undefined,
+  car: undefined,
+  ctx: undefined,
+  winW: undefined,
+  winH: undefined,
+  init: function(id) {
+    this.canvasDom = document.getElementById(id);
+    this.ctx = this.canvasDom.getContext("2d");
+    this.setDimensions();
+    this.setHandlers();
+    this.car = new RaceCar(this.ctx, "images/car.png", this.winW, this.winH);
+  },
+  setDimensions: function() {
+    this.canvasDom.setAttribute("width", window.innerWidth);
+    this.canvasDom.setAttribute("height", window.innerHeight);
+    this.winH = window.innerHeight;
+    this.winW = window.innerWidth;
+  },
+  setHandlers: function() {
+    window.onresize = () => this.setDimensions();
+  },
+  drawFilledSquares: function() {
+    this.ctx.fillStyle = "grey"; // cambia los colores de relleno
+    this.ctx.fillRect(10, 0, 900, this.winW);
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(0, 0, 100, this.winW);
+    this.ctx.fillRect(810, 0, 100, this.winW);
+  },
+  drawLine: function() {
+    this.ctx.strokeStyle = "green";
+    this.ctx.lineWidth = 20;
+    this.ctx.setLineDash([60, 30]);
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = "white";
+    this.ctx.moveTo(450, 0);
+    this.ctx.lineTo(450, this.winW);
+    this.ctx.stroke();
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = "white";
+    this.ctx.moveTo(450, 0);
+    this.ctx.lineTo(450, this.winW);
+    this.ctx.stroke();
+    this.ctx.fill();
+    this.ctx.closePath();
+  },
+  drawAll: function() {
+    this.car.draw();
+  }
+};
+
+class RaceCar {
+  constructor(ctx, url, winW, winH) {
+    this.ctx = ctx;
+
+    this.img = new Image();
+    this.img.src = url;
+    this.winH = winH;
+
+    this.winW = winW;
+    this.posX = 0;
+    this.vel = 10;
+
+    this.carWidth = 700;
+  }
+  draw() {
+    this.ctx.drawImage(this.img, this.posX, 450, 0, 0);
+  }
+
+  moveLeft() {
+    if (this.posX > 0) this.posX -= this.vel;
+  }
+
+  moveRight() {
+    if (this.posX < this.winW - this.carWidth) this.posX += this.vel;
+  }
+}
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
-
+    DrawApp.init("mycanvas");
+    DrawApp.drawFilledSquares();
+    DrawApp.drawLine();
+    DrawApp.drawAll();
   }
 };
