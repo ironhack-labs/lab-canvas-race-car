fa867014a849ac491ac5f424ea4bc5096a245f59
gabaldonrico <gabaldon.1617@gmail.com>
2019-05-03 09:24:33 +0200

    Interation 3

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,10 +4,10 @@ window.onload = function() {
   };
 }
  
-  function startGame() {
-    RaceCar.init('mycanvas')
+function startGame() {
+  RaceCar.init('mycanvas')
 
-  }
+}
 
 const RaceCar = {
   version: '1.0',
@@ -16,24 +16,65 @@ const RaceCar = {
   author: 'María',
   canvasDom: undefined,
   ctx: undefined,
-  winW: undefined,
+  winW: 500,
   winH: undefined,
+  roadVel: 0,
   init: function (id) {
       this.canvasDom = document.getElementById(id)
       this.ctx = this.canvasDom.getContext('2d')
       this.setDimensions()
-      this.drawRoad()   
+      this.car = new Car(this.ctx,this.winW)
+      this.setEventListeners()
+      this.drawAll()
+      this.obstacles = []
+  
   },
   setDimensions: function () {
-    this.canvasDom.setAttribute('width', 500)
+    this.canvasDom.setAttribute('width', this.winW)
     this.canvasDom.setAttribute('height', window.innerHeight)
-    this.winW = 500
     this.winH = window.innerHeight
   },
   setHandlers: function () {
     window.onresize = () => this.setDimensions()
   },
+
+  drawAll: function () {
+    this.count= 0
+    setInterval(() => {
+      this.clear()
+      this.drawRoad() 
+      this.car.draw()
+      if(this.count >= 30){
+        this.obstacles.push(new Obstacles(this.ctx, this.winW))
+        this.count= 0
+      }this.count ++
+      this.obstacles.forEach((elm) => {
+        elm.makeObstacle()
+        elm.drawMovingObstacle()
+      })
+      this.roadVel -=7
+      this.ctx.lineDashOffset = this.roadVel
+    }, 100)
+  },
+  
+  clear: function () {
+      this.ctx.clearRect(0, 0, window.innerHeight, window.innerWidth)
+  },
+  setEventListeners: function () {
+      document.onkeyup = event => {
+        if (event.keyCode == 37) {
+        this.car.moveLeft()
+        }
+        if (event.keyCode == 39) {
+          this.car.moveRight()
+          } 
+      }
+  },
+
   drawRoad: function () {
+    
+    this.ctx.setLineDash([])
+
     this.ctx.fillStyle = "grey"                                            // cambia los colores de relleno
     this.ctx.fillRect(0,0, this.winW, this.winH)
 
@@ -63,30 +104,64 @@ const RaceCar = {
     this.ctx.lineTo(this.winW-70, 0)
     this.ctx.stroke()
 
+    this.drawDashedLine()
+    
+
+  },
+
+  drawDashedLine: function () {
     this.ctx.lineWidth = 10
     this.ctx.beginPath()
     this.ctx.setLineDash([30, 30])
     this.ctx.moveTo(this.winW/2,this.winH)
     this.ctx.lineTo(this.winW/2, 0)
     this.ctx.stroke()
-
-  },
-//   drawLine: function () {
-//     this.ctx.strokeStyle = 'green'
-//     this.ctx.lineWidth = 20
-//     this.ctx.setLineDash([60, 30])
-//     this.ctx.beginPath()
-//     this.ctx.moveTo(100, this.winH / 2)
-//     this.ctx.lineTo(this.winW - 100, this.winH / 2)
-//     this.ctx.stroke()
-// },
+    
+  }
 
 }
+class Car {
+  constructor(ctx, winW) {
+    this.ctx = ctx
+    this.carPic = new Image()
+    this.carPic.src = "images/car.png"
+    this.winW = winW
+    this.posX = 200 
+    this.vel = 10
+    
+  }
+  draw() {
+      this.ctx.drawImage(this.carPic, this.posX, 400, 100, 200)
+  
+  }
+  moveLeft() {
+    if (this.posX > 0) this.posX -= this.vel
+    console.log(this.posX)
 
 
+  }
+  
+  moveRight() {
+    if (this.posX < 400) this.posX += this.vel
+    console.log(this.posX)
+  }
+}
 
-
-
-
-
-
+class Obstacles {
+  constructor(ctx, winW) {
+    this.ctx = ctx
+    this.positionY = 10
+    this.positionX = Math.floor(Math.random() * (300 + 1 - 80)) + 80
+    this.width = winW / 3
+    this.height = 15
+    this.vel = 10
+   
+    }
+  makeObstacle(){
+    this.ctx.fillStyle = "green" // cambia los colores de relleno
+    this.ctx.fillRect(this.positionX,this.positionY, this.width, this.height)
+  }
+  drawMovingObstacle() {
+    this.positionY += this.vel
+  }
+}
