ff28fbe5c2127ec92b080651767b71dd9466bcde
Jesús García Sanchéz <jesusangelgarciasanchez@MacBook-Air-de-Jesus.local>
2019-06-13 20:06:09 +0200

    collisions

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,26 @@
 window.onload = function() {
+  let canvas;
+  let ctx;
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
+    canvas = document.getElementById("canvas")
+    ctx = canvas.getContext("2d");
 
   }
+
+  function printBoard() {
+   
+
+    ctx.linewidth = 10
+    ctx stroke style 
+    setlinedash 12 10 
+    move to 255 0
+    lineto 225 height 
+    stroke 
+  }
+  
+
 };
