86204549d69445bf1ac52f4e5991785d26c1e4fa
JORGE MARTINEZ CRUZ <jemc0810@gmail.com>
2019-06-13 12:56:02 -0500

    Nada aun

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,37 @@
+const canvas = document.getElementById('#fondo')
+const ctx = canvas.getContext('2d')
+
+ctx.fillRect(10, 10, 10, 10)
+
+// class Fondo {
+//   constructor(x,y){
+//     this.x = 0
+//     this.y = 0
+//     this.height = canvas.height
+    
+//     ctx.beginPath()
+//     ctx.moveTo(x,y)
+//     ctx.fillStyle = "green"
+//     ctx.fillRect(0,0, 50, this.height)
+//     ctx.stroke()
+//     ctx.closePath()
+//     this.draw()
+//   }
+ 
+// }
+
+
+
+/*
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
+
+
+
   function startGame() {
 
   }
-};
+};*/
