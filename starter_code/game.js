fb011564efd5de6e3076f83ebc71cbfd7890f95d
carlos.trujillo <carlos@riverettecom>
2019-06-13 21:47:09 +0200

    obstacles added not random yet

--- starter_code/game.js
+++ starter_code/game.js
@@ -4,6 +4,14 @@ var canvasDomEl = document.querySelector("#canvasGame");
 /** @type CanvasRenderingContext2D */
 var ctx = canvasDomEl.getContext("2d")
 
+// let obstaculo = {
+//   cantidad:[null,null],
+//   posIniX: M,
+//   posFinX: 0,
+//   posVert: 0,
+//   setWidth: 20
+// }
+
 var w = 500
 var h = 700 
 
@@ -26,12 +34,11 @@ function setCanvasDim () {
   
 }
 setCanvasDim()
-drawRectGrey()
-drawGreenLines()
-// drawWhiteLines()
-drawWhiteMidLine()
-// moveMidLine ()
-// drawCar()
+// drawRectGrey()
+// drawGreenLines()
+
+// drawWhiteMidLine()
+
 
 function drawRectGrey() {
   ctx.fillStyle = `#808080`
@@ -66,24 +73,7 @@ function drawWhiteLines () {
   ctx.closePath();
 }
 function drawWhiteMidLine() {
-//   ctx.strokeStyle = `#FFFFFF`
-//   ctx.lineWidth = 10;
-//   ctx.beginPath();
-// ctx.setLineDash([30, 30]);
-// ctx.lineDashOffset = 10
-// ctx.moveTo(w/2,h)
-// // dashedLineY ++
-//   ctx.lineTo(w/2,h)
-//   ctx.stroke()
-//   ctx.closePath();
-// ctx.beginPath()
-// ctx.lineWidth = 4
-// ctx.strokeStyle = "#f2f2f2"
-// ctx.setLineDash([50, 10])
-// ctx.moveTo(w / 2 , 800)
-// // dashedLineY ++
-// ctx.lineTo(w / 2, 0)
-// ctx.stroke()
+
 ctx.beginPath()
 ctx.moveTo(w/2,separatorY)
 ctx.setLineDash([20,8])
@@ -95,13 +85,53 @@ ctx.closePath();
 
 
 }
+function drawBrownThing() {
+  
+    ctx.fillStyle = `blue`
+  ctx.beginPath();
+  ctx.rect(400,i+200,500,10);
+  ctx.fill();
+  ctx.closePath();
+  ctx.restore()
+  
+  }
+  function drawBrownThing2() {
+  
+    ctx.fillStyle = `brown`
+  ctx.beginPath();
+  ctx.rect(120,i,100,50);
+  ctx.fill();
+  ctx.closePath();
+  ctx.restore()
+  
+  }
+  function drawBrownThing3() {
+  
+    ctx.fillStyle = `black`
+  ctx.beginPath();
+  ctx.rect(170,i+150,200,50);
+  ctx.fill();
+  ctx.closePath();
+  ctx.restore()
+  
+  }
+
 // 
 function drawCar() {
   ctx.clearRect(0,0,w,h)
+  setCanvasDim()
   drawRectGrey()
 drawGreenLines()
 drawWhiteLines()
 drawWhiteMidLine()
+drawBrownThing()
+drawBrownThing2()
+drawBrownThing3()
+// drawBrownThing()
+// drawObs()
+// timerObs()
+// timerObs()
+
   ctx.drawImage(img, dataCar.x, dataCar.y,100,150)
 }
 
@@ -128,28 +158,76 @@ window.onkeydown = function (e) {
   drawCar()
 }
 var separatorY = 700
+var i = 0
+// var j = 300
+
 
 function moveMidLine (){
   setInterval (() => {
    
     separatorY += 2
-//     ctx.strokeStyle = `#FFFFFF`
-//   ctx.lineWidth = 10;
-//   ctx.beginPath();
-// ctx.setLineDash([30, 30]);
-// ctx.lineDashOffset = i 
-// // ctx.translate(i,dataCar.y)
-// ctx.moveTo(w/2,0)
-//   ctx.lineTo(w/2,h)
-//   ctx.stroke()
-//   ctx.closePath();
-// drawWhiteMidLine()
+    if (i === 700){
+      // j += Math.random()*10
+      
+      i -= 700
+
+    } else {
+      i += 1
+
+    }
+// drawObs()
 drawCar()
    
-  },60)}
+  },10)}
 
-  moveMidLine()
+  // moveMidLine()
+
+
+  function drawObs() {
+    let length =50
+    let ancho = 50
+    
+    for(var i=0; i < h ; i+= 10){
+      
+      ctx.fillStyle = `#880000`
+      ctx.save()
+      ctx.rotate(i)
+    ctx.beginPath();
+    
+    ctx.rect(50,i,length,ancho);
+    ctx.fill();
+    ctx.closePath();
+    ctx.restore()
+    
+    }
+    
+
+   
+    
+  }
+  
+// drawObs()
+// function timerObs() {
+// setInterval (() => {
+  
   
 
+//   ctx.save()
+//   ctx.fillStyle = `brown`
+// ctx.beginPath();
+// ctx.rect(30,i,300,50);
+// ctx.fill();
+// ctx.closePath();
+// ctx.restore()
+ 
+ 
+// },100)
+
+// }
+
+// timerObs()
+moveMidLine()
+// drawObs()
 
 
+ 
