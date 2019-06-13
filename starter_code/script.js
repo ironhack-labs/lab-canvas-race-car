17c46c54391123e46668b8ea0c3498c5d50d525a
Francho Garcia <franchog79@gmail.com>
2019-06-13 19:22:51 +0200

    Pending to the colision finish

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,21 +1,33 @@
 window.onload = function() {
-    /** @type HTMLCanvasElement */
+
+    //CANVAS INFORMATION ========================
     const canvasWidth = 420
     const canvasHeight = 700
+        /** @type HTMLCanvasElement */
     var canvasDOMEl = document.querySelector("#road");
+    /** @type CanvasRenderingContext2D */
     var ctx = canvasDOMEl.getContext("2d")
     canvasDOMEl.setAttribute("width", canvasWidth)
     canvasDOMEl.setAttribute("height", canvasHeight)
+        //offset for the middle
     var offset = 0
 
     var car = new Image();
     car.src = "./images/car.png"
-    const carWidth = 60
-    const carHeigth = 100
+    const carWidth = 50
+    const carHeigth = carWidth * 2
     let dataCar = {
         x: canvasWidth / 2 - carWidth / 2,
         y: canvasHeight - carHeigth - 20
     }
+
+    let obstacle = {
+        widthObstacle: Math.floor(Math.random() * (270 - carWidth + 1) + carWidth),
+        xPos: Math.floor(Math.random() * ((70 - carWidth) + 1) + 70),
+        yPos: 0,
+        thicknessObstacle: 10
+    }
+    let arrayObstacles = []
 
     loadBoard()
     movingTheLine()
@@ -58,12 +70,14 @@ window.onload = function() {
         ctx.rect(0, 0, canvasWidth, canvasHeight)
         ctx.fillStyle = "rgb(0, 128, 0)"
         ctx.fill()
+        ctx.closePath()
 
         //road
         ctx.beginPath()
         ctx.rect(40, 0, 340, 700)
         ctx.fillStyle = "rgb(128, 128, 128)"
         ctx.fill()
+        ctx.closePath()
 
         //external lines
         ctx.beginPath()
@@ -74,6 +88,7 @@ window.onload = function() {
         ctx.rect(360, 0, 10, 700)
         ctx.fillStyle = "white"
         ctx.fill()
+        ctx.closePath()
     }
 
     function drawMiddleLine() {
@@ -85,6 +100,7 @@ window.onload = function() {
         ctx.lineTo(canvasWidth / 2, 700)
         ctx.lineWidth = 3
         ctx.stroke()
+        ctx.closePath()
     }
 
     function clearRoad() {
@@ -106,7 +122,50 @@ window.onload = function() {
     }
 
     function paintCar() {
-
         ctx.drawImage(car, dataCar.x, dataCar.y, carWidth, carHeigth)
     }
+
+    function initObstacles() {
+        for (cont = 0; cont < arrayObstacles.length; cont++) {
+            arrayObstacles[cont] = new obstacle;
+        }
+    }
+
+    function removeObstacle() {
+        arrayObstacles.shift
+    }
+
+    function addObstacle() {
+        arrayObstacles.pop
+    }
+
+    function moveObstacle(increment) {
+        for (cont = 0; cont < arrayObstacles.length; cont++) {
+            arrayObstacles[cont].yPos += increment
+        }
+    }
+
+    function printObstacle() {
+        for (cont = 0; cont < arrayObstacles.length; cont++) {
+            ctx.beginPath()
+            ctx.rect(arrayObstacles.xPos, arrayObstacles.yPos, arrayObstacles.width, arrayObstacles.height)
+            ctx.fillStyle = "rgb(135,0,7)"
+            ctx.fill()
+            ctx.closePath()
+        }
+    }
+
+    function hasColisioned() {
+        if (
+            dataCar.x + dataCar.width >= arrayObstacles[0].xPos &&
+            arrayObstacles[0].xPos + arrayObstacles[0].width >= dataCar.x &&
+            dataCar.y + dataCar.height >= arrayObstacles[0].yPos &&
+            arrayObstacles[0].yPos + arrayObstacles[0].height >= dataCar.y
+        ) {
+            return true
+        } else {
+            return false
+        }
+    }
+
 };
