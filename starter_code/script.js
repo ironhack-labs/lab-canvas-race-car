4c7b85c1285780868746fcb591fa742ca79a7cc1
Edgar Herera <contactoedgarherrera@gmail.com>
2019-06-13 15:02:03 -0500

    Iteration 2 | Done.

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,14 @@
-window.onload = function() {
+window.onload = function () {
   let canvas = document.querySelector('canvas')
   let context = canvas.getContext('2d')
-  document.getElementById("start-button").onclick = function() {
+
+  document.getElementById("start-button").onclick = function () {
     startGame();
   };
+
+  let img = {
+    imgCar: './images/car.png'
+  }
 
   // Draw Line function
   const drawLine = (xMove, yMove, xLine, yLine, strokeWidth, color) => {
@@ -15,21 +20,6 @@ window.onload = function() {
     context.stroke()
   }
 
-  // Background
-  context.beginPath()
-  context.fillStyle = 'gray'
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  context.stroke()
-
-  // Green Lines
-  drawLine(0, 0, 0, canvas.height, 100, 'green')
-  drawLine(canvas.width, 0, canvas.width, canvas.height, 100, 'green')
-
-  drawLine(80, 0, 80, canvas.height, 20, 'white')
-  drawLine(320, 0, 320, canvas.height, 20, 'white')
-
-
-  
 
   const middleLines = (num) => {
     let sum = 0
@@ -38,9 +28,72 @@ window.onload = function() {
       sum += 50
     }
   }
-  middleLines(12)
+  class Car {
+    constructor(x, y, img) {
+      this.x = x
+      this.y = y
+      this.width = 25
+      this.height = 35
+      this.img = new Image()
+      this.img.src = img
+    } 
+    draw() {
+      context.drawImage(this.img, this.x, this.y, this.width, this.height)
+    } 
+    
+    moveRight() {
+      if(this.x > canvas.width - this.width - 10) return
+      this.x += 10
+    }
+    
+    moveLeft() {
+      if(this.x < 0) return
+      this.x -= 10
+    }
+    
+    moveUp() {
+      if(this.y < 0 + this.height - 40) return
+      this.y -= 100 
+    }
+    
+    moveDown() {
+      if (this.y > canvas.height - (this.height + this.height / 2)) return
+      this.y += 50
+    }
+    isTouching(blocks) {
+      return (
+        this.x < blocks.x + blocks.width &&
+        this.x + this.width > blocks.x &&
+        this.y < blocks.y + blocks.height &&
+        this.y + this.height > blocks.y
+      )
+    }
+  }
 
+  const car = new Car(100, 100, img.imgCar)
+
+  // Init
+  const drawRoad = () => {
+    // Background
+    context.beginPath()
+    context.fillStyle = 'gray'
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.stroke()
+
+    // Green Lines
+    drawLine(0, 0, 0, canvas.height, 100, 'green')
+    drawLine(canvas.width, 0, canvas.width, canvas.height, 100, 'green')
+
+    drawLine(80, 0, 80, canvas.height, 20, 'white')
+    drawLine(320, 0, 320, canvas.height, 20, 'white')
+
+    // Middle lines
+    middleLines(12)
+
+    car.draw()
+  }
+  
   function startGame() {
-
+    drawRoad()
   }
 };
