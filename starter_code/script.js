72f3f0a740743ec5be900b44845f88952f95ff1c
sofia <sofleos@gmail.com>
2019-06-13 20:25:41 -0500

    Saved

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,104 @@
+const canvas = document.querySelector('#background')
+const ctx = canvas.getContext('2d')
+let interval = 0
+let bars = []
+let frames
+
+class Car {
+
+  constructor () {
+    this.width = 25
+    this.height = 35
+    this.x = 250
+    this.y = 950
+    this.image = new Image()
+    this.image.src = './images/car.png'
+
+  }
+  draw() {
+
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+  }
+}
+ 
+const car = new Car()
+
+function background (){
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height)
+  ctx.fillStyle = 'gray'
+  ctx.fillRect(50,0,400,ctx.canvas.height)
+}
+/*
+class Bars {
+  constructor (x, width){
+    this.x = x
+    this.y = 0
+    this.width = width
+    this.height = 5
+  }
+
+} */
+
+
+    
+function generateBars() {
+  let rndX = Math.random() * canvas.width
+  let rndHeight = Math.random() * canvas.height
+  ctx.fillStyle = 'red'
+  ctx.fillRect(rndX,0,rndHeight,5)
+
+}
+  /*
+  bars.push(fillRect())
+  bars.forEach(bars => {
+   bars.draw()
+    
+  })
+  
+ }
+
+
+function drawBars() {
+  if (frames % 250 === 0) {
+    generateBars()    
+  }
+
+}
+*/
+  
+
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+      
+   
     startGame();
   };
 
   function startGame() {
+    
+    if (interval) return
+    interval = setInterval(update, 1000/120)
+   
 
   }
-};
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  background()
+  car.draw()
+  generateBars()
+     }
+
+
+addEventListener('keydown', (e) => {
+  if (e.keyCode === 39 ) {
+     car.x += 25
+  }  
+ 
+  else if(e.keyCode === 37) {
+   car.x-=25
+   } 
+})
