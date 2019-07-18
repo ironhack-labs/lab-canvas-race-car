0e19d5113ebaaca5485d69e10e8a00532f233e5f
Julia Ramos Guedes <50717855+juliaramosguedes@users.noreply.github.com>
2019-07-18 13:00:15 -0300

    done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,76 +1,93 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    myGameArea.start();
-  };
-};
-
-const myObstacles = [];
+let myObstacles = [];
+const canvas = document.getElementById('game')
+const ctx = canvas.getContext("2d");
 
 const myGameArea = {
-  canvas: document.createElement("canvas"),
   frames: 0,
   totalScore: 0,
+
   start: function() {
-    this.canvas.width = 450;
-    this.canvas.height = 600;
-    this.context = this.canvas.getContext("2d");
-    document.getElementById('game-board').appendChild(this.canvas);
+    this.clear();
+    this.board();
+    player.updateCar();
     this.interval = setInterval(updateGameArea, 20);
   },
+
   clear: function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   },
+
   stop: function() {
     clearInterval(this.interval);
   },
+
   score: function() {
-    var points = Math.floor(this.frames / 180);
-    this.totalScore = points;
-    this.context.font = "18px arial";
-    this.context.fillStyle = "white";
-    this.context.fillText("Score: " + points, 300, 50);
+    this.totalScore = Math.floor(this.frames / 180);
+    ctx.font = "18px arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score: " + this.totalScore, 300, 50);
   },
+
   board: function() {
     // road
-    this.context.fillStyle = 'green';
-    this.context.fillRect(0, 0, 30, this.canvas.height);
-    this.context.fillRect(this.canvas.width - 30, 0, 30, this.canvas.height);
-    this.context.fillStyle = 'dimgray';
-    this.context.fillRect(30, 0, 10, this.canvas.height);
-    this.context.fillRect(this.canvas.width - 40, 0, 10, this.canvas.height);
-    this.context.fillRect(50, 0, this.canvas.width - 100, this.canvas.height);
-
-    // middle line
-    this.context.strokeStyle = 'white';
-    this.context.lineWidth = 5;
-    this.context.setLineDash([20,15])
-    this.context.beginPath();
-    this.context.moveTo(this.canvas.width / 2, 0);
-    this.context.lineTo(this.canvas.width / 2, this.canvas.height);
-    this.context.stroke();
-    this.context.closePath();
-  }
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 30, canvas.height);
+    ctx.fillRect(canvas.width - 30, 0, 30, canvas.height);
+    ctx.fillStyle = 'dimgray';
+    ctx.fillRect(30, 0, 10, canvas.height);
+    ctx.fillRect(canvas.width - 40, 0, 10, canvas.height);
+    ctx.fillRect(50, 0, canvas.width - 100, canvas.height);
+  },
 };
 
+const middleLine = {
+  y1: 0,
+  y2: -600,
+  draw: function() {
+    // line 1
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 5;
+    ctx.setLineDash([20,20])
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, this.y1);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+    ctx.closePath();
+
+    // line 2
+    ctx.setLineDash([20,20])
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, this.y2);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.stroke();
+    ctx.closePath();
+  },
+  
+  update: function() {
+    this.y1++
+    this.y2++
+    if (this.y1 === 600) this.y1 = 0
+    if (this.y2 === 0) this.y2 = -600
+  }
+}
+
 class Component {
-  constructor(width, height, color, x, y) {
+  constructor(width, height, x, y) {
     this.width = width;
     this.height = height;
-    this.color = color;
+    this.color = 'darkred';
     this.x = x;
     this.y = y;
     this.speedX = 0;
   }
 
   updateCar() {
-    const ctx = myGameArea.context;
     const carImg = new Image();
     carImg.src = './images/car.png';
     ctx.drawImage(carImg, this.x, this.y, 32, 64);
   }
 
   updateObs() {
-    const ctx = myGameArea.context;
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
@@ -112,11 +129,13 @@ class Component {
   }
 }
 
-const player = new Component(32, 64, "red", 217, 490);
+const player = new Component(32, 64, 210, 490);
 
 function updateGameArea() {
   myGameArea.clear();
   myGameArea.board();
+  middleLine.draw();
+  middleLine.update();
   player.newPos();
   player.updateCar();
   updateObstacles();
@@ -132,35 +151,47 @@ function updateObstacles() {
 
   myGameArea.frames += 1;
   if (myGameArea.frames % 180 === 0) {
-    const y = myGameArea.canvas.height;
+    const y = canvas.height;
     const minHeight = 150;
-    const maxHeight = 250;
+    const maxHeight = 200;
     const width = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
     const minGap = 50;
     const maxGap = 200;
     const gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
     myObstacles.push(
-      new Component(width, 20, "darkred", gap, 0)
+      new Component(width, 20, gap, 0)
     );
   }
 }
 
 function checkGameOver() {
-  const crashed = myObstacles.some(obstacle => player.crashWith(obstacle));
-
+  let crashed = myObstacles.some(obstacle => player.crashWith(obstacle));
   if (crashed) {
     myGameArea.stop();
     setTimeout(() => {
       myGameArea.clear();
-      myGameArea.context.font = "bold 48px arial";
-      myGameArea.context.fillStyle = "darkred";
-      myGameArea.context.fillText("GAME OVER!", 70, 100);
-      myGameArea.context.fillStyle = "black";
-      myGameArea.context.fillText("Your final score is: ", 25, 200);
-      myGameArea.context.fillText(myGameArea.totalScore, 200, 250);
+      ctx.font = "bold 48px arial";
+      ctx.fillStyle = "darkred";
+      ctx.fillText("GAME OVER!", 70, 100);
+      ctx.fillStyle = "black";
+      ctx.fillText("Your final score is: ", 25, 200);
+      ctx.fillText(myGameArea.totalScore, 200, 250);
+
     },1500)
   }
 }
+
+window.onload = function() {
+  document.getElementById("start-button").onclick = function() {
+    myObstacles = [];
+    myGameArea.totalScore = 0;
+    myGameArea.frames = 0;
+    myGameArea.stop();
+    player.x = 210;
+    player.y = 490;
+    myGameArea.start();
+  };
+};
 
 document.onkeydown = function(e) {
   switch (e.keyCode) {
