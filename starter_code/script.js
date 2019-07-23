7592b09c64d075752890ccc219ed35a068f0f295
Bana Hagos <banahagos@Banas-MacBook-Pro.local>
2019-07-23 21:31:34 +0200

    in progress

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,28 @@
+function draw() {
+  ctx.beginPath();
+  ctx.fillRect(0,0, 100, 500)
+  ctx.fillStyle = "green"
+
+  ctx.beginPath();
+  ctx.fillRect(400, 0, 100, 500)
+  ctx.fillStyle = "green"
+
+  let img = new Image();   
+  img.src = "images/car.png";
+  ctx.drawImage(img, 200, 250);
+}
+
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
+    ctx = document.getElementById('race-car').getContext('2d');
+    draw()
     startGame();
   };
 
-  function startGame() {
+  
 
+  function startGame() {
+    draw()
   }
 };
