8b173043b60337d04885fec7645b80fedbba9bf0
Niklas Babel <niklas@wane.co>
2019-07-23 21:30:03 +0200

    painted canvas, no functionality

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,31 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
+function draw() {
+
+  ctx.beginPath();
+  ctx.fillRect(0, 0, 100, 600);
+  ctx.fillStyle = 'green';
+
+  ctx.beginPath();
+  ctx.fillRect(300, 0, 100, 600);
+  ctx.fillStyle = 'green';
+
+  let img = new Image();
+  img.src = './images/car.png';
+  ctx.drawImage(img, 125, 300);
+
+}
+
+window.onload = function () {
+
+  document.getElementById("start-button").onclick = function () {
+    ctx = document.getElementById('race-car').getContext('2d');
+    draw()
     startGame();
   };
 
-  function startGame() {
+  document.onkeydown
 
+
+  function startGame() {
+    draw()
   }
-};
+}
