e93db46d1c4295988c05b8f5b4f6731d6adfb9f1
Chanty Herrmann <chanty@Chantys-MacBook-Air.local>
2019-07-24 00:00:24 +0200

    game setup done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,45 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
+let carObj;
+
+//drawGame
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var x = canvas.width
+  var y = canvas.height
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, x, y)
+  ctx.fillStyle = "grey"
+  ctx.fillRect(50, 0, 500, y)
+  //lines
+  ctx.setLineDash([10, 15, 10, 5]);
+  ctx.moveTo(x / 2, 0);
+  ctx.lineTo(x / 2, y);
+  ctx.stroke();
+  ctx.drawImage(carObj, 265, 450, 75, 150)
+
+
+
+}
+
+
+
+//show boardGame when button clicked
+
+//arrowLeft 37 arrowRight 38
+
+
+window.onload = function () {
+  document.getElementById("start-button").onclick = function () {
     startGame();
   };
 
   function startGame() {
 
   }
+  carObj = new Image();
+  carObj.onload = function () {
+    draw();
+  };
+  carObj.src = "/Users/chanty/Desktop/labs/lab-canvas-race-car/starter_code/images/car.png";
 };
+
