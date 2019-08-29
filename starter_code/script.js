6106a64c79e0fcb8dcfda44c186566f0d8d0aeb6
pauromeropau <pauromeropau@gmail.com>
2019-08-29 20:12:45 +0200

    Third Iteration: Make Player´s Car move right and left

--- starter_code/script.js
+++ starter_code/script.js
@@ -10,17 +10,30 @@ window.onload = function() {
     const h = 800;
     const w2 = w / 2;
     const h2 = h / 2;
-    const PI = Math.PI;
+    const speed = 40;
+    let counter = 0;
+    const speedLine = 5;
+
+    // const PI = Math.PI;
     // const PI_DOUBLE = Math.PI * 2;
     // const PI_HALF = Math.PI / 2;
+      setInterval(() => {
+      ctx.clearRect(0, 0, w, h);
+      setCanvasDimensions();
+      setRoad();
+      setCar();
+      moveCar();
+      counter++;
+    }, 1000/60);
+
     function setCanvasDimensions() {
       // x axis
       myCanvasDOMEl.setAttribute("width", `${w}px`);
       // y axis
       myCanvasDOMEl.setAttribute("height", `${h}px`);
-    } setCanvasDimensions();
+    }
 
-    function setRoad (){
+    function setRoad() {
       ctx.beginPath();
       ctx.rect(0, 0, w, h);
       ctx.fillStyle = "green";
@@ -28,37 +41,63 @@ window.onload = function() {
       ctx.closePath();
 
       ctx.beginPath();
-      ctx.rect(50, 0, (w-100), h);
+      ctx.rect(50, 0, w - 100, h);
       ctx.fillStyle = "grey";
       ctx.fill();
       ctx.closePath();
 
       ctx.beginPath();
-      ctx.rect(70, 0, (w-585), h);
+      ctx.rect(70, 0, w - 585, h);
       ctx.fillStyle = "white";
       ctx.fill();
       ctx.closePath();
 
       ctx.beginPath();
-      ctx.rect(515, 0, (w-585), h);
+      ctx.rect(515, 0, w - 585, h);
       ctx.fillStyle = "white";
       ctx.fill();
       ctx.closePath();
-
+      
+      ctx.strokeStyle = "white";
       ctx.beginPath();
-      ctx.rect(295, 0, (w-585), h);
-      ctx.fillStyle = "white";
-      ctx.fill();
+      ctx.lineWidth = 10;
+      ctx.moveTo(w2, h + counter * speedLine);
+      ctx.lineTo(w2, 0);
+      ctx.setLineDash([20]);
+      ctx.stroke();
       ctx.closePath();
+    }
+    let positions = {
+      x: w2 - 60,
+      y: h2 + 190
+    };
 
-    
-    } setRoad ();
+    function setCar() {
+      let car = new Image();
+      car.src = "./images/canoa.png";
+      // car.onload = function(){
+      ctx.drawImage(car, positions.x, positions.y, 100, 200);
+      // }
+    }
 
-    setInterval(() => {
-     
-
-
-    }, 10);
+    function moveCar() {
+      window.onkeydown = function(e) {
+        switch (e.key) {
+          case "ArrowLeft":
+            positions.x -= speed;
+            if (positions.x <= 63) {
+              positions.x = 63;
+            }
+            break;
+          case "ArrowRight":
+            positions.x += speed;
+            if (positions.x >= 420) {
+              positions.x = 420;
+            }
+            break;
+        }
+      };
+    }
   }
 };
 
