9da64974109a108e344b957fe73263b3a2ae4afc
alejandrogomez <alegomez1@me.com>
2019-08-30 14:50:53 -0400

    Second draft

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,49 +1,105 @@
 window.onload = function () {
   document.getElementById("start-button").onclick = function () {
     startGame();
+    drawBackground();
+
+   
+
+
   };
+
+  var canvas = document.querySelector('#canvas')
+  var ctx = canvas.getContext("2d")
+
+
+  let obstacle = {
+     yPos : 20,
+    
+    create: function(){
+      ctx.fillStyle = "#008100"
+      ctx.fillRect(70, this.yPos, 300, 50)
+    },
+    moveDown: function(){
+      this.yPos += 40
+      ctx.fillRect(70, this.yPos, 300, 50)
+      
+
+    }
+
+  }
+
 
   function startGame() {
     console.log("start game clicked")
-    var c = document.getElementById("board");
-    //Left Board
-    var leftGreenRect = c.getContext("2d");
-    var leftGrayRect = c.getContext("2d");
-    var leftWhiteRect = c.getContext("2d");
 
-    leftGreenRect.fillStyle = "#008100";
-    leftGreenRect.fillRect(20, 20, 30, 900);
+    // //Draw Racecar---DOES NOT WORK---
+    // const car = new Image()
+    // car.src = "images/car.png"
+    // ctx.drawImage(car, 20, 100, 100, 100)
 
-    leftGrayRect.fillStyle = "#808080"
-    leftGrayRect.fillRect(50,20, 10, 900)
-
-    leftWhiteRect.fillStyle = "white"
-    leftWhiteRect.fillRect(60,20,10,900)
-
-    //Middle Board
-    var middleGrayRect = c.getContext("2d")
-
-    middleGrayRect.fillStyle = "#808080"
-    middleGrayRect.fillRect(70, 20, 500, 900)
-
-    var dash = c.getContext("2d")
-    dash.setLineDash([10]);
-    dash.style = "white"
-    dash.strokeRect(320, 20, 1, 900);
-    dash.fillStyle = "white"
-
-    //Right Board
-    var rightWhiteRect = c.getContext("2d")
-    var rightGrayRect = c.getContext("2d")
-    var rightGreenRect = c.getContext("2d")
-
-    rightWhiteRect.fillStyle = "white"
-    rightWhiteRect.fillRect(570,20,10,900)
-
-    rightGrayRect.fillStyle = "#808080"
-    rightGrayRect.fillRect(580, 20, 10, 900)
-
-    rightGreenRect.fillStyle = "#008100"
-    rightGreenRect.fillRect(590, 20, 30, 900)
   }
+
+  let car = {
+    xPos: 300,
+    draw: function () {
+      console.log(this.xPos)
+      ctx.fillStyle = "#008100"
+      ctx.fillRect(this.xPos, 500, 50, 100)
+    },
+    clear: function () {
+      console.log("clear working...kinda")
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+
+  document.onkeydown = function (e) {
+
+    switch (e.keyCode) {
+
+      case 37: // left arrow   
+        car.xPos -= 25
+        car.clear()
+        drawBackground()
+        car.draw()
+        obstacle.create();
+        obstacle.moveDown();
+    
+   
+        break;
+      case 39: // right arrow
+
+        car.xPos += 25
+        car.clear()
+        drawBackground()
+        car.draw()
+
+        break;
+    }
+  };
+
+  function drawBackground() {
+
+    //Green Rectangles
+    ctx.fillStyle = "#008100";
+    ctx.fillRect(20, 20, 30, 900);
+    ctx.fillRect(590, 20, 30, 900)
+    //Gray Rectangles
+    ctx.fillStyle = "#808080"
+    ctx.fillRect(50, 20, 10, 900)
+    ctx.fillRect(70, 20, 500, 900)
+    ctx.fillRect(580, 20, 10, 900)
+    //White Rectangles
+    ctx.fillStyle = "white"
+    ctx.fillRect(60, 20, 10, 900)
+    ctx.fillStyle = "white"
+    ctx.fillRect(570, 20, 10, 900)
+
+    //Dash Line
+    ctx.style = "black"
+    ctx.strokeRect(320, 20, 1, 900);
+    ctx.fillStyle = "white"
+  }
+
+ 
+
 };
