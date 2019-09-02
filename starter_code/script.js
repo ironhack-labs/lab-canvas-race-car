dd5717387ac6e62ffae33bd8fb11d65e7a7ecdb8
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-09-02 19:29:09 -0400

    Added obstacles

--- starter_code/script.js
+++ starter_code/script.js
@@ -6,31 +6,16 @@ canvas.height = window.innerHeight;
 canvasWidth = canvas.width;
 canvasHeight = canvas.height;
 
-
-// class Car {
-//   // Initial x and y to pass 
-//   // X: 23.2/ 50*canvasWidth, Y: 17/20*canvasHeight
-//   // imgSrc:  "./images/car.png"
-//   constructor(x, y, imgSrc) {
-//     this.x = 23.2 / 50 * window.innerWidth/2;
-//     this.y = 17 / 20 * window.innerHeight;
-//   }
-
-//   moveLeft() {
-//     this.x--;
-//   }
-
-//   moveRight() {
-//     this.x++;
-//   }
-// }
+const obstacles = [];
 
 let car = {
-  x : 23.2 / 50 * window.innerWidth/2,
-  y : 17 / 20 * window.innerHeight,
-  moveLeft: function() {this.x--},
-  moveRight: function() {this.x++}
-}
+  x: 23.2 / 50 * window.innerWidth / 2,
+  y: 17 / 20 * window.innerHeight,
+  moveLeft: function () { this.x -= 5},
+  moveRight: function () { this.x += 5},
+
+  distance: 0
+};
 
 function drawGameBoard() {
   // Colors
@@ -47,7 +32,7 @@ function drawGameBoard() {
   ctx.fillRect(canvasWidth / 12, 0, canvasWidth / 36, canvasHeight);
   ctx.fillRect((32 / 36 * canvasWidth), 0, canvasWidth / 36, canvasHeight);
 
-// Draws the grass
+  // Draws the grass
   ctx.fillStyle = grassGreen;
   ctx.fillRect(0, 0, canvasWidth / 12, canvasHeight);
   ctx.fillRect((11 / 12 * canvasWidth), 0, canvasWidth / 12, canvasHeight);
@@ -57,15 +42,15 @@ function drawRoadMarkers() {
   markerWhite = "#F3F3F3";
 
   ctx.strokeStyle = markerWhite;
-  ctx.lineWidth = canvasWidth/130;
-  // ctx.lineDashOffset = offSet;
-  ctx.setLineDash([canvasHeight/30,canvasWidth/40]);
-  ctx.moveTo((canvasWidth/2), 0);
-  ctx.lineTo((canvasWidth/2), canvasHeight);
+  ctx.lineWidth = canvasWidth / 130;
+  ctx.lineDashOffset = -car.distance*5 % 40;
+  ctx.setLineDash([canvasHeight / 30, canvasWidth / 40]);
+  ctx.moveTo((canvasWidth / 2), 0);
+  ctx.lineTo((canvasWidth / 2), canvasHeight);
   ctx.stroke();
 }
 
-function drawCar(){
+function drawCar() {
   img = new Image();
   img.src = "./images/car.png";
 
@@ -74,11 +59,45 @@ function drawCar(){
   }
 }
 
-function updateCanvas(){
+function generateObstacle(){
+  const minLength = 27 / 36 * canvasWidth * 0.35;
+  const maxLength = 27 / 36 * canvasWidth - 75;
+  const obstacleLength = Math.floor(Math.random() * (maxLength - minLength) + 1) + minLength;
+  const obstacleSide = Math.floor(Math.random() * 2) == 0 ? 9 / 72 * canvasWidth : 63 / 72 * canvasWidth - obstacleLength;
+
+  return {
+    x: obstacleSide,
+    y: 0,
+    length: obstacleLength
+  };
+}
+
+function updateObstacles(){
+  const obstacleRed = "#890000"
+
+  if(car.distance % 250 == 0){
+    obstacles.push(generateObstacle());
+  }
+
+  for(let i = 0; i < obstacles.length; i++){
+    obstacles[i].y += 2;
+
+    if(obstacles[i].y > canvasHeight){
+      obstacles.splice(i, 1);
+    }
+
+    ctx.fillStyle = obstacleRed;
+    ctx.fillRect(obstacles[i].x, obstacles[i].y, obstacles[i].length, 20)
+  }
+}
+
+function updateCanvas() {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   drawGameBoard();
   drawRoadMarkers();
+  updateObstacles();
   drawCar();
+  car.distance += 5;
 }
 
 window.onload = function () {
@@ -86,24 +105,27 @@ window.onload = function () {
     startGame();
   };
 
-  document.onkeydown = function(e){
-    switch(e.keyCode){
-      case 37: 
-        car.moveLeft(); 
-        break;
-      case 39: 
-        car.moveRight();
-        break;
-    }
-    updateCanvas();
-  }
-
   drawGameBoard();
   drawRoadMarkers();
   drawCar();
 
   function startGame() {
-    updateCanvas();
+
+    document.onkeydown = function (e) {
+      switch (e.keyCode) {
+        case 37:
+          car.moveLeft();
+          break;
+        case 39:
+          car.moveRight();
+          break;
+      }
+      // car.distance -= 5;
+      updateCanvas();
+    }
+
+    setInterval(updateCanvas, 50);
+    // updateCanvas();
   }
 };
 
