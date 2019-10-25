1f6385220ed835385f14144d4a1efcf0158bc14c
Antoine BERNIER <antoine.bernier@gmail.com>
2019-10-25 10:38:28 +0200

    script at bottom

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,7 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
-
-  function startGame() {
-
-  }
+document.getElementById("start-button").onclick = function() {
+  startGame();
 };
+
+function startGame() {
+  
+}
