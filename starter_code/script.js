b01e819bb1915a3c56123e540823f294f0d2e0f0
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-10-31 18:21:46 +0000

    race car3 iteration done

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,5 +1,99 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+
+
+
+const drawRoad = () => {
+  //barra verde esquerda
+  context.fillStyle = 'green';
+  context.fillRect(0, 0, 40, 750);
+
+  //barra verde direita
+  context.fillStyle = 'green';
+  context.fillRect(410, 0, 40, 750);
+
+  //Estrada cinzenta
+  context.fillStyle = 'grey';
+  context.fillRect(40, 0, 370, 750);
+
+  //linha branca esquerda
+  context.fillStyle = 'white';
+  context.fillRect(55, 0, 15, 750);
+
+  //linha branca direita
+  context.fillStyle = 'white';
+  context.fillRect(380, 0, 15, 750);
+
+  //linha riscada centro
+  context.strokeStyle = 'white';
+  context.lineWidth = 5;
+  context.beginPath();
+  context.setLineDash([15, 30]);
+  context.moveTo(225, 0);
+  context.lineTo(225, 750);
+  context.stroke();
+  context.closePath();
+}
+
+
+//CAR class
+
+class Car {
+  constructor(x, y) {
+    this.x = 200;
+    this.y = 225;
+  }
+  moveLeft() {
+    this.x--;
+  }
+  moveRight() {
+    this.x++;
+  }
+}
+
+const car = new Car(0, 0);
+
+
+//imagem carro
+const drawCar = () => {
+  const IMAGE_URL = './images/car.png';
+  const image = new Image();
+  image.src = IMAGE_URL;
+  image.addEventListener('load', () => {
+    const imageWidth = image.width / 2;
+    const imageHeight = image.height / 2;
+    context.drawImage(image, car.x, car.y, imageWidth, imageHeight);
+  });
+}
+
+//move the car right(left)
+
+window.addEventListener('keydown', (event) => {
+  // Stop the default behavior (moving the screen to the left/up/right/down)
+  event.preventDefault();
+
+  // React based on the key pressed
+  switch (event.keyCode) {
+    case 37:
+      car.moveLeft();
+      console.log('left');
+      drawEverything();
+      break;
+
+    case 39:
+      car.moveRight();
+      console.log('right');
+      drawEverything();
+      break;
+  }
+});
+
+
+
+window.onload = function () {
+  document.getElementById("start-button").onclick = function () {
     startGame();
   };
 
@@ -7,3 +101,16 @@ window.onload = function() {
 
   }
 };
+console.log(context.fillStyle);
+
+
+
+
+function drawEverything() {
+
+  context.clearRect(0, 0, 450, 750);
+  drawRoad();
+  drawCar();
+}
+
+drawEverything();
