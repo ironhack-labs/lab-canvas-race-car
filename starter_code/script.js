6b65aeef5ea57e4ee515ad78e128d8ad48d4218b
eduardo <eduardo@DESKTOP-LPB6IGQ.localdomain>
2019-11-01 01:52:34 +0100

    con carretera infinita

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,63 +1,88 @@
-class Car {
-  constructor(x, y){
-    this.x = x;
-    this.y = y;
-  }
-}
-
-let car = new Car(115,340);
-
 /** @type HTMLCanvasElement */
 let canvasDOMEl = document.getElementById("canvas");
-
 
 /** @type CanvasRenderingContext2D */
 let ctx = canvasDOMEl.getContext("2d");
 
-let w = 300;
-let h = 500;
+class Board {
 
-function setCanvasDimensions() {
-  canvasDOMEl.setAttribute("height", h);
-  canvasDOMEl.setAttribute("width", w);
+constructor(height, width){
+    this.height = height;
+    this.width = width;
+  } 
+
+setCanvasDimensions() {
+    canvasDOMEl.setAttribute("height", this.height);
+    canvasDOMEl.setAttribute("width", this.width);
+  }
+
 }
 
-window.onresize = setCanvasDimensions;
+class Background {
 
-setCanvasDimensions();
+  constructor(pos0,pos1,pos2,pos3,pos4,pos5){
+    this.arr = [pos0,pos1,pos2,pos3,pos4,pos5]
+  }
 
-/*setInterval(() => {
-    ctx.save();
-    ctx.translate(randomInt(0, w), randomInt(0, h));
-    new ConcentricCircles(randomInt(5, 5));
-    ctx.restore();
-}, 10)*/
+  drawBackground(){
+    ctx.fillStyle= "grey";
+    ctx.fillRect(20,0,260,500);
+    ctx.fillStyle= "white";
+    ctx.fillRect(30,0,10,500);
+    ctx.fillStyle= "white";
+    ctx.fillRect(260,0,10,500);
+    ctx.fillRect(145,0,10,500);
+    ctx.fillStyle= "grey";
+    ctx.fillRect(145,this.arr[0],10,50);
+    ctx.fillRect(145,this.arr[1],10,50);
+    ctx.fillRect(145,this.arr[2],10,50);
+    ctx.fillRect(145,this.arr[3],10,50);
+    ctx.fillRect(145,this.arr[4],10,50);
+    ctx.fillRect(145,this.arr[5],10,50);
+  
+  }
 
-function drawBackground(){
-  ctx.fillStyle= "grey";
-  ctx.fillRect(20,0,260,500);
-  ctx.fillStyle= "white";
-  ctx.fillRect(30,0,10,500);
-  ctx.fillStyle= "white";
-  ctx.fillRect(260,0,10,500);
 }
 
+class Car {
+
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+
+}
+
+let canvas = new Board(500,300);
+
+let back = new Background(-50,50,150,250,350,450)
+
+let car = new Car(115,340);
+
+canvas.setCanvasDimensions();
+
+back.drawBackground();
 
 let carImage = new Image();
 carImage.src = "images/car.png";
-
 
 carImage.onload = function(){
   ctx.drawImage(carImage,car.x,car.y,70,100);
 }
 
-drawBackground();
+window.onload = function() {
+  document.getElementById("start-button").onclick = function(btn) {
+    btn.preventDefault();
+    startGame();
+  }
+};
 
 function startGame(){
-  
+  debugger;
   setInterval(function() {
-    ctx.clearRect(0,0,w,h);
-    drawBackground();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    canvas.setCanvasDimensions();
+    back.drawBackground();
     window.onkeydown = function(e) {
       switch (e.key) {
         case "ArrowRight":
@@ -68,62 +93,13 @@ function startGame(){
           break;
       }
     }
+    for(let i=0; i<back.arr.length; i++){
+      if(back.arr[i] === 500){
+        back.arr[i] = -100;
+      }else{
+        back.arr[i] += 1;
+      }
+    }
     ctx.drawImage(carImage,car.x,car.y,70,100);
   },1000/60);
 } 
-  
-  
-  
-  
-
-
-window.onload = function() {
-  document.getElementById("start-button").onclick = function(btn) {
-    btn.preventDefault();
-    startGame();
-  }
-};
-
-
-
-/*let coords = {
-  x: w2,
-  y: h2
- };
- let radius = 100;
- let color = "black";
- function drawCircle(color) {
-  ctx.clearRect(0, 0, w, h);
-  ctx.beginPath();
-  ctx.fillStyle = color;
-  ctx.arc(coords.x, coords.y, radius, 0, PI_DOUBLE);
-  ctx.fill();
-  ctx.closePath();
- }
- let speed = 30;
- window.onkeydown = function(e) {
-  switch (e.key) {
-    case "ArrowRight":
-      coords.x += speed;
-      break;
-    case "ArrowUp":
-      coords.y -= speed;
-      if (coords.y < -radius) {
-        coords.y = h + radius;
-      }
-      break;
-    case "ArrowLeft":
-      coords.x -= speed;
-      break;
-    case "ArrowDown":
-      coords.y += speed;
-      if (coords.y > h + radius) {
-        coords.y = -radius;
-      }
-      break;
-  }
-  if (e.code === "Space") {
-    color = "red";
-  }
-  drawCircle(color);
- };*/
