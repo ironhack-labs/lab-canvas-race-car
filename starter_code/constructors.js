b6b9be009eb2fca76eff0b1b25597cd211643df0
Alfredo del Canto <adelcanto@Alfredos-MacBook-Pro.local>
2019-11-01 01:16:03 +0100

    Iteration 5 - DONE

--- starter_code/constructors.js
+++ starter_code/constructors.js
@@ -1,15 +1,45 @@
 class Rectangles {
-    constructor(x, y, w, h, color, speed) {
+    constructor(x, y, w, h, color, speed, initialPos, finalPos, carImg) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.initialPos = initialPos;
+        this.finalPos = finalPos;
+        this.dashInterval = [30, 15];
         this.color = color;
         this.speed = speed;
+        this.lineWidth = 5;
+        this.carImg = carImg;
     }
 
     moveDown() {
         this.y += this.speed;
     }
 
-} 
+    drawRectangle() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        ctx.closePath();
+    }
+
+    drawLines() {
+        ctx.beginPath();
+        ctx.lineWidth = this.lineWidth;
+        ctx.setLineDash(this.dashInterval);//[30, 15]);
+        ctx.moveTo(this.initialPos[0], this.initialPas[1]); //w / adj / 2, -h + counter - 5000);
+        ctx.lineTo(this.finalPos[0], this.finalPos[1]);//w / adj / 2, h + counter);
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+    }
+
+    drawCar() {
+        ctx.drawImage(this.carImg, this.x, this.y, 80, 160)
+        carImg.onload = function () {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.closePath();
+        }
+    } 
+}
--- starter_code/script.js
+++ starter_code/script.js
@@ -32,42 +32,58 @@ let coords = {
   y: h - h / 4
 }
 
-let roadSpeed = 10;
+let roadSpeed = 2;
 let carSteering = 10;
 let fr = 1000 / 60;
 let limLeft = w / 20;
 let limRight = w / adj - w / 20 - w / 80;
 let counter = 0;
+let obstacles = [];
+
+
+let linesIn = 0;
+function linesMovement() {
+  linesIn += roadSpeed;
+  return linesIn;
+}
+
+function createObstacles(x, y, w, h, clr, spd) {
+  obstacles.push(new Rectangles(x, y, w, h, clr, spd));
+}
+
+let j= 0
+for (let i = 0; i < 60; i++) {
+  createObstacles((randomInt(limLeft, limRight - 300)), j , randomInt(150, 250), 40, "#870006", roadSpeed);
+  j -= 400;
+}
+
+let grass = new Rectangles(0, 0, w/adj ,h, "#008100");
+let road = new Rectangles((w / adj - w / (adj * 1.2)) / 2, 0, w / (adj * 1.2), h, "#808080");
+let lineLeft = new Rectangles(limLeft, 0, w / 80, h, "white");
+let lineRight = new Rectangles(limRight, 0, w / 80, h, "white");
+let midLine = new Rectangles(0,0,0,0,"white",0,[w / adj / 2, -h + counter - 5000], [w / adj / 2, h + counter])
+// let car = new Rectangles(coords.x, coords.y, 50, 50);
+// console.log(car);
+// console.log(car.drawCar())
 
 function startGame() {
-  setInterval(() => {
+  var intervalID = setInterval(() => {
     ctx.clearRect(0, 0, w, h);
 
-    ctx.beginPath();
-    ctx.fillStyle = "#008100";
-    ctx.fillRect(0, 0, w / adj, h);
-    ctx.closePath();
+    grass.drawRectangle();    
 
-    ctx.beginPath();
-    ctx.fillStyle = "#808080";
-    ctx.fillRect((w / adj - w / (adj * 1.2)) / 2, 0, w / (adj * 1.2), h)
-    ctx.closePath();
+    road.drawRectangle();
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(limLeft, 0, w / 80, h);
-    ctx.closePath();
+    lineLeft.drawRectangle();
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(limRight, 0, w / 80, h);
-    ctx.closePath();
+    lineRight.drawRectangle();
 
+    // midLine.drawLines();
     ctx.beginPath();
     ctx.lineWidth = 5;
     ctx.setLineDash([30, 15]);
-    ctx.moveTo(w / adj / 2, 0);
-    ctx.lineTo(w / adj / 2, h);
+    ctx.moveTo(w / adj / 2, -h + counter - 5000);
+    ctx.lineTo(w / adj / 2, h + counter);
     ctx.strokeStyle = "white";
     ctx.stroke();
 
@@ -82,7 +98,6 @@ function startGame() {
         case "ArrowLeft":
           coords.x -= carSteering;
           break;
-
       }
     }
 
@@ -90,24 +105,32 @@ function startGame() {
     ctx.drawImage(carImg, car.x, car.y, 80, 160)
     carImg.onload = function () {
       ctx.beginPath();
-      ctx.fillStyle = "red";
-      // ctx.fillRect(car.x, car.y, 80, 160);
       ctx.closePath();
     }
 
-    // createObstacles(limLeft + randomInt(limLeft, limRight), 300, limLeft - limLeft + randomInt(limLeft, limRight), 40, "#870006", roadSpeed)
+    obstacles.forEach(obstacle => {
+        ctx.beginPath();
+        ctx.fillStyle = obstacle.color;
+        ctx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
+        ctx.closePath();
+        obstacle.moveDown();
+    })
 
-    // function createObstacles(x, y, w, h, clr, spd) {
-    //   //timeout para cambiar frecuencia de obstaculos
-    //   let obstacle = new Rectangles(x, y, w, h, clr, spd);
+    counter = counter + 2;
+
+    obstacles.forEach(obstacle =>{
+      if(car.x + 80 > obstacle.x &&
+        car.x < obstacle.x + obstacle.w &&
+        car.y < obstacle.y + obstacle.h &&
+        car.y + 160 > obstacle.y){
+        clearInterval(intervalID);
+        let overlayDOMEl = document.querySelector("#overlay");
+        overlayDOMEl.style.display= "flex";
+        }
+    })
     
-    //   ctx.beginPath();
-    //   ctx.fillStyle = obstacle.color;
-    //   ctx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
-    //   ctx.closePath();
-    
-      // moveDown();
-  
+
+
 
   }, fr);
 }
