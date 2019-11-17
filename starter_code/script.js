b4892bd61fa85550a01bb0493596ab63a4bd0b44
Ardash <56517452+ardache@users.noreply.github.com>
2019-11-16 22:31:44 -0600

    begin
    Sat

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,20 @@
+let canvas;
+
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
   function startGame() {
+    canvas = document.getElementById("road");
+    ctx = canvas.getContext("2d");
+    createBoard();
+  }
 
+  function createBoard() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+   
+    ctx.filleRect(0, 0, canvas.width, canvas.height);
   }
 };
