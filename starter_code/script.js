2e4f005016167987e3a32701a7f949e2b5cd88f8
Analin Flores <foglzerika@gmail.com>
2019-11-18 22:29:46 -0600

    Adding lines

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,79 @@
-window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
-    startGame();
-  };
-
-  function startGame() {
-
+window.onload = function(){
+  // canvas
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext("2d");
+  // global
+  let raceCarImage = './images/car.png';
+  //clases declaration
+  class Board{
+    constructor(canvas){
+      this.x = 0;
+      this.y = 0;
+      this.width = canvas.width;
+      this.height = canvas.height;
+    }
+   
   }
-};
+  class Line{
+    constructor(x,y,w,h,color){
+      this.x = x ? x : 0;
+      this.y = y ? y : 0;
+      this.w = w ? w : 50;
+      this.h = h ? h : 50;
+      this.color = color ? color : 'green';
+      
+    }
+    draw(){
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+  
+  class raceCar{
+    constructor(){
+      this.x= 150;
+      this.y = 50;
+      this.width = 50;
+      this.height = 40;
+      this.image = new Image ();
+      this.image.src= raceCarImage;
+      this.image.onload = this.draw.bind(this);
+    }
+    draw(){
+       //abajo
+       if (this.y < canvas.height - this.height) this.y += 2;
+       ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+  }
+  //instances
+  let board = new Board(canvas);
+  let yardRight = new Line(0 , 0 , 30 , canvas.height);
+  let yardLeft = new Line (470 , 0,30,canvas.height);
+  let lineLeft = new Line (30,0, 15, canvas.height, 'gray');
+  let lineRight = new Line(455,0,15,canvas.height,'gray');
+  let speedway = new Line(55, 0, 390, canvas.height, 'gray');
+  let centraLine = new Line(250, 0, 10, canvas.height, '#fff');
+  //main functions
+  
+  function start(){
+    yardRight.draw();
+    yardLeft.draw();
+    lineLeft.draw();
+    lineRight.draw();
+    speedway.draw();
+    centraLine.draw();
+  }
+  
+  function update(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    board.draw();
+  
+  }
+  
+    document.getElementById("start-button").onclick = function() {
+      start();
+    };
+  };
