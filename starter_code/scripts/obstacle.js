ffe5b7d28e23146a73b68dee64fdf82a61586541
Gustavo Jordao <gustavojordao@Gustavos-MacBook-Pro.local>
2019-11-25 10:00:05 -0500

    Obstacle image missing fix

--- starter_code/scripts/obstacle.js
+++ starter_code/scripts/obstacle.js
@@ -7,20 +7,20 @@ class Obstacle {
         this.width = 100;
         this.height = 150;
         this.img = new Image();
-        this.img.source = "./images/obstacle.png";
+        this.img.src = "./images/obstacle.png";
     }
 
     draw() {
         this.canvas = document.getElementById("canvas");
         this.ctx = this.canvas.getContext("2d");
-        console.log("drawing obstacle ---->>>>> ", this.img, this.img.source);
+        console.log("drawing obstacle ---->>>>> ", this.img, this.img.src);
         this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     move() {
         if (Math.floor(Math.random() * 20) % 3 === 0) {
             console.log("moving ---- ", this.y, this.x);
-            this.y += 0.5;
+            this.y += 3;
         }
     }
 }
