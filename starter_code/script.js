9b77fc83e0ae884022648a6e0f2cccbc963a3a81
ERICK SICARD <esrfree@gmail.com>
2019-11-25 21:36:01 -0500

    player moves and collision

--- starter_code/script.js
+++ starter_code/script.js
@@ -24,20 +24,15 @@ function init() {
   // new object instances
   let road = new Component( "./images/road.PNG", 0, 0, 500, 780 );
   let car = new Component( "./images/car.png", 225, 670, 50, 100 );
-  let obstacles = [];
-  obstacles.push( new Component( "./images/car_grey.png", 100, 20, 50, 100 ) );
-  obstacles.push( new Component( "./images/car_orange.png", 250, 20, 50, 100 ) );
-  obstacles.push( new Component( "./images/car_yellow.png", 250, 20, 50, 100 ) );
-  obstacles.push( new Component( "./images/car_red.png", 250, 20, 50, 100 ) );
+  let obstacles = [
+    new Component( "./images/car_grey.png", 100, 20, 50, 100 ),
+    new Component( "./images/car_orange.png", 250, 20, 50, 100 ),
+    new Component( "./images/car_yellow.png", 250, 20, 50, 100 ),
+    new Component( "./images/car_red.png", 250, 20, 50, 100 )
+  ];
   
   // draw components
-  drwaComp( ctx, road );
-  drwaComp( ctx, car );
-  obstacles.forEach( car => drwaComp( ctx, car ))
-  //drwaComp( ctx, obstacles[0] );
-  //drwaComp( ctx, obstacles[1] );
-
-  animation( ctx, road, car, obstacles )
+  animation( ctx, road, car, obstacles );
 }
 
 function animation( ctx, road, car, obstacles ) {
@@ -47,14 +42,10 @@ function animation( ctx, road, car, obstacles ) {
     drwaComp( ctx, road );
     drwaComp( ctx, car );
     drwaComp( ctx, obstacles[order] );
-    //drwaComp( ctx, rockect );
-    obstacles[order].posY += 2
-
-    if( obstacles[order].posY >= 800 ) {
-      obstacles[order].posY = 20;
-      order = Math.floor( Math.random() * obstacles.length );
-      obstacles[order].posX = Math.floor(Math.random() * 400);
-    }
+    //drwaComp( ctx, obstacles[order + 1] );
+    moveCar( car );
+    let compCollision = collision( car, obstacles[order] );
+    order = randomCars( obstacles[order], compCollision, order, obstacles );
   }, 1000/200 )
 }
 
@@ -63,15 +54,47 @@ function drwaComp ( ctx, obj ) {
   let img = new Image();
   img.src = obj.imgScr;
 
-  //img.addEventListener('load', function(){
-  //  ctx.drawImage(img, obj.posX, obj.posY, obj.imgW, obj.imgH);
-  //})
-
   ctx.drawImage(img, obj.posX, obj.posY, obj.imgW, obj.imgH);
+}
+
+function randomCars( obj, collisionOccur, idx, objArr ) {
+  obj.posY += 1;
+  if( collisionOccur || obj.posY >= 800 ) {
+    obj.posY = 20;
+    obj.posX = Math.floor(Math.random() * 400);
+    idx = Math.floor( Math.random() * objArr.length );
+  }
+  return idx;
 }
 
 function clear( ctx ) {
   ctx.clearRect(0, 0, canva.width, canva.height)
+}
+
+function moveCar( obj ) {
+  document.onkeydown = event => {
+    let keyCodes = [37, 38, 39, 40];
+    const key = event.keyCode;    
+    
+    if ( keyCodes.includes(key) ) {
+      console.log(keyCodes.includes(key));
+      event.preventDefault();
+      if ( key === 37 && obj.posX >= 0 ) console.log(obj.posX -= 20)
+      else if ( key === 39 && obj.posX <= 500 - obj.imgW ) obj.posX += 20
+      else if ( key === 38 && obj.posY >= 10 ) obj.posY -= 20
+      else if ( key === 40 && obj.posY <= 760 - obj.imgH ) obj.posY += 20
+    }   
+  }
+}
+
+function collision( car, obs ) {
+  let colRight = ( (car.posX + car.imgW >= obs.posX) && (car.posX <= obs.posX) );
+  let colLeft = ( (car.posX <= obs.posX + obs.imgW) && (car.posX + car.imgW >= obs.posX + obs.imgW) );
+  let colTop = ( (car.posY + 20 <= obs.posY + obs.imgH) && (car.posY + car.imgH >= obs.posY + obs.imgH) );
+  let colBot = ( (car.posY + car.imgH - 20 >= obs.posY) && (car.posY <= obs.posY) );
+  
+  if ( (colRight || colLeft) && (colTop || colBot)) return true;
+  return false;
 }
 
 // ********************************************************** //
