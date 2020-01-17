6b521853545c125c3ce73bf2d071020fee0fdd48
Ricardo-leGo <sofiamejiamuro@gmail.com>
2020-01-17 08:13:55 -0600

    PP-WDFT-MEX-ADRIAN-ACEVEDO-RICARDO-LEYVA

---
+++ starter_code/generateRivals.js
@@ -0,0 +1,8 @@
+function generateRivals() {
+  let img, rndom;
+  if (frames % 100)
+         = 0;
+  rndom = Math.random() * canvas.width;
+  if (Math.random >= .5)
+    CO.push(new Rival(rival => Rival.draw(0, rndom)));
+}
--- starter_code/script.js
+++ starter_code/script.js
@@ -3,9 +3,9 @@ ctx= canvas.getContext('2d');
 const screenY = screen.height
 const screenX = screen.width
 canvas.width = screenX/2
-canvas.height = screenY
+canvas.height = screenY-150
 let rayitas = 12
-
+let CO = [];
 
 let movil 
 
@@ -31,6 +31,7 @@ class background{
     ctx.fillRect(110, 0,20,canvas.height)
     ctx.fillRect(canvas.width-130, 0,20,canvas.height)
 
+    
 
     for (let i = 0; i < rayitas; i++) {
       ctx.fillRect(canvas.width/2, i*100,10 ,canvas.height/20)
@@ -50,14 +51,11 @@ constructor(x,y){
       this.draw()
     }
 }
-
-
   draw () {
       ctx.drawImage(this.img, this.x, this.y, 100, 200)
   }
 
-
-  moveleft(){
+    moveleft(){
     if(this.x > 100){
     this.x -= 10
     this.move()
@@ -74,18 +72,69 @@ constructor(x,y){
       this.x+=2
     }
 
+
 }
+
+
+
+class Rival{
+  constructor(x,vy,radio, color){
+    this.x = x
+    this.y = vy
+    this.radio = radio
+    this.color = color
+    
+
+  }
+  draw (){
+    this.y+= 10
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radio, 0, 2 * Math.PI),true;
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+     }
+
+}
+
+function generateRivals(){
+  let img, rnd, color,radio,vy=0
+  // if (Math.random() >= 0.5) img
+  // else img = img
+  if (frames % 25 === 0) {
+    rnd = ((Math.random() * canvas.width)-200)+200
+ 
+    color = 'rgb('+ Math.floor(Math.random()*255) +','+Math.floor(Math.random()*255) +','+ Math.floor(Math.random()*255)+')'
+    radio = Math.round(Math.random()*100)
+    vy=Math.round(Math.random()*5)
+    CO.push(new Rival(rnd,vy, radio, color))
+    console.log(rnd,vy, radio, color);
+    
+  }
+}
+
+function drawrivals(){
+  generateRivals()
+  CO.forEach(el => el.draw());
+}
+
+
+
+
 function update(){
     
   ctx.clearRect(0, 0, screenX,screenY);
   frames++
+ 
   carretera.draw()
-
   movil.draw()
+
+  drawrivals()
 
 
   
 }
+
 
 function startGame() {
   if(!interval){
@@ -94,8 +143,20 @@ function startGame() {
   
 }
 
+// function generateObstacules{
+//    let img, stone{
+//      if (frames = 100% === 0) {
+//        stone = Math.random() * screen.width
+//      if(Math.random() => 0.5 ) img = img.obstaculo
+
+       
+//      }
+//    }
+// }
+
+
   window.onload = function() {
-      document.getElementById("start-button").onclick = function() {
+      document.getElementById("start-button").onclick = function() {  
       startGame();
 
       carretera = new background()
@@ -119,8 +180,6 @@ function startGame() {
           return movil.moverigth()
 
        }
-        
-
 
     }
   )
