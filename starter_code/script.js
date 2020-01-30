4a8876b8187861179d42d157a15c91d67a1e0777
Dan Burton <danburts@Dans-MBP.localdomain>
2020-01-30 14:16:07 +0000

    error on lines

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,9 +1,49 @@
 window.onload = function() {
-  document.getElementById("start-button").onclick = function() {
+  const $canvas = document.querySelector('canvas');
+  const context = $canvas.getContext('2d');
+  document.getElementById('start-button').onclick = function() {
     startGame();
   };
 
-  function startGame() {
+  function drawBoard() {
+    context.fillStyle = 'green';
+    context.fillRect(0, 0, 550, 800);
 
+    context.fillStyle = 'gray';
+    context.fillRect(20, 0, 510, 800);
+
+    context.strokeStyle = 'white';
+    context.beginPath();
+    context.lineWidth = 10;
+    context.moveTo(40, 0);
+    context.lineTo(40, 800);
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.lineWidth = 10;
+    //context.strokeStyle = 'white';
+    context.moveTo(510, 0);
+    context.lineTo(510, 800);
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.setLineDash([25, 20]);
+    context.lineWidth = 5;
+    context.moveTo(275, 0);
+    context.lineTo(275, 800);
+    context.stroke();
+    context.closePath();
+
+    const carImageUrl = './images/car.png';
+    const carImage = new Image();
+    carImage.src = carImageUrl;
+  }
+
+  function startGame() {
+    //console.log('im running');
+    context.clearRect(0, 0, $canvas.width, $canvas.height);
+    drawBoard();
   }
 };
