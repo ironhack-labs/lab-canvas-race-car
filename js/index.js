d23692b3e195ee812f1800fec8eb1340137a4a7a
dkoushha <dimakoushha@gmail.com>
2020-03-03 21:39:40 +0100

    update js files

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let ctx = document.getElementById("canvas").getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let roadImg = new Image();
+    roadImg.src = "images/road.png";
+    let carImg = new Image();
+    carImg.src = "images/car.png";
+    ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(carImg, 220, 500, 50, 100);
+    window.requestAnimationFrame(startGame);
+  }
 };
