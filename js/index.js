de01328b3a4bb8417f2e6b5566f5bdec8d6d8f8b
Manuel <mbuergel@gmx.de>
2020-03-03 21:46:28 +0100

    will continue...

--- js/index.js
+++ js/index.js
@@ -1,7 +1,41 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+
   };
 
-  function startGame() {}
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+  
+  let road = new Image()
+  road.src = "images/road.png"
+
+  let car = new Image()
+  car.src = "images/car.png"
+
+
+  function startGame() {
+
+    let draw = () => {
+    
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.drawImage(road, 0, 0, canvas.width, canvas.height)
+
+      ctx.drawImage(car, 180, 250, 100, 200)
+
+    }
+
+    requestAnimationFrame(draw)
+  
+  }
+
+  
+
+
+
+
+
+
+
+
 };
