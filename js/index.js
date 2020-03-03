03cacf0f808ca306988764d99c21135d515f9733
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-03-03 21:42:32 +0100

    BER-WDPT Edison and Dima

--- js/index.js
+++ js/index.js
@@ -3,5 +3,19 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let ctx = document.querySelector('canvas').getContext('2d')
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    let roadImg = new Image()
+    roadImg.src = 'images/road.png'
+    let car = new Image()
+    car.src = 'images/car.png'
+
+    
+    ctx.drawImage( roadImg, 0, 0, canvas.width, canvas.height)
+    ctx.drawImage( car, 225, 575, 50, 100)
+    window.requestAnimationFrame(startGame)
+  }
 };
+
+
