f13e9a0c93c46949a8c0763c3ba058efe14074bc
Sonja Lesch <sonjalesch@gmx.de>
2020-03-05 19:05:33 +0100

    have to do iteration 5

--- js/index.js
+++ js/index.js
@@ -9,28 +9,48 @@ window.onload = () => {
   let road = new Image();
   road.src = "images/road.png";
 
-  let car = new Image();
-  car.src = "images/car.png";
+  let carIMG = new Image();
+  carIMG.src = "images/car.png";
 
+  let car = {
+    x: 225,
+    y: 0,
+    img: carIMG
+  }
+
+  //call 60 Times per second
   let frameCounter = 0
+
+  let gameRunning = true
 
   let draw = () => {
     frameCounter++
 
     ctx.drawImage(road, 0, 0, canvas.width, canvas.height)
-    ctx.drawImage(car, 0, 0)
+    ctx.drawImage(car.img, car.x, 250, 50, 100)
+
+    if (car.x <= -2 || car.x >= 460) {
+      gameRunning = false
+    }
 
     // drawing some helpter output to see how many re-renders the canvas did
-    ctx.font = '30pt Tahoma'
-    ctx.fillStyle = 'red'
-    ctx.fillText('frame counter : ${frameCounter}', 0, 50)
-
-    window.requestAnimationFrame(draw);
+    ctx.font = '30px Tahoma'
+    ctx.fillStyle = 'green'
+    ctx.fillText(`frame counter : ${frameCounter}`, 0, 50)
+    if (gameRunning) {
+      window.requestAnimationFrame(draw)
+      ;} else {
+        alert('GAME OVER')
+      }
   }
 
   document.onkeydown = (event) => {
-    if (event.key === 'ArrowRight') {}
-    if (event.key === 'ArrowLeft') {}
+    if (event.key === 'ArrowRight') {  
+      car.x += 5
+    }
+    if (event.key === 'ArrowLeft') { 
+      car.x -= 5
+    }
   }
 
   function startGame() {
