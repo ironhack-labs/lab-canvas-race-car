ab91ffd33efb9c63ce54c74f6ce111068ef0ae9c
Damaris Goebel <damaris.goebel@gmx.de>
2020-03-05 19:49:15 +0100

    “second-try”

--- js/index.js
+++ js/index.js
@@ -3,73 +3,69 @@ window.onload = () => {
     startGame();
   };
 
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
 
-  let canvas = document.getElementById ('canvas')
-  let ctx = canvas.getContext ('2d')
+  let roadImg = new Image();
+  roadImg.src = 'images/road.png';
 
-
-  let roadImg = new Image ();
-  roadImg.src ="images/road.png";
-
-  let carImg = new Image ();
-  carImg.src="images/car.png";
+  let carImg = new Image();
+  carImg.src = 'images/car.png';
 
   let car = {
-    x:300,
-    y:500,
+    x: 300,
+    y: 500,
     width: 50,
     height: 100,
+    speed: 20,
 
+    img: carImg,
 
-    rightArrowPressed: function () {
-      this.x += 25
+    rightArrowPressed: function() {
+      this.x += 25;
     },
-    leftArrowPressed: function () {
-      this.x -= 25
+    leftArrowPressed: function() {
+      this.x -= 25;
     },
 
-    update: function () {
-      ctx.drawImage(car,this.x,this.y,this.width, this.height)
+    update: function() {
+      ctx.drawImage(car, this.x, this.y, this.width, this.height);
     }
-  }
+  };
 
-  let frameCounter=0
+  let frameCounter = 0;
 
   function startGame() {
-    draw()
+    draw();
   }
 
-
+  let gameRunning = true
+  //called 60 times per seconds
   let draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      ctx.drawImage( roadImg, 0, 0, canvas.width, canvas.height)
-      ctx.drawImage (carImg,300,450,60,120)
-      frameCounter++
-car.update()
+    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    frameCounter++;
+    ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(car.img, car.x, car.y, 60, 120);
+
+    // drawing some helper output to see the output how often it renders
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'blue';
+    ctx.fillText(`frame counter: ${frameCounter}`, 0, 50);
+    if (gameRunning){
       window.requestAnimationFrame(draw)
+    } 
+  };
+
+
+  document.onkeydown = function(event) {
+    switch (event.keyCode) {
+      case 39:
+        car.rightArrowPressed();
+        break;
+      case 37:
+        car.leftArrowPressed();
+        break;
     }
-    
-
-  
-
-
-
-  document.onkeydown = function (event) {
-   
-   switch (event.keyCode) {
-     case 39: 
-     car.rightArrowPressed();
-     break;
-     case 37: 
-     car.leftArrowPressed();
-     break;
-    }
-    
-  }
-
-
-
-
-}
-
-
+    update();
+  };
+};
