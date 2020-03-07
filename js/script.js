8acc473d829d140ac8b917de4c078304057b08a8
Ben Balderas <benbalderas3@gmail.com>
2020-03-07 14:23:38 -0600

    added obstacles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let frames = 0;
 let timerId;
+let obstacles = [];
 
 class Vehicle {
     constructor(x, y) {
@@ -29,12 +30,26 @@ class Background {
     }
 
     draw() {
-        this.y -= 3;
+        this.y += 3;
 
-        if (this.y < -canvas.height) this.y = 0;
+        if (this.y > canvas.height) this.y = 0;
 
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.img, this.x, this.y + canvas.height, this.width, this.height);
+        ctx.drawImage(this.img, this.x, this.y - canvas.height, this.width, this.height);
+    }
+}
+
+class Obstacle {
+    constructor(x, y, width) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = 24;
+    }
+
+    draw() {
+        this.y += 3
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
@@ -42,12 +57,33 @@ class Background {
 const road = new Background();
 const car = new Vehicle((canvas.width / 2) - 40, canvas.height - 160 - 40);
 
+function generateObstacles() {
+    if (frames % 100 === 0) {
+        const minWidth = canvas.width / 3
+        const maxWidth = canvas.width - car.width - 110
+        const randomWidth = minWidth + Math.random() * (maxWidth - minWidth)
+        const randomX = 55 + Math.random() * ((canvas.width - randomWidth) - 55)
+        const obstacle = new Obstacle(randomX, 0, randomWidth)
+        obstacles = [...obstacles, obstacle]
+    }
+}
+
+function drawObstacles() {
+    obstacles.forEach((obstacle, index) => {
+        obstacle.draw()
+    })
+}
+
 // Update function
 function update() {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    road.draw()
-    car.draw()
+
+    road.draw();
+    car.draw();
+
+    generateObstacles();
+    drawObstacles()
 
     timerId = requestAnimationFrame(update);
 }
@@ -59,7 +95,19 @@ window.onload = () => {
     };
 
     function startGame() {
-        road.draw();
+        addEventListener('keydown', event => {
+            const keyLeft = 37;
+            const keyRight = 39;
+
+            if (event.keyCode === keyLeft && car.x > 0 + car.width) {
+                car.x -= car.width / 2
+            }
+
+            if (event.keyCode === keyRight && car.x < canvas.width - car.width * 2) {
+                car.x += car.width / 2
+            }
+        })
+
         requestAnimationFrame(update);
     }
 };
