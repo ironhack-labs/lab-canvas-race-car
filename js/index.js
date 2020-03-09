480591791c442f3d7f6fce9e086003fc2212731b
mateescumihaela <miha.mateescu@gmail.com>
2020-03-09 22:03:07 +0000

    iterations 1 & 2 complete

--- js/index.js
+++ js/index.js
@@ -3,5 +3,29 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+let road = new Image();
+road.src = 'images/road.png';
+
+let car = new Image ();
+car.src = 'images/car.png';
+
+let frameCounter = 0;
+
+let draw = () => {
+frameCounter++;
+
+context.drawImage(road, 0, 0);
+context.drawImage(car, 0, 0);
+
+window.requestAnimationFrame(draw);
+
+}
+
+function startGame() {
+draw(); 
+}
 };
+
