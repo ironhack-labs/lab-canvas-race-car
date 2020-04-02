41235540e38fd69f06dc8ff1cc1c5c93b865f8b6
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-02 13:28:44 -0600

    Pista conitnua

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+    startGame()
+  }
 
-  function startGame() {}
-};
+  function startGame() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    bgimg.draw()
+    requestAnimationFrame(startGame)
+  }
+}
+
+var bg = new Image()
+bg.src = './images/road.png'
+var bgimg = {
+  img: bg,
+  y: 0,
+  speed: -1,
+  draw: function() {
+    this.y++
+    if (this.y > canvas.height) this.y = 0
+    context.drawImage(this.img, 0, this.y, canvas.width, canvas.height)
+    context.drawImage(this.img, 0, this.y - canvas.height, canvas.width, canvas.height)
+  }
+}
