ebddb86569383581cf8e63d7fb75eb9cfb78dfbd
Kevin McCallum <kevingmcc@gmail.com>
2020-04-02 14:11:14 -0600

    POO

--- js/index.js
+++ js/index.js
@@ -1,28 +1,57 @@
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+// const canvas = document.querySelector('canvas')
+// const context = canvas.getContext('2d')
+class GameCanvas {
+  constructor() {
+    this.canvas = document.querySelector('canvas')
+    this.context = canvas.getContext('2d')
+    this.img = new Image()
+    this.y = 0
+  }
+
+  drawBg() {
+    this.y++
+    if (this.y > this.canvas.height) this.y = 0
+    this.context.drawImage(this.img, 0, this.y, this.canvas.width, this.canvas.height)
+    this.context.drawImage(
+      this.img,
+      0,
+      this.y - this.canvas.height,
+      this.canvas.width,
+      this.canvas.height
+    )
+  }
+}
+
+class Car {
+  constructor() {
+    this.x = 0
+    this.y = 0
+  }
+  moveUp() {
+    this.y -= 10
+  }
+  moveDown() {
+    this.y += 10
+  }
+  moveLeft() {
+    this.x -= 10
+  }
+  moveRight() {
+    this.x += 10
+  }
+}
+
+const gameCanvas = new GameCanvas()
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
+    gameCanvas.img.src = './images/road.png'
     startGame()
   }
 
   function startGame() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    bgimg.draw()
+    gameCanvas.context.clearRect(0, 0, canvas.width, canvas.height)
+    gameCanvas.drawBg()
     requestAnimationFrame(startGame)
-  }
-}
-
-var bg = new Image()
-bg.src = './images/road.png'
-var bgimg = {
-  img: bg,
-  y: 0,
-  speed: -1,
-  draw: function() {
-    this.y++
-    if (this.y > canvas.height) this.y = 0
-    context.drawImage(this.img, 0, this.y, canvas.width, canvas.height)
-    context.drawImage(this.img, 0, this.y - canvas.height, canvas.width, canvas.height)
   }
 }
