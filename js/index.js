62c07c5abbcad63b79c1278c29ce7c69bd7ceb8e
Alba Gustems <albagustems@gmail.com>
2020-04-24 20:10:20 +0200

    Iteration 3 (move the car) finished

--- js/index.js
+++ js/index.js
@@ -4,17 +4,50 @@ const ctx = canvas.getContext('2d');
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+
+
+
+
   }
 };
 
+// Function that initializes and shows the road and the car
 function startGame() {
   road.inicialize();
   road.show();
   car.inicialize();
   car.show();
+
+document.addEventListener('keydown', (event) => {
+  switch(event.key){
+    case "ArrowRight":
+      if(car.x < 450){
+      car.x += 40;
+      ctx.clearRect(0,0,500,700);
+      road.show();
+      car.show();
+      console.log(event.key)   
+      break;
+      }
+    
+    case "ArrowLeft":
+      if(car.x > 30){
+        car.x -= 40;
+        ctx.clearRect(0,0,500,700);
+        road.show();
+        car.show();
+        console.log(event.key) 
+        console.log(event.key)
+        break;
+      }
+  }
+})
+
+
 }
 
 
+// Creating the road object
 const road = {
   img: null,
   x: 0,
@@ -32,8 +65,9 @@ const road = {
   show: function () {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
-}
+};
 
+// Creating the car object
 const car = {
     img: null,
     x: 330,
@@ -41,18 +75,18 @@ const car = {
     width: 0,
     height: 0,
 
-    inicialize: function () {
-      this.imgCar = new Image();
-      this.imgCar.src = "/images/car.png";
-      this.width = (158 / 319) * this.height;
-      this.height = 80*1.7;
-    },
+  inicialize: function () {
+    this.imgCar = new Image();
+    this.imgCar.src = "/images/car.png";
+    this.width = (158 / 319) * this.height;
+    this.height = 80*1.7;
+  },
 
-    show: function(){
-      ctx.drawImage(this.imgCar, this.x, this.y, this.width, this.height);
-    }
-
-
+  show: function(){
+    ctx.drawImage(this.imgCar, this.x, this.y, this.width, this.height);
   }
+};
+
+
 
 
