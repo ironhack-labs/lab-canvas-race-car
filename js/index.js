8d3dd85f060c557b321deca3701b99af8d36e39a
Alba Gustems <albagustems@gmail.com>
2020-04-24 23:00:52 +0200

    Iteration 4 and 5 (creating and moving the obstacles) finished

--- js/index.js
+++ js/index.js
@@ -4,47 +4,48 @@ const ctx = canvas.getContext('2d');
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
-
-
-
-
   }
 };
 
 // Function that initializes and shows the road and the car
 function startGame() {
+  // Function to define the road
   road.inicialize();
+  // Function to draw the road
   road.show();
+  // Function to define the car
   car.inicialize();
+  // Function to draw the card
   car.show();
 
-document.addEventListener('keydown', (event) => {
-  switch(event.key){
-    case "ArrowRight":
-      if(car.x < 450){
-      car.x += 40;
-      ctx.clearRect(0,0,500,700);
-      road.show();
-      car.show();
-      console.log(event.key)   
-      break;
-      }
-    
-    case "ArrowLeft":
-      if(car.x > 30){
-        car.x -= 40;
-        ctx.clearRect(0,0,500,700);
+  // Event listener to move the car left and right
+  document.addEventListener('keydown', (event) => {
+    switch(event.key){
+      case "ArrowRight":
+        if(car.x < 450){
+        ctx.clearRect(0,550,500,150);
+        car.x += 40;
         road.show();
         car.show();
-        console.log(event.key) 
-        console.log(event.key)
+        console.log(event.key)   
         break;
-      }
-  }
-})
+        }
+      
+      case "ArrowLeft":
+        if(car.x > 30){
+          ctx.clearRect(0,550,500,150);
+          car.x -= 40;
+          road.show();
+          car.show();
+          console.log(event.key) 
+          console.log(event.key)
+          break;
+        }
+    }
+  })
 
-
-}
+  updateGame();
+};
 
 
 // Creating the road object
@@ -54,6 +55,7 @@ const road = {
   y: 0,
   width: 0,
   height: 0,
+  rollY: 0,
 
   inicialize: function () {
     this.img = new Image();
@@ -63,7 +65,14 @@ const road = {
   },
 
   show: function () {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.img, this.x, this.rollY, this.width, this.height);
+    ctx.drawImage(this.img, this.x, this.rollY - canvas.height , this.width, this.height);
+    this.rollY++;
+
+    if(this.rollY >= canvas.height){
+      this.rollY = 0;
+    }
+
   }
 };
 
@@ -87,6 +96,77 @@ const car = {
   }
 };
 
+function updateGame(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  road.show();
+  car.show();
+  obstacles.createObstacle();
+  obstacles.show();
+  requestAnimationFrame(updateGame);
+  if(checkColision()){
+    gameOver();
+  }
+}
 
+// Creating the obstacles object
+var obstacles = {
+  obstacles: [],
+  frames: 0,
 
+  createObstacle: function(){
+    if(this.frames%240 == 0){
+      let minwidth = canvas.width/6;
+      let maxwidth = canvas.width/4;
 
+      let newObstacle = {
+        x: 0,
+        y: 0,
+        width: (Math.random()*maxwidth)+minwidth,
+        height: 50,
+      }
+
+      newObstacle.x = Math.random()*canvas.width;
+      this.obstacles.push(newObstacle);
+      console.log("New obstacle", newObstacle)
+      }    
+  },
+
+  show: function(){
+    this.frames++;
+    this.obstacles = this.obstacles.map(obstacle => {
+      obstacle.y += 1;
+      return obstacle;
+    });
+    this.obstacles.forEach(obstacle => {
+      console.log(obstacle);
+      ctx.save();
+      ctx.fillStyle ='red'; 
+      ctx.fillRect(obstacle.x,obstacle.y,obstacle.width,obstacle.height);
+      ctx.restore();
+    })
+  }
+};
+
+// Function to check if the car collides with an obstacle
+function checkColision(){
+  return obstacles.obstacles.some(obstacle => {
+    if(obstacle.y + obstacle.height >= car.y && obstacle.y <= car.y + car.height + (car.height/2)){
+      if(obstacle.x + obstacle.width >= car.x && obstacle.x <= car.x + car.width){
+        return true;
+      }
+    }
+    return false;
+  });
+}
+
+// Function that trigers a game over and the final score
+function gameOver(){
+  if(checkColision){
+    // Code to create a black screen and the final score
+  }
+}
+
+var score = {
+  // Code to create the scoreboard
+
+}
