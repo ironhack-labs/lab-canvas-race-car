3d5e27ecde1f0a2efb2f08ffd52b59ecb656d491
David de Lamo <d.lamo.valverde@gmail.com>
2020-04-24 17:57:37 +0200

    infinity road added

--- js/index.js
+++ js/index.js
@@ -1,40 +1,51 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-function roadDraw() {
-  const road = new Image();
-  road.src = './images/road.png';
-  road.addEventListener('load', () => {
-    context.drawImage(road, 0, 0, canvas.width, canvas.height);
-  });
-}
-
-function restartRoad() {
-  context.clearRect(0,0,canvas.width,canvas.height);
-  roadDraw();
+const road = {
+  x: 0,
+  y: 0,
+  speed: 1/16,
+  width: canvas.width,
+  height: canvas.height,
+  img: new Image(),
+  move: function() {
+    road.y += road.speed;
+    road.y %= road.height;
+  },
+  draw: function() {
+    road.img.src = './images/road.png';
+    context.drawImage(road.img, road.x, road.y, road.width, road.height);
+    context.drawImage(road.img, road.x, road.y - road.height, road.width, road.height);
+  }
 }
 
 const car = {
-  x: canvas.width/2 - 25,
-  y: canvas.height - 120,
-  width: 50,
-  height: 319*50 / 158,
+  x: canvas.width/2 - 30,
+  y: canvas.height - 150,
+  width: 60,
+  height: 319*60 / 158,
+  img: new Image(),
   carDraw: () => {
-    const img = new Image();
-    img.src = './images/car.png';
-    img.addEventListener('load', () => {
-      context.drawImage(img, car.x, car.y, car.width, car.height);
-    });
+    car.img.src = './images/car.png';
+    context.drawImage(car.img, car.x, car.y, car.width, car.height);
   },
   moveCar: (dir) => {
     if (dir === 'left') {
-      car.x -= 5;
+      car.x -= 10;
     } else if (dir === 'right') {
-      car.x += 5;
+      car.x += 10;
     }
-    restartRoad();
-    car.carDraw();
   }
+}
+
+function updateRoad() {
+  context.save();
+  road.move();
+  context.clearRect(0 , 0, canvas.width, canvas.height);
+  road.draw();
+  car.carDraw();
+  context.restore();
+  //requestAnimationFrame(updateRoad);
 }
 
 function handleKeyEvent(event) {
@@ -50,8 +61,14 @@ window.onload = () => {
     startGame();
   };
   function startGame() {
-    roadDraw();
-    car.carDraw();
+    road.img.addEventListener('load', updateRoad);
+    //requestAnimationFrame(updateRoad);
+    setInterval(updateRoad,16);
     document.addEventListener('keydown', handleKeyEvent);
   }
 };
+
+// Hacer un set timeout para empezar la animación de la carretera
+// en ese tiempo, cuando se pique al button dibujar un 3
+// Limpiar y dibujar un 2 despues de 1 segundo
+// Despues del 1 empezar la animación.
