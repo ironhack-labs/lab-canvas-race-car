6f69f44dfb73cfff4d975c6e99db97e96ed5597a
David de Lamo <d.lamo.valverde@gmail.com>
2020-04-25 19:50:55 +0200

    lab done

--- js/index.js
+++ js/index.js
@@ -1,89 +1,123 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-//const obstacles = [];
 
 const road = {
   x: 0,
   y: 0,
-  speed: 1/16,
+  speed: 5,
   width: canvas.width,
   height: canvas.height,
   obstacles : [],
   score: 0,
   img: new Image(),
+  // Método para poder realizar la carretera infinita
   move: function() {
     road.y += road.speed;
     road.y %= road.height;
   },
+  // Dibuja la carretera
   draw: function() {
     road.img.src = './images/road.png';
     context.drawImage(road.img, road.x, road.y, road.width, road.height);
     context.drawImage(road.img, road.x, road.y - road.height, road.width, road.height);
+  },
+  // Función que crea obstáculos y los añade en el array
+  createObstacles: function() {
+    const obst = new Obstacle();
+    road.obstacles.push(obst);
+  },
+  // Desplaza y dibuja los obstáculos creados
+  updateObstacles: function() {
+    context.fillStyle = 'red';
+    road.obstacles.forEach(obst => obst.y += road.speed);
+    road.obstacles.forEach(obst => context.fillRect(obst.x, obst.y, obst.width, obst.height));
+  },
+  // Aumenta  el marcador cada vez que desaparece un obstáculo y dibuja el marcador
+  updateScore: function() {
+    road.obstacles.forEach(obst => {
+      if (obst.y > road.height) {
+        road.score++;
+        road.obstacles.shift();
+      }
+    });
+    context.font = '24px sans-serif';
+    context.fillStyle = 'white';
+    context.fillText('Score: ' + road.score, 300, 40);
+  },
+  // Dibuja una imagen the game over y muestra el marcador final
+  gameOver: function() {
+    road.draw();
+    const img = new Image();
+    img.src = './images/gameover.png';
+    img.addEventListener('load', function() {
+      context.drawImage(img, canvas.width/2 - 228, canvas.height/2 - 196, 456, 192);
+    });
+    context.font = '96px sans-serif';
+    context.fillStyle = 'white';
+    context.fillText('Score: ', 109, 500);
+    context.fillText(road.score, 310, 600);
   }
 }
 
 const car = {
-  x: canvas.width/2 - 30,
+  x: canvas.width/2 - 25,
   y: canvas.height - 150,
-  width: 60,
-  height: 319*60 / 158,
+  width: 50,
+  height: 319*50 / 158,
   img: new Image(),
-  carDraw: () => {
+  // Dibuja el coche al final de la carretera
+  carDraw: function() {
     car.img.src = './images/car.png';
     context.drawImage(car.img, car.x, car.y, car.width, car.height);
   },
-  moveCar: (dir) => {
+  // Desplaza en el eje horizontal el coche
+  moveCar: function(dir) {
     if (dir === 'left') {
-      car.x -= 10;
+      car.x -= 20;
     } else if (dir === 'right') {
-      car.x += 10;
+      car.x += 20;
     }
+  },
+  // Comprueba si el coche ha colisionado con algún obstáculo
+  checkCrash: function() {
+    if (road.obstacles[0] != undefined) {
+      if (road.obstacles[0].y > car.y - road.obstacles[0].height && road.obstacles[0].y < car.y + car.height) {
+        for (let i = car.x; i < car.x + car.width; i++) {
+          if (i > road.obstacles[0].x && i < road.obstacles[0].x + road.obstacles[0].width) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 }
-
+// Clase para crear los obstáculos
 class Obstacle {
   constructor() {
-    this.x = Math.floor(Math.random()*150 + 70);
+    this.x = Math.floor(Math.random()*200 + 70);
     this.y = 0;
-    this.width = Math.floor(Math.random()*canvas.width*0.05 + canvas.width*0.4);
+    this.width = Math.floor(Math.random()*canvas.width*0.05 + canvas.width*0.3);
     this.height = 20;
   }
 }
-
+// Llamada recursiva para dibujar la carretera, el coche, los obstáculos y el marcador. Mientras el coche no choque, sigue la recursividad.
 function updateRoad() {
   context.save();
   road.move();
   context.clearRect(0 , 0, canvas.width, canvas.height);
   road.draw();
   car.carDraw();
-  context.fillStyle = 'red';
-  updateObstacles();
-  updateScore();
+  road.updateObstacles();
+  road.updateScore();
   context.restore();
-  //requestAnimationFrame(updateRoad);
+  if (!car.checkCrash()) {
+    requestAnimationFrame(updateRoad);
+  } else {
+    road.gameOver();
+  }
 }
-
-function createObstacles() {
-  const obst = new Obstacle();
-  road.obstacles.push(obst);
-}
-
-function updateObstacles() {
-  road.obstacles.forEach(obst => obst.y += road.speed);
-  road.obstacles.forEach(obst => context.fillRect(obst.x, obst.y, obst.width, obst.height));
-}
-function updateScore() {
-  road.obstacles.forEach(obst => {
-    if (obst.y > road.height) {
-      road.score++;
-      road.obstacles.shift();
-    }
-  });
-  context.font = '24px sans-serif';
-  context.fillStyle = 'white';
-  context.fillText('Score: ' + road.score, 300, 40);
-}
-
+// Detecta la tecla izquierda y derecha
 function handleKeyEvent(event) {
   if (event.code === 'ArrowLeft') {
     car.moveCar('left');
@@ -97,15 +131,8 @@ window.onload = () => {
     startGame();
   };
   function startGame() {
-    road.img.addEventListener('load', updateRoad);
-    //requestAnimationFrame(updateRoad);
-    setInterval(updateRoad, 16);
-    setInterval(createObstacles, 1000);
+    requestAnimationFrame(updateRoad);
+    setInterval(road.createObstacles, 1200);
     document.addEventListener('keydown', handleKeyEvent);
   }
 };
-
-// Hacer un set timeout para empezar la animación de la carretera
-// en ese tiempo, cuando se pique al button dibujar un 3
-// Limpiar y dibujar un 2 despues de 1 segundo
-// Despues del 1 empezar la animación.
