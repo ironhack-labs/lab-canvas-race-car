80053bb0a1c50abd977c960cd6e432c053fb20aa
Marc De Mena <mdemena@gmail.com>
2020-04-26 12:08:55 +0200

    Iteration 3 finished

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ const road = {
     this.show();
   },
   show: function () {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
@@ -31,14 +32,37 @@ const car = {
   init: function (_img) {
     this.img = _img;
     this.width = 80;
+    this.speed = 10;
     this.height = (319 / 158) * this.width;
     this.x = (canvas.width / 2) - (this.width/2);
     this.y = canvas.height - 25 - this.height;
-    this.show();
+    this.move('');
   },
-  show: function () {
+  move: function (_keyCode) {
+    switch(_keyCode){
+      case 'ArrowLeft':
+        if ((this.x - this.speed) > 0){
+          this.x -= this.speed;
+        }
+        break;
+      case 'ArrowRight':
+        if ((this.x + this.speed + this.width) < canvas.width){
+          this.x += this.speed;
+        }
+        break;
+      case 'ArrowUp':
+        if ((this.y - this.speed) > 0){
+          this.y -= this.speed;
+        }
+        break;
+      case 'ArrowDown':
+        if ((this.y + this.speed + this.height) < canvas.height){
+          this.y += this.speed;
+        }
+        break;
+    }
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
+  },
 }
 
 window.onload = () => {
@@ -51,4 +75,9 @@ window.onload = () => {
     road.init(this.imgRoad);
     car.init(this.imgCar);
   }
+
+  document.addEventListener('keydown', event => {
+    road.init(this.imgRoad);
+    car.move(event.code);
+  });
 };
