1afc4935ab77cc578c5836b65c72ed5d273c96ea
Guillem Millan <millerguillem@gmail.com>
2020-05-12 02:02:50 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+var inicio;
 
 const road = {
   img: null,
@@ -7,80 +8,200 @@ const road = {
   y: 0,
   width: 0,
   height: 0,
-  inicializar: function(){
-    this.img = new Image();
-    this.img.src = "images/road.png";
+
+  inicializar: function () {
     this.width = canvas.width;
     this.height = canvas.height;
+    this.img = new Image();
+    this.img.src = "images/road.png";
+    this.img.onload = () => ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
   },
-  mostrar: function(){
-    console.log("mostrar");
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-}
-
-const car ={ 
-  img: null,
-  x: parseInt(canvas.eidth) /2-20,
-  y: canvas.height /2+140,
-  speedX: 4,
-  inicializar: function(){ 
-  this.img = new Image();
-  this.img.src = "images/car.png";
-  this.width = 40;
-  this.height = (319/158)*this.width;
-  this.img.onload = this.mostrar();
-},  
-  mostrar:function(){
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  mostrar: function () {
+    ctx.drawImage(this.img, 0, this.y, this.width, this.height);
+    ctx.drawImage(this.img, 0, this.y - this.img.height, this.width, this.height);
+    this.y += 1;
+    if (this.y >= this.img.height) {
+      this.y = 3;
     }
+  }
+};
+
+const car = {
+
+  img: null,
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  speedX: 0,
+  speedY: 0,
+  misPuntos: 0,
+
+  inicializar: function () {
+    this.x = canvas.width / 2 - 50;
+    this.y = canvas.height - 140;
+    this.width = 80;
+    this.height = 319 / 158 * this.width;
+    this.img = new Image();
+    this.img.onload = () => ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    this.img.src = 'images/car.png';
+  },
+
+
+  movimiento: function () {
+    this.x += this.speedX;
+    if (this.x >= road.width - this.width) {
+      this.x = road.width - this.width;
+    }
+    if (this.x <= 0) {
+      this.x = 0;
+    }
+  },
+
+  left: function () {
+    return this.x;
+  },
+
+  right: function () {
+    return this.x + this.width;
+  },
+
+  top: function () {
+    return this.y;
+  },
+
+  bottom: function () {
+    return this.y + this.height;
+  },
+
+  mostrar: function () {
+    ctx.drawImage(this.img, this.x, this.y, 50, 319 * 50 / 158);
+  },
+
+  puntos: function () {
+    this.misPuntos = Math.floor(frames / 20);
+    ctx.font = "15px Arial";
+    ctx.fillStyle = "yellow";
+    ctx.fillText("Score: " + this.misPuntos, canvas.width - 400, 50);
+  },
+
+  choque: function (objeto) {
+    return !(
+      this.bottom() < objeto.top() || this.top() > objeto.bottom() || this.right() < objeto.left() || this.left() > objeto.right()
+    );
+  }
+};
+
+document.onkeydown = function (i) {
+  switch (i.which) {
+    case 37: car.speedX -= 2;
+      break;
+    case 39: car.speedX += 2;
+      break;
+  }
+};
+
+document.onkeyup = function (i) {
+  car.speedX = 0;
+  car.speedY = 0;
+};
+
+class Objeto {
+  constructor(width, height, x, y, color){
+    this.width= width;
+    this.height= height;
+    this.x= x;
+    this.y= y;
+    this.color=color;
+    this.speedX=0;
+    this.speedY=0;
+  }
+
+ mostrar() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  top(){
+    return this.y;    
+}
+bottom(){
+    return this.y + this.height;
+
+}
+right(){
+    return this.x + this.width;
+
+}
+left(){
+    return this.x;
+}
 }
 
-class objects{
-  constructor(){
-    this.x=Math.floor(Math.random()* 150)+20;
-    this.y =0;
-    this.width = Math.floor(math.random()*100)+20;
-    this.height=10;
-    this.speed= 120;
+var objetos = [];
+
+let frames = 0;
+
+function updateobjetos() {
+  for (i = 0; i < objetos.length; i++) {
+    objetos[i].y += 1;
+    objetos[i].mostrar();
   }
-  showObject(){
-    ctx.fillstyle='red';
-    ctx.fillRect(this.x,this.y,this.width,this.height);
-  }
-  moveObject(){
-    this.y+=this.speed;
+  frames += 1;
+  if (frames % 200 === 0) {
+    var y = 0;
+    var menosAncho = 20;
+    var masAncho = 200;
+    var width = Math.floor(
+      Math.random() * (masAncho - menosAncho + 1) + menosAncho
+    );
+    var x = Math.floor(
+      Math.random() * (masAncho - menosAncho + 1) + menosAncho
+    );
+    objetos.push(
+      new Objeto (width, 20, x, y,"red"));
   }
 }
 
+function final() {
+  var chocado = objetos.some(function (objeto) {
+    return car.choque(objeto);
+  });
+  if (chocado) {
+    console.log(chocado);
+    cancelAnimationFrame(inicio);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "red";
+    ctx.fillText("GAME OVER!", canvas.width*0.26, canvas.height*0.4);
+    ctx.fillStyle = "yellow";
+    ctx.fillText("Score: " + car.misPuntos + " puntos", canvas.width*0.25, canvas.height*0.5);
+  }
+}
 
-function newPosition(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+function startGame() {
+  road.inicializar();
+  car.inicializar();
+}
+
+function actulaizarJuego() {
   road.mostrar();
+  car.movimiento();
   car.mostrar();
-  objects.mostrar();
-  requestAnimationFrame(update);
-  }
-
-
-
-function move(event){
-  if (event.code == "ArrowLeft" && car.x >= 0){
-    car.x-=car.speedX;
-  }
-  if (event.code == "ArrowRight" && car.x + car.width <= canvas.width) {
-    car.x+=car.speedX;
-  }
+  car.puntos();
+  updateobjetos();
+  inicio = requestAnimationFrame(actulaizarJuego);
+  final();
 }
 
-window.onload= () =>{
+
+window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
-    document.onkeydown = move;
-    requestAnimationFrame(update);
+    actulaizarJuego();
+    inicio = requestAnimationFrame(actulaizarJuego);
   };
-  function startGame(){
-    road.inicializar();
-    road.mostrar();
-  }
-}
+};
