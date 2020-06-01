49c6ff5229dfcd157cb35e70b38cb0292fbbdf6c
Andrea Astorga <andrea.astorga95@gmail.com>
2020-06-01 17:58:16 +0200

    Iterations 4 & 5

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ class Game {
         this.ctx = ctx;
         this.background = new Background(ctx);
         this.car = new Car(ctx);
+        this.obstacles = [];
+        this.tick = 0;
         this.intervalId = null;
         
     }
@@ -12,6 +14,7 @@ class Game {
             this.clear();
             this.draw();
             this.move();
+            this.addObstacle();
 
         },1000 / 60)
     }
@@ -19,10 +22,23 @@ class Game {
     draw() {
         this.background.draw();
         this.car.draw();
+        this.obstacles.forEach(obst => {
+            obst.draw();
+        });
     }
 
     move() {
         this.car.move();
+        this.obstacles.forEach(obst => {
+            obst.move();
+        });
+    }
+
+    addObstacle() {
+        if (this.tick++ === 150) {
+            this.tick = 0;
+            this.obstacles.push(new Obstacle(ctx))
+        }
     }
 
     clear() {
--- js/obstacles.js
+++ js/obstacles.js
@@ -2,10 +2,28 @@ class Obstacle {
     constructor (ctx) {
         this.ctx = ctx;
 
-        this.dist = Math.random() 
-        this.x = Math.random() > 
+        // this._leftLimit = this.ctx.canvas.width * 0.10;
+        // this._rightLimit = this.ctx.canvas.width * 0.80;
 
-        this.w = this.ctx.canvas.width;
-        this.h = Math.random() * 40 + 50;
+        this._leftLimit = 200;
+        this._rightLimit = 300;
+        
+        this.w = Math.floor(Math.random() * (0.5 * this.ctx.canvas.width) + 100);
+        this.h = 50;
+
+        this.x = Math.random(this._leftLimit) * this._rightLimit;
+        this.y = 0;
+
+        this.vy = 2;
+
+    }
+
+    draw() {
+        this.ctx.fillStyle = '#B82F55';
+        this.ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+
+    move() {
+        this.y += this.vy;
     }
 }
