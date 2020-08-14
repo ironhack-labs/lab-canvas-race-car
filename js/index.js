80cafa30bc0da656eb2eed72aa5c427889fac3ce
edvicaty <edvicaty@hotmail.com>
2020-08-13 19:31:18 -0500

    WDFTMEX - Yvan y Edgar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const $canvas = document.querySelector("canvas");
 const context = $canvas.getContext("2d");
-let intervalId;
-//Car class
+let score = 0;
 class Car {
   constructor(x, y, imageUrl) {
     this.x = x;
@@ -15,41 +14,70 @@ class Car {
     context.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
   moveRight() {
-    this.x += 5;
+    if (this.x > 440) this.x = 440;
+    this.x += 10;
   }
   moveLeft() {
-    this.x -= 5;
+    if (this.x < 20) this.x = 20;
+    this.x -= 10;
+  }
+  isTouching(obstacle) {
+    return (
+      this.x < obstacle.x + obstacle.width &&
+      this.x + this.width > obstacle.x &&
+      this.y < obstacle.y + obstacle.height &&
+      this.y + this.height > obstacle.y
+    );
   }
 }
-
-//New car
+class Obstacle extends Car {
+  constructor(x, width) {
+    super(x, -30);
+    this.width = width;
+    this.height = 30;
+    this.color = "red";
+  }
+  draw() {
+    context.fillStyle = this.color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+    this.y += 3;
+  }
+}
 const car = new Car(
   canvas.width / 2 - 20,
   canvas.height - 100,
   "../images/car.png"
 );
-
-//Event listener
-document.addEventListener("keydown", function (event) {
-  if (event.keyCode === 39) {
-    car.moveRight();
-  } else if (event.keyCode === 37) {
-    car.moveLeft();
-  }
+let obstacles = [];
+let intervalId,
+  frames = 0;
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode === 39) car.moveRight();
+  else if (e.keyCode === 37) car.moveLeft();
 });
-
-//Window on load
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
-
   function startGame() {
-    intervalId = setInterval(update, 1000 / 60);
+    score = 0;
+    clearInterval(intervalId);
+    intervalId = setInterval(updateCanvas, 1000 / 60);
+    frames = 0;
+    obstacles = [];
   }
 };
-
-//draw Board
+function updateCanvas() {
+  frames++;
+  score = Math.floor(frames / 120) - 2 < 0 ? 0 : Math.floor(frames / 120) - 2;
+  clearCanvas();
+  createObstacle();
+  drawBoard();
+  checkCollision();
+}
+function clearCanvas() {
+  context.clearRect(0, 0, $canvas.width, $canvas.height);
+}
 function drawBoard() {
   //board background
   context.fillStyle = "gray";
@@ -66,18 +94,48 @@ function drawBoard() {
   for (let i = 0; i < 20; i++) {
     context.fillRect($canvas.width / 2 - 3, i * 35, 6, 20);
   }
-  //draw car
   car.draw();
+  obstacles.forEach((obstacle, index) => {
+    if (obstacle.y > 700) {
+      obstacles.slice(index, 1);
+    } else obstacle.draw();
+  });
+  printScore();
+}
+function createObstacle() {
+  if (frames % 120 === 0) {
+    const minWidth = 50;
+    const maxWidth = 360;
+    const randomWidth = Math.floor(
+      Math.random() * (maxWidth - minWidth) + minWidth
+    );
+    const minX = 20;
+    const maxX = 480 - randomWidth;
+    const randomX = Math.floor(Math.random() * (maxX - minX) + minX);
+    obstacles.push(new Obstacle(randomX, randomWidth));
+  }
+}
+function checkCollision() {
+  obstacles.forEach((obstacle) => {
+    if (car.isTouching(obstacle)) {
+      gameOver();
+      clearInterval(intervalId);
+    }
+  });
+}
+function printScore() {
+  context.fillStyle = "black";
+  context.font = "40px Sans-serif";
+  context.fillText(`score : ${score}`, 170, 40);
 }
 
-//clear canvas
-
-function clearCanvas() {
-  context.clearRect(0, 0, $canvas.width, $canvas.height);
-}
-
-//update function
-function update() {
-  clearCanvas();
-  drawBoard();
+function gameOver() {
+  context.fillStyle = "black";
+  context.fillRect(50, 150, 400, 400);
+  context.font = "50px Sans-serif";
+  context.fillStyle = "red";
+  context.fillText("Game Over", 60, 200);
+  context.fillStyle = "white";
+  context.font = "40px Sans-serif";
+  context.fillText(`Your final score is ${score}`, 50, 300, 350);
 }
