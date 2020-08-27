479ad4fdb6ff64f1ac22f03ef32ac6fdbd64cd55
Javier de Salas <chinostrike@gmail.com>
2020-08-28 00:23:38 +0200

    Collisions

--- js/index.js
+++ js/index.js
@@ -19,28 +19,54 @@ const carRaceApp = {
       w: undefined,
       h: undefined,
     },
+    obstacles: [],
+    frames: 0,
+    score: 0,
 
     init(id) {
       this.canvasId = id
       this.ctx = document.getElementById(this.canvasId).getContext('2d')
       this.setDimensions()
+      this.setEventListeners()
 
       this.drawRoad()
 
       this.placeCar()
-      this.drawCar()     
+      this.drawCar()  
+
+      this.moveOn()
+   
     },
 
     setDimensions() {
       const canvas = document.getElementById(this.canvasId)
-
+      
       this.canvasSize = {
-           w: canvas.width,
-           h: canvas.height
-        }
+        w: canvas.width,
+        h: canvas.height
+      }
     },
-  
-     drawRoad() {
+    
+    moveOn() {
+      this.car.imgInstance.onload= e => {
+      setInterval(() => {
+        this.frames++
+
+        this.clearScreen()
+        this.frames % 70 === 0 ? this.addObstacle() : null
+        this.drawRoad()
+        this.drawCar()
+        this.moveObstacles()
+        this.checkCollisions()
+      }, 50)
+    }
+    },
+
+    clearScreen() {
+      this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
+    },
+
+    drawRoad() {
       this.ctx.fillStyle = 'green'
       this.ctx.fillRect(0, 0, this.canvasSize.w, this.canvasSize.h)
 
@@ -64,23 +90,54 @@ const carRaceApp = {
     },
 
     placeCar() {
-     this.car = new Car (this.ctx, this.canvasSize.w /2 - 25, this.canvasSize.h  - 125, 50, 100, 'car.png')
-     console.log(this.car)
-    
+     this.car = new Car (this.ctx, this.canvasSize.w /2 - 25, this.canvasSize.h  - 125, 50, 100, 'car.png', this.canvasSize.w)
+     
      this.carImage = new Image()
      this.carImage.src = `images/${this.car.imgName}`
     },
 
     drawCar() {
       this.car.renderCar()
+    },
+
+    addObstacle() {
+      this.obstacles.push(this.obstacle = new Obstacle (this.ctx))
+      console.log (this.obstacle)
+    },
+
+    moveObstacles() {
+      this.obstacles.forEach(e => {
+        e.moveO()
+        })
+    },
+
+    setEventListeners() {
+      document.onkeydown = e => {
+        e.keyCode === 37 ? this.car.move('left') : null
+        e.keyCode === 39 ? this.car.move('right') : null  
+      }
+    },
+
+    checkCollisions() {
+      this.obstacles.forEach(e => {
+        if (this.car.carPos.x < e.obstaclePos.x + e.obstacleSize.w &&
+          this.car.carPos.x + this.car.carSize.w > e.obstaclePos.x &&
+          this.car.carPos.y < e.obstaclePos.y + e.obstacleSize.h &&
+          this.car.carPos.y + this.car.carSize.h > e.obstaclePos.y) {
+            alert('Crash & Burn')
+            console.log(e)
+          }
+          else{
+            console.log('ppp')
+          }
+      })
     }
   
     
 }
 
-
 class Car {
-  constructor (ctx, posX, posY, carW, carH, imgName) {
+  constructor (ctx, posX, posY, carW, carH, imgName, canvasSizeW) {
     this.ctx = ctx
     this.carPos = {
       x: posX,
@@ -92,6 +149,7 @@ class Car {
     }
     this.imgName = imgName
     this.imgInstance = undefined
+    this.canvasSizeW = canvasSizeW
 
     this.initCar()
   }
@@ -102,8 +160,54 @@ class Car {
   }
 
   renderCar() {
-    this.imgInstance.onload = e => this.ctx.drawImage(this.imgInstance, this.carPos.x, this.carPos.y, this.carSize.w, this.carSize.h)
+    this.ctx.drawImage(this.imgInstance, this.carPos.x, this.carPos.y, this.carSize.w, this.carSize.h)
   }
+
+  move(direction) {
+    if (this.carPos.x > 50 && this.carPos.x < this.canvasSizeW - 70) {
+      direction === 'left' ? this.carPos.x -= 10 : null
+      direction === 'right' ? this.carPos.x += 10 : null
+    }
+    else if (this.carPos.x <= 50) {
+      this.carPos.x = 57
+    }
+    else if ( this.carPos.x >= this.canvasSizeW - 90) {
+      this.carPos.x = this.canvasSizeW - 97  
+    } 
+  }
+}
+
+class Obstacle {
+  constructor (ctx, canvasSizeH) {
+    this.ctx = ctx
+    this.obstaclePos = {
+      x: 300* Math.random(),
+      y: 0
+    }
+    this.obstacleSize = {
+      w: 150* Math.random() + 100,
+      h: 30
+    }
+    this.drawObstacle()
+    this.canvasSizeH = canvasSizeH
+  }
+
+  drawObstacle(){
+    this.ctx.fillStyle = '#810'
+      this.ctx.fillRect(this.obstaclePos.x, this.obstaclePos.y, this.obstacleSize.w, this.obstacleSize.h)
+  }
+
+  moveO() {
+    this.obstaclePos.y += 4
+    this.drawObstacle()
+    if (this.obstaclePos.y > this.canvasSizeH) {
+      return 5
+    } 
+    else {
+      return 0
+    }
+  }
+  
 }
 
 
