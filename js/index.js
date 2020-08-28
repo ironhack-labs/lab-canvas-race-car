de353ff0d556e982330eccbda30b7078832245f4
yordancurty <yordancurty@gmail.com>
2020-08-28 09:58:25 -0300

    almost done

--- js/index.js
+++ js/index.js
@@ -1,82 +1,142 @@
-window.onload = () => {
-  let imgRoad = new Image();
-  imgRoad.src = "./images/road.png";
-  let imgCar = new Image();
-  imgCar.src = "./images/car.png";
-  let crash = new Audio();
-  crash.src = './images/NFF-car-hit.wav';
+// window.onload = () => {
+//   document.getElementById("start-button").onclick () => {
+//     console.log("STARTED");
+//     startGame();
 
-  
-  document.getElementById('start-button').onclick = () => {
-    setInterval(startGame, 17);
+//   };
+
+//   function startGame() {
+//     ctx.clearRect(0, 0, 500, 700);
+//     ctx.drawImage(roadImg, 0, 0, 500, 700);
+//   }
+// };
+
+window.onload = () => {
+  //CANVAS
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  let interval;
+  let frames = 0;
+
+  // accessing images
+  const roadImg = new Image();
+  roadImg.src = "./images/road.png";
+  const carImg = new Image();
+  carImg.src = "./images/car.png";
+  const obstacleImg = new Image();
+  obstacleImg.src = "./images/obstacle.png";
+
+  document.getElementById("start-button").onclick = () => {
+    console.log("STARTED");
+    startGame();
   };
 
-  let carX = 225;
+  document.addEventListener("keydown", (arrowKey) => {
+    console.log("BOTÃO PRESSIONADO");
 
+    if (arrowKey.keyCode == 37) {
+      player.turnLeft();
+      console.log("mov para esquerda chamado");
+    }
 
-  document.addEventListener('keydown', function(e){ 
-    movement(e.keyCode);  
+    if (arrowKey.keyCode == 39) {
+      player.turnRight();
+      console.log("mov para direita chamado");
+    }
   });
 
+  // CAR CLASS WITH ITS METHODS
+  class Car {
+    constructor() {
+      this.width = 76;
+      this.height = 76 * (319 / 158);
+      this.positionX = canvas.width / 2;
+      this.positionY = canvas.height - (this.height + 50);
+      this.speed = 10;
+    }
 
-  function movement(keycode){
- 
-      switch (keycode){
+    drawPlayer() {
+      ctx.drawImage(
+        carImg,
+        this.positionX,
+        this.positionY,
+        this.width,
+        this.height
+      );
+    }
 
-        case 37 :
-          carX <= 0 ?
-          carX +=10 :
-          carX -=10;
-          break;
-  
-        case 39 :
-          carX >= 450 ?
-          carX -=10 :
-          carX +=10;
-          break;  
+    turnRight() {
+      if (this.positionX < canvas.width - 112) {
+        this.positionX += this.speed;
       }
+    }
+
+    turnLeft() {
+      if (this.positionX > 40) {
+        this.positionX -= this.speed;
+      }
+    }
   }
 
-  let listOfObstacles = [];
-  let frames = 500;
-  let counter = 0;
-
-  function createObstacle(param){
-
-    let randomWidth = Math.random() * 380 - 60;
-    let randomX = (Math.random() * 380 + 60) - randomWidth;
-    let y = 0;
-    
-    let obstacle = {
-      width: randomWidth,
-      x: randomX,
-      y:y
+  // OBSTACLES CLASS AND ITS METHODS
+  class Obstacle {
+    constructor(width, positionX) {
+      
+      this.positionX = positionX;
+      this.positionY = -30;
+      this.width = width;
+      this.height = 30;
     }
 
-   listOfObstacles.push(obstacle);
-
-  };
-
-  let canvas = document.getElementById("canvas");
-  let ctx = canvas.getContext('2d');
-
-  function startGame() { 
-    ctx.clearRect(0, 0, 500, 700)
-    ctx.drawImage(imgRoad,0,0,500,700); 
-    ctx.drawImage(imgCar,carX,550,50,100); 
-    console.log('hi')
-    crash.play();
-    
-    listOfObstacles.forEach((obstacle, i) => {
-      obstacle.y += 1
-      ctx.beginPath();
-      ctx.fillRect(obstacle.x,obstacle.y,obstacle.width,30);
-      })
-    
-    if(counter % frames === 0) {
-      createObstacle(ctx);
-      console.log('funciona')
+    moveDown() {
+      this.positionY += 10;
     }
-    counter += 1
+  }
+
+  const obstacleArray = [];
+
+  function updateObstacles() {
+    frames++;
+    console.log(frames);
+    if (frames % 120 === 0) {
+      let y = canvas.height;
+      let minWidth = 76;
+      let maxWidth = 335;
+      let realWidth = Math.floor(
+        Math.random() * (maxWidth - minWidth) + minWidth
+      );
+      let positionX =
+        Math.floor(Math.random() * (canvas.width - 76)) - realWidth;
+      obstacleArray.push(new Obstacle(realWidth, positionX));
+    }
+
+    obstacleArray.forEach((elem) => {
+      console.log(elem);
+      elem.moveDown();
+      ctx.drawImage(
+        obstacleImg,
+        elem.positionX,
+        elem.positionY,
+        elem.width,
+        elem.height
+      );
+    });
+  }
+
+  function updateGameArea() {
+    // drawing objects
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    player.drawPlayer();
+    updateObstacles();
+  }
+
+  //setting player
+  const player = new Car();
+
+  function startGame() {
+    //setting interval
+    interval = setInterval(updateGameArea, 20);
   }
 };
