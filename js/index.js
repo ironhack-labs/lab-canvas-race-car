d8572e268b5af1b6e38979a11fc15a1dd4e96f54
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-22 12:40:31 -0500

    refactoring image put on canvas

--- js/index.js
+++ js/index.js
@@ -5,29 +5,22 @@ window.onload = () => {
   };
 
   function startGame() {
-    // $context = document.getElementById('canvas').getContext('2d')
-    // $context =
-     img = new Image();
+    const context = document.getElementById("canvas").getContext("2d");
+    img = new Image();
     img.src = "./images/road.png";
-    // // console.log(image);
+    context.drawImage(img, 80, 0);
+    // console.log(context.width);
     // $context.drawImage(image, 0, 0, $context.width, $context.height);
 
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
+    // var ctx = c
     // var img = document.getElementById("scream");
-    ctx.drawImage(img, 10, 10);
 
     // $context = image
     // debugger
     // $context.fillStyle = "#FF0000";
     // $context.fillRect(0, 0, 150, 75);
 
-    // let frames = window.frames; // or // var frames = window.parent.frames;
-    // console.log(frames);
-    // for (var i = 0; i < frames.length; i++) {
-    //   // do something with each subframe as frames[i]
-    //   frames[i].document.body.style.background = "red";
-    //
+
     // }
 
   }
