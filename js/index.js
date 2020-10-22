6e871153540bf1d4c08519e8553b2afe2dd6f34a
Luizzmy <luizzmy@example.com>
2020-10-22 16:22:39 -0500

    iteracion 4 completa

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const $canvas = document.querySelector('canvas')
 const $ctx=$canvas.getContext('2d')
 
+let frames=0
+let ratio=200
+
+let obstacles=[]
+
+
 const image = new Image()
 const carImg = new Image()
 carImg.src = "./images/car.png"
@@ -35,12 +41,45 @@ class Character{
         this.x++;
         break;
       case 'Left':
-          if (this.x<=0) return
+          if (this.x<=0) return;
           this.x--
+          break;
       default: 
       throw new Error('invalid direction')
     }
   }
+}
+
+class Obstacle {
+  constructor(x, width) {
+    this.x = x
+    this.y = 0
+    this.width = width
+    this.height = 30
+  }
+  draw() {
+    this.y++
+    $ctx.fillStyle = "forestgreen"
+    $ctx.fillRect(this.x, this.y, this.width, this.height)
+  }
+}
+
+function generateObstacles() {
+  // if (frames % 150 === 0) ratio -= 10
+  if (frames % ratio === 0) {
+    const min = 100
+    const max = 400 //$canvas.width
+    const randomWidth = Math.floor(Math.random() * (max - min))
+    //const gap = 100
+    obstacles.push(new Obstacle(100, randomWidth))
+    //obstacles.push(
+      //new Obstacle(randomWidth + gap, $canvas.width - randomHeight - gap)
+    //)
+  }
+}
+
+function drawObstacles() {
+  obstacles.forEach(obs => obs.draw())
 }
 
 
@@ -48,10 +87,15 @@ const board= new Board(0,0)
 const character= new Character(200,400,carImg.src)
 
 
+
 function Update() {
+  frames++
   clearCanvas()
   board.draw()
   character.draw()
+  generateObstacles()
+  drawObstacles()
+  console.log(obstacles)
 }
 
 
