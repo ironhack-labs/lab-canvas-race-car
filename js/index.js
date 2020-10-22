6ab5a9b1dfaf8fd26009348e1652716721414fff
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-22 17:45:20 -0500

    on click solution

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
-//when we click on the Start Game button, we need to create the canvas and display the road.
-//SINTAXIS
-// void ctx.drawImage(image, dx, dy, dWidth, dHeight);
-// void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+const imgRoad = new Image();
+const imgCar = new Image();
+
+const $canvas = document.querySelector("canvas");
+const ctx = $canvas.getContext("2d");
+imgRoad.src = "./images/road.png";
+imgCar.src = "./images/car.png";
+
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
   function startGame() {
-    const $canvas = document.querySelector("canvas");
-    const ctx = $canvas.getContext("2d");
 
-    imgRoad = new Image();
-    imgRoad.src = "./images/road.png";
+
     ctx.drawImage(imgRoad, 0, 0, $canvas.width, $canvas.height);
 
 
     //iterattion 2
 
-    imgCar = new Image();
     imgCar.vel = 10
     imgCar.xUbication= 1
     imgCar.yUbication= 1
 
-    imgCar.src = "./images/car.png";
 
     let carHeight = imgCar.height / 2
     let carWidth = imgCar.width / 2
