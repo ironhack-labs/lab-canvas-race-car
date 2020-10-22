6a7528953ded3a17aa0c518ba6316343e10f96e6
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-22 10:13:09 -0500

    draw a red rectangle

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+//when we click on the Start Game button, we need to create the canvas and display the road.
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    $context = document.getElementById('canvas').getContext('2d')
+
+    $context.fillStyle = "#FF0000";
+    $context.fillRect(0, 0, 150, 75);
+    
+  }
+
+
+
 };
