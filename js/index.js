126717f3338ec0075c00b0f2c0616ad803e1a1e7
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-22 19:44:03 -0500

    added code ayhan

--- js/index.js
+++ js/index.js
@@ -1,107 +1,132 @@
-const imgRoad = new Image();
-const imgCar = new Image();
-
-const $canvas = document.querySelector("canvas");
-const ctx = $canvas.getContext("2d");
-imgRoad.src = "./images/road.png";
-imgCar.src = "./images/car.png";
-
-let gameInterval
-let ratio = 0
-let obstacles=[]
-
-
-
+//when we click on the Start Game button, we need to create the canvas and display the road.
+//SINTAXIS
+// void ctx.drawImage(image, dx, dy, dWidth, dHeight);
+// void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-  let y = $canvas.height / 2
-  function startGame() {
 
-    // if (gameInterval) return
-    gameInterval = setInterval(updateGame, 1000 / 60)
-    ctx.drawImage(imgRoad, 0, 0, $canvas.width, $canvas.height);
+  const $canvas = document.querySelector("canvas");
+  const ctx = $canvas.getContext("2d");
+  let gameInterval
+  let ratio = 0
+  let obstacles=[]
 
+  class Road {
+    constructor(){
+      this.x = 0
+      this.y = 0
+      this.width = $canvas.width
+      this.height = $canvas.height
+      this.img = new Image()
+      this.img.src = "./images/road.png"
+    }
+    draw(){
+      if(this.y < -$canvas.height) this.y = 0
+      this.y++
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+      ctx.drawImage(this.img, this.x, this.y - $canvas.height, this.width, this.height)
+    }
+  }
 
-    //iterattion 2
+  class Car {
+    constructor(x,y){
+      this.x = x
+      this.y = y
+      this.width = 79
+      this.height = 159
+      this.img = new Image()
+      this.img.src = "./images/car.png"
 
-    imgCar.vel = 0
-    imgCar.xUbication = 0
-    imgCar.yUbication = 0
-
-
-    let carHeight = imgCar.height / 2
-    let carWidth = imgCar.width / 2
-
-    let startAreaWidth = $canvas.width / 2 - (carWidth / 2);
-
-    let startAreaHeigth = $canvas.height / 2
-
-
-    ctx.drawImage(imgCar, startAreaWidth, startAreaHeigth, carWidth, carHeight);
-
-
-    // iterattion 3
-
-    imgCar.moveLeft = function() {
-      imgCar.xUbication -= 15
-    };
-    imgCar.moveRight = function() {
+    }
+    draw(){
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+    moveLeft(){
+      this.x -= 15
+    }
+    moveRight(){
       this.x += 15
     }
-    imgRoad.draw = function(){
-      if(this.y < -$canvas.height) this.y = 0
-      imgRoad.y++
-      ctx.drawImage(imgRoad, imgRoad.x, imgRoad.y, imgRoad.width, imgRoad.height)
-      ctx.drawImage(imgRoad, imgRoad.x, imgRoad.y - $canvas.height, imgRoad.width, imgRoad.height)
+  }
+
+  class Obstacle {
+    constructor(x, width) {
+      this.x = x
+      this.y = $canvas.height
+      this.width = this.width
+      this.height = 50
     }
-    imgCar.draw = function(){
-      ctx.drawImage(imgCar, this.x, this.y, this.width, this.height)
+    draw() {
+      this.y--
+      ctx.fillStyle = "crimson"
+      ctx.fillRect(this.x, this.y, this.width, this.height)
     }
+  }
+
+  let x =20
+  // let x = $canvas.width / 2 - (Car.width / 2)
+  let y = $canvas.height / 2
+  let car = new Car(210,500)
+  let road = new Road()
 
 
+  function startGame() {
+    car.draw()
+    if (gameInterval) return
+    gameInterval = setInterval(updateGame, 1000 / 60)
+  }
 
-
-    function updateGame() {
-      //recalcular el estado de los elementos
-      // x++
-      //limpiar el canvas
-      // clearCanvas()
-      //dibujar los elementos
-      // ctx.fillRect(x, 0, 10, 10)
-      // board.draw()
-      y++
-      // generateObstacles()
-      // clearCanvas()
-      imgCar.draw()
-      imgRoad.draw()
-      // drawObstacles()
-
+  //TODAVIA NO FUNCIONA
+  function generateObstacles() {
+    if (frames % ratio === 0) {
+      const min = 100
+      const max = $canvas.width - 100
+      const randomWidth = Math.floor(Math.random() * (max - min))
+      obstacles.push(new Obstacle(randomWidth,0))
+      obstacles.push(
+        new Obstacle(randomWidth, $canvas.width - randomWidth)
+      )
     }
+  }
+  function drawObstacles() {
+    obstacles.forEach(obs => obs.width)
+  }
 
-    document.onkeydown = e => {
-      switch (e.key) {
-        case "ArrowLeft":
-          imgCar.moveLeft()
-          console.log("LEFT now you're at x:", imgCar.xUbication);
-          break
-        case "ArrowRight":
-          imgCar.moveLeft()
-          console.log("RIGTH now you're at x:", imgCar.xUbication);
-          break
-        default:
-          break
-      }
-    }
-
-
-
-
-    ///end sttarGame
+  function updateGame() {
+    //recalcular el estado de los elementos
+    // x++
+    //limpiar el canvas
+    // clearCanvas()
+    //dibujar los elementos
+    // ctx.fillRect(x, 0, 10, 10)
+    // board.draw()
+    y++
+    generateObstacles()
+    clearCanvas()
+    road.draw()
+    car.draw()
+    drawObstacles()
 
   }
 
-
-
+  function clearCanvas() {
+    ctx.clearRect(0, 0, $canvas.width, $canvas.height)
+  }
+  //iterattion 3
+  document.onkeydown = e => {
+    switch (e.key) {
+      case "ArrowLeft":
+        console.log("left")
+        car.moveLeft()
+        break;
+      case "ArrowRight":
+        console.log("right")
+        car.moveRight()
+        break;
+      default:
+        break;
+    }
+  }
 };
