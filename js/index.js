a25c522d22e861603467567b478de47a480849dc
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-22 19:09:23 -0500

    intento de unificar ambos coodigos fallido

--- js/index.js
+++ js/index.js
@@ -6,24 +6,29 @@ const ctx = $canvas.getContext("2d");
 imgRoad.src = "./images/road.png";
 imgCar.src = "./images/car.png";
 
+let gameInterval
+let ratio = 0
+let obstacles=[]
+
 
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
+  let y = $canvas.height / 2
   function startGame() {
 
-
+    // if (gameInterval) return
+    gameInterval = setInterval(updateGame, 1000 / 60)
     ctx.drawImage(imgRoad, 0, 0, $canvas.width, $canvas.height);
 
 
     //iterattion 2
 
     imgCar.vel = 0
-    imgCar.xUbication= 0
-    imgCar.yUbication= 0
+    imgCar.xUbication = 0
+    imgCar.yUbication = 0
 
 
     let carHeight = imgCar.height / 2
@@ -39,32 +44,52 @@ window.onload = () => {
 
     // iterattion 3
 
-
-
-    imgCar.move = function (dir){
-      switch (dir) {
-        case "LEFT":
-          if (imgCar.xUbication <= 0) return
-          return (imgCar.xUbication -= imgCar.vel)
-        case "RIGHT":
-          if (imgCar.xUbication >= $canvas.width - imgCar.width) return
-          return (imgCar.xUbication += imgCar.vel)
-        default:
-          // throw new Error("Invalid direction")
-      }
+    imgCar.moveLeft = function() {
+      imgCar.xUbication -= 15
+    };
+    imgCar.moveRight = function() {
+      this.x += 15
+    }
+    imgRoad.draw = function(){
+      if(this.y < -$canvas.height) this.y = 0
+      imgRoad.y++
+      ctx.drawImage(imgRoad, imgRoad.x, imgRoad.y, imgRoad.width, imgRoad.height)
+      ctx.drawImage(imgRoad, imgRoad.x, imgRoad.y - $canvas.height, imgRoad.width, imgRoad.height)
+    }
+    imgCar.draw = function(){
+      ctx.drawImage(imgCar, this.x, this.y, this.width, this.height)
     }
 
+
+
+
+    function updateGame() {
+      //recalcular el estado de los elementos
+      // x++
+      //limpiar el canvas
+      // clearCanvas()
+      //dibujar los elementos
+      // ctx.fillRect(x, 0, 10, 10)
+      // board.draw()
+      y++
+      // generateObstacles()
+      // clearCanvas()
+      imgCar.draw()
+      imgRoad.draw()
+      // drawObstacles()
+
+    }
 
     document.onkeydown = e => {
       switch (e.key) {
         case "ArrowLeft":
-          imgCar.xUbication-=1
-          console.log("LEFT now you're at x:",imgCar.xUbication);
-          return imgCar.move("LEFT")
+          imgCar.moveLeft()
+          console.log("LEFT now you're at x:", imgCar.xUbication);
+          break
         case "ArrowRight":
-        imgCar.xUbication+=1
-        console.log("RIGTH now you're at x:",imgCar.xUbication);
-          return imgCar.move("RIGHT")
+          imgCar.moveLeft()
+          console.log("RIGTH now you're at x:", imgCar.xUbication);
+          break
         default:
           break
       }
