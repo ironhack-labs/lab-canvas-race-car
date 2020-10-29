58beb9777c7b5f04ef36cabc251ebad456952d93
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-29 16:07:27 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -36,9 +36,9 @@ window.onload = () => {
 
     drawRectangle() {
        this.ctx.fillStyle = 'green'
-       this.ctx.fillRect(this.canvasSize.w / 2 - 100, this.canvasSize.h / 2 - 100, 200, 200)
+       this.ctx.fillRect(0, 0, 500, 700)
        this.ctx.fillStyle = 'grey'
-       this.ctx.fillRect(this.canvasSize.w / 200 - 50, this.canvasSize.h / 2 - 50, 100, 100)
+       this.ctx.fillRect(0, 0, 300, 700)
     },  
   
   }
