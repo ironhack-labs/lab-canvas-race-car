79004c986f31936db36f8f8f9a505eec3c77ffce
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-29 16:11:17 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ window.onload = () => {
        this.ctx.fillStyle = 'green'
        this.ctx.fillRect(0, 0, 500, 700)
        this.ctx.fillStyle = 'grey'
-       this.ctx.fillRect(0, 0, 300, 700)
+       this.ctx.strokeRect(0, 0, 600, 700)
     },  
   
   }
