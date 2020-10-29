210eb639b583397a797dbdb64a7e5823f384a9e0
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-10-29 18:28:11 +0000

    in process

--- js/index.js
+++ js/index.js
@@ -1,7 +1,58 @@
 window.onload = () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d"); 
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  let x = 230
+  let y = 500
+  function startGame() {
+    
+    drawTheRoad()
+    drawTheCar()
+    
+    
 
-  function startGame() {}
+  }
+  
+
+  function drawTheRoad() {
+    const road = new Image();
+    road.onload = function(){
+      ctx.drawImage(road,0,0,500,700)
+    }
+    road.src = './images/road.png'
+  } 
+
+  function drawTheCar() {
+    const img = new Image();
+    img.onload = function () {
+      ctx.drawImage(img, x, y, 40, 80)
+    };
+    img.src = './images/car.png'
+  }
+
+  function moveLeft() {
+    x-= 20           
+  }
+
+  function moveRight() {
+    x+= 20
+  }
+
+  function refreshCanvas(){
+    ctx.clearRect(0,0,500,700);
+    drawTheRoad();
+    drawTheCar();
+  }
+
+  document.addEventListener('keydown', (event) => {
+    if(event.key === 'ArrowLeft'){
+      moveLeft()
+    } else if (event.key === 'ArrowRight'){
+      moveRight()
+    }
+    
+    refreshCanvas()
+  })
 };
