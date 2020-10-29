2b16aec0986ae1d3ee65873661ca3862db5f4dfa
Lara Morgana <laraanjiru@gmail.com>
2020-10-29 21:52:10 +0100

    Iteration 3 finished

--- js/App.js
+++ js/App.js
@@ -7,7 +7,8 @@ const controlledApp = {
   canvasTag: undefined,
   ctx: undefined,
   frames: 0,
-  ball: undefined,
+  road: undefined,
+  car: undefined,
   keys: {
     left: 37,
     right: 39,
@@ -21,41 +22,31 @@ const controlledApp = {
     this.canvasTag = document.getElementById(id);
     this.ctx = this.canvasTag.getContext("2d");
     this.setDimensions();
-    this.createBall();
+    this.createRoad();
+    this.createCar();
     this.drawAll();
     this.setEventListeners();
 
-    console.log(this.ctx); // Puedes ver el contexto por consola
+    console.log(this.ctx);
   },
 
   setDimensions() {
-    this.canvasSize = {
-      w: window.innerWidth,
-      h: window.innerHeight,
-    };
     this.canvasTag.setAttribute("width", this.canvasSize.w);
     this.canvasTag.setAttribute("height", this.canvasSize.h);
   },
 
   createRoad() {
-    this.road = new Road(
-      this.ctx,
-      this.canvasSize.w,
-      this.canvasSize.h,
-      100,
-      100,
-      "road.png"
-    );
+    this.road = new Road(this.ctx, 0, 0, 500, 700, "road.png");
   },
 
-  createBall() {
-    this.ball = new Ball(this.ctx, 200, 200, 100, 100, "road.png");
+  createCar() {
+    this.car = new Car(this.ctx, 200, 200, 100, 100, "car.png");
   },
 
   setEventListeners() {
     document.onkeydown = (e) => {
-      e.keyCode === this.keys.left ? this.ball.move("left") : null;
-      e.keyCode === this.keys.right ? this.ball.move("right") : null;
+      e.keyCode === this.keys.left ? this.car.move("left") : null;
+      e.keyCode === this.keys.right ? this.car.move("right") : null;
     };
   },
 
@@ -64,7 +55,8 @@ const controlledApp = {
       this.frames++;
       this.frames % 50 === 0 ? this.generateObstacle() : null;
       this.clearScreen();
-      this.ball.draw();
+      this.road.draw();
+      this.car.draw();
     }, 70);
   },
 
@@ -72,44 +64,74 @@ const controlledApp = {
     this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h);
   },
 
-  generateObstacle() {
-    console.log("NUEVO OBSTÁCULO! CUIDAO QUE VA!");
-  },
+  generateObstacle() {},
 };
 
-class Ball {
-  constructor(ctx, ballPosX, ballPosY, ballWidth, ballHeight, ballImage) {
+class Road {
+  constructor(ctx, roadPosX, roadPosY, roadWidth, roadHeight, roadImage) {
     this.ctx = ctx;
-    this.ballPos = {
-      x: ballPosX,
-      y: ballPosY,
+    this.roadPos = {
+      x: roadPosX,
+      y: roadPosY,
     };
-    this.ballSize = {
-      w: ballWidth,
-      h: ballHeight,
+    this.roadSize = {
+      w: roadWidth,
+      h: roadHeight,
     };
-    this.imageName = ballImage;
-    this.ballInstance = undefined;
+    this.imageName = roadImage;
+    this.roadInstance = undefined;
     this.init();
   }
 
   init() {
-    this.ballInstance = new Image();
-    this.ballInstance.src = `images/${this.imageName}`;
+    this.roadInstance = new Image();
+    this.roadInstance.src = `images/${this.imageName}`;
   }
 
   draw() {
     this.ctx.drawImage(
-      this.ballInstance,
-      this.ballPos.x,
-      this.ballPos.y,
-      this.ballSize.w,
-      this.ballSize.h
+      this.roadInstance,
+      this.roadPos.x,
+      this.roadPos.y,
+      this.roadSize.w,
+      this.roadSize.h
+    );
+  }
+}
+
+class Car {
+  constructor(ctx, carPosX, carPosY, carWidth, carHeight, carImage) {
+    this.ctx = ctx;
+    this.carPos = {
+      x: carPosX,
+      y: carPosY,
+    };
+    this.carSize = {
+      w: carWidth,
+      h: carHeight,
+    };
+    this.imageName = carImage;
+    this.carInstance = undefined;
+    this.init();
+  }
+
+  init() {
+    this.carInstance = new Image();
+    this.carInstance.src = `images/${this.imageName}`;
+  }
+
+  draw() {
+    this.ctx.drawImage(
+      this.carInstance,
+      this.carPos.x,
+      this.carPos.y,
+      this.carSize.w,
+      this.carSize.h
     );
   }
 
   move(dir) {
-    dir === "left" ? (this.ballPos.x -= 20) : null;
-    dir === "right" ? (this.ballPos.x += 20) : null;
+    dir === "left" ? (this.carPos.x -= 20) : null;
+    dir === "right" ? (this.carPos.x += 20) : null;
   }
 }
