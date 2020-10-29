203b573109a6ced59209898e41bfd1dcf56a1455
Monch87 <montserrat.mosqueda@gmail.com>
2020-10-29 15:36:41 +0100

    commit

--- js/index.js
+++ js/index.js
@@ -3,5 +3,50 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    drawingApp.drawRectangle()
+  }
 };
+
+
+ 
+ 
+ const drawingApp = {
+   name: 'Drawing app',
+   canvasTag: undefined,
+   ctx: undefined,
+   canvasSize: {
+     w: undefined,
+     h: undefined
+    },
+
+    init(id) {
+      this.canvasTag = document.getElementById(id)
+      this.ctx = this.canvasTag.getContext('2d')
+      this.setDimensions()
+      
+      console.log(this.ctx)    
+     },
+
+  setDimensions() {
+    this.canvasSize.w = window.innerWidth *.4
+    this.canvasSize.h = window.innerHeight
+    this.canvasTag.setAttribute('width', this.canvasSize.w)
+    this.canvasTag.setAttribute('height', this.canvasSize.h)
+  },
+
+
+    drawRectangle() {
+       this.ctx.fillStyle = 'red'
+       this.ctx.fillRect(this.canvasSize.w / 2 - 100, this.canvasSize.h / 2 - 100, 200, 200)
+       this.ctx.fillStyle = 'green'
+       this.ctx.fillRect(this.canvasSize.w / 2 - 50, this.canvasSize.h / 2 - 50, 100, 100)
+       this.ctx.fillStyle = 'black'
+       this.ctx.fillRect(100, this.canvasSize.h / 2 - 10, this.canvasSize.w - 200, 20)
+    },  
+  
+  }
+
+
+
+
