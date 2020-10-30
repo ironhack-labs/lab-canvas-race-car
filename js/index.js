dacfab9714d19df905cb70e5e0aec83239862569
carmen valencia <carmenpilar1990@gmail.com>
2020-10-30 17:32:47 +0100

    Movimiento coche

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
+"use strict";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = "./images/road.png";
+const carImg = new Image();
+carImg.src = "./images/car.png";
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
-  };
   function startGame() {
-    this.context = document.querySelector("#canvas").getContext("2d");
-    let roadImg = new Image();
-    roadImg.src = './images/road.png';
-    this.context.drawImage(roadImg, 100, 0, 400, 540);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(carImg, 225, 550, 50, 110);
   }
+}
 };
---
+++ js/player.js
@@ -0,0 +1,40 @@
+let carX = 225
+let direction = 'right'
+
+const createRoad = () => {
+const road = new Image()
+road.src = '../images/road.png'
+road.onload = () => {
+    ctx.drawImage(road, 0, 0, 500, 700)
+}
+}
+const createCar = () => {
+let car = new Image()
+car.src = '../images/car.png'
+car.onload = () => {
+    ctx.drawImage(car, carX, 580, 50, 100)
+}
+}
+//Movimiento derecha e izquierda:
+const moveCar = () => {
+    if(direction==='right' && carX<400){
+    createRoad()
+    carX+=10
+    createCar()
+    } else if (direction === 'left' && carX>50){
+    createRoad()
+    carX-=10
+    createCar()
+    }
+    }
+    document.addEventListener('keydown', (event)=>{
+    if(event.key === 'ArrowLeft'){
+        direction = 'left'
+        moveCar()
+    } else if(event.key === 'ArrowRight'){
+        direction='right'
+        moveCar()
+    } 
+    });
+
+
