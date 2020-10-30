dcf61e4ac01bcbf66ca9e934d3a9a0a30e8fda64
carmen valencia <carmenpilar1990@gmail.com>
2020-10-30 14:13:13 +0100

    Road

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
+  function startGame() {
+    this.context = document.querySelector("#canvas").getContext("2d");
+    let roadImg = new Image();
+    roadImg.src = './images/road.png';
+    this.context.drawImage(roadImg, 100, 0, 400, 540);
+  }
 };
