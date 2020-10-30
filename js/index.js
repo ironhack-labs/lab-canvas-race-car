5b89d579042e8e68fe40cb6ae96f97c679e6bb6b
Elisenda <elisenda.monserrat@gmail.com>
2020-10-30 12:53:59 +0100

    Iteration 1 & 2 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
-};
+  const ctx = document.querySelector("canvas").getContext("2d");
+
+  function startGame() {
+    drawRoad();
+    drawCar();
+  }
+
+  function drawRoad() {
+    const roadImg = new Image();
+    roadImg.onload = function () {
+      ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
+    }
+    roadImg.src = "./images/road.png";
+  }
+
+  function drawCar() {
+    const carImg = new Image();
+    const carImgX = (canvas.width / 2) -20;
+    const carImgY = 630;
+    carImg.onload = function () {
+      ctx.drawImage(carImg, carImgX, carImgY, 40, 60);
+    }
+    carImg.src = "./images/car.png";
+  }
+}
+
