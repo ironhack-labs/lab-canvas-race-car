3c68a3fd6fd0ad129d2f50b60a99fac4a7d51634
Daina Slezaite <daina.slezaite@gmail.com>
2020-10-31 13:22:36 +0100

    iteration 3, 4, 5 done

--- js/classes.js
+++ js/classes.js
@@ -2,12 +2,32 @@ class Game {
     constructor() {
         this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.score = 0;
     }
     startGame() {
-        let road = new Road();
-        let car = new Car();
         this.ctx.drawImage(road.roadImage, 0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(car.carImage, car.x, car.y, car.width, car.height);
+        this.showScore();
+    }
+    updateCanvas() {
+        setInterval(() => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.startGame();
+            obstacleArr.forEach(obstacle => {
+                obstacle.draw();
+                obstacle.move();
+            })
+        }, 20)
+    }
+    startObstacles() {
+        setInterval(() => {
+            obstacleArr.push(new Obstacle());
+        }, 3000);
+    }
+    showScore() {
+        this.ctx.fillStyle = 'white';
+        this.ctx.font = '20px Arial';
+        this.ctx.fillText(`Score: ${this.score}`, 70, 30);
     }
 }
 
@@ -29,5 +49,29 @@ class Car {
         this.height = 100;
         this.carImage = new Image();
         this.carImage.src = "./images/car.png";
+    }
+}
+
+class Obstacle {
+    constructor() {
+        this.x = this.getRandomX();
+        this.y = 0;
+        this.width = this.getRandomWidth();
+        this.height = 20;
+        this.dy = 2;
+        this.draw();
+    }
+    getRandomX() {
+        return Math.floor(Math.random() * (game.canvas.width - 50));
+    }
+    getRandomWidth() {
+        return Math.floor(Math.random() * (300 - 150 + 1)) + 150;
+    }
+    draw() {
+        game.ctx.fillStyle = 'red';
+        game.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    move() {
+        this.y += this.dy;
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
-
+let game = new Game();
+let road = new Road();
+let car= new Car();
+let obstacleArr = [];
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    let game = new Game();
     game.startGame();
+    game.startObstacles();
+    game.updateCanvas();
   };
 }
 
-
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37:
+      if(car.x > 0) {
+        car.x -= 20;
+      }
+      break;
+    case 39:
+      if(car.x < game.canvas.width - car.width) {
+        car.x += 20;
+      }
+      break;
+  }
+});
 
 
 
