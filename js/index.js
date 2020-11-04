054c111ff3d18d8a5c321ce47c730880358e1269
victorizquier2 <victor.izquierdo1985@gmail.com>
2020-11-04 20:10:47 +0100

    Ejercicio visto en clase

--- js/index.js
+++ js/index.js
@@ -1,70 +1,175 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
-  const myGameArea = {
-    canvas: document.getElementById('canvas'),
-    frames: 0,
-    start: function () {
-      this.canvas.width = 500;
-      this.canvas.height = 700;
-      this.context = this.canvas.getContext('2d');
-      this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function(){
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-  }
 
-  class Component {
-    constructor(_image, _src, _x, _y, _width, _height) {
-      this.image = _image;
-      this.src = _src;
-      this.width = _width;
-      this.height = _height;
+  //Cargamos nuestro entorno 2D
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'brown';
+
+  //CLASES
+  class Obstacle {
+    constructor(_x, _y, _width){
       this.x = _x;
       this.y = _y;
-    }
-    update(){
-      const ctx = myGameArea.context;
-      return ctx;
+      this.width = _width;
     }
   }
-  function startGame(){
-    console.log('Pulsaste el botón de Start!');
-    const road = new Component(new Image(), '../images/road.png', 0, 0, 500, 700);
-    const car = new Component(new Image(), '../images/car.png', 218, 512, 64, 128);
 
-    document.addEventListener('keydown', (event) => {
-      if(event.key === 'ArrowRight'){
-        //console.log("pulsaste derecha!");
-        car.x += 9;
-        roadImage.onload();
-        carImage.onload();
-      }else if(event.key === 'ArrowLeft'){
-        //console.log("Pulsaste izquierda!");
-        car.x -= 9;
-        roadImage.onload();
-        carImage.onload();
+  //VARIABLES
+  
+  let _carX = 225;
+  let dateRightNow = Date.now();
+  const obstacles = [];
+  let endGame = false;
+  let score = 0;
+  let clickable = true;
+
+  //MUSIC
+  let backgroundAudio = new Audio('../sounds/SoundHelix-Song-1.mp3');
+  let gameOverAudio = new Audio('../sounds/SoundHelix-Song-15.mp3');
+
+  // DOM MANIPULATION
+  document.getElementById('start-button').onclick = (event) => {
+    if(clickable){
+      gameOverAudio.pause();
+      backgroundAudio.currentTime = 0;
+      backgroundAudio.play();
+      event.target.classList.add('unclickable-button');
+      clickable = false;
+      endGame = false;
+      obstacles.length = 0;
+      score = 0;
+      startGame();
+    }
+  };
+
+  document.addEventListener('keydown', (event) => {
+
+    if(event.key === 'ArrowRight'){
+      if(!(_carX === 385)){
+        _carX += 10;
+      }
+    } 
+    
+    if(event.key === 'ArrowLeft'){
+      if(!(_carX === 65)){
+        _carX -= 10;
+      }
+    }
+
+  });
+
+  //FUNCTIONS
+
+  const renderImages = ()=>{
+    const background = new Image();
+    background.src = '../images/road.png';
+    drawBackground(background);
+  };
+
+  const renderScore = ()=>{
+    ctx.font = '20px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
+    ctx.fillText(`Score: ${score}`, 110, 20);
+    ctx.fillStyle = 'brown';
+  };
+
+  const drawBackground = (_background)=>{
+    _background.onload = ()=>{
+      ctx.drawImage(_background, 0, 0, 500, 700);
+      createCar();
+    }
+  };
+
+  const createCar = ()=>{
+    const car = new Image();
+    car.src = '../images/car.png';
+    drawCar(car);
+  };
+
+  const drawCar = (_car)=>{
+    _car.onload = ()=>{
+      ctx.drawImage(_car, _carX, 580, 50, 100);
+    }
+  };
+
+  const getRandomXForObstacle = ()=>{
+    return Math.floor(Math.random() * 285) + 65; 
+  };
+
+  const createObstacle = ()=>{
+
+    if(Date.now() - dateRightNow >= 1000){
+      dateRightNow = Date.now()
+      const newObstacle = new Obstacle(getRandomXForObstacle(), 0, 100);
+      obstacles.push(newObstacle)
+    }
+  };
+
+  const drawObstacles = () =>{
+    obstacles.forEach((obstacle)=>{
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, 25);
+    })
+    
+  }
+
+  const updateObstacles = ()=>{
+    
+    obstacles.forEach((obstacle) => {
+      obstacle.y += 3;
+    })
+  }
+
+  const checkForCollision = ()=>{
+    obstacles.forEach((obstacle)=>{
+
+      if(obstacle.y === 579){
+
+        if(_carX >= obstacle.x && _carX <= (obstacle.x + obstacle.width)){
+          endGame = true;
+        } else if((_carX + 50) >= obstacle.x && (_carX + 50) <= (obstacle.x + obstacle.width)){
+          endGame = true;
+        } else {
+          score++;
+        }
 
       }
-    });
-    //console.log("Hiciste click!");
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const roadImage = road.image;
-    roadImage.src = road.src;
-    roadImage.onload = () => {
-      ctx.drawImage(roadImage, road.x, road.y, road.width, road.height);
-    };
-    const carImage = car.image;
-    carImage.src = car.src;
-    carImage.onload = () => {
-      ctx.drawImage(carImage, car.x, car.y, car.width, car.height);
-    };
+
+    })
   }
-  function updateGameArea(){
-    myGameArea.clear();
-    car.update();
+
+  const renderGameOverText = ()=>{
+    ctx.font = '50px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillText('GAME OVER', 250, 350);
   }
+
+  // [newObstacle.x, newObstacle.x + newObstacle.width]
+
+  startGame = () => {
+    if(!endGame){
+      renderImages();
+      renderScore();
+
+      createObstacle();
+      drawObstacles();
+      updateObstacles();
+
+      checkForCollision();
+      requestAnimationFrame(startGame);
+      
+    } else {
+      renderGameOverText();
+      renderScore();
+      document.getElementById('start-button').classList.remove('unclickable-button');
+      clickable = true;
+      backgroundAudio.pause();
+      gameOverAudio.currentTime = 0;
+      gameOverAudio.play();
+
+    }
+    
+    
+  }
+
 };
