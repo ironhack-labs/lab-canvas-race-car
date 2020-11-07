b14864d3f9b6a76268359bdfbd420561cacea8a4
¨Christian <christianaresvillan@gmail.com>
2020-11-07 18:04:40 +0100

    Done

--- js/index.js
+++ js/index.js
@@ -1,70 +1,142 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-class Road {
-  constructor() {
-    this.x = 500;
-    this.y = 700;
-    const img = new Image()
-    img.addEventListener('load', () => {
-      this.img = img;
-      this.draw();
-    });
-    img.src = './images/road.png'
-  }
-  draw() {
-    ctx.drawImage(this.img, 0, 0, 500, 700)
-  }
-}
-class Car {
-  constructor() {
-    this.x = 230;
-    this.y = 600;
-    const img = new Image()
-    img.addEventListener('load', () => {
-      this.img = img;
-      this.draw();
-    });
-    img.src = './images/car.png'
-  }
-  moveRight() {
-    this.x+= 9
-    if(this.x > 450){
-      this.x = 450
-    }
-  }
-  moveLeft() {
-    this.x-= 9
-    if(this.x < 10){
-      this.x = 10
-    }
-  }
-  draw() {
-    ctx.drawImage(this.img, this.x, this.y, 40, 80)
-  }
-}
-function startGame() {
-  const road = new Road()
-  const car = new Car()
-  document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-      case 'ArrowLeft':
-        car.moveLeft();
-        console.log('left', car);
-        break;
-      case 'ArrowRight':
-        car.moveRight();
-        console.log('right', car);
-    }
-    updateCanvas();
-  });
-  function updateCanvas() {
-    ctx.clearRect(0, 0, 500, 700);
-    road.draw();
-    car.draw();
-  }
-}
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+
+  class Obstacle {
+    constructor(x, y, width) {
+      this.x = x
+      this.y = y
+      this.width = width
+    }
   };
-}
+
+
+
+  let carX = 230;
+  let dateRightNow = Date.now();
+  const obstacles = [];
+  let endGame = false;
+  let score = 0;
+  let clickable = true;
+
+  document.getElementById('start-button').onclick = (event) => {
+    if (clickable) {
+      event.target.classList.add('unclickable-button')
+      clickable = false
+      endGame = false
+      obstacles.length = 0
+      score = 0
+      startGame();
+    }
+  };
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      if (!(carX === 385)) {
+        carX += 10
+      }
+    };
+    if (event.key === 'ArrowLeft') {
+      if (!(carX === 65)) {
+        carX -= 10
+      }
+    }
+  });
+
+
+  const renderImages = () => {
+    const background = new Image()
+    background.src = '../images/road.png'
+    drawBackground(background)
+  };
+
+  const renderScore = () => {
+    ctx.font = '20px sans-serif'
+    ctx.textAlign = 'center'
+    ctx.fillStyle = 'white'
+    ctx.fillText(`Score: ${score}`, 110, 20)
+    ctx.fillStyle = 'brown'
+  };
+
+  const drawBackground = (background) => {
+    background.onload = () => {
+      ctx.drawImage(background, 0, 0, 500, 700)
+      createCar()
+    }
+  };
+
+  const createCar = () => {
+    const car = new Image()
+    car.src = '../images/car.png'
+    drawCar(car)
+  };
+
+  const drawCar = (car) => {
+    car.onload = () => {
+      ctx.drawImage(car, carX, 580, 50, 100)
+    }
+  };
+
+  const randomObstacle = () => {
+    return Math.floor(Math.random() * 285) + 65
+  };
+
+  const createObstacle = () => {
+    if (Date.now() - dateRightNow >= 1000) {
+      dateRightNow = Date.now()
+      const newObstacle = new Obstacle(randomObstacle(), 0, 100)
+      obstacles.push(newObstacle)
+    }
+  };
+
+  const drawObstacles = () => {
+    obstacles.forEach((obstacle) => {
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, 25)
+    })
+  };
+
+  const updateObstacles = () => {
+
+    obstacles.forEach((obstacle) => {
+      obstacle.y += 3
+    })
+  };
+
+  const checkForCollision = () => {
+    obstacles.forEach((obstacle) => {
+      if (obstacle.y === 579) {
+        if (carX >= obstacle.x && carX <= (obstacle.x + obstacle.width)) {
+          endGame = true
+        } else if ((carX + 50) >= obstacle.x && (carX + 50) <= (obstacle.x + obstacle.width)) {
+          endGame = true
+        } else {
+          score++
+        }
+      }
+    })
+  };
+
+  const renderGameOverText = () => {
+    ctx.font = '50px sans-serif'
+    ctx.textAlign = 'center'
+    ctx.fillText('GAME OVER', 250, 350)
+  };
+
+  startGame = () => {
+    if (!endGame) {
+      renderImages()
+      renderScore()
+      createObstacle()
+      drawObstacles()
+      updateObstacles()
+      checkForCollision()
+      requestAnimationFrame(startGame)
+    } else {
+      renderGameOverText()
+      renderScore()
+      document.getElementById('start-button').classList.remove('unclickable-button')
+      clickable = true
+    }
+  }
+};
