6283f6237a4afa8770f9e36f89ea3339fef14187
Thiarhes <injesusservice@hotmail.com>
2020-11-19 22:37:48 -0300

    WDPT SAO Kleber e Thiarhes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
+const ctx = document.getElementById("canvas").getContext("2d");
+const img = new Image();
+img.src = "../images/road.png";
+
+class Car {
+  constructor() {
+    this.x = 220;
+    this.y = 600;
+    const img = new Image();
+    img.addEventListener("load", () => {
+      // Once image loaded => draw
+      this.img = img;
+      this.draw();
+    });
+    img.src = "../images/car.png";
+  }
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, 50, 80)
+  }
+}
+const car = new Car();
+
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(img, 0, 0, 500, 700);
+    car.draw();
+  }
 };
