ef321676f42b515c2150cdb8bc83d9bfdcd1ea35
Sebastian Montoya <seb246@gmail.com>
2020-11-20 21:32:47 -0500

    finished iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+const canvas = document.querySelector(`#canvas`);
+const context = canvas.getContext(`2d`);
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+
+  const drawBackground = () => {
+    const backgroundImg = new Image();
+    backgroundImg.src = `./images/road.png`;
+    backgroundImg.addEventListener(`load`, () => {
+      context.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+    });
+  }
+
+  function startGame() {
+    drawBackground();
+  }
 };
