93e26129fd9be5be79224f0f15c84a37fda5720f
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-11-20 22:49:25 -0500

    finally able to put images

--- js/index.js
+++ js/index.js
@@ -1,39 +1,30 @@
-const canvas = document.getElementById('game-board');
+const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
+
+
 window.onload = () => {
+  
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-}
-  let x = 500
-  let y = 700
-  function startGame() 
+  function startGame() {}
+};
+
 
 
 // iteration 1 & 2: Draw the game board & car 
 
-const road = {
-  x:400,
-  y:600,
-  width:300,
-  height:300,
-};
-
 const roadImg = new Image();
 roadImg.src = './images/road.png';
+roadImg.addEventListener('load', () => {
+  // context.drawImage(Image, dX, dY, dWidth, dHeight);
+  context.drawImage(roadImg,50,20,400,500);
+});
 
-const carImg = {
-  x:300,
-  y:500,
-  width:70,
-  height:70,
-};
-  
 const carImg = new Image();
-carImg.src = './image/car.png';
-
-const drawEverything = () => {
-  context.drawImage(roadImg, road.x, road.y, road.width, road.Height);
-  context.drawImage(carImg, car.x, car.y, car.width, car.Height);
-  };
+carImg.src = './images/car.png';
+carImg.addEventListener('load', () => {
+  // context.drawImage(Image, dX, dY, dWidth, dHeight);
+  context.drawImage(carImg,130 ,400 , 70, 70);
+});
