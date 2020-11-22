15dabb71322253b2a14e1165398d2791d4974519
Anna Mosley <annahmosley@gmail.com>
2020-11-22 07:28:28 -0500

    car can now move

--- js/index.js
+++ js/index.js
@@ -1,32 +1,66 @@
+const cl = (...p) => console.log(...p);
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
   function startGame() {
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    const road = {
-      x: 0,
-      y: 0,
-      width: 500,
-      height: 700,
-    };
-    const roadImg = new Image();
-    roadImg.src = "../images/road.png";
-    roadImg.addEventListener("load", () => {
-      context.drawImage(roadImg, road.x, road.y, road.width,road.height); 
-    });
-    const car = {
-      x: 200,
-      y: 500,
-      width: 90,
-      height: 100,
-    }
-    const carImg= new Image();
-    carImg.src= "../images/car.png";
-    carImg.addEventListener("load", () => {
-      context.drawImage(carImg, car.x, car.y, car.width,car.height); 
-    });
+    drawingLoop()
   }
 };
+
+const score= 0
+const roadImg= new Image();
+roadImg.src='../images/road.png'
+const road= {
+  x:0,
+  y:0,
+  width: canvas.width,
+  height:canvas.height,
+};
+
+const carImg = new Image();
+carImg.src='../images/car.png'
+const car = {
+  x: 240,
+  y: 600,
+  width: 35,
+  height: 80,
+}
+
+function drawEverything() {
+  context.drawImage(roadImg, road.x, road.y, road.width, road.height);
+
+  context.fillStyle = 'white';
+  context.font = '20px Arial';
+  context.fillText(`Score: ${score}`, 75, 695);
+
+  context.drawImage(carImg, car.x, car.y, car.width, car.height);
+}
+document.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "ArrowLeft":
+    case "KeyA":
+      cl("arrow left");
+      if (car.x >= 45) car.x -= 10;
+      break;
+    case "ArrowRight":
+    case "KeyD":
+      cl("arrow right");
+      if (car.x <= canvas.width - car.width - 45) car.x += 10;
+      break;
+    default:
+      console.log("You can use only arrows and ASDW!");
+  }
+});
+const clearCanvas = () => {
+  context.clearRect(0,0, canvas.width, canvas.height);
+};
+
+const drawingLoop = () => {
+  clearCanvas()
+  drawEverything()
+  requestAnimationFrame(drawingLoop);
+};
+
