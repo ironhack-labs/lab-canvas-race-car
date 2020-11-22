77f9f9a28ab9b6b3f612405584256d15ba3308ce
mondono85 <mondono85@gmail.com>
2020-11-22 16:11:56 -0500

    done

---
+++ js/game.js
@@ -0,0 +1,8 @@
+class game {
+    constructor(){
+        this.const.canvas = document.getElementById('canvas');this.const.context = canvas.getContext('2d');
+        this.score= 0;
+
+
+    }
+}
--- js/index.js
+++ js/index.js
@@ -3,5 +3,32 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    
+  }
 };
+const road = {
+  x:50,
+  y:20,
+  width: 400,
+  height: 500
+};
+const roadImg = new Image();
+roadImg.src = './images/road.png';
+roadImg.addEventListener('load', () => {
+  // context.drawImage(Image, dX, dY, dWidth, dHeight);
+  context.drawImage(roadImg,road.x, road.y, road.width, road.height);
+});
+
+const car = {
+  x:130,
+  y:400,
+  width: 70,
+  height: 70
+};
+const carImg = new Image();
+carImg.src = './images/car.png';
+carImg.addEventListener('load', () => {
+  // context.drawImage(Image, dX, dY, dWidth, dHeight);
+  context.drawImage(carImg,car.x, car.y, car.width, car.height);});
+  
