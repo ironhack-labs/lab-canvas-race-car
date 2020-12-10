c0c72a7733013df06187c9ba2f7e34ee690a78eb
Preacherush <jlrascon.job@gmail.com>
2020-12-10 21:42:43 +0100

    First commit and steps.

---
+++ js/game.js
@@ -0,0 +1,31 @@
+
+class Game {
+    constructor(ctx) {
+      this.ctx = ctx
+      this.background = new Background(ctx)
+  
+      this.interval = undefined
+    }
+  
+    start() {
+      this.inverval = setInterval(() => {
+        this.clear()
+  
+        this.draw()
+        
+        this.move()
+      }, 1000 / 60)
+    }
+  
+    clear() {
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
+    }
+  
+    draw() {
+      this.background.draw()
+    }
+  
+    move() {
+      this.background.move()
+    }
+  }
---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const game = new Game(ctx)
+game.start()
