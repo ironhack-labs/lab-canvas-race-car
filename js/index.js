68e405a43eb00ef25896b251d458a7ce527eabbd
Discordante <diegocerratos@gmail.com>
2020-12-10 21:30:29 +0100

    Add iteration-1 with motion

--- js/index.js
+++ js/index.js
@@ -3,5 +3,11 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+
+    const game = new Game(ctx)
+    game.start()
+  }
 };
