469440f5c3d3aa32d8c0da2f6167c71b3358eae1
Sebastian Septien <sebsepdus@hotmail.com>
2021-01-21 22:26:59 -0600

    Falta voltear el sentido de los obstaculos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,164 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const myObstacles = [];
 
-  function startGame() {}
+const myGameArea = {
+  canvas: document.createElement("canvas"),
+  frames: 0,
+  start: function () {
+    this.canvas.width = 500;
+    this.canvas.height = 700;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    // call updateGameArea() every 20 milliseconds
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function () {
+    clearInterval(this.interval);
+  },
+  score: function () {
+    const points = Math.floor(this.frames / 5);
+    this.context.font = "18px serif";
+    this.context.fillStyle = "black";
+    this.context.fillText(`Score: ${points}`, 350, 50);
+  }
 };
+
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    // new speed properties
+    this.speedX = 0;
+    this.speedY = 0;
+
+    const img = new Image();
+    img.addEventListener('load', () => {
+      // Once image loaded => draw
+      this.img = img;
+      this.draw();
+    });
+    img.src = "../images//car.png";
+
+  }
+
+  draw() {
+    let ctx = myGameArea.context;
+    ctx.drawImage(this.img, this.x, this.y, 50, 50);
+  }
+
+
+  update() {
+    let ctx = myGameArea.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
+  crashWith(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
+}
+
+const player = new Component(0, 0, "red", 250, 0);
+
+function updateGameArea() {
+  myGameArea.clear();
+  // update the player's position before drawing
+  player.newPos();
+  player.update();
+  player.draw();
+  // update the obstacles array
+  updateObstacles();
+  // check if the game should stop
+  checkGameOver();
+  // update and draw the score
+  myGameArea.score();
+}
+
+myGameArea.start();
+
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 38: // up arrow
+      player.speedY -= 1;
+      break;
+    case 40: // down arrow
+      player.speedY += 1;
+      break;
+    case 37: // left arrow
+      player.speedX -= 1;
+      break;
+    case 39: // right arrow
+      player.speedX += 1;
+      break;
+  }
+};
+
+document.onkeyup = function (e) {
+  player.speedX = 0;
+  player.speedY = 0;
+};
+
+function updateObstacles() {
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].y += -1;
+    myObstacles[i].update();
+  }
+
+  myGameArea.frames += 1;
+  if (myGameArea.frames % 120 === 0) {
+    let y = myGameArea.canvas.height;
+    let minWidth = 20;
+    let maxWidth = 200;
+    let width = Math.floor(
+      Math.random() * (maxWidth - minWidth + 1) + minWidth
+    );
+    let minGap = 150;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+
+    // constructor(width, height, color, x, y) myObstacles.push(new Component(-width, 30, "brown", 450, y));
+    (Math.floor(Math.random() * 2)) === 0 ? myObstacles.push(new Component(width, 30, "brown", 50, y)) : myObstacles.push(new Component(-width-gap, 30, "brown", 440, y));
+
+
+
+
+  }
+}
+
+function checkGameOver() {
+  const crashed = myObstacles.some(function (obstacle) {
+    return player.crashWith(obstacle);
+  });
+
+  if (crashed) {
+    myGameArea.stop();
+  }
+}
