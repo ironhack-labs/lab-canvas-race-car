e335bd35d5544d8ef72c7b9c8c40577d8c799580
Graziele Dantas <elentary@gmail.com>
2021-04-01 23:28:22 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
-};
+  function startGame() {
+    const img = new Image();
+    const car = new Image();
+    car.src = `./images/car.png`;
+    img.src = `./images/road.png`;
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.drawImage(car, 225, 620, 50, 80);
+    };
+  };
+}
