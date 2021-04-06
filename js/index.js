e093e27271a070ff89479a5258322da7be0a7f42
Buckley212 <rob36611@gmail.com>
2021-04-06 18:04:13 -0400

    done even close to done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+    const canvas = document.getElementById('canvas');
 
-  function startGame() {}
+    const c = canvas.getContext('2d');
+    let deltaX = 0;
+    car_image = new Image();
+    car_image.src = '/ironHack/lab-canvas-race-car/images/car.png';
+    background_image = new Image();
+      background_image.src = '/ironHack/lab-canvas-race-car/images/road.png';
+
+    const background = () => {
+        c.drawImage(background_image, 0, 0, 500, 900)
+    }
+
+    const car = () => {
+      c.drawImage(car_image, 200 + deltaX, 700, 100, 150)
+    }
+
+    background();
+    car();
+
+    window.addEventListener("keydown", moveSomething, false);
+
+    function moveSomething(e) {
+      switch(e.keyCode) {
+          case 37:
+              deltaX -= 4
+              break;
+          case 39:
+              deltaX += 4
+              break;
+      }
+      c.clearRect(0,0,canvas.width,canvas.height)
+      background();
+      car()
+    }
+  }
 };
