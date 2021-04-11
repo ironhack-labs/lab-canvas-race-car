9231e93d54ab9a0430b7e92445ba479a2240c49d
silvia rodriguez <silrodcar@gmail.com>
2021-04-11 15:54:23 +0200

    iteration 1  and 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+const roadImg = new Image();
+roadImg.src = '../images/road.png';
+
+
+const carImg = new Image();
+carImg.src = '../images/car.png';
+carImg.width = 50;
+carImg.height = 50;
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(roadImg, 0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.drawImage(carImg, 225, 640, carImg.width, carImg.height )
+  }
+
 };
+
+
+// class Car {
+//   constructor() {
+//     this.img = new Image();
+//     this.img.src = carImg.src = '../images/car.png';
+
+  
+// }
+// }
+
