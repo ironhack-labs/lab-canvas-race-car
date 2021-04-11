c4cd672c26287252a227f0914b7cea7c4aa8d087
ZsokaM <zsokamontvai@gmail.com>
2021-04-11 16:44:42 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,96 +1,179 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
+    myGameArea.init();
   };
 
-  const ctx = document.getElementById("canvas").getContext('2d');
+  const myObstacles= [];
 
-  //class for the road 
-  class Field{
-    constructor(x,y, width, height){
-      this.img = new Image();
-      this.img.src="/images/road.png";
-      this.width = width;
-      this.height = height;
-      this.x = x,
-      this.y = y;
-    }
+const myGameArea = {
+  canvas: document.getElementById('canvas'),
+  frames: 0,
+  init: function(){
+  this.ctx = this.canvas.getContext('2d');
+  this.interval = setInterval(updateGameArea, 20)
+  },
+  bg: function(){
+    const backgroundImg = new Image();
+    backgroundImg.src = "/images/road.png";
+    this.ctx.drawImage(backgroundImg, 0, 0, 500, 700);
+  },
+  clear: function(){
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function(){
+    clearInterval(this.interval)
+  },
+  score: function(){
+    const points = Math.floor(this.frames / 10);
+    this.ctx.font = '30px Arial';
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillText(`Score: ${points}`, 350, 50);
+  },
+}
 
-    drawRoad(){
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-    }
-  }
 
-//class for the player 
-  class Player {
-    constructor(x, y, width, height){
-      this.img = new Image();
-      this.img.src="/images/car.png";
-      this.width = width;
-      this.height = height;
-      this.x = x,
-      this.y = y;
-      this.velocity = 4;
-    }
-
-    drawCar(){
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-    }
-
-    moveRight(){
-      this.x += this.velocity;
-    }
-
-    moveRight(){
-      this.y  -= this.velocity;
-    }
-
-    offRoad(){
-      if(this.x<50){
-        this.x=50
-      } else if(this.x + this.width > 400){
-        this.x = 400 - this.width;
-      }
-    }
-  }
-
-  //class for the walla
-  class Obstacle{
-    constructor(x, width){
-      this.x = x;
-      this.y = 0;
-      this.width = width;
-    }
-
-    drawWall(){
-      ctx.fillStyle = rgb(250, 100, 100);
-      ctx.fillRect(this.x, this.y, this.width, 50);
-    }
-  }
-
-  //and now lets create and move things around
-  let road = new Field(0,0, 500, 700);
-  let car1 = new Player(230, 600, 50, 70);
-
-  function startGame() {
-    ctx.clearRect(0,0, ctx.width, ctx.height)
-    road.drawRoad();
-    car1.drawCar();
+class Player {
+  constructor(x, y, width, height){
+    this.img = new Image();
+    this.img.src="/images/car.png";
+    this.width = width;
+    this.height = height;
+    this.x = x,
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
    
   }
 
-  document.addEventListener('keydown', function (event){
-    switch (event.code){
-      case "ArrowLeft":
-        car1.moveLeft()
+  update(){
+    const context = myGameArea.ctx;
+    context.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
+
+  newPos(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+ 
+  crashWith(obstacle) {
+    return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+  }
+}
+
+
+class Obstacle {
+  constructor(x, y, width, height){
+    this.width = width;
+    this.height = height;
+    this.x = x,
+    this.y = y;  
+  }
+
+  update(){
+    const context = myGameArea.ctx;
+    context.fillStyle = 'red';
+    context.fillRect(this.x, this.y, this.width, this.height)
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+}
+
+const car1 = new Player(225, 600, 30, 60);
+
+function updateObstacles() {
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].y += 1;
+    myObstacles[i].update();
+  }
+
+  myGameArea.frames += 1;
+  if (myGameArea.frames % 150 === 0) {
+    let y = myGameArea.canvas.width;;
+    let minWidth = 20;
+    let maxWidth = 400;
+    let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+    let minGap = 120;
+    let maxGap = 250;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    myObstacles.push(new Obstacle(0, 0, width, 20));
+    myObstacles.push(new Obstacle(width+gap, 0, y-width-gap, 20));
+  }
+}
+
+function checkGameOver() {
+  const crashed = myObstacles.some(function (obstacle) {
+    return car1.crashWith(obstacle);
+  });
+
+  if (crashed) {
+    myGameArea.stop();
+    const context = myGameArea.ctx;
+    context.fillStyle = 'red';
+    context.fillText(`Your final Score is: ${myGameArea.points}`, 40, 350);
+  }
+}
+
+
+
+function updateGameArea(){
+  myGameArea.clear();
+  myGameArea.bg();
+  car1.newPos();
+  car1.update();
+  updateObstacles();
+  checkGameOver();
+  myGameArea.score();
+ }
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case "ArrowUp": 
+      car1.speedY -= 1;
+      console.log('up')
       break;
-      case "ArrowRight":
-        car1.moveRight()
+    case "ArrowDown": 
+      car1.speedY += 1;
+      console.log('down')
       break;
-      default:
-        return
-    } 
-    car1.offRoad()
-  })
+    case "ArrowLeft": 
+      car1.speedX -= 1;
+      console.log('left')
+      break;
+    case "ArrowRight": 
+      car1.speedX += 1;
+      console.log('right')
+      break;
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  car1.speedX = 0;
+  car1.speedY = 0;
+});
+
 };
 
