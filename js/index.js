dfc16acfed98ccb0383c09462a1aa7849adc7e44
Julia Häußler <julia@haeuszler.de>
2021-06-17 21:43:53 +0200

    Iteration 1-3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,48 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-  function startGame() {}
-};
+let backgroundPosY = 0;
+let background = new Image();
+background.src = '/images/road.png';
+background.onload = function() {
+  ctx.drawImage(background, 0, backgroundPosY, 500, 700);
+  }
+
+let car = new Image();
+let carPosX = 210;
+car.src = '/images/car.png';
+car.onload = ()=>{
+  ctx.drawImage(car, carPosX, 520, 80, (80 / car.width) * car.height);
+}
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, 500, 700);
+  ctx.drawImage(background, 0, backgroundPosY, 500, 700);
+  ctx.drawImage(car, carPosX, 520, 80, (80 / car.width) * car.height);
+  backgroundPosY ++
+}
+
+
+document.onkeydown = function (event) {
+  console.log("event.keyCode", event.keyCode)
+  // left
+  if (event.keyCode === 37) {
+    carPosX -= 10
+  }
+  // right
+  if (event.keyCode === 39) {
+    carPosX += 10
+  }
+  updateCanvas();
+}
+
+setInterval(updateCanvas, 1000 / 60)
+
+
+
+
+
+
+
+
+
