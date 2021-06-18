4568fd2418acb83bf409f650cbd81d8d418dba82
Victor Campos <vccacampos@outlook.com>
2021-06-17 22:36:43 -0300

    Update index.js
    Class car done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  const canvas = document.querySelector('canvas')
+  const ctx = canvas.getContext('2d')
 
   function startGame() {}
-};
+  class car {
+    constructor(img, x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.img = img
+      }
+
+    draw = () => {
+        ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+    }
+}
+}
