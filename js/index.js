d5f4fb0231d8e825f175d18c15a8e48e4821be0f
Espe Cabrer <mecabrerv@gmail.com>
2021-08-20 00:40:03 +0200

    Iteración 1,2,3,4 y 5
    Pte iteración 6

--- js/index.js
+++ js/index.js
@@ -1,40 +1,54 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+
 class Car {
 
   constructor () {
-  this.carWidth = canvas.width/10;
-  this.carHeight = canvas.height/8;
-  this.carPositionX = (canvas.width/2) - (this.carWidth/2);
-  this.carPositionY =  canvas.height - 130;
-
-  const img = new Image();
-
-  img.addEventListener('load', () => {
-      this.img = img;
-      setTimeout(() => {
-
-      this.draw() },200 )
-    });
-    img.src = "/images/car.png";
+    this.carWidth = canvas.width/10;
+    this.carHeight = canvas.height/8;
+    this.carPositionX = (canvas.width/2) - (this.carWidth/2);
+    this.carPositionY =  canvas.height - 130;
+    this.img = new Image();
+    this.img.src = "/images/car.png";
   }
+
   moveLeft() {
     this.carPositionX -= 25;
   }
+
   moveRight() {
     this.carPositionX += 25;
   }
+  
   draw() {
     ctx.drawImage(this.img, this.carPositionX, this.carPositionY, this.carWidth, this.carHeight);
   }
 }
 
 
+  class Obstacle {
+
+    constructor () {
+      this.obstacleWidth = (Math.random() * (canvas.width * 0.75))
+      this.obstacleHeight = 15;
+      this.obstaclePositionX = Math.random() * (canvas.width - this.obstacleWidth);
+      this.obstaclePositionY = 0;
+      this.img = new Image();
+      this.img.src = "/images/barro.jpeg";
+    }
+
+    draw() {
+      ctx.drawImage(this.img, this.obstaclePositionX, this.obstaclePositionY, this.obstacleWidth, this.obstacleHeight);
+    }
+  } 
+  
+  
+// Función para limpiar el Canvas
+
 function clearCanvas(){
   ctx.clearRect(0,0,canvas.width,canvas.height)
 }
-
 
 
 
@@ -44,21 +58,29 @@ window.onload = () => {
   };
 
   function startGame() {
+
+    
+
     const road = new Image()
     road.src = "/images/road.png";
+    const car = new Car()
+    obstaclesArr = [];
+    obstaclesArr.push(new Obstacle()); 
+    let step = 0;
 
   
+
     setTimeout( () => {
      ctx.drawImage(road, 0,0, canvas.width, canvas.height);
     }, 100);
-
-    const car = new Car()
-    console.log(car)
 
     function updateCanvas(){
       clearCanvas();
       ctx.drawImage(road, 0,0, canvas.width, canvas.height)
       car.draw()
+      obstaclesArr.forEach(obstacle => {
+        obstacle.draw()
+      })
     }
 
     document.addEventListener('keydown', e => {
@@ -72,21 +94,21 @@ window.onload = () => {
           console.log('right');
           break;
       }
-      updateCanvas();
     });
 
-  /*   function moveCar (direction) {
-      
-      carPositionX = carPosition + direction;
+    setInterval( () => {
+      updateCanvas()
+    }, 100);
 
-      clearCanvas();
- */
-   /*    setTimeout( () => {
-        drawCanvas()
-      }, 500); */
-
-
- //     ctx.requestAnimationFrame(moveCar());
-    }
+    setInterval( () => {
+      obstaclesArr.forEach(obstacle => {
+        obstacle.obstaclePositionY += 5
+      })
+    }, 100);
+  
+    setInterval( () => {
+        obstaclesArr.push(new Obstacle()); 
+    }, 5000);
+  
   }
-
+}
