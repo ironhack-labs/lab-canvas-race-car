730f9c74eef0f69cc94a34bed202b74f88f034d8
eduvadillo <87086693+eduvadillo@users.noreply.github.com>
2021-08-19 19:10:40 +0200

    el coche se mueve!

--- js/index.js
+++ js/index.js
@@ -1,12 +1,44 @@
-const startCanva = document.getElementById("canvas");
-const ctx = startCanva.getContext("2d");
+//Linkar canvas
 
-//clase coche
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-class car {
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//Clase coche
+
+class Car {
   constructor() {
-    this.carPositionX = canvas.width / 2 - carWidth / 2;
+    this.carWidth = canvas.width / 10;
+    this.carHeight = canvas.height / 8;
+    this.carPositionX = canvas.width / 2 - this.carWidth / 2;
     this.carPositionY = canvas.height - 130;
+    this.lives = 99;
+    //Pintar coche --> 1.cargar la imagen; 2.pintar la imagen
+    let img = new Image();
+    img.addEventListener("load", () => {
+      this.img = img;
+      setTimeout(() => this.draw(), 500);
+    });
+    img.src = "/images/car.png";
+  }
+
+  moveLeft() {
+    this.carPositionX -= 10;
+  }
+  moveRight() {
+    this.carPositionX += 10;
+  }
+  draw() {
+    ctx.drawImage(
+      this.img,
+      this.carPositionX,
+      this.carPositionY,
+      this.carWidth,
+      this.carHeight
+    );
   }
 }
 
@@ -15,51 +47,55 @@ window.onload = () => {
     startGame();
   };
 
-  function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // 700 and 450 are canvas width and height
-  }
-
-  function drawCanvas() {
-    ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(car, carPositionX, carPositionY, carWidth, carHeigth);
-  }
-
   function startGame() {
-    // pintar carretera --> cargar imagen --> dibujar imagen
-
+    //Pintar carretera --> 1.cargar la imagen; 2.pintar la imagen
     const road = new Image();
     road.src = "/images/road.png";
+    setTimeout(
+      () => ctx.drawImage(road, 0, 0, canvas.width, canvas.height),
+      100
+    );
 
-    setTimeout(() => {
-      ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
-    }, 500);
+    //Crear coche
+    const car = new Car();
 
-    // pintar coche --> cargar imagen --> dibujar imagen
+    //crear movimiento del coche
 
-    const car = new Image();
-    car.src = "/images/car.png";
+    document.addEventListener("keydown", (e) => {
+      switch (e.code) {
+        case "ArrowLeft":
+          car.moveLeft();
+          break;
+        case "ArrowRight":
+          car.moveRight();
+          break;
+      }
+      updateCanvas();
+    });
 
-    let carWidth = canvas.width / 10;
-    let carHeigth = canvas.height / 8;
-    //let carPositionX = canvas.width / 2 - carWidth / 2;
-    //let carPositionY = canvas.height - 130;
-
-    setTimeout(() => {
-      ctx.drawImage(car, carPositionX, carPositionY, carWidth, carHeigth);
-    }, 500);
-
-    //  Que el coche se mueva a los lados --> 1 borrar canvas, 2 redibujar con la nueva posicion
-
-    function moveCar(direction) {
-      //actualizamos valor del dibujo
-      carPositionX = carPositionX + direction;
-
-      // borrar canvas actual
+    //Que el coche se mueva a los lados -->
+    function updateCanvas() {
+      //Borrar el canvas actual
       clearCanvas();
-      // redibujar todo
-      drawCanvas();
-      // Que se mueva constantemente
-      ctx.requestAnimationFrame(moveCar());
+      //Volver a Dibujar con la nueva posicion
+      ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
+      //Hacer que se mueva
+      car.draw();
     }
+
+    //setTimeout ( () => car.draw()
+    //,500);
+
+    //Que el coche se mueva a los lados -->
+    /*     function moveCar (direction){
+      //Actualizamos valor del dibujo
+      carPositionX = carPositionX + direction;
+      //Borrar el canvas actual
+      clearCanvas ();
+      //Volver a Dibujar con la nueva posicion
+      drawCanvas ();
+      //Hacer que se mueva
+      ctx.requestAnimationFrame(moveCar());
+    } */
   }
 };
