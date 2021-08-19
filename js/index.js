f40810e71c7037d1f946588938d9309595553c93
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-19 14:08:01 +0200

    first push

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+	document.getElementById('start-button').onclick = () => {
+		startGame();
+	};
 
-  function startGame() {}
+	function startGame() {
+		alert('hello');
+		const canvas = document.getElementById('canvas');
+		const img = canvas.getContext('2d');
+
+		//creating the image of the road
+		const backGroundImage = new Image();
+		backGroundImage.src = '../images/road.png';
+		this.img.drawImage(backGroundImage, 0, 0, canvas.width, canvas.height);
+	}
 };
