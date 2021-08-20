4386b6fe55d1a45916dc5444da8f78d3e1252439
unknown <martarrubio@telefonica.net>
2021-08-20 09:40:54 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,60 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+    draw();
+    drawImage();
+    
   };
 
-  function startGame() {}
+  function startGame() {
+    
+  }
+
+  function draw(){
+    ctx.beginPath();
+    ctx.fillStyle = 'green'
+    ctx.fillRect(0, 0, 330, 700);
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = 'grey'
+    ctx.fillRect(25, 0, 280, 700);
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(175, 0);
+    ctx.lineTo(175, 700);
+    ctx.stroke();
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = 'white';
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(40, 0);
+    ctx.lineTo(40, 700);
+    ctx.stroke();
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = 'white';
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(290, 0);
+    ctx.lineTo(290, 700);
+    ctx.stroke();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'white';
+    ctx.closePath();}
+  
+  let img = Image ()
+  document.getElementById(car.png)
+  img.src = ".\images\car.png"
+  function drawImage(){
+    ctx.drawImage(car, 25, 400, 50, 50);
+    
+  }
+
+
 };
