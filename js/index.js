ce39433fbabdfdf3ad4ed3704a07e6affd80dcbe
lauraAlabau <arlaura3@gmail.com>
2021-08-21 13:43:07 +0200

    Primer commit antes de clase

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
+ /*    raceCap.init */
     startGame();
   }; 
+
+
     
   function startGame() {
-
+    var myObstacles = [];
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
   
@@ -20,8 +23,8 @@ window.onload = () => {
     // ***** COCHE ***** //
     class Car {
       constructor() {
-        this.x = 210;
-        this.y = 500;
+        this.x = (canvas.width/2)-40;
+        this.y = (canvas.height)-200;
         
         // Creala imagen de coche
         const carImg = new Image();
@@ -42,7 +45,7 @@ window.onload = () => {
 
       draw() {
         ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);
-        ctx.drawImage(this.carImg, this.x, this.y,  this.carImg.width/2, this.carImg.height/2);
+        ctx.drawImage(this.carImg, this.x, this.y,  this.carImg.width/2, this.carImg.height/2);// imagen, posición X, posición Y, ancho y alto
         
       }
     }
@@ -57,14 +60,43 @@ window.onload = () => {
       updateCanvas();
     })
 
+
+    // ***** OBSTACULOS ***** //
+    let speed1 = 0;
+
+    function drawObstacles(x, y, w, h, color) {
+      speed1 += 1;
+      ctx.fillStyle = color;
+      ctx.fillRect(x, y, w, h);
+      var minWidth = 20;
+      var maxWidth = 400;
+      var width = Math.floor(Math.random() * (maxWidth - minWidth+ 1) + minWidth)
+      myObstacles = [];
+      for (i = 0; i < myObstacles.length; i++) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+      }
+      myObstacles.push(new Component(0, 0, width, 20, 'red'));
+
+    }   
+
+
+
+    // Retorna un entero aleatorio entre min (incluido) y max (excluido)
+  // ¡Usando Math.round() te dará una distribución no-uniforme!
+
     // ***** UPDATE CANVAS ***** //
     function updateCanvas() {
     
       ctx.clearRect(0, 0, 500, 700);
-      car.draw()
+      car.draw();
+      // redraw the canvas
+      
+      drawObstacles(0, speed1, (Math.floor(Math.random() * (450 - 50)) + 50), 50, 'red');
+      requestAnimationFrame(updateCanvas);
     }
     
-    updateCanvas()
+    
 
    
 
@@ -74,7 +106,7 @@ window.onload = () => {
 
 
   
-  
+    updateCanvas()
   
   }
 };
