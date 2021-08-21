a0d05c45e749dcb9404303ab70d41f6b4641f6f9
alfredorca <alfredorca@gmail.com>
2021-08-21 17:28:38 -0400

    Just add car and road

--- js/index.js
+++ js/index.js
@@ -3,5 +3,34 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+
+  function startGame() {
+    //DOM CONSTANTS
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    function drawRoad() {
+    ctx.drawImage(road, 0, 0, canvas.width, canvas.height)
+    }
+
+    const road = new Image();
+    road.src = '/images/road.png';
+    road.addEventListener('load', () => {
+      // this.road = road;
+      drawRoad();
+    });
+    
+
+    class raceCar {
+      constructor () {
+        this.carX = canvas.width / 2;
+        this.carY = 500;
+        this.carHeight = 100;
+        this.carWidth = 50;
+        const car = new Image();
+        car.src = '/images/car.png';        
+      }
+    }
+
+
+    }
 };
