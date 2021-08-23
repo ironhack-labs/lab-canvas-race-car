dc483496c160701a4920db56f059bee4c4ce5a82
andrekleine <andrekleine@gmail.com>
2021-08-23 15:01:11 -0300

    Lab done

---
+++ js/game.js
@@ -0,0 +1,156 @@
+class Game {
+    constructor(
+      canvas, context, field, player, obstacleConstructor,
+    ) {
+      this.canvas = canvas;
+      this.context = context;
+      this.field = field;
+      this.player = player;
+      this.ObstacleConstructor = obstacleConstructor;
+      this.animationId = 0;
+      this.obstacles = [];
+      this.fieldSpeed = 2;
+      this.newObstacleRateFPS = 120;
+      this.carSpeed = {
+        initialSpeed: 0,
+        speedIncrement: 1.2,
+      };
+      this.frames = 0;
+      this.isGameOver = false;
+      this.score = {
+        points: 0,
+        pointsIncrementFPS: 30,
+        htmlElement: document.querySelector('#game-score'),
+      };
+  
+      this.context.font = '40px Comic Sans';
+    }
+  
+    configureKeyboardControls() {
+      document.onkeydown = (event) => {
+        if (!this.isGameOver) {
+          this.carSpeed.initialSpeed += this.carSpeed.speedIncrement;
+  
+          this.player.move(event.keyCode, this.carSpeed.initialSpeed);
+        }
+      };
+  
+      document.onkeyup = () => {
+        this.carSpeed.initialSpeed = 0;
+      };
+    }
+  
+    startGame() {
+      this.clearField();
+  
+      this.field.drawField();
+      this.field.move(this.fieldSpeed);
+  
+      this.player.drawPlayer();
+  
+      this.createObstacles();
+      this.moveObstacles();
+      this.checkClearObstacles();
+  
+      this.checkCollision();
+  
+      this.updateScore();
+  
+      this.frames += 1;
+  
+      if (this.isGameOver) {
+        window.cancelAnimationFrame(this.animationId);
+  
+        this.showFinalGameStats();
+      } else {
+        this.animationId = window.requestAnimationFrame(() => this.startGame());
+      }
+    }
+  
+    clearField() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+  
+    createObstacles() {
+      if (this.frames % this.newObstacleRateFPS === 0) {
+        const newObstacle = this.generateRandomObstacle();
+  
+        this.obstacles.push(newObstacle);
+      }
+    }
+  
+    moveObstacles() {
+      this.obstacles.forEach((obstacle) => {
+        obstacle.drawObstacle();
+        obstacle.move(this.fieldSpeed);
+      });
+    }
+  
+    checkClearObstacles() {
+      this.obstacles.forEach((obstacle, index) => {
+        if (obstacle.posY >= this.canvas.height) {
+          this.obstacles.splice(index, 1);
+        }
+      });
+    }
+  
+    generateRandomObstacle() {
+      const randomSize = this.generateRandomNumber(100, 325);
+  
+      const maxPosX = this.canvas.width - randomSize;
+      const randomPosX = this.generateRandomNumber(0, maxPosX);
+  
+      const newObstacle = new this.ObstacleConstructor(
+        this.canvas, this.context, randomPosX, 0, randomSize, 30, 'black',
+      );
+  
+      return newObstacle;
+    }
+  
+    checkCollision() {
+      this.obstacles.forEach((obstacle) => {
+        if (this.player.crashWith(obstacle)) {
+          this.isGameOver = true;
+        }
+      });
+    }
+  
+    updateScore() {
+      if (this.frames % this.score.pointsIncrementFPS) {
+        this.score.points += 1;
+      }
+  
+      this.drawScore();
+    }
+  
+    drawScore() {
+      this.score.htmlElement.innerText = this.score.points;
+    }
+  
+    showFinalGameStats() {
+      setTimeout(() => {
+        this.clearField();
+  
+        this.context.fillStyle = 'black';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  
+        this.context.textAlign = 'center';
+        this.context.font = '50px Comic Sans';
+        this.context.fillStyle = 'red';
+        this.context.fillText('Game Over!', this.canvas.width / 2, this.canvas.height / 3);
+  
+        this.context.fillStyle = 'white';
+        this.context.fillText('Your final score is:', this.canvas.width / 2, this.canvas.height / 3 + 70);
+  
+        this.context.fillText(this.score.points, this.canvas.width / 2, this.canvas.height / 3 + 140);
+      }, 1000);
+    }
+  
+    generateRandomNumber(minValue, maxValue) {
+      const min = minValue;
+      const max = maxValue;
+      const random = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+      return random;
+    }
+  }
--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
 
-  function startGame() {}
+    const fieldImg = new Image();
+    fieldImg.src = './images/road.png';
+
+    const carImg = new Image();
+    carImg.src = './images/car.png';
+
+    fieldImg.onload = () => {
+      carImg.onload = () => {
+        const field = new Field(
+          canvas, context, 0, 0, canvas.width, canvas.height, fieldImg,
+        );
+        const player = new Player(
+          canvas, context, 220, 550, 60, 120, carImg,
+        );
+
+        const game = new Game(canvas, context, field, player, Obstacle);
+
+        game.configureKeyboardControls();
+        game.startGame();
+      };
+    };
+  };
 };
