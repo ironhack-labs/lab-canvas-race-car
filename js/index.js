a76a86680893cac9e7c9862a710ad3b93a4b673f
Alvin Sewram <alvinsewram@Alvins-MacBook-Pro.local>
2021-08-24 11:03:05 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,55 @@
+let canvas = document.getElementById("canvas");
+
+ctx = canvas.getContext("2d");
+
+class Sprite {
+  constructor(x, y, width, height, img) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+  }
+
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  loadImg(callback) {
+    const image = new Image();
+    image.src = this.img;
+    image.addEventListener("load", () => {
+      this.img = image;
+      this.draw();
+      if (callback) {
+        callback();
+      }
+    });
+  }
+}
+
+class Road extends Sprite {
+  constructor() {
+    super(0, 0, 400, 600, "./images/road.png");
+  }
+}
+
+class Car extends Sprite {
+  constructor() {
+    super(170, 470, 55, 110, "./images/car.png");
+  }
+}
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const road = new Road();
+    road.loadImg(() => {
+      const car = new Car();
+      car.loadImg();
+    });
+  }
 };
