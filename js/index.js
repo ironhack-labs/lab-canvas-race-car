952e4829251d290c8937c0c81638a25b9fbe3e42
sarebro <sarebro77@gmail.com>
2021-08-26 17:18:29 -0500

    updated game start, displaying road and car

--- js/index.js
+++ js/index.js
@@ -3,29 +3,56 @@ window.onload = () => {
     startGame();
   };
 
+  drawRoad();
+  drawCar(250, 600);
+
   function startGame() {
-    let canvas = document.getElementById("canvas")
-    canvas.style.background = "url('/images/road.png')";
-    canvas.style.backgroundRepeat = "no-repeat";
-    canvas.style.backgroundSize = "cover";
+    
+    
   }
 };
 
-
+function drawRoad() {
 const ctx = document.getElementById('canvas').getContext('2d');
-const img = new Image();
-imgScale = 640/480;
-img.onload = function() {
-  ctx.drawImage(img, 215, 525,50*imgScale,150);
+
+ctx.clearRect(0, 0, 500, 700);
+
+const road = new Image();
+road.src = "../images/road.png"
+
+road.onload = function() {
+  ctx.drawImage(road, 0, 0, canvas.width, canvas.height)
+ }
 };
 
-img.src = '/images/car.png';
+function drawCar() {
+  const ctx = document.getElementById('canvas').getContext('2d');
 
-const ctx2 = document.getElementById('canvas').getContext('2d');
-const img2 = new Image();
-imgScale = 640/480;
-img2.onload = function() {
-  ctx2.drawImage(img2, 0, 0,350*imgScale,600);
-};
+  const car = new Image();
+  car.src = "../images/car.png"
+
+  car.onload = function() {
+    ctx.drawImage(car, x, y, x + 50, y + 100);
+ }
+}
+
+
+
+
+
+// const img = new Image();
+// imgScale = 640/480;
+// img.onload = function() {
+//   ctx.drawImage(img, 215, 525,50*imgScale,150);
+// };
+
+// img.src = '/images/car.png';
+
+// const ctx2 = document.getElementById('canvas').getContext('2d');
+// const img2 = new Image();
+// imgScale = 640/480;
+// img2.onload = function() {
+//   ctx2.drawImage(img2, 0, 0,350*imgScale,600);
+// };
  
-img2.src = '/images/road.png';
+// img2.src = '/images/road.png';
