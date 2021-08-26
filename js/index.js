9c1f40c2f5c8c4b60b2463f9f5ae3c6917268a71
sarebro <sarebro77@gmail.com>
2021-08-26 15:24:49 -0500

    added start game function and road, car img

--- js/index.js
+++ js/index.js
@@ -3,5 +3,29 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let canvas = document.getElementById("canvas")
+    canvas.style.background = "url('/images/road.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "cover";
+  }
 };
+
+
+const ctx = document.getElementById('canvas').getContext('2d');
+const img = new Image();
+imgScale = 640/480;
+img.onload = function() {
+  ctx.drawImage(img, 215, 525,50*imgScale,150);
+};
+
+img.src = '/images/car.png';
+
+const ctx2 = document.getElementById('canvas').getContext('2d');
+const img2 = new Image();
+imgScale = 640/480;
+img2.onload = function() {
+  ctx2.drawImage(img2, 0, 0,350*imgScale,600);
+};
+ 
+img2.src = '/images/road.png';
