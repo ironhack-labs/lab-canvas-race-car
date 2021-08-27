84bb8ff293c53e1cd1daca0d2ded852714e47ecf
jbaker825 <jbaker825@gmail.com>
2021-08-27 13:41:35 -0500

    iteration 1 & 2

--- js/index.js
+++ js/index.js
@@ -3,5 +3,51 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    drawRoad();
+    drawCar(235, 650);
+  }
 };
+
+
+function drawRoad() {
+  // Link our canvas to JS
+  const canvas = document.getElementById('canvas')
+
+  // Get context of our canvas
+  const ctx = canvas.getContext('2d')
+
+  ctx.clearRect(0, 0, 500, 700);
+  // canvas.style.backgroundImage = 'url(../image/road.png)'
+
+  // Creates a new Image
+  const road = new Image();
+
+  // Connects image to the image file
+  road.src = "./images/road.png"
+
+  // Ensures that when the road is called by the canvas...
+  road.onload = function () {
+    // It draws the image inside of the canvas 
+    ctx.drawImage(road, 0, 0, canvas.width, canvas.height);
+  };
+}
+
+function drawCar(x, y) {
+  // Link our canvas to JS
+  const canvas = document.getElementById('canvas')
+
+  // Get context of our canvas
+  const ctx = canvas.getContext('2d')
+
+  const car = new Image();
+  car.src = './images/car.png'
+
+  car.onload = function () {
+    // It draws the image inside of the canvas 
+    ctx.drawImage(car, x, y, 25, 50);
+  };
+}
+
+
+
