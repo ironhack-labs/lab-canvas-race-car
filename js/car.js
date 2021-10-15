92ef9a0b51c07f475756c90c154c509cf2ed50ec
Michael Seitz <michael.s.seitz@gmail.com>
2021-10-15 22:13:37 +0200

    car shows damage now

--- js/car.js
+++ js/car.js
@@ -12,9 +12,28 @@ class Car {
         this.speed = 9;
         this.posX = canvas.clientWidth * 0.5;
         this.poxY = canvas.clientHeight * 0.8;
+        this.health = 3;
     }
 
     drawCar() {
+        switch (this.health) {
+            case 3:
+                this.url = "/images/car.png";
+        this.img.src = this.url;
+                break;
+            case 2:
+                this.url = "/images/car-damaged1.png";
+        this.img.src = this.url;
+                break;
+            case 1:
+                this.url = "/images/car-damaged2.png";
+        this.img.src = this.url;
+                break;
+            case 0:
+                break;
+            default:
+                break;
+        }
         this.ctx.drawImage(
             this.img,
             this.posX - this.carWidth / 2,
@@ -28,25 +47,25 @@ class Car {
     }
 
     updateCarPos() {
-        if (this.checkBoundaries())
-            this.posX += this.direction * this.speed;
+        if (this.checkBoundaries()) this.posX += this.direction * this.speed;
     }
 
     checkBoundaries() {
-        if (this.posX < canvas.clientWidth * 0.1)
-            this.posX += 1 * this.speed;
+        if (this.posX < canvas.clientWidth * 0.1) this.posX += 1 * this.speed;
         else if (this.posX > canvas.clientWidth * 0.9)
             this.posX += -1 * this.speed;
-        else
-            return true
+        else return true;
     }
 
-    checkCrash(obstacleCar){
-        let isSamePosX = obstacleCar.posX < this.posX+this.carWidth/2 &&
-        obstacleCar.posX > this.posX-this.carWidth/2
-        let isSamePosY = obstacleCar.posY > this.poxY-this.carHeight*0.2
-        
-        if (isSamePosY && isSamePosX)
-            return true
+    checkCrash(obstacleCar) {
+        let isSamePosX =
+            obstacleCar.posX < this.posX + this.carWidth / 2 &&
+            obstacleCar.posX > this.posX - this.carWidth / 2;
+        let isSamePosY = obstacleCar.posY > this.poxY - this.carHeight * 0.2;
+
+        if (isSamePosY && isSamePosX) {
+            this.health--;
+            return true;
+        }
     }
 }
