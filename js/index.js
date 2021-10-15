9126d18727103d8d9408400df6936b9c5e322821
Michael Seitz <michael.s.seitz@gmail.com>
2021-10-15 19:32:59 +0200

    Obstacles are now removed, if they enter the bottom

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const canvas = document.querySelector("canvas");
 const road = new Road(canvas);
 const car = new Car(canvas);
-const obstacle = new Obstacle(canvas)
+//const obstacle = new Obstacle(canvas)
+const obstacle = []
+obstacle.push(new Obstacle(canvas))
 
 window.onload = () => {
     document.getElementById("start-button").onclick = () => {
@@ -18,10 +20,18 @@ function update() {
     road.drawRoad();
     car.drawCar();
     car.updateCarPos();
-    obstacle.drawCar();
-    obstacle.updateCarPos();
+    // obstacle.drawCar();
+    // obstacle.updateCarPos();
+    obstacle.forEach(obstacle => {obstacle.drawCar()});
+    obstacle.forEach(obstacle => {obstacle.updateCarPos()});
     // driving
     steering();
+    // obstackle clearout by out of bounds
+    for (element in obstacle){
+      if (obstacle[element].checkBoundaries()){
+        obstacle.splice(element, 1)
+      }
+    }
 }
 
 function steering() {
