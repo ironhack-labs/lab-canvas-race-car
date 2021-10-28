a4414b46dde516066aae5b43900d68573a79d8ca
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-28 16:42:11 +0200

    Iteración 1 completada

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
+  
+  function startGame() {
+    const imgRoad = new Image()
+    imgRoad.src = '/images/road.png'
+    imgRoad.onload = ()=>{
+      ctx.drawImage(imgRoad, 0, 0, 500, 700)
+    }
+  }
 };
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
