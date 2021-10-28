377a0ad921574a070495bb7466777343c4837d25
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-28 19:10:46 +0200

    Iteración 3 completada

--- js/index.js
+++ js/index.js
@@ -1,20 +1,8 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
-  // FUNCTIONS
-  function startGame() {
-    drawRoad()
-    drawCar()
-  }
-  function drawRoad() {
-    ctx.drawImage(loadImages.road, 0, 0, 500, 700)
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
-  }
-  function drawCar() {
-    ctx.drawImage(loadImages.car, 250, 580, 50, 100)
-  }
-  const loadImages = []
+// LOAD ALL IMAGES
+const loadImages = []
   let counterForLoadedImages = 0
   const imagesLinks = [
     {link: '/images/road.png', name:'road'},
@@ -30,7 +18,76 @@ window.onload = () => {
       loadImages[image.name] = img
     }
   })
+  // CLASSES
+
+  class Car{
+    constructor(){
+      this.x = 250;
+      this.y = 550;
+      this.speedX = 0;
+      this.speedY = 0;
+      this.width = 50;
+      this.heigth = 100;
+    }
+  }
+  const car = new Car()
+window.onload = () => {
+  document.getElementById('start-button').onclick = () => {
+    startGame();
+  };
+  // FUNCTIONS
+  function startGame() {
+    drawRoad()
+    drawCar()
+    updateCar()
+    requestAnimationFrame(startGame)
+  }
+  function drawRoad() {
+    ctx.drawImage(loadImages.road, 0, 0, 500, 700)
+
+  }
+  function drawCar() {
+    ctx.drawImage(loadImages.car, car.x, car.y, car.width, car.heigth)
+  }
+  function updateCar(){
+  car.x += car.speedX
+  car.y += car.speedY
+  checkLimits()
+  }
+  function checkLimits(){
+    if(car.x > 405){
+      car.x = 405
+    }
+    if(car.x < 50){
+      car.x = 50
+    }
+    if(car.y > 600){
+      car.y = 600
+    }
+    if(car.y < 0){
+      car.y = 0
+    }
+  }
   
+  // EVENT LISTENERS
+
+  document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowLeft'){
+      car.speedX = -3
+    }else if(event.key === 'ArrowRight'){
+      car.speedX = +3
+    }else if(event.key === 'ArrowUp'){
+      car.speedY = -3
+    }else if(event.key === 'ArrowDown'){
+      car.speedY = +3
+    }
+  })
+  document.addEventListener('keyup', function(event){
+    if(event.key === 'ArrowLeft' || event.key === 'ArrowRight'){
+      car.speedX = 0
+    }
+    if(event.key === 'ArrowUp' || event.key === 'ArrowDown'){
+      car.speedY = 0
+    }
+  })
 };
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
