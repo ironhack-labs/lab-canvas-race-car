897ab76c4e5ecc20609de1f3ef8b2ce97f85a0b7
JoseLValdes <jolvaldgo@gmail.com>
2021-10-28 18:57:05 +0200

    Back ground and car

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+let background = new Image();
+background.src = "../images/road.png";
+
+let car = new Image();
+car.src = "../images/car.png";
+
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background,0,0,500,700);
+    ctx.drawImage(car,0,0,50,50); 
+     
+
+  }
 };
+
