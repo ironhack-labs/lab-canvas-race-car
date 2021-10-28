8e250b83c53974de14d61fac5efef1630fcf89b3
nachbr91 <nabero91@gmail.com>
2021-10-28 17:38:25 +0200

    Iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext ('2d')
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const road = new Image();
+    road.src = "/images/road.png";
+    road.onload = ()=>{
+      ctx.drawImage (road, 0, 0, 500, 700)
+    }
+  }
 };
