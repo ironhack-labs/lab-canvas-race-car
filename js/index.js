f0829b2486a98bcb6f6b5e1390056158cdcba3fa
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-28 17:29:29 +0200

    Actualizando Iteraciones 1 y 2

--- js/index.js
+++ js/index.js
@@ -2,19 +2,34 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-  
+  // FUNCTIONS
   function startGame() {
-    const imgRoad = new Image()
-    imgRoad.src = '/images/road.png'
-    imgRoad.onload = ()=>{
-      ctx.drawImage(imgRoad, 0, 0, 500, 700)
-      const imgCar = new Image()
-      imgCar.src = '/images/car.png'
-      imgCar.onload = ()=>{
-        ctx.drawImage(imgCar, 250, 580, 50, 100)
-      }
-    }
+    drawRoad()
+    drawCar()
   }
+  function drawRoad() {
+    ctx.drawImage(loadImages.road, 0, 0, 500, 700)
+
+  }
+  function drawCar() {
+    ctx.drawImage(loadImages.car, 250, 580, 50, 100)
+  }
+  const loadImages = []
+  let counterForLoadedImages = 0
+  const imagesLinks = [
+    {link: '/images/road.png', name:'road'},
+    {link: '/images/car.png', name:'car'},
+    {link: '/images/logo.png', name:'logo'},
+    {link: '/images/arrows.png', name:'arrows'}
+  ]
+  imagesLinks.forEach(function (image){
+    let img = new Image()
+    img.src = image.link
+    img.onload = ()=>{
+      counterForLoadedImages ++
+      loadImages[image.name] = img
+    }
+  })
   
 };
 const canvas = document.getElementById('canvas')
