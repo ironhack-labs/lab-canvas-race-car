887ce8ebc5b6b0d3bc5425467bcba0b2a2cf2e85
Fede <f.gomezlara@outlook.es>
2021-10-28 17:40:36 +0200

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
+const canvas = document.getElementById('canvas') //Target canvas
+const ctx = canvas.getContext ('2d') //Make the environment 2d
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const road = new Image(); //Create new image for the road
+    road.src = "/images/road.png";
+
+    road.onload = ()=>{ //Load road
+      ctx.drawImage (road,0,0,500,700)
+    }
+
+  }
 };
