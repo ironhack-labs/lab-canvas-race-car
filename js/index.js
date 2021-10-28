f87c4adfebfec90604f7e524420feea00f4faedf
Aarón López Barros <aaronlopezbarros@MacBook-Pro-de-Ana.local>
2021-10-28 16:58:05 +0200

    Iteración 2 completada

--- js/index.js
+++ js/index.js
@@ -8,8 +8,14 @@ window.onload = () => {
     imgRoad.src = '/images/road.png'
     imgRoad.onload = ()=>{
       ctx.drawImage(imgRoad, 0, 0, 500, 700)
+      const imgCar = new Image()
+      imgCar.src = '/images/car.png'
+      imgCar.onload = ()=>{
+        ctx.drawImage(imgCar, 250, 580, 50, 100)
+      }
     }
   }
+  
 };
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
