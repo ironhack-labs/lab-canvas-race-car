a6dd4d4410d3401e7d03640a3e97c23695cd2c4c
JoseLValdes <jolvaldgo@gmail.com>
2021-10-29 19:35:01 +0200

    Iteration 3

--- js/index.js
+++ js/index.js
@@ -1,81 +1,87 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
-let background = new Image();
+const background = new Image();
 background.src = "../images/road.png";
 
-let imageCar = new Image();
+const imageCar = new Image();
 imageCar.src = "../images/car.png";
 
-class Car{
-  constructor(){
+class Car {
+  constructor() {
     this.x = 225;
     this.y = 610;
     this.speedX = 0;
     this.width = 50;
     this.height = 70;
   }
-
 }
 
 const car = new Car();
 
-document.addEventListener('keydown', (event)=>{
-  if(event.key === "ArrowRight"){
-    car.speedX = 3;  
-    console.log("+1D")
-    console.log(car.speedX);
-  } else if(event.key === "ArrowLeft"){
-    car.speedX = -3;  
-    console.log("+1I")
+
+/*******HELPERS********/
+
+const checkIfInBounds = () => {
+  if (car.x > 450) {
+    car.x = 450;
   }
-})
-
-
-
-document.addEventListener('keyup', (event)=>{
-  if(event.key === "ArrowRight" || event.key === "ArrowLeft"){
-    car.speedX = 0
+  if (car.x < 0) {
+    car.x = 0;
   }
-  if(event.key === "ArrowUp" || event.key === "ArrowDown"){
-    car.speedX = 0
-  }
-})
+};
 
-const updateCar = ()=>{
+const updateCar = () => {
   car.x = car.x + car.speedX;
-  //checkIfInBounds()
-}
+  checkIfInBounds();
+};
 
-const drawCar = ()=>{
-  ctx.drawImage(imageCar, car.x,car.y,car.width,car.height);
-}
-const drawBackGround =()=>{
-  ctx.drawImage(background,0,0,500,700)
-}
+const drawCar = () => {
+  ctx.drawImage(imageCar, car.x, car.y, car.width, car.height);
+};
+const drawBackGround = () => {
+  ctx.drawImage(background, 0, 0, 500, 700);
+};
 
-const clearCanvas = ()=>{
-  ctx.clearRect(100, 610, 100,70)
-}
+const clearCanvas = () => {
+  ctx.clearRect(100, 610, 100, 70);
+};
+
+const updateCanvas = () => {
+  updateCar();
+  clearCanvas();
+  requestAnimationFrame(updateCanvas);
+  drawBackGround();
+  drawCar();
+};
+
+/*******EVENTOS********/
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    car.speedX = 3;
+  } else if (event.key === "ArrowLeft") {
+    car.speedX = -3;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowRight" || event.key === "ArrowLeft") {
+    car.speedX = 0;
+  }
+  if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+    car.speedX = 0;
+  }
+});
 
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
   function startGame() {
-    //ctx.drawImage(background,0,0,500,700);
-    //drawCar();
-    //ctx.drawImage(imageCar, car.x,car.y,car.width,car.height);
+    ctx.drawImage(background, 0, 0, 500, 700);
+    ctx.drawImage(imageCar, car.x, car.y, car.width, car.height);
+    updateCanvas();
   }
 };
-
-const updateCanvas = ()=>{ 
-  clearCanvas()
-  updateCar();
-  drawBackGround();
-  drawCar();
-  requestAnimationFrame(updateCanvas); 
-}
-
-updateCanvas();
