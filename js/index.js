66145c18079abcc4dc915948afe4e72829c881a8
KuraiTsukino <sandym3000@hotmail.com>
2021-11-04 23:37:52 -0600

    res23

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 const $canvas = document.querySelector("canvas");
 const $button = document.querySelector("button");
-const ctx = $canvas.getContext ("2d");
-const board = new Board();
-const p1 = new Character(0,0);
+const ctx = document.getElementById('canvas'). $canvas.getContext ("2d");
 const obstacle = new Obstacle(500, 413);
+const board = new Board();
 
 let frames = 0;
 
@@ -30,39 +29,43 @@ class Board {
   } 
 }
 
-class Character {
+class Character{
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 30;
-    this.height = 50;
-    this.image = new Image();
+    this.x = x
+    this.y = y
+    this.width = 30
+    this.height = 50
+    this.image = new Image()
+    this.image.src = "/images/car.png"
     this.move = 8;
-    this.image.src = "/images/car.png";
+    
   }
-
+  // va a dibujar una imagen
   draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    // su posición es a la derecha
     this.x++;
+    // para que no se salga del margen
     if (this.x > $canvas.width - this.width)
       this.x = $canvas.width - this.width;
     
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
-
+  // métodos de movimientos
   moveLeft() {
     this.x -= this.move;
   }
   moveRigth() {
     this.x += this.move;
   }
-  isTouching(obj){
-    return (
-      this.y < obj.y + obj.height &&
-      this.y + this.height > obj.y;
-      this.x < obj.x + obj.width &&
-      this.x + this.width > obj x)
+  //isTouching(obj){
+  //  return (
+  //    this.y < obj.y + obj.height &&
+   //   this.y + this.height > obj.x;
+   //   this.x < obj.x + obj.width &&
+     // this.x + this.width > obj y)
   }
-}
+// Una instancia, un objeto creado a partir de una clase.
+  const p1 = new Character(500,700);
 
 class Obstacle extends Character {
   constructor(x, y) {
@@ -93,7 +96,7 @@ function checkCollitions() {
     }
   });
 }
-
+// modifica el estado del personaje
 function checkKeys() {
   document.onkeydown = (event) => {
     switch (event.key) {
@@ -112,13 +115,16 @@ function checkKeys() {
 
 function update() {
   frames++;
+  //recalculamos el estado de nuestro programa
   checkKeys();
   generateObstacle();
   
+  //limpia el canvas
   ctx.clearRect(0, 0, $canvas.width, $canvas.height);
 
-  board.draw();
+  //dibuja al personaje
   p1.draw()
+  board.draw();
   drawObstacle();
 
 }
@@ -129,8 +135,6 @@ window.onload = () => {
   };
 
   function startGame() {
-    update();
-  
     setInterval(() => {
       update();
     }, 1000/60);
