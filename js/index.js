0a94a626998971db8c00918db2ea4f7de544877d
Carlos Cordon <carloscordon82@gmail.com>
2021-11-06 14:01:39 -0400

    Solved Lab

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 let selection = document.querySelectorAll("input");
+let scoreLabel = document.querySelector(".score");
+let finalScoreLabel = document.querySelector(".finalScore");
 
 class Sprite {
   constructor(x, y, width, height, image, speed) {
@@ -11,18 +13,14 @@ class Sprite {
   }
 
   draw() {
-    try {
-      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    } catch (error) {}
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 
   loadImage() {
     const newImage = new Image();
     newImage.src = this.image;
-    newImage.addEventListener("load", () => {
-      this.image = newImage;
-      this.draw();
-    });
+    this.image = newImage;
+    this.draw();
   }
 }
 
@@ -46,7 +44,7 @@ class Car extends Sprite {
     }
   }
 
-  stop(e) {
+  stop() {
     this.speed = 0;
   }
 }
@@ -64,14 +62,16 @@ class Obstacle extends Sprite {
 }
 
 let canvas = document.getElementById("canvas");
-
 ctx = canvas.getContext("2d");
+
 const obstacleImages = [
   { link: "./images/obs1.png", width: 235, height: 47 },
   { link: "./images/obs2.png", width: 47, height: 72 },
   { link: "./images/obs3.png", width: 203, height: 55 },
   { link: "./images/obs4.png", width: 224, height: 64 },
 ];
+
+let score = 0;
 const road = new Road();
 const car = new Car();
 let difficulty = 4;
@@ -81,7 +81,6 @@ let globalSpeed = 0;
 const carSpeed = 15;
 const cushion = 10;
 const obstacles = [];
-
 function redraw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   road.draw();
@@ -112,11 +111,7 @@ function createObstacle() {
   if (nextObstacle >= difficulty * 10) {
     nextObstacle = 0;
     let randomObstacle = Math.floor(Math.random() * 4);
-    console.log(
-      obstacleImages[randomObstacle].width,
-      obstacleImages[randomObstacle].height,
-      obstacleImages[randomObstacle].link
-    );
+
     obstacles.push(
       new Obstacle(
         Math.floor(
@@ -134,7 +129,12 @@ function createObstacle() {
 }
 function destroyObstacle() {
   for (i = 0; i < obstacles.length; i++) {
-    if (obstacles[i].y > 800) obstacles.splice(i, 1);
+    if (obstacles[i].y > 575) {
+      obstacles.splice(i, 1);
+      score += 10;
+      scoreLabel.innerHTML = `SCORE: ${score}`;
+      finalScoreLabel.innerHTML = `YOUR FINAL SCORE IS ${score}`;
+    }
   }
 }
 
@@ -149,6 +149,15 @@ function checkCollision() {
       return true;
   }
 }
+
+function drawInitialBoard() {
+  road.loadImage();
+  road.image.onload = function () {
+    road.draw();
+  };
+}
+
+drawInitialBoard();
 
 async function startGame() {
   if (selection[0].checked) {
@@ -170,8 +179,7 @@ async function startGame() {
     car.stop(e);
   });
 
-  await road.loadImage();
-  await car.loadImage();
+  car.loadImage();
 
   let play = setInterval(() => {
     moveCar();
@@ -180,7 +188,10 @@ async function startGame() {
     destroyObstacle();
     if (!checkCollision()) {
       redraw();
-    } else clearInterval(play);
+    } else {
+      clearInterval(play);
+      $("#myModal").modal("show");
+    }
   }, 33);
 }
 
