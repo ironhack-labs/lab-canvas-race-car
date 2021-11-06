10ccbd90bf406539b0158194df1643ef75b51b78
Edgar Uco <edgarin@gmail.com>
2021-11-06 13:17:13 -0500

    Lab14 draw car fixed

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-window.onload = () => {
+ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
-  };
-  function startGame() {
+  }; 
+   function startGame() {
       update();
-  }
+  } 
 
 };
 
 const $canvas = document.querySelector('canvas');
-const $button = document.querySelector('button');
 const ctx = $canvas.getContext('2d');
-
 let frames = 0;
 const walls = [];
 
@@ -42,24 +40,26 @@ class Road {
 
 class Car {
   constructor(x, y) {
-    this.x = 235;
-    this.y = 350;
+    this.x = x;
+    this.y = y;
     this.width = 60;
     this.height = 100;
     this.image = new Image();
-    this.move = 8;
+    this.move = 20;
     this.image.src = "/images/car.png";
   }
   draw() {
-    //this.y--;
-    if (this.y < -this.height) this.y = 0;
-      ctx.drawImage(
-        this.image,
-        this.x,
-        this.y + this.height,
-        this.width,
-        this.height,
-    );
+   if (this.y > $canvas.height - this.height) {
+       this.y = $canvas.height - this.height;
+   }
+   if (this.x > $canvas.width - this.width) {
+       this.x = $canvas.width - this.width;
+   }
+   if (this.x < 0) {
+       this.x = 0;
+   }
+
+   ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
     moveLeft() {
       this.x -= this.move;
@@ -68,58 +68,71 @@ class Car {
       this.x += this.move;
     }
   
+  isTouching(obj) {
+    return (
+        this.x < obj.x + obj.width &&
+        this.x + this.width > obj.x &&
+        this.y < obj.y + obj.height &&
+        this.y + this.height > obj.y
+    );
+  }
+  
 }
 
-class Wall extends Car {
-  constructor(x, y) {
-    super(x, y);
+class Wall {
+  constructor(x, y, width) {
     this.x = x;
     this.y = y;
-    this.width = 100;
-    this.height = 60;
+    this.width = width;
+    this.height = 30;
+    this.image = new Image();
     this.image.src ="/images/brick.png"
   }
   draw() {
-    this.y++;
+    this.y ++;
     ctx.drawImage(this.image,this.x,this.y, this.width,this.height)
   }
-
 }
 
 
 const road = new Road();
-const car = new Car();
-
-function start() {
-    // setInterval(() => {
-    update();
-    // }, 1000 / 60);
-}
+const car = new Car(220, 200);
 
 function update() {
     // 1. calcular o recalcular el estado de nuestro programa
     frames++;
   generateWalls();
-    // 2. Limpiar el canvas
+  checkCollitions();
+  // 2. Limpiar el canvas
     ctx.clearRect(0, 0, $canvas.width, $canvas.height);
     // 3. Dibujar los elementos
-    road.draw();
-    car.draw();
-    drawWalls();
-    checkKeys();
-    requestAnimationFrame(update);
+  road.draw();
+  car.draw();
+  drawWalls();
+  checkKeys();
+/*   gameOver(); */
+  requestAnimationFrame(update);
 }
-
 
 // Funciones de apoyo
 
+function checkCollitions() {
+  walls.forEach((wall) => {
+    if (car.isTouching(wall)) {
+      alert('Game Over');
+          }
+  });
+}
+
 function generateWalls() {
   if (frames % 200 === 0) {
-    const x = Math.floor(Math.random() * 251)+ 50;
-    console.log(x);
-    const wall = new Wall(x, 100);
-    console.log(wall);
-    walls.push(wall);
+    const x = Math.floor(Math.random() * 251) + 50;
+    const width = Math.floor(Math.random() * 251) + 50;
+    if (width < 20) {
+      return width += 20
+    }
+    const wallposition = new Wall(x,0, width);
+    walls.push(wallposition);
   }
 }
 
@@ -129,6 +142,7 @@ function drawWalls() {
 
 function checkKeys() {
   document.onkeydown = (event) => {
+    event.preventDefault(event);
     switch (event.key) {
       case "ArrowLeft":
         car.moveLeft();
@@ -144,5 +158,3 @@ function checkKeys() {
   };
    
 }
-
-$button.onclick = start;
