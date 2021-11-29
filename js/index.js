7d6f0e76c3e1aea6feecc02f87622c7e85c505cd
Vicente Garcia <vicentegarciaayllon@gmail.com>
2021-11-29 20:42:10 +0100

    finish iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const board = new Board(ctx);
+  }
 };
+
