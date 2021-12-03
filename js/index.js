8ad2869611592c060f71e7bcda6c747360d07f54
Vitor Afonso <vittokm@hotmail.com>
2021-12-03 20:30:28 +0000

    stuck in  iteration 4

--- js/index.js
+++ js/index.js
@@ -4,40 +4,103 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    updateCanvas();
+  }
 };
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-class RoadBoard {
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+const width = canvas.width;
+const height = canvas.height;
+
+const img = new Image();
+img.addEventListener('load', ()=>{
+  this.img = img;
+  this.drawBoard();
+});
+img.src = "images/road.png";
+
+
+
+function drawBoard(){
+  context.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+
+
+class PlayersCar {
 
   constructor(){  
 
-    this.col = 0;
-    this.row = 0;
-    
+    this.x = 200;
+    this.y = 500;
+
     const img = new Image();
-    img.addEventListener('load', ()=>{
+    img.addEventListener('load', () => {
       this.img = img;
-      this.draw();
+      this.drawCar();
     });
-    img.src = "../images/road.png";
+    img.src = "images/car.png";
   }
   
-  moveUp(){
-    this.row--;
-  }
-  moveDown(){
-      this.row++;
-  }
   moveRight(){
-      this.col++;
+    if(this.x === 375) {
+      return;
+    }
+    this.x+= 25;
   }
   moveLeft() {
-      this.col--;
+    if(this.x === 50) {
+      return;
+    }
+    this.x-= 25;
+
   }
-  draw(){
-      ctx.drawImage(this.img, this.col, this.row, canvas.width, canvas.height);
+  drawCar(){
+    context.drawImage(this.img, this.x, this.y, 75, 150);
   }
 }
 
-const board = new RoadBoard();
+
+function obstacles (x, y) {
+
+  context.fillStyle = "#F00";
+  context.fillRect(x, y, x, 25);
+
+  
+}
+
+const car = new PlayersCar();
+
+window.addEventListener('keydown', (event) => {
+  // Stop the default behavior (moving the screen to the left/up/right/down)
+  event.preventDefault();
+
+  // React based on the key pressed
+  switch (event.keyCode) {
+    case 37:
+      car.moveLeft(); //left
+      break;
+    case 39:
+      car.moveRight(); //right
+      break;
+  }
+  updateCanvas();
+});
+
+function clearCanvas(){
+  context.clearRect(0,0,width,height);  
+  drawBoard();
+  car.drawCar();
+}
+let speed = 0;
+
+//Could not understand how to make obstacles (iteration 4)...
+function updateCanvas() {
+
+  speed+=5; 
+  clearCanvas();
+  obstacles(0,speed, Math.floor(Math.random() * 8) * 50, 25);
+  requestAnimationFrame(updateCanvas);
+}
