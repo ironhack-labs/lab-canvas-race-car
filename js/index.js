33c32380a7b2a9b06be6b6d988c17e7d360515a9
Vitor Afonso <vittokm@hotmail.com>
2021-12-03 17:34:06 +0000

    added road background to the board

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// jshint esversion:6
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
@@ -5,3 +6,38 @@ window.onload = () => {
 
   function startGame() {}
 };
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+class RoadBoard {
+
+  constructor(){  
+
+    this.col = 0;
+    this.row = 0;
+    
+    const img = new Image();
+    img.addEventListener('load', ()=>{
+      this.img = img;
+      this.draw();
+    });
+    img.src = "../images/road.png";
+  }
+  
+  moveUp(){
+    this.row--;
+  }
+  moveDown(){
+      this.row++;
+  }
+  moveRight(){
+      this.col++;
+  }
+  moveLeft() {
+      this.col--;
+  }
+  draw(){
+      ctx.drawImage(this.img, this.col, this.row, canvas.width, canvas.height);
+  }
+}
+
+const board = new RoadBoard();
