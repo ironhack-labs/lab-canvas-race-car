37b6626ea7486f5140e33f3e6360788902e7cf2c
Michael Foley <foley.p.michael@gmail.com>
2021-12-03 21:35:58 +0000

    added road and car on click of start button.

--- js/index.js
+++ js/index.js
@@ -3,5 +3,63 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  
+  const width = canvas.width;
+  const height = canvas.height;
+
+  function drawBoard () {
+    const boardImg = new Image();
+    boardImg.src = '/images/road.png';
+    boardImg.onload = () => {
+      ctx.drawImage(boardImg, 0, 0, width, height);
+    }
+  }
+
+  function drawCar () {
+    const carImage = new Image();
+    carImage.src = '/images/car.png';
+    carImage.onload = () => {
+      ctx.drawImage(carImage, 225, 525, 50, 100);
+    }
+  }
+
+  class Car {
+    constructor(col, row){
+      this.col = col;
+      this.row = row;
+    }
+    moveLeft(){
+        this.col -= 25;
+        drawCar();
+    }
+    moveRight(){
+        this.col += 25;
+        drawCar();
+    }   
+  }
+
+  const player = new Car(225, 525);
+
+//   document.addEventListener("keydown", (e) => {
+//     switch (e.keyCode) {
+//       case 37:
+//         player.moveLeft();
+//         break;
+//       case 39:
+//         player.moveRight();
+//         break;
+//     } 
+// });
+
+
+  function startGame() {
+    document.getElementById('start-button').onclick = function(){
+      drawBoard ();
+      drawCar ();
+  }
+  }
 };
+
+startGame();
