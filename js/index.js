ff3811da2e581a97844ed560a01c67b52b620d6b
joaonacho <joaonacho@gmail.com>
2021-12-04 18:45:48 +0000

    iteration 3 - done

--- js/index.js
+++ js/index.js
@@ -13,75 +13,84 @@ window.onload = () => {
 //start game funcion - it's called on window.onload
 //should load the background road image & player
 function startGame() {
+  updateAll();
+}
+
+//update all
+function updateAll() {
+  ctx.clearRect(0, 0, width, height);
   background.drawBck();
   player1.drawPlayer();
+  requestAnimationFrame(updateAll);
 }
 
 //background class
 class Background {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
+  constructor() {
+    // this.x = x;
+    // this.y = y;
     this.width = width;
     this.height = height;
     this.bckImg = new Image();
-    this.bckImg.src = "./../images/road.png";
+    this.bckImg.src = "images/road.png";
   }
 
   drawBck() {
-    this.bckImg.onload = () => {
-      ctx.drawImage(this.bckImg, this.x, this.y, this.width, this.height);
-    };
+    ctx.drawImage(this.bckImg, 0, 0, this.width, this.height);
   }
 }
 
-let background = new Background(0, 0, width, height);
+let background = new Background();
 
 //car class
 class Player {
   constructor(x, y, width, height) {
-    this.img = new Image();
-    this.img.src = "./../images/car.png";
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.img = new Image();
+    this.img.src = "./../images/car.png";
   }
 
   drawPlayer() {
-    this.img.onload = () => {
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    };
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
   clearCanvas() {
     ctx.clearRect(0, 0, this.width, this.height);
   }
 
-  //car moves
-  // moveUp() {
-  //   this.clearCanvas();
-  //   this.y--;
-  //   this.drawPlayer();
-  // }
+  // car moves
+  moveRight() {
+    this.clearCanvas();
+    this.x++;
+    this.drawPlayer();
+  }
 
-  // moveRight() {
-  //   this.clearCanvas();
-  //   this.x++;
-  //   this.drawPlayer();
-  // }
+  moveLeft() {
+    this.clearCanvas();
+    this.x--;
+    this.drawPlayer();
+  }
 
-  // moveDown() {
-  //   this.clearCanvas();
-  //   this.y++;
-  //   this.drawPlayer();
-  // }
-
-  // moveLeft() {
-  //   this.clearCanvas();
-  //   this.x--;
-  //   this.drawPlayer();
-  // }
+  move(key) {
+    switch (key) {
+      case "ArrowLeft":
+        // if (this.x > 0) {
+        this.moveLeft();
+        // }
+        break;
+      case "ArrowRight":
+        this.moveRight();
+        break;
+    }
+  }
 }
 
-let player1 = new Player(0, 0, 75, 150);
+let player1 = new Player(205, 500, 75, 150);
+
+//player 1 moves
+document.addEventListener("keydown", (keyboardEvent) => {
+  player1.move(keyboardEvent.key);
+});
