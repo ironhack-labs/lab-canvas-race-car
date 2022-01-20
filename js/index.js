c02be46179f22a8e4f9394528f5833e2ed66c4d7
Laura Del valle <l.delvallebermejo@gmail.com>
2022-01-20 21:25:06 +0100

    car

--- js/index.js
+++ js/index.js
@@ -1,38 +1,21 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+// window.onload = () => {
+ //document.getElementById('start-button').onclick = () => {
+   // startGame();
+  //};
+  
+  //function startGame() {
 
-  function startGame() {
+    drawingRoad.init()
+
+
+
+
     
-    const = gameSize: { w: "500", h: "700" }
-    const = ctx: undefined,
-    init() {
-          this.setContext()
-          this.setSize()
-      },
-    function setContext() {
-          this.ctx = document.querySelector('#myCanvas').getContext('2d')
-          console.log(this.ctx)
-      },
-    function setSize() {
-          this.gameSize = {
-              w: window.innerWidth,
-              h: window.innerHeight
-          }
-          document.querySelector('#myCanvas').setAttribute('width', this.gameSize.w)
-          document.querySelector('#myCanvas').setAttribute('height', this.gameSize.h)
-      },
-    function drawFilledRectangle() {
-          this.ctx.fillStyle = 'red'
-          this.ctx.fillRect(this.gameSize.w / 2 - 50, this.gameSize.h / 2 - 50, 100, 100)
-          this.ctx.fillStyle = 'green'
-          this.ctx.fillRect(this.gameSize.w / 2 - 100, this.gameSize.h / 2 - 25, 200, 50)
-      },
 
 
 
 
 
-  }
-};
+
+  //}
+//};
