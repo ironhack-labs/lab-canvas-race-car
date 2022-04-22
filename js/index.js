a1156153cdd75eedd8bc05230d132d9aa8fb65c5
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-04-22 10:42:52 -0500

    Agregar carro y movimiento

--- js/index.js
+++ js/index.js
@@ -1,43 +1,79 @@
 //CLASSES
-class Background{
-  constructor(){
+class Background {
+  constructor() {
     this.x = 0;
-    this.y= 0;
+    this.y = 0;
     this.width = 500;
     this.height = 700;
     this.image = new Image();
-    this.image.src = "/images/road.png"
+    this.image.src = "/images/road.png";
   }
 
-  draw(){
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+}
+
+class RaceCar {
+  constructor() {
+    this.x = 215;
+    this.y = 550;
+    this.width = 70;
+    this.height = 120;
+    this.image = new Image();
+    this.image.src = "/images/car.png";
+  }
+  
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  move(keyboard) {
+    switch (keyboard) {
+      case 37: //left
+        if (this.x >= 35) {
+          this.x -= 10;
+        }
+        break;
+  
+      case 39: //RIGHT
+        if (this.x <= 395) {
+          this.x += 10;
+        }
+    }
   }
 }
 
-
-
-//VARIABLES NECESSARIAS 
-const canvas =  document.getElementById("canvas");
-const ctx = canvas.getContext("2d")
+//VARIABLES NECESSARIAS
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 let frames = 0;
 const background = new Background();
-
+const raceCar = new RaceCar();
 
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
   function startGame() {
-    requestId =  requestAnimationFrame(updateGame);
-
+    requestId = requestAnimationFrame(updateGame);
   }
 
-  function updateGame(){
+  function updateGame() {
     frames++;
-    ctx.clearRect(0,0, 500, 700);
+    ctx.clearRect(0, 0, 500, 700);
     background.draw();
-    console.log("hello")
-  }
-};
+    raceCar.draw();
 
+    if (requestId){
+      requestAnimationFrame(updateGame);
+    }
+  }
+
+  addEventListener("keydown", (event)=>{
+    raceCar.move(event.keyCode);
+  })
+
+};
