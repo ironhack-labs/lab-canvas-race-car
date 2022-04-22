b4b6b631cfe29974904b5ad8659c1e91c1974bed
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-04-22 10:23:40 -0500

    Add background

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
+//CLASSES
+class Background{
+  constructor(){
+    this.x = 0;
+    this.y= 0;
+    this.width = 500;
+    this.height = 700;
+    this.image = new Image();
+    this.image.src = "/images/road.png"
+  }
+
+  draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+  }
+}
+
+
+
+//VARIABLES NECESSARIAS 
+const canvas =  document.getElementById("canvas");
+const ctx = canvas.getContext("2d")
+let frames = 0;
+const background = new Background();
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    requestId =  requestAnimationFrame(updateGame);
+
+  }
+
+  function updateGame(){
+    frames++;
+    ctx.clearRect(0,0, 500, 700);
+    background.draw();
+    console.log("hello")
+  }
 };
+
