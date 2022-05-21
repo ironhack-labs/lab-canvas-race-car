997a76896e0e826cd01e35ddaf721baa589aea7a
begoczb <begocz.31@gmail.com>
2022-05-21 16:39:41 +0200

    add all iterations

--- js/index.js
+++ js/index.js
@@ -1,116 +1,181 @@
-window.onload = () => {
-  document.getElementById("start-button").onclick = () => {
-    startGame();
-  };
-  const canvas = document.querySelector("#canvas");
-
-  const ctx = canvas.getContext("2d");
-  const backgroundImage = new Image();
-  backgroundImage.src = "./images/road.png";
-  const carImage = new Image();
-  carImage.src = "./images/car.png";
-
-  const obstacleArray = [];
-
-  class Obstacle {
-    constructor() {
-      this.width = Math.floor(Math.random() * 200) + 100;
-      this.height = 20;
-      this.positionX = Math.floor(Math.random() * canvas.width - this.width);
-      this.positionY = 0;
-    }
-
-    draw() {
-      ctx.fillRect(this.positionX, this.positionY, this.width, this.height);
-    }
+class Game {
+  constructor() {
+    this.canvas = null;
+    this.ctx = null;
+    this.road = null;
+    this.car = null;
+    this.obstacles = [];
+    this.moveSpeed = 5;
+    this.intervalId = null;
+    this.frameCount = 0;
+    this.init();
   }
 
-  function moveObstacle() {
-    for (let i = 0; i < obstacleArray.length; i++) {
-      obstacleArray[i].positionY += 10;
-    }
+  init() {
+    this.canvas = document.querySelector("#canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.getElementById("start-button").onclick = () => {
+      this.startGame();
+    };
   }
 
-  console.log(obstacleArray);
-
-  const car = {
-    positionX: canvas.width / 2.2,
-    positionY: canvas.height - 90,
-
-    move(direction) {
-      if (direction === "left" && car.positionX > 0) {
-        car.positionX -= 10;
-      } else if (
-        direction === "right" &&
-        car.positionX < canvas.width - carImage.width / 4
-      ) {
-        car.positionX += 10;
-      }
-    },
-
-    draw() {
-      ctx.drawImage(
-        carImage,
-        car.positionX,
-        car.positionY,
-        carImage.width / 4,
-        carImage.height / 4
-      );
-    },
-  };
-
-  function createObstacle() {
-    obstacleArray.push(new Obstacle());
-  }
-
-  function drawObstacle() {
-    obstacleArray.forEach((element) => element.draw());
-  }
-
-  //Make render function to draw all objects, put inside interval in update, on gameStart() only run update.
-
-  //Make update function that handles all obstacle functions
-  function updateObstacles() {
-    let counter = 0;
-    setInterval(() => {
-      counter++;
-      if (counter % 3 === 0) {
-        createObstacle();
-      } else if (counter % 10 === 0) {
-        drawObstacle();
-      } else if (counter % 15 === 0) {
-        moveObstacle();
-      }
-    }, 1000);
-  }
-
-  function resetCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-  const obstacle1 = new Obstacle();
-
-  function startGame() {
-    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-    updateObstacles();
+  startGame() {
+    this.car = new Car(this.canvas, this.ctx);
+    this.road = new Background(this.canvas, this.ctx, this.moveSpeed);
+    this.drawAll();
 
     window.addEventListener("keydown", (event) => {
       switch (event.code) {
         case "ArrowLeft":
-          car.move("left");
-          // console.log(`Moving left!!`);
+          this.car.move("left");
+          console.log(`Moving left!!`);
           break;
         case "ArrowRight":
-          car.move("right");
-          // console.log(`Moving right!!`);
+          this.car.move("right");
+          console.log(`Moving right!!`);
 
           break;
       }
     });
-
-    setInterval(() => {
-      resetCanvas();
-      ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-      car.draw();
-    }, 1000 / 60);
   }
-};
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  drawAll() {
+    if (this.frameCount % 60 === 0) {
+      this.obstacles.push(new Obstacle(this.canvas, this.ctx, this.moveSpeed));
+    }
+    this.clear();
+    this.road.draw();
+    this.road.move();
+    this.car.draw();
+
+    for (let i = 0; i < this.obstacles.length; i++) {
+      this.obstacles[i].draw();
+      this.obstacles[i].move();
+      if (this.collisionCheck(this.obstacles[i])) {
+        cancelAnimationFrame(this.intervalId);
+        return;
+      }
+    }
+
+    this.frameCount++;
+    this.intervalId = requestAnimationFrame(() => this.drawAll());
+  }
+
+  collisionCheck(obstacle) {
+    const carFront = this.car.y;
+    const carRear = carFront + this.car.height;
+    const carLeft = this.car.x;
+    const carRight = carLeft + this.car.width;
+    const withinX =
+      obstacle.x + obstacle.width > carLeft && obstacle.x < carRight;
+    const withinY = obstacle.y > carFront && obstacle.y < carRear;
+
+    return withinX && withinY;
+  }
+}
+const newGame = new Game();
+
+class Car {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.image = new Image();
+    this.width = 100 / 2;
+    this.height = 200 / 2;
+    this.x = (this.canvas.width - this.width) / 2;
+    this.y = this.canvas.height - this.height - 50;
+    this.init();
+  }
+
+  init() {
+    this.image.src = "./images/car.png";
+    this.draw();
+  }
+
+  draw() {
+    // console.log(this.image);
+    this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  move(direction) {
+    if (direction === "left" && this.x > 20) {
+      this.x -= 15;
+    } else if (
+      direction === "right" &&
+      this.x < this.canvas.width - this.width / 4 - 60
+    ) {
+      this.x += 15;
+    }
+  }
+}
+
+class Background {
+  constructor(canvas, ctx, moveSpeed) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.image = new Image();
+    this.y = 0;
+    this.moveSpeed = moveSpeed;
+    this.init();
+  }
+
+  init() {
+    this.image.src = "./images/road.png";
+    this.draw();
+  }
+  draw() {
+    this.ctx.drawImage(this.image, 0, this.y, canvas.width, canvas.height);
+
+    this.ctx.drawImage(
+      this.image,
+      0,
+      this.y - this.canvas.height,
+      canvas.width,
+      canvas.height
+    );
+  }
+
+  move() {
+    this.y += this.moveSpeed;
+    if (this.y >= this.canvas.height) {
+      this.y = 0;
+    }
+  }
+}
+
+class Obstacle {
+  constructor(canvas, ctx, moveSpeed) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.width = Math.floor((Math.random() * this.canvas.width) / 2 - 20) + 40;
+    this.height = 15;
+    this.x = Math.floor((Math.random() * this.canvas.width) / 2) + 20;
+    this.y = 0;
+    this.moveSpeed = moveSpeed;
+  }
+
+  draw() {
+    //Tried different solid color for every obstacle, but the color changes every time the obstacle is redrawn
+    //uncomment at your own risk!!
+    // this.ctx.fillStyle = `rgb(
+    //   ${Math.floor(Math.random() * 255)},
+    //   ${Math.floor(Math.random() * 255)},
+    //   0)`;
+    this.ctx.fillStyle = "orange";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    this.y += this.moveSpeed;
+  }
+}
+
+// console.log(obstacleArray);
+
+//Make render function to draw all objects, put inside interval in update, on gameStart() only run update.
+
+//Make update function that handles all obstacle functions
