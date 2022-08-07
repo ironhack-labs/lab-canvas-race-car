c52500cbd51e1c79c8d3381dce2d8e5e55b16687
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-07 18:14:16 -0300

    incomplete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+const canvas = document.getElementById('canvas'); 
+const ctx = canvas.getContext('2d');
+
+const roadImg = new Image();
+roadImg.src = './images/road.png';
+
+const imgCar = new Image();
+imgCar.src = './images/car.png';
+
+function startGame() {
+  ctx.drawImage(roadImg, 0, 0, 350, 550);
+  ctx.drawImage(imgCar, 150, 420, 50, 100);
+}; 
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
-  };
+  }};
 
-  function startGame() {}
-};
+
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
