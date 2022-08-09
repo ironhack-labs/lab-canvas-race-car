cfb1dec556f8fd7528347f8bfb1aac2a94e562dd
Hugo Lomba <hmlomba@gmail.com>
2022-08-09 00:21:56 -0300

    done

--- js/index.js
+++ js/index.js
@@ -10,7 +10,27 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 carImage = new Image();
 carImage.src = "./images/car.png";
-// const carImg = (carImg.src = "./images/car.png");
+let frames = 0;
+const myObstacles = [];
+
+function updateObstacles() {
+  frames += 1;
+  if (frames % 120 === 0) {
+    let x = canvas.width;
+    let minHeight = 20;
+    let maxHeight = 200;
+    let height = Math.floor(
+      Math.random() * (maxHeight - minHeight + 1) + minHeight
+    );
+    let minGap = 50;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    myObstacles.push(new Component(10, height, "green", x, 0));
+    myObstacles.push(
+      new Component(10, x - height - gap, "green", x, height + gap)
+    );
+  }
+}
 
 // CLASSE DO CARRO
 
@@ -41,6 +61,51 @@ class CarObject {
     ctx.clearRect(car.x, car.y, car.width, car.heigth);
   }
 }
+
+class Component {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.speedX = 0;
+    this.speedY = 0;
+  }
+  draw() {
+    const ctx = myGameArea.ctx;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+
+  crashWith(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
+}
+// class Obstacle {
+//   constuctor ()
+// }
 
 // FIM DAS DECLARAÇÕES DE CLASSE
 
