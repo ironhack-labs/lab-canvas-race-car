36823bde178b875a388225dc801f8895abae78cb
jdlms <lamorey.joshua@gmail.com>
2022-08-14 12:06:29 +0200

    sunday commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+let bg;
+let player;
 
-  function startGame() {}
-};
+function preload() {
+  player = loadImage("images/car.png");
+}
+
+function setup() {
+  const canvas = createCanvas(600, 650);
+  canvas.parent("game-board");
+  bg = loadImage("images/road.png");
+}
+
+function draw() {
+  background(bg);
+  image(player, 5, 5);
+}
