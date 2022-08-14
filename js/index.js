edd8596ef683e67521d4f88dceb09da0afcd6a3c
jdlms <97018563+jdlms@users.noreply.github.com>
2022-08-14 16:12:56 +0200

    second sunday commit

--- js/index.js
+++ js/index.js
@@ -9,9 +9,42 @@ function setup() {
   const canvas = createCanvas(600, 650);
   canvas.parent("game-board");
   bg = loadImage("images/road.png");
+  player.resize(40, 80);
+  speed = 4;
 }
+
+//keys
+const A_Key = 65;
+const D_Key = 68;
+const initialX = 350;
+const initialY = 550;
+let x = initialX;
+let y = initialY;
 
 function draw() {
   background(bg);
-  image(player, 5, 5);
+
+  //initial coords, x&y, boundries
+
+  // let leftWall = 54;
+  // let rightWall = 518;
+
+  // let xc = constrain(350, leftWall, rightWall);
+
+  image(player, x, y);
+
+  if (keyIsDown(A_Key)) {
+    x -= speed;
+    console.log(x);
+  }
+  if (keyIsDown(D_Key)) {
+    x += speed;
+    console.log(x);
+  }
 }
+
+//a keycode = 65
+//b keycode = 68
+
+//left wall = 54
+// right wall =518
