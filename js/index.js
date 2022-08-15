05445bf387ab059fd87928bf1f11399e5f9a2e23
jdlms <97018563+jdlms@users.noreply.github.com>
2022-08-15 20:28:42 +0200

    monday night push

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 let bg;
 let player;
+let obstacles = [];
+let obstacle;
+
+let gravity = 180.0;
 
 function preload() {
   player = loadImage("images/car.png");
@@ -13,7 +17,7 @@ function setup() {
   speed = 4;
 }
 
-//keys
+//keys, initial coords, x&y, boundries, all kinds of things!
 const A_Key = 65;
 const D_Key = 68;
 const initialX = 350;
@@ -21,25 +25,53 @@ const initialY = 550;
 let x = initialX;
 let y = initialY;
 
+let leftWall = 54;
+let rightWall = 516;
+
+let leftRectX = 0;
+let leftRectY = 20;
+
 function draw() {
   background(bg);
-
-  //initial coords, x&y, boundries
-
-  // let leftWall = 54;
-  // let rightWall = 518;
-
-  // let xc = constrain(350, leftWall, rightWall);
-
   image(player, x, y);
+
+  const obstaclesOn = true;
+
+  noStroke();
+  fill("brown");
+
+  let leftRectLength = random(200, 250);
+  if (obstaclesOn) {
+    leftRectY = leftRectY + 1;
+  }
+
+  if (leftRectY > 660) {
+    leftRectY = 20;
+    leftRectLength = random(200, 250);
+  }
+
+  let leftRect = rect(leftRectX, leftRectY, leftRectLength, 40);
 
   if (keyIsDown(A_Key)) {
     x -= speed;
-    console.log(x);
+    if (x < leftWall) {
+      x = leftWall;
+    }
   }
   if (keyIsDown(D_Key)) {
     x += speed;
-    console.log(x);
+    if (x > rightWall) {
+      x = rightWall;
+    }
+  }
+}
+
+class Obstacle {
+  constructor() {
+    if (obstaclesOn) {
+      this.width = 5 + random(40);
+      this.height = 10 + random(40);
+    }
   }
 }
 
@@ -47,4 +79,4 @@ function draw() {
 //b keycode = 68
 
 //left wall = 54
-// right wall =518
+// right wall =516
