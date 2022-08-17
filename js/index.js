6cd7ca62bbe8f8a28ac275933da0c342165dd150
Walison Abram <abramwhas@gmail.com>
2022-08-17 20:15:27 -0300

    incomplete lab

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
-
-  function startGame() {}
+  
 };
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+function startGame() {
+  const roadImg = new Image();
+    roadImg.src = './images/road.png';
+    roadImg.onload = function() {
+    ctx.drawImage(roadImg, 0, 0, 500, 700);
+  }
+  car.start()
+}
+const car = {
+  start: function () {
+    const carImg = new Image();
+    carImg.src = './images/car.png';
+    carImg.onload = function() {
+      ctx.drawImage(carImg, 225, 600, 50, 100);
+    }
+  }
+}
+
