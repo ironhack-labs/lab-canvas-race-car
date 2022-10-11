f365380ed1f45e11a449ef54be0478922fc20961
Arcadio REYES <arcadioreyesph@gmail.com>
2022-10-11 18:28:16 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,50 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
   function startGame() {}
 };
+
+// The Road
+
+const img = new Image();
+img.src = "images/road.png";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const backgroundImage = {
+  img: img,
+  y: 0,
+  speed: -1,
+
+  move: function () {
+    this.y += this.speed;
+    this.y %= canvas.height;
+  },
+
+  draw: function () {
+    ctx.drawImage(this.img, 0, this.y);
+
+    if (this.speed < 0) {
+      ctx.drawImage(this.img, 0, this.y + this.img.height);
+    } else {
+      ctx.drawImage(this.img, 0, this.y - canvas.height);
+    }
+  },
+};
+
+function updateCanvas() {
+  backgroundImage.move();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImage.draw();
+
+  requestAnimationFrame(updateCanvas);
+}
+
+// start calling updateCanvas once the image is loaded
+img.onload = updateCanvas;
+
+// The Car
