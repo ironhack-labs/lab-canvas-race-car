157d0ba6300920d3ddfd5ab9c27476cad65f64e9
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-11-19 15:45:05 +0100

    iteration #2 completed

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ const startGame = () => {
   const board = new Image();
   board.src = "../images/road.png";
   board.onload = () => ctx.drawImage(board, 0, 0);
+
+  const car = new Image();
+  car.src = "../images/car.png"
+  const carScale = 319/158;
+  car.onload = () => ctx.drawImage(car,282/2 - 25, 420 - 50*carScale, 50, 50*carScale)
 };
 
 window.onload = () => {
