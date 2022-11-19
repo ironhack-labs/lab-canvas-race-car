f09bc557ae46d554d0973e1e984375aa27d40543
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-11-19 15:31:32 +0100

    iteration #1 completed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const startGame = () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
 
-  function startGame() {}
+  const board = new Image();
+  board.src = "../images/road.png";
+  board.onload = () => ctx.drawImage(board, 0, 0);
+};
+
+window.onload = () => {
+  document.getElementById("start-button").onclick = () => startGame();
 };
