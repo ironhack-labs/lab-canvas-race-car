69111d873c859472c794d9caaa648c757f15e9c5
Thor Löfgren <lofgrenthor@gmail.com>
2022-11-20 11:11:13 +0100

    Complete Iteration 1 & 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,52 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+class GameArea {
+  constructor() {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvasWidth = this.canvas.width;
+    this.canvasHeight = this.canvas.height;
+    this.frames = 0;
+  }
 
-  function startGame() {}
+  startGame() {
+    const roadImg = new Image();
+    roadImg.src = "../images/road.png";
+    this.ctx.drawImage(roadImg, 0, 0, this.canvasWidth, this.canvasHeight);
+
+    drawGameArea();
+  }
+}
+const gameArea = new GameArea();
+
+class GameObject {
+  constructor(xPos, yPos, width, height) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Car extends GameObject {
+  constructor(xPos, yPos, width, height) {
+    super(xPos, yPos, width, height);
+  }
+
+  update() {
+    const ctx = gameArea.ctx;
+    const carImg = new Image();
+    carImg.src = "../images/car.png";
+    ctx.drawImage(carImg, this.xPos, this.yPos, this.width, this.height);
+  }
+}
+
+const car = new Car(210, 610, 50, 70);
+
+function drawGameArea() {
+  car.update();
+}
+
+window.onload = () => {
+  document.getElementById("start-button").onclick = () => {
+    gameArea.startGame();
+  };
 };
