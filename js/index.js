1d743424bf23be40b075c46a3e83a8e4f4f4c534
Nicole <92593875+ozzleme@users.noreply.github.com>
2022-11-20 21:46:44 +0530

    iteration 1 and 2 - without constructor

--- js/index.js
+++ js/index.js
@@ -5,12 +5,29 @@ window.onload = () => {
 
   function startGame() {
     // create the canvas and display the road , image in images foler, canvas HTML is in starter code  
-    const canvas = document.getElementById('canvas');
+   const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    const img = new Image(); // create new image element 
-    img.onload = function() {
-    ctx.drawImage(img, 10, 70, 500, 700)
+    const background = new Image(); // make image
+    const car = new Image(); // make image
+    const images = [background, car]; // key 
+    const count = images.length; 
+    background.onload = car.onload = counter;
+    background.src = '/images/road.png';
+    car.src = '/images/car.png';
+    background.onload = function() {
+    ctx.drawImage(background, 10, 70, 500, 700)
     }
-    img.src = '/images/road.png'
+    let carX = 230;
+    let carY = 620;
+    car.onload = function() {
+      ctx.drawImage(car, carX, carY, 50, 75)
+    }
+    function counter() {
+      count--;
+      if (count === 0) drawImages();
+  } 
+
 };
 }
+
+
