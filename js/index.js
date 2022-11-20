60acc384655750ac99a243b5f44ce61edc843baa
Nicole <92593875+ozzleme@users.noreply.github.com>
2022-11-20 08:59:50 +0530

    iteration 1

--- js/index.js
+++ js/index.js
@@ -3,5 +3,14 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    // create the canvas and display the road , image in images foler, canvas HTML is in starter code  
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image(); // create new image element 
+    img.onload = function() {
+    ctx.drawImage(img, 10, 70, 500, 700)
+    }
+    img.src = '/images/road.png'
 };
+}
