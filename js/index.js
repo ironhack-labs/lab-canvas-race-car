834b438877de8ea50cd44a857127fd1fe314d3e6
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2022-11-22 16:34:32 +0100

    Iteration 1 attempt

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+    console.log('Started!')
   };
+}
 
-  function startGame() {}
-};
+  function startGame() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    const background = new Image();
+    background.src = '../images/road.png';
+    background.onload = function(){
+      ctx.drawImage(background, 500, 700, 0, 0);
+    };
+  }
+
+
