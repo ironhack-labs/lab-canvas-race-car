7b1cb78c8ea13c709a4b59124f2eed8270342c1d
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2022-11-25 19:05:40 +0100

    still..

--- js/game.js
+++ js/game.js
@@ -2,7 +2,9 @@ class Game {
 	constructor(canvasId) {
 		this.canvas = document.getElementById(canvasId);
 		this.ctx = this.canvas.getContext("2d");
+        this.player = new Player(this.ctx, this.canvas.width / 2, this.canvas.height - 100);
         this.bg = new Background(this.ctx);
+
     }
 
     start (){
