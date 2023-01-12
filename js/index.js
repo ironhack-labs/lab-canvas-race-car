487a8fe61ece1bd91b79bc410b12123619fb9491
Johannes Chorzempa <jchorzempa@posteo.org>
2023-01-12 21:12:38 +0100

    Add image on button click

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ window.onload = () => {
 	const canvas = document.getElementById('canvas');
 	const ctx = canvas.getContext('2d');
 
-	let img = new Image();
-	img.src = 'images/road.png';
+	function startGame() {
+		let img = new Image();
+		img.src = 'images/road.png';
 
-	img.onload = () => {
-		ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-	};
-
-	function startGame() {}
+		img.onload = () => {
+			ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+		};
+	}
 };
