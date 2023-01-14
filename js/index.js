6d9fbde05651d5975a16a41d2ef4a7de7c190a62
michael07200 <mickmhtong@gmail.com>
2023-01-14 11:37:50 +0100

    build: Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const backgroundImg = new Image(); // Create new <img> element
+    backgroundImg.src = "images/road.png"; // Set source path
+    backgroundImg.onload = () => {
+      ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+    };
+  }
 };
