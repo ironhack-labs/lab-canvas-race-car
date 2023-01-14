5665462d69f14110da79ba53a77ee22fb2e7a16a
michael07200 <mickmhtong@gmail.com>
2023-01-14 11:58:00 +0100

    build: Iteration 2

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+class Player {
+  constructor() {
+    this.width = 100;
+    this.height = 200;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.image = new Image();
+    this.image.src = "images/car.png";
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+  update() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
+
+const player = new Player();
+
+function startGame() {
+  const backgroundImg = new Image();
+  backgroundImg.src = "images/road.png";
+  backgroundImg.onload = () => {
+    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+    player.draw();
+  };
+}
+
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
-
-  function startGame() {
-    const backgroundImg = new Image(); // Create new <img> element
-    backgroundImg.src = "images/road.png"; // Set source path
-    backgroundImg.onload = () => {
-      ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
-    };
-  }
 };
