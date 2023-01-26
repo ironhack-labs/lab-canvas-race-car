0a8e25b687bb522443979bd91ccfe809a42f917a
Tomás Belmar <tomasbelmar@MacBook-Air-de-Tomas.local>
2023-01-26 21:37:29 +0000

    lab-race-car

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+/**  @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+  const background = new Image();
+  background.src = "/images/road.png";
+
+  const car = new Image();
+  car.src = "/images/car.png";
+
+  background.onload = function () {
+  ctx.drawImage(background, 0, 0, 500, 700);
+};
+  car.onload = function () {
+  ctx.drawImage(car, 225, 300, 50, 100);
+};
+  }
 };
