9ca72c6d94cb998c9588bdc37d0192ae3c457a4e
Tiago Rato <tiagorato13@hotmail.com>
2023-01-26 21:13:16 +0000

    submit lab finished, extra bonuses =)

---
+++ js/component.js
@@ -0,0 +1,90 @@
+/** @type {HTMLCanvasElement} */
+
+class Component{
+    constructor(x, y, w, h, color, ctx) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.ctx = ctx;
+        this.speedX = 0;
+        this.speedY = 0;
+
+        this.img = new Image();
+        this.img.src = '/images/car.png'
+    }
+
+    draw() {
+        this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    }
+
+    newPosition() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    top() {
+        return this.y;
+    }
+
+    bottom() {
+        return this.y + this.h
+    }
+
+    left() {
+        return this.x;
+    }
+
+    right() {
+        return this.x + this.w;
+    }
+
+    crashWith(enemy){
+        return !(
+            this.bottom() < enemy.top() ||
+            this.top() > enemy.bottom() ||
+            this.right() < enemy.left() ||
+            this.left() > enemy.right()
+        )
+    }
+};
+
+class Enemy {
+    constructor(x, y, w, h, color, ctx) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.ctx = ctx;
+        this.speedX = 0;
+        this.speedY = 0;
+
+        this.img = new Image();
+        this.img.src = '/images/logs.png';
+    }
+
+    draw() {
+        this.ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    }
+
+    newPosition() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    top() {
+        return this.y
+    }
+    bottom() {
+        return this.y + this.h
+    }
+    left() {
+        return this.x
+    }
+    right() {
+        return this.x + this.w
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+/** @type {HTMLCanvasElement} */
 
-  function startGame() {}
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+const startButton = document.getElementById('start-button');
+
+//Creating the player
+const player = new Component(220, 550, 50, 100, 'Image', ctx);
+
+startButton.onclick = function(){
+    const game = new Game(ctx, 500, 700, player);
+    game.start();
 };
+
+document.addEventListener('keydown', (e) => {
+    switch(e.code){
+        case 'ArrowLeft':
+            player.speedX -= 2;
+            break;
+        case 'ArrowRight':
+            player.speedX += 2;
+            break;
+    }
+});
+
+document.addEventListener('keyup', () => {
+    player.speedX = 0;
+    player.speedY = 0;
+});
