c4d738f308893f3110ace1d57dfab83e02b90511
DuarteFranca <duartefranca_@outlook.com>
2023-01-26 22:08:57 +0000

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+/** @type {HTMLCanvasElement} */
 
-  function startGame() {}
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+
+const startButton = document.getElementById('start-button');
+
+const player = new Component(220, 530, 70, 150, 'Image', ctx);
+
+startButton.onclick = function(){
+    const game = new Game(ctx, 500, 700, player);
+    game.start();
 };
+
+document.addEventListener('keydown', (e) => {
+    switch(e.code){
+        case 'ArrowLeft':
+            player.speedX -= 2;
+            break;
+        case 'ArrowRight':
+            player.speedX += 2;
+            break;
+    }
+});
+
+document.addEventListener('keyup', () => {
+    player.speedX = 0;
+    player.speedY = 0;
+});
