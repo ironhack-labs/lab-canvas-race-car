a5adcfb9f4a8b36fd78a14d5f4898c78656fef79
Vanessa Vieira <vanessafvieira22@gmail.com>
2023-01-26 19:54:02 +0000

    lab

--- js/game.js
+++ js/game.js
@@ -39,19 +39,19 @@ class Game {
         }
         if (this.frames % 180 === 0) {
           let x = 1200;
-          //calculate the height of the columns/enemies
+          
           let minWidth = 10;
           let maxWidth = 200;
           let height = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minHeight);
-          //these variables control the size of the gap between obstacles
+         
           let minGap = 75;
           let maxGap = 200;
-          //this creates the gap
+       
           let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-          //add the obstacles to the array
-          //top obstacle
+        
+    
           this.obstacles.push(new Component(y, 0, 50, this.width, 'green', this.ctx));
-          //bottom obstacle
+    
           this.obstacles.push(new Component(y, this.width + gap, 50, x - height - gap, 'blue', this.ctx));
         }
       }
