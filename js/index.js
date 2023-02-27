01d6b4c750dac4bfe948a8caf4af865be86e5426
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-27 19:53:48 +0100

    Added Canvas & Declared background source

--- js/index.js
+++ js/index.js
@@ -3,5 +3,12 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let obstacleArray = [];
+      const canvas = document.getElementById('canvas');
+      const ctx = canvas.getContext('2d');
+      
+      const canvasBg = new Image();
+      canvasBg.src = "../images/road.png";
+  }
 };
