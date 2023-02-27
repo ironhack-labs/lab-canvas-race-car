0c5030aed7d4d4e54a121c479f28d700ad01ca5c
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-27 19:55:40 +0100

    Added Car class with properties and moveRight method

--- js/index.js
+++ js/index.js
@@ -23,5 +23,24 @@ window.onload = () => {
           ctx.drawImage(this.img, 0, 0);
         }
       }
+
+      class Car {
+        //Sets the properties for the player
+        constructor(x,y){
+          this.x = x;
+          this.y = y;
+          this.img = '../images/car.png';
+          this.score = 0;
+        }
+        //Moves the car to the right & checks if car is inside the Game
+        moveRight(){
+          const car = new Image();
+          car.src = this.img;
+          this.x += 20;
+          ctx.clearRect(0,0, canvas.width, canvas.height);
+          raceGame.loadBackground();
+          this.checkPosition()
+        }
+      }
   }
 };
