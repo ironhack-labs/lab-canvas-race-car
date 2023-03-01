ddc5477f96bcf5b6fe582f2d929c7619072e3272
Mauricio Benavente <mauriciobenavente97@icloud.com>
2023-03-01 18:08:26 +0100

    add iteration 1 and 2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,26 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+} 
 
-  function startGame() {}
-};
+
+ 
+
+  function startGame() {
+
+    let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let roadImage = document.createElement("img")
+roadImage.src= "images/road.png";
+ctx.drawImage(roadImage, 0, 0, canvas.width, canvas.height);
+let carImage = document.createElement("img")
+    carImage.src = "images/car.png";
+    ctx.drawImage(carImage, 215, 500, 60, 110 );
+
+ }
+
+
+
+
+
+
