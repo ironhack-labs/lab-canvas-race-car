27f1c64b1cefbead1d0682986bef15c8810d2477
Yo-Jia Chen <donna880815@gmail.com>
2023-03-02 21:35:42 +0100

    add background and a movable car

--- js/index.js
+++ js/index.js
@@ -5,3 +5,31 @@ window.onload = () => {
 
   function startGame() {}
 };
+let road
+let car
+
+function preload() {
+  road = loadImage('/images/road.png');
+  car = loadImage('/images/car.png');
+}
+
+function setup() {
+  createCanvas(282, 600);
+  image(road, 0, 0);
+}
+
+let x = 0
+function draw(){
+  image(road, 0, 0);
+
+  image(car, 150 + x , 350, 50, 80)
+}
+function keyPressed(){
+  if (keyCode === LEFT_ARROW){
+    x = x - 5;
+    console.log("left")
+  }else if(keyCode === RIGHT_ARROW){
+    x = x + 5;
+    console.log("right")
+  }
+}
