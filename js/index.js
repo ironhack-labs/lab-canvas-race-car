eed0268165d1bb22e8644b5a549273824233b48d
Lena Imdahl <mail@lenaimdahl.de>
2023-03-03 17:52:54 +0100

    added Iteration 4

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
-function startGame() {}
-
 //Iteration 1, 2
 let img;
 let car;
 let carX = 0;
-
 const initialX = 200;
+//obstacles
+let oneRectX = 70;
+let oneRectY = -50;
+
+let twoRectX = 310;
+let twoRectY = -250;
+
+let threeRectX = 250;
+let threeRectY = 100;
+
+let fourRectX = 100;
+let fourRectY = 250;
 
 function preload() {
   img = loadImage("../images/road.png");
@@ -21,12 +30,37 @@ function draw() {
   image(img, 0, 0, 482, 741);
 
   //adding constrains
-
   let leftWall = 55;
   let rightWall = 400;
 
   const roadLimits = constrain(carX, leftWall, rightWall);
   image(car, roadLimits, 550, 50, 100);
+
+  // rectangle
+  fill(color("#cf2558"));
+  rect(oneRectX, oneRectY, 200, 20);
+  rect(twoRectX, twoRectY, 100, 20);
+  rect(threeRectX, threeRectY, 150, 20);
+  rect(fourRectX, fourRectY, 100, 20);
+  //animates the shapes in a straight line
+  oneRectY++;
+  twoRectY++;
+  threeRectY++;
+  fourRectY++;
+
+  //moves the shapes back when they move off the canvas
+  if (oneRectY > 700) {
+    oneRectY = -50;
+  }
+  if (twoRectY > 700) {
+    twoRectY = -50;
+  }
+  if (threeRectY > 700) {
+    threeRectY = -50;
+  }
+  if (fourRectY > 700) {
+    fourRectY = -50;
+  }
 }
 //Iteration 3
 function keyPressed() {
