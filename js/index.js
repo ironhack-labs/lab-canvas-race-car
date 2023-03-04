aca633091c2ae9798b4a6e77c5f8d369e38967a3
“Zuzana <zuzana@getyourguide.com>
2023-03-04 18:04:18 +0100

    started

--- js/index.js
+++ js/index.js
@@ -3,5 +3,30 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+  }
+
+
 };
+
+
+let road;
+let car;
+
+
+function preload() {
+  road = loadImage('images/road.png');
+  car = loadImage('images/car.png');
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+}
+
+function draw() {
+  image(road, 0, 0, 282, 600);
+  image(car, 115 , 370, 50, 80)
+
+}
+
