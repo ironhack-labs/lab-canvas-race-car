0c7bef9c60fb44b87e318d2c7913077c7e063265
RonnySk <ronny_sk@hotmail.com>
2023-03-04 16:43:41 +0100

    Add RoadImg, CarImg, and New obstacles.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,89 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+// window.onload = () => {
+//     document.getElementById('start-button').onclick = () => {
+//         startGame();
+//     };
+//     function startGame() {}
+  
+//   };
 
-  function startGame() {}
-};
+let imgRoad;
+let imgCar;
+let obstacle;
+let car;
+
+
+function preload(){
+    imgRoad = loadImage('/images/road.png');
+    imgCar = loadImage('/images/car.png');
+    
+}
+
+function setup(){
+    createCanvas(500,690);
+    obstacle = new Obstacles();
+    car = new Car() 
+}
+
+
+class Car {
+    constructor() {
+        this.x = 218;
+        this.y = 570;
+        this.w = 60;
+        this.h = 90
+    }
+
+    draw(){
+   
+        image(imgCar, this.x, this.y,this.w ,this.h,0 ,0);
+    }
+    
+    moveRight() {
+        this.x += 15;
+    }
+    
+    moveLeft() {
+        this.x -= 15;
+    }
+    
+}
+
+function keyPressed(){
+    if (keyCode === LEFT_ARROW) {
+        car.moveLeft()
+    } 
+    
+    if (keyCode === RIGHT_ARROW) {
+        car.moveRight()
+    }
+}
+
+function draw(){
+    image(imgRoad,0,0,500,690);
+    // image(imgCar, carX, carY,carW,carH,0,0)
+    car.draw();
+    
+    obstacle.draw();
+
+    collisionDetection(car, obstacle)
+   
+}
+
+
+ 
+function collisionDetection(rect1, rect2) {
+
+  if (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.h + rect1.y > rect2.y
+  ) {
+    console.log(alert("Game Over"));
+
+    // let gameOverH1 = document.createElement("h1");
+    // gameOverH1.innerHMTL = "Game Over"
+  } 
+}
+
+ 
---
+++ js/obstacles.js
@@ -0,0 +1,19 @@
+class Obstacles {
+    constructor() {
+        this.x = Math.random() * (350 - 65) + 65;
+        this.y = 0;
+        this.w = Math.random() * (180 - 65) + 65;
+        this.h = 55;
+    }
+    
+    draw(){
+        fill('#990000')
+        rect(this.x, this.y, this.w, this.h)
+        this.y ++;
+    }
+}
+
+
+
+
+
