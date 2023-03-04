5f1c4968e8dcedcca3bde1203c1bc9314ca70d3f
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-03-04 12:50:39 +0530

    solved-lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,119 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
-
-  function startGame() {}
-};
+    document.getElementById('start-button').onclick = () => {
+      startGame();
+    };
+  
+    function startGame() {
+      let canvas = document.getElementById("canvas");
+      let ctx = canvas.getContext("2d");
+  
+      let roadImage = document.createElement("img");
+      roadImage.src = "images/road.png";
+  
+      const playerCar = new Image();
+      playerCar.src = 'images/car.png';
+      const carWidth = 50;
+      const carHeight = 120;
+      let carX = (canvas.width - carWidth) / 2;
+      let carY = canvas.height - carHeight - 10;
+  
+      const obstacles = [];
+      const obstacleWidth = 25;
+      const obstacleHeight = 60;
+      const obstacleSpeed = 5;
+  
+      let score=0;
+      
+  
+      function createObstacle() {
+        const x = Math.floor(Math.random() * (canvas.width - obstacleWidth));
+        const obstacle = { x, y:0, width: obstacleWidth, height: obstacleHeight };
+        obstacles.push(obstacle);
+      }
+  
+      setInterval(createObstacle, 1000);
+  
+      // Update and redraw the road images, car and obstacles on every frame
+      function updateGame() {
+  
+  
+        // Draw the first road image at its current position
+        ctx.drawImage(roadImage, 0, roadY1, canvas.width, canvas.height);
+  
+        // Draw the second road image above the first one
+        ctx.drawImage(roadImage, 0, roadY2, canvas.width, canvas.height);
+  
+        // Draw the car at its current position
+        ctx.drawImage(playerCar, carX, carY, carWidth, carHeight);
+  
+         // Draw and update the obstacle
+        for (let i = 0; i < obstacles.length; i++) {
+          const obstacle = obstacles[i];
+          obstacle.y += obstacleSpeed;
+          
+          ctx.fillStyle = "red";
+          ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+  
+          // Check for collision with the car
+          if (carX < obstacle.x + obstacle.width &&
+              carX + carWidth > obstacle.x &&
+              carY < obstacle.y + obstacle.height &&
+              carY + carHeight > obstacle.y) {
+            
+                // GO alert
+            alert(`Game over! Your score is ${score}`);
+        
+          }
+  
+          // Check if the obstacle has moved out of the canvas and remove it
+          if (obstacle.y > canvas.height) {
+            obstacles.splice(i, 1);
+            score++;
+          }
+        }
+  
+        // Draw score
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText(`Score: ${score}`, 10, 50);
+  
+        // Update the positions of the road images
+        roadY1 += 5;
+        roadY2 += 5;
+  
+        // If the first road image has moved out of the canvas, resetting its position above the second image
+        if (roadY1 > canvas.height) {
+          roadY1 = roadY2 - canvas.height;
+        }
+  
+        // If second road image has moved out of the canvas, resetting its position above the first image
+        if (roadY2 > canvas.height) {
+          roadY2 = roadY1 - canvas.height;
+        }
+  
+        requestAnimationFrame(updateGame);
+      }
+  
+      let roadY1 = 0;
+      let roadY2 = -canvas.height;
+  
+      //  Adding event listener for keydown event
+      document.addEventListener("keydown", event => {
+        if (event.code === "ArrowLeft") {
+          // Move the car to the left
+          carX -= 10;
+          if (carX < 0) {
+            carX = 0; // to Keep car within left boundary
+          }
+        }
+        else if (event.code === "ArrowRight") {
+          // Move car to the right
+          carX += 10;
+          if (carX + carWidth > canvas.width) {
+            carX = canvas.width - carWidth; // Keep car within right boundary
+          }
+        }
+      });
+      
+      updateGame();
+    }}
