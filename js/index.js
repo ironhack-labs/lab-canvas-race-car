491418330d292b4f18c409a75658d9d365981c9d
Lena Imdahl <mail@lenaimdahl.de>
2023-03-04 13:05:51 +0100

    fixed movement of obstacles

--- js/index.js
+++ js/index.js
@@ -1,20 +1,49 @@
-//Iteration 1, 2
+class obstacle {
+  /* Constructor expects parameters for
+  fill color, x and y coordinates that
+  will be used to initialize class properties.
+  */
+  constructor(cColor, x, y) {
+    this.color = cColor;
+    this.x = x;
+    this.y = y;
+    this.speed = 0;
+  }
+
+  start(speed) {
+    // method expects parameter!
+    this.speed = speed;
+  }
+
+  display() {
+    // method!
+    fill(this.color);
+    rect(this.x, this.y, 100, 20);
+  }
+
+  move() {
+    //animates the shapes in a straight line
+    this.y += this.speed;
+
+    //moves rect randomly back when they move off the canvas
+    if (this.y > 700) {
+      this.y = random(-500, -100) + random(100, 500);
+      this.x = random(50, 700);
+    }
+  }
+}
+
 let img;
 let car;
 let carX = 0;
 const initialX = 200;
-//obstacles
-let oneRectX = 70;
-let oneRectY = -50;
 
-let twoRectX = 310;
-let twoRectY = -250;
+////obstacles
 
-let threeRectX = 250;
-let threeRectY = 100;
-
-let fourRectX = 100;
-let fourRectY = 250;
+let obs1;
+let obs2;
+let obs3;
+let obs4;
 
 function preload() {
   img = loadImage("../images/road.png");
@@ -23,6 +52,15 @@ function preload() {
 
 function setup() {
   createCanvas(480, 740);
+  obs1 = new obstacle("#cf2558", 70, 300);
+  obs2 = new obstacle("#cf2558", 70, 300);
+  obs3 = new obstacle("#cf2558", 70, 300);
+  obs4 = new obstacle("#cf2558", 70, 300);
+
+  obs1.start(1);
+  obs2.start(1);
+  obs3.start(1);
+  obs4.start(1);
 }
 
 function draw() {
@@ -36,33 +74,17 @@ function draw() {
   const roadLimits = constrain(carX, leftWall, rightWall);
   image(car, roadLimits, 550, 50, 100);
 
-  // rectangle
-  fill(color("#cf2558"));
-  rect(oneRectX, oneRectY, 200, 20);
-  rect(twoRectX, twoRectY, 100, 20);
-  rect(threeRectX, threeRectY, 150, 20);
-  rect(fourRectX, fourRectY, 100, 20);
-  //animates the shapes in a straight line
-  oneRectY++;
-  twoRectY++;
-  threeRectY++;
-  fourRectY++;
+  obs1.display();
+  obs2.display();
+  obs3.display();
+  obs4.display();
 
-  //moves the shapes back when they move off the canvas
-  if (oneRectY > 700) {
-    oneRectY = -50;
-  }
-  if (twoRectY > 700) {
-    twoRectY = -50;
-  }
-  if (threeRectY > 700) {
-    threeRectY = -50;
-  }
-  if (fourRectY > 700) {
-    fourRectY = -50;
-  }
+  obs1.move();
+  obs2.move();
+  obs3.move();
+  obs4.move();
 }
-//Iteration 3
+//keybord movement
 function keyPressed() {
   console.log("Hello some key was pressed!");
   console.log(keyCode);
@@ -71,7 +93,6 @@ function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
     carX += 20;
   }
-
   if (keyCode === LEFT_ARROW) {
     carX -= 20;
   }
