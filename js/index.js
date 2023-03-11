df2ea6c1e0a5246acd8ca206591ee9f8003617bf
Cedric Aim <cedric.aim@gmail.com>
2023-03-11 12:47:12 +0100

    iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
+    createRoad();
     startGame();
   };
 
   function startGame() {}
 };
+function createRoad() {
+  const img = document.createElement("img");
+  img.src = "/images/road.png";
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  };
+}
