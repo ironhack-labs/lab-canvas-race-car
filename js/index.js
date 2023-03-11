f10703ff0fdc27a3c37da30627ed1a43ac8a7e91
byref17 <124827023+byref17@users.noreply.github.com>
2023-03-11 15:23:38 +0100

    iteration 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
 
-  function startGame() {
-  const road = document.querySelector('#canvas');  
-  road.style.backgroundImage= 'url("../images/road.png")'
-  road.style.backgroundSize= "cover"
-  }
+document.getElementById('start-button').onclick = () => {
+  startGame();
 };
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d')
+
+function startGame() {
+  canvas.style.backgroundImage = 'url("../images/road.png")'
+  canvas.style.backgroundSize = "cover"
+}
+
+class Car {
+  constructor(x0, y0) {
+    this.x = x0;
+    this.y = y0;
+
+    const img = document.createElement('img'); // <img>
+    img.src = "../images/car.png";
+    this.image = img
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, 60, 105)
+  }
+
+}
+const car = new Car(220, 600) // { x: , y: , draw: f() }
+car.draw()
+
+
+
+
+
+
+
+
