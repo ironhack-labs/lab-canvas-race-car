ec484d580c6b7b27c67cc531369d719d907e3b6b
Rayane <alirayane14@gmail.com>
2023-03-11 12:55:11 +0100

    iteration 2

--- js/index.js
+++ js/index.js
@@ -8,5 +8,11 @@ const road = new Image();
 road.src = '../images/road.png';
 road.onload = () => ctx.drawImage(road, 0, 0, 500, 700);
 
+const car = new Image();
+car.src = '../images/car.png';
+car.onload = () => ctx.drawImage(car, 250,450, 50, 80);
+
+
+
   function startGame() {}
 };
