a9e3a0292e961362aee8ed1a2875d2f3d3b3c665
Cedric Aim <cedric.aim@gmail.com>
2023-03-12 15:58:24 +0100

    iteration 6 avec gestion des collisions et affichage du score

--- js/index.js
+++ js/index.js
@@ -1,42 +1,29 @@
-class Road {
-  constructor(x, y) {
+class Component {
+  constructor(x, y, imageSrc, width, height) {
     this.x = x;
     this.y = y;
-
-    const img = document.createElement("img");
-    img.src = "../images/road.png";
-    img.addEventListener("load", () => {
-      this.image = img;
-    });
+    this.image = document.createElement("img");
+    this.image.src = imageSrc;
+    this.width = width;
+    this.height = height;
   }
   draw() {
-    if (!this.image) return;
-    ctx.drawImage(
+    myGameArea.ctx.drawImage(
       this.image,
       this.x,
       this.y,
-      myGameArea.canvas.width,
-      myGameArea.canvas.height
+      this.width,
+      this.height
     );
   }
 }
 
-class Car {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+class Car extends Component {
+  constructor(x, y, imageSrc, width, height) {
+    super(x, y, imageSrc, width, height);
     this.speedX = 0;
+  }
 
-    const img = document.createElement("img");
-    img.src = "../images/car.png";
-    img.addEventListener("load", () => {
-      this.image = img;
-    });
-  }
-  draw() {
-    if (!this.image) return;
-    ctx.drawImage(this.image, this.x, this.y, 40, 100);
-  }
   newPos() {
     this.x += this.speedX;
   }
@@ -53,7 +40,12 @@ class Car {
     return this.height + this.y;
   }
   checkCrashWith(obstacle) {
-    return false;
+    return (
+      this.right() > obstacle.left() &&
+      this.top() < obstacle.bottom() &&
+      this.bottom() > obstacle.top() &&
+      this.left() < obstacle.right()
+    );
   }
 }
 class Obstacle {
@@ -66,8 +58,8 @@ class Obstacle {
   }
 
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.fillStyle = this.color;
+    myGameArea.ctx.fillStyle = this.color;
+    myGameArea.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   top() {
     return this.y;
@@ -86,26 +78,38 @@ class Obstacle {
 
 const myGameArea = {
   canvas: document.querySelector("#canvas"),
+  ctx: this.canvas.getContext("2d"),
   frames: 0,
   gameOver: false,
+  score: 0,
+  clear: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.gameOver = false;
+  },
 };
 
-const ctx = myGameArea.canvas.getContext("2d");
-
-const car = new Car(230, 580);
-const road = new Road(0, 0);
+const car = new Car(230, 580, "../images/car.png", 40, 100);
+const road = new Component(
+  0,
+  0,
+  "../images/road.png",
+  myGameArea.canvas.width,
+  myGameArea.canvas.height
+);
 const obstacles = [];
 
 function startGame() {
+  myGameArea.clear();
+  console.log("reset");
   updateGame();
 }
 
 function updateGame() {
-  ctx.clearRect(0, 0, myGameArea.canvas.width, myGameArea.canvas.height);
   road.draw();
   car.draw();
   car.newPos();
   updateObstacles();
+  printScore();
   checkGameOver();
   if (!myGameArea.gameOver) requestAnimationFrame(updateGame);
 }
@@ -124,6 +128,11 @@ function updateObstacles() {
   }
 
   obstacles.forEach((obstacle) => {
+    // Supprime les obstacles qui sont hors de l'air de jeu et incrémente le score
+    if (obstacle.y > myGameArea.canvas.height) {
+      obstacles.splice(obstacles.indexOf(obstacle), 1);
+      myGameArea.score++;
+    }
     obstacle.draw();
     obstacle.y += 1;
   });
@@ -133,6 +142,24 @@ function checkGameOver() {
   myGameArea.gameOver = obstacles.some((obstacle) =>
     car.checkCrashWith(obstacle)
   );
+  if (myGameArea.gameOver) printGameOver();
+}
+
+function printScore() {
+  myGameArea.ctx.fillStyle = "white";
+  myGameArea.ctx.font = "30px Arial";
+  myGameArea.ctx.fillText(`Score : ${myGameArea.score}`, 65, 30);
+}
+
+function printGameOver() {
+  myGameArea.ctx.fillStyle = "black";
+  myGameArea.ctx.fillRect(0, 200, 500, 300);
+  myGameArea.ctx.font = "48px Arial";
+  myGameArea.ctx.fillStyle = "red";
+  myGameArea.ctx.fillText("Game Over", 120, 350);
+  myGameArea.ctx.fillStyle = "white";
+  myGameArea.ctx.fillText("Your final Score :", 80, 400);
+  myGameArea.ctx.fillText(myGameArea.score, 230, 450);
 }
 
 window.onload = () => {
@@ -144,10 +171,10 @@ window.onload = () => {
 document.addEventListener("keydown", function (event) {
   switch (event.code) {
     case "ArrowLeft":
-      if (car.x > 40) car.speedX -= 1;
+      if (car.x > 40) car.speedX = -1;
       break;
     case "ArrowRight":
-      if (car.x < myGameArea.canvas.width - 80) car.speedX += 1;
+      if (car.x < myGameArea.canvas.width - 80) car.speedX = 1;
       break;
   }
 });
