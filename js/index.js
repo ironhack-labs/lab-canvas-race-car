440643d5b4f1caf285fd73241ced0fc5d666e401
Cedric Aim <cedric.aim@gmail.com>
2023-03-12 19:45:53 +0100

    ajustement sur la vitesse de la voiture et la gestion des bords

--- js/index.js
+++ js/index.js
@@ -1,31 +1,9 @@
 class Component {
-  constructor(x, y, imageSrc, width, height) {
+  constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
-    this.image = document.createElement("img");
-    this.image.src = imageSrc;
     this.width = width;
     this.height = height;
-  }
-  draw() {
-    myGameArea.ctx.drawImage(
-      this.image,
-      this.x,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-}
-
-class Car extends Component {
-  constructor(x, y, imageSrc, width, height) {
-    super(x, y, imageSrc, width, height);
-    this.speedX = 0;
-  }
-
-  newPos() {
-    this.x += this.speedX;
   }
   top() {
     return this.y;
@@ -39,6 +17,28 @@ class Car extends Component {
   bottom() {
     return this.height + this.y;
   }
+}
+class ComponentImage extends Component {
+  constructor(x, y, imageSrc, width, height) {
+    super(x, y, width, height);
+    this.image = document.createElement("img");
+    this.image.src = imageSrc;
+    this.width = width;
+    this.height = height;
+  }
+  draw() {
+    myGameArea.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
+class Car extends ComponentImage {
+  constructor(x, y, imageSrc, width, height) {
+    super(x, y, imageSrc, width, height);
+    this.speedX = 0;
+  }
+  newPos() {
+    this.x += this.speedX;
+  }
+
   checkCrashWith(obstacle) {
     return (
       this.right() > obstacle.left() &&
@@ -48,31 +48,14 @@ class Car extends Component {
     );
   }
 }
-class Obstacle {
+class Obstacle extends Component {
   constructor(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
+    super(x, y, width, height);
     this.color = color;
-    this.x = x;
-    this.y = y;
   }
-
   draw() {
     myGameArea.ctx.fillStyle = this.color;
     myGameArea.ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-  top() {
-    return this.y;
-  }
-
-  right() {
-    return this.x + this.width;
-  }
-  left() {
-    return this.x;
-  }
-  bottom() {
-    return this.height + this.y;
   }
 }
 
@@ -82,36 +65,43 @@ const myGameArea = {
   frames: 0,
   gameOver: false,
   score: 0,
+  car: null,
+  road: null,
+  obstacles: [],
   clear: function () {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.car = new Car(230, 580, "../images/car.png", 40, 100);
+    this.road = new ComponentImage(
+      0,
+      0,
+      "../images/road.png",
+      myGameArea.canvas.width,
+      myGameArea.canvas.height
+    );
+    this.frames = 0;
+    this.score = 0;
     this.gameOver = false;
+    this.obstacles = [];
   },
 };
 
-const car = new Car(230, 580, "../images/car.png", 40, 100);
-const road = new Component(
-  0,
-  0,
-  "../images/road.png",
-  myGameArea.canvas.width,
-  myGameArea.canvas.height
-);
-const obstacles = [];
-
 function startGame() {
   myGameArea.clear();
-  console.log("reset");
   updateGame();
 }
 
 function updateGame() {
-  road.draw();
-  car.draw();
-  car.newPos();
+  updateRoadAndCar();
   updateObstacles();
   printScore();
   checkGameOver();
   if (!myGameArea.gameOver) requestAnimationFrame(updateGame);
+}
+
+function updateRoadAndCar() {
+  myGameArea.road.draw();
+  myGameArea.car.draw();
+  myGameArea.car.newPos();
 }
 
 function updateObstacles() {
@@ -119,18 +109,16 @@ function updateObstacles() {
   if (myGameArea.frames % 150 === 0) {
     let minWidth = 20;
     let maxWidth = 200;
-    let width = Math.floor(
-      Math.random() * (maxWidth - minWidth + 1) + minWidth
-    );
-    obstacles.push(
+    let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+    myGameArea.obstacles.push(
       new Obstacle(width, 20, "red", 40 + Math.floor(Math.random() * 420), 0)
     );
   }
 
-  obstacles.forEach((obstacle) => {
+  myGameArea.obstacles.forEach((obstacle) => {
     // Supprime les obstacles qui sont hors de l'air de jeu et incrémente le score
     if (obstacle.y > myGameArea.canvas.height) {
-      obstacles.splice(obstacles.indexOf(obstacle), 1);
+      myGameArea.obstacles.splice(myGameArea.obstacles.indexOf(obstacle), 1);
       myGameArea.score++;
     }
     obstacle.draw();
@@ -139,8 +127,8 @@ function updateObstacles() {
 }
 
 function checkGameOver() {
-  myGameArea.gameOver = obstacles.some((obstacle) =>
-    car.checkCrashWith(obstacle)
+  myGameArea.gameOver = myGameArea.obstacles.some((obstacle) =>
+    myGameArea.car.checkCrashWith(obstacle)
   );
   if (myGameArea.gameOver) printGameOver();
 }
@@ -162,29 +150,31 @@ function printGameOver() {
   myGameArea.ctx.fillText(myGameArea.score, 230, 450);
 }
 
-window.onload = () => {
-  document.getElementById("start-button").onclick = () => {
-    startGame();
-  };
-};
-
 document.addEventListener("keydown", function (event) {
   switch (event.code) {
     case "ArrowLeft":
-      if (car.x > 40) car.speedX = -1;
+      if (myGameArea.car.x > 40) myGameArea.car.speedX = -5;
+      else myGameArea.car.x = 40;
       break;
     case "ArrowRight":
-      if (car.x < myGameArea.canvas.width - 80) car.speedX = 1;
+      if (myGameArea.car.x < myGameArea.canvas.width - 80) myGameArea.car.speedX = 5;
+      else myGameArea.car.x = myGameArea.canvas.width - 80;
       break;
   }
 });
 document.addEventListener("keyup", function (event) {
   switch (event.code) {
     case "ArrowLeft":
-      car.speedX = 0;
+      myGameArea.car.speedX = 0;
       break;
     case "ArrowRight":
-      car.speedX = 0;
+      myGameArea.car.speedX = 0;
       break;
   }
 });
+
+window.onload = () => {
+  document.getElementById("start-button").onclick = () => {
+    startGame();
+  };
+};
