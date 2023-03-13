61066e5989ee824b7464cde359a03ca4779f212e
Singabe Djonouma <singabe-xd@hotmail.fr>
2023-03-13 21:25:38 +0100

    done

--- js/index.js
+++ js/index.js
@@ -8,6 +8,17 @@ ctx.drawImage(roadimg, 0, 0, canvas.width, canvas.height);
 };
 roadimg.src = 'images/road.png';
 
+let frames = 0; // variable que l'on va incrementer dans la fonction draw pour eviter un NaN
+
+   function score() {
+    const points = Math.floor(frames / 5);
+    ctx.font = '18px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText(`Points: ${points}`, 100, 50);
+  }
+
+
+
 const voiture = {
   x: 230,
   y: 550,
@@ -19,18 +30,64 @@ voiture.image.onload = function(){
 };
 voiture.image.src = 'images/car.png'
 
+
+class Obstacle {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+
+  update() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  moveDown() {
+    this.y += 5; // cela correspond à la vitesse des objects
+  }
+
+  updateObstaclePosition() {
+    if (this.y > canvas.height) {
+      this.y = -50; // cela permet de renitialiser les obstacles
+      this.x = Math.floor(Math.random() * (canvas.width - 50)); // Random x position
+    }
+  }
+}
+
+
+const obstacles = [];
+
+
+function createObstacles() {
+  for (let i = 0; i < 3; i++) { // cela choisit le nombre d'obstacles
+    const x = Math.floor(Math.random() * (canvas.width - 50)); // Random x position
+    const y = Math.floor(Math.random() * -500); // Random y position above the canvas
+    const obstacle = new Obstacle(50, 50, 'red', x, y); // Create a new obstacle
+    obstacles.push(obstacle); // Add the obstacle to the list
+  }
+}
+createObstacles();
+
 function draw() {
   
   ctx.clearRect(0,0, canvas.width,canvas.height)
   
   ctx.drawImage(roadimg, 0, 0, canvas.width, canvas.height)
   ctx.drawImage(voiture.image, voiture.x, voiture.y,50,100)
- 
-  
+  score();
+  frames++;
+  for (let obstacle of obstacles){
+    obstacle.updateObstaclePosition();   
+    obstacle.update();
+    obstacle.moveDown();
+  }
   
   requestAnimationFrame(draw)
 }
-draw()
+
 
 document.addEventListener('keydown', function (event) {
   console.log('une touche vient d etre appuyee', event.keyCode)
@@ -53,8 +110,10 @@ document.addEventListener('keydown', function (event) {
 })
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
+    draw();
+    
   };
-
-  function startGame() {}
+  
 };
+
+
