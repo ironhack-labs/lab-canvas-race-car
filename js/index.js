74a08e4d8b1f9c1e4045dc3484a481db9da5348b
MijyPark <parkmijy2021@gmail.com>
2023-04-06 23:37:01 +0200

    done.but not actually

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+const canvas = document.querySelector("canvas");
+canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
@@ -5,3 +9,22 @@ window.onload = () => {
 
   function startGame() {}
 };
+
+  const img = new Image();
+  img.onload = function() {
+  ctx.drawImage(img, 0, 0, 500, 700);
+  };
+  img.src = "./images/road.png";
+  ctx.save();
+  
+  ctx.fillStyle = 'red';
+  ctx.save();
+  
+  ctx.fillRect(100,50,150,25);
+  ctx.save();
+  ctx.fillRect(20,200,400,25);
+  ctx.save();
+  ctx.fillRect(30,400,150,25);
+  ctx.save();
+  ctx.fillRect(220,550,250,25);
+  ctx.save();
