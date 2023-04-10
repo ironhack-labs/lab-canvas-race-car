ee8716a1aafc947d42ca2f8c7daba0cf391926dd
tom dav <kobe242tomy@icloud.com>
2023-04-10 20:12:42 +0200

    iteration5

--- js/index.js
+++ js/index.js
@@ -1,83 +1,97 @@
-
-document.getElementById('start-button').onclick = () => {
+document.getElementById('start-button').addEventListener('click', () => {
   startGame();
-};
+});
 
-//GAME AREA
+// GAME AREA
 const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 
-//START
+// START
 function startGame() {
-  canvas.style.backgroundImage = 'url("images/road.png")'
-  canvas.style.backgroundSize = "cover"
-  car.draw()
+  canvas.style.backgroundImage = 'url("images/road.png")';
+  canvas.style.backgroundSize = "cover";
+  car.draw();
+  drawLoop();
 }
 
-//CAR 🏎️
-class Car { 
+// CAR 🏎️
+class Car {
   constructor(x0, y0) {
     this.x = x0;
     this.y = y0;
 
-    const img = document.createElement('img'); // <img>
+    const img = document.createElement('img');
     img.src = "images/car.png";
 
     this.image = img;
-    img.addEventListener('load', () => {   //loading img ou --> img.onload = function(){}
-      this.image = img})
-    }
+    img.addEventListener('load', () => {
+      this.image = img;
+    });
+  }
 
   draw() {
-    if (!this.image) return//si img == "rien" pas de de function
-    ctx.drawImage(this.image, this.x, this.y, 60, 105)//trace l'image "car"
+    if (!this.image) return;
+    ctx.drawImage(this.image, this.x, this.y, 60, 105);
+  }
+}
+// NEW CAR 🏎️🏎️
+let car = new Car(220, 555);
+
+//OBSTACLE 🧱
+class Obstacles{
+  constructor(x,y,h,w,color) {
+    this.x = x;
+    this.y = y;
+    this.h = h;
+    this.w = w;
+    this.color = "red";
+  }
+
+  draw(){
+    ctx.fillRect(this.x, this.y, this.w, this.h)
+    ctx.fillStyle = this.color
   }
 
   update() {
-    
+    this.y += 2; // Changez cette valeur pour modifier la vitesse de déplacement des obstacles
   }
 
-}
-
-//NEW CAR 🏎️
-let car = new Car(220, 555) // { x: , y: , draw: f() }
+} 
+let obstacle = new Obstacles  (113, 0, 26, 124, "red");
 
 
 
-//ANIMATION LOOP (+Update??)
-function drawLoop() {
-  ctx.clearRect(0,0, 500,700) // 🧽
-  car.draw()
-  requestAnimationFrame(drawLoop)// draw & clear img 16FPS
-}
-
-document.addEventListener('keydown', function (event) {
-  console.log('keydown!!!!!!!!' ,event.keycode)
-  switch (event.Keycode) {
-
-    case 37:
-      console.log("gauche")//car.x -= 100
-      break;
-    case 39:
-      console.log("gauche")//car.x += 100
-      break;
-  }
-})
-
-  /*
-
-  pourquoi ca ne reagit pas à l'Event...
-
-    ????????????
-    newPos(){
-    this.x += ; 
-    this.y += ;
+// ADD KEY EVENT LISTENER
+function update() {
+  document.addEventListener('keydown', function (event) {
+    console.log('keydown!!!!!!!!', event.keyCode);
+    switch (event.keyCode) {
+      case 37:
+        console.log("gauche");
+        car.x -= 50;
+        break;
+      case 39:
+        console.log("droite");
+        car.x += 50;
+        break;
     }
-    ????????????
+  });
+}
+
+update();
+
+// ANIMATION LOOP
+function drawLoop() {
+  ctx.clearRect(0, 0, 500, 700);
+  car.draw();
+  obstacle.draw();
+  obstacle.update();
+  requestAnimationFrame(drawLoop);
+}
 
 
 
-    */
+
 
 
 
