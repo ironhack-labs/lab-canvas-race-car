d0836a9629142e85a3f2ea22301bea508b303d3b
Ana Rak <anarak@Anas-MBP.lan>
2023-04-21 08:31:37 +0100

    done

---
+++ js/game.js
@@ -0,0 +1,47 @@
+class Game {
+  constructor(ctx, width, height, player) {
+    this.ctx = ctx;
+    this.width = width;
+    this.height = height;
+    this.player = player;
+    this.intervalId = null;
+    this.frames = 0;
+    this.enemies = [];
+  }
+
+  start() {
+    this.intervalId = setInterval(this.update, 10);
+  }
+  update = () => {
+    this.frames++;
+    this.clear();
+    this.player.newPos();
+    this.player.draw();
+
+    //   this.updateEnemies();
+    //   this.checkGameOver();
+  };
+  //Stops the game
+  stop() {
+    //   clearInterval(this.intervalId);
+  }
+  //Clears canvas
+  clear() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  //updates the enemies
+  updateEnemies() {}
+
+  checkGameOver() {
+    const crashed = this.enemies.some((enemy) => {
+      return this.player.crashWith(enemy);
+    });
+    if (crashed) {
+      this.stop();
+      this.fillStyle = "red";
+      this.ctx.font = "72px Arial";
+      this.ctx.fillText("Game Over", 0, this.height / 2);
+    }
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,53 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const player = new Player(212.5, 500, 75, 75, ctx);
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const game = new Game(ctx, canvas.width, canvas.height, player);
+    game.start();
+  }
 };
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      player.speedX -= 1;
+      break;
+    case "ArrowRight":
+      player.speedX += 1;
+      break;
+  }
+});
+
+//Stop Speed
+
+document.addEventListener("keyup", () => {
+  player.speedX = 0;
+  player.speedY = 0;
+});
+
+// class Player{
+//   constructor(x,y,w,h,ctx){
+//       this.x = x;
+//       this.y = y;
+//       this.w = w;
+//       this.h = h;
+//       this.color = color;
+//       this.ctx = ctx;
+//       this.speedX = 0;
+//       this.speedY = 0;
+//   }
+
+//   draw() {
+//     const playerImage = new Image();
+//     playerImg.src = '../images/car.png';
+//     this.img = playerImage;
+//   }
+
+// }
