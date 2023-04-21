cc77e6196c55d5ac83be8ad08c23ec687b42c79e
Gui Dantas <guicha2002@gmail.com>
2023-04-21 10:37:04 +0100

    LAB

---
+++ js/game.js
@@ -0,0 +1,72 @@
+class Game {
+    constructor(ctx, width, height, player){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+        this.player = player;
+        this.intervalId = null;
+        this.frames = 0;
+        this.enemies = []
+        //road image
+        const roadImg = new Image();
+        roadImg.src = "../images/road.png";
+        this.img= roadImg;
+    }
+
+    start(){
+        this.intervalId = setInterval(this.update, 10)
+    }
+
+    update = () => {
+        this.frames++;
+        this.drawRoad();
+        this.player.newPos();
+        this.player.draw();
+        this.updateEnemies();
+        this.checkGameOver();
+    }
+
+    //Stops game
+    stop(){
+        clearInterval(this.intervalId);
+    }
+
+    //Clears Canvas
+    drawRoad(){
+        this.ctx.drawImage(this.img, 0, 0, 500, 700);
+    }
+
+    //Update Enemies
+    updateEnemies() {
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].y += 1; // Enemy moves down
+            this.enemies[i].draw(); // continue to draw enemy
+        }
+    
+        if (this.frames % 200 === 0) {
+            let y = -100; // Start above the canvas
+            let minWidth = 50; // at least 20px of min width
+            let maxWidth = 100; // max width 
+    
+            let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+    
+            let x = Math.floor(Math.random() * (this.width - width));
+    
+            this.enemies.push(new Component(x, y, width, 50, 'red', this.ctx));
+        }
+    }
+    
+
+    checkGameOver(){
+        const crashed = this.enemies.some((enemy) => {
+            return this.player.crashWith(enemy);
+        });
+
+        if(crashed){
+            this.stop();
+            this.fillStyle = 'red';
+            this.ctx.font = '72px Arial';
+            this.ctx.fillText('Game Over', 0, this.height/2);
+        }
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
+// JS Initialization
+console.log("JS is loaded");
+
+// Canvas Initialization
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    startGame();
+    const game = new Game(ctx, canvas.width, canvas.height, player);
+    game.start();
   };
+}
 
-  function startGame() {}
-};
+
+// Create tha Player
+const player = new Player(212, 550, 75, 125, ctx);
+
+
+// Move the Player
+document.addEventListener("keydown", (e)=>{
+    switch (e.code){
+        case "ArrowUp":
+            player.speedY -=1;
+            break;
+        case "ArrowDown":
+            player.speedY +=1;
+            break;
+        case "ArrowLeft":
+            player.speedX -=1;
+            break;
+        case "ArrowRight":
+            player.speedX +=1;
+            break;
+    }
+})
+
+// Stop Speed
+document.addEventListener("keyup", () => {
+    player.speedX = 0;
+    player.speedY = 0;
+})
+
