caca53059cb39632d650f1fa1af7bfe6606c2545
Francisco Branco <franciscomcb782@gmail.com>
2023-04-21 09:01:54 +0100

    done

---
+++ js/components.js
@@ -0,0 +1,59 @@
+class Car {
+  constructor() {
+    this.x = 200;
+    this.y = 600;
+
+    //Car image
+    const carImg = new Image();
+    carImg.addEventListener("load", () => {
+      this.carImg = carImg;
+    });
+    carImg.src = "../images/car.png";
+  }
+
+  moveLeft() {
+    this.x -= 10;
+  }
+
+  moveRight() {
+    this.x += 10;
+  }
+
+  draw() {
+    ctx.drawImage(this.carImg, this.x, this.y, 40, 80);
+  }
+
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.h;
+  }
+
+  left() {
+    return this.x;
+  }
+
+  right() {
+    return this.x + this.w;
+  }
+
+  moveLimiter() {
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    if (this.x >= canvas.width - 40) {
+      this.x = canvas.width - 40;
+    }
+  }
+
+  crashWith(enemy) {
+    return (
+      this.bottom() > enemy.top() &&
+      this.top() < enemy.bottom() &&
+      this.left() < enemy.right() &&
+      this.right() > enemy.left()
+    );
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const playerCar = new Car();
+const game = new Game(ctx, canvas.width, canvas.height, playerCar);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      playerCar.moveLeft();
+      break;
+    case "ArrowRight":
+      playerCar.moveRight();
+      break;
+  }
+});
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    game.start();
+  }
 };
