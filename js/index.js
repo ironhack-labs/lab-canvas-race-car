6f06f472d0f0ee7a623a818304b8e6b45f78f4dc
Elnaz Fe <elnaz.farrokhi1988@gmail.com>
2023-04-21 09:08:51 +0100

    week2day4

--- js/index.js
+++ js/index.js
@@ -3,5 +3,33 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
-};
+  function startGame() {
+    // Get a reference to the canvas element and its context
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    // Load the road image
+    const roadImage = new Image();
+    roadImage.src = 'images/road.png';
+
+    // Load the road image
+    const carImage = new Image();
+    carImage.src = 'images/car.png';
+
+      roadImage.onload = function() {
+      carImage.onload = function() {
+
+        // Draw the road image on the canvas
+        ctx.drawImage(roadImage, 0, 0, canvas.width, canvas.height);
+  
+        // Draw the car image on the canvas
+        const carWidth = 60;
+        const carHeight = 120;
+        const carX = canvas.width / 2 - carWidth / 2;
+        const carY = canvas.height - carHeight;
+  
+        ctx.drawImage(carImage, carX, carY, carWidth, carHeight);
+      }
+    }
+  }
+}
