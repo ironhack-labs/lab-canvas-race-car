389903b085ab29f6e5b7a6a25359408814cf90dd
Bernard Cardoso <be1.0@hotmail.com>
2023-05-01 13:59:23 +0100

    First Iteration!

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+
+    this.speedX = 0; // used to update the X position
+    this.speedY = 0; // used to update the Y position
+  }
+}
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    
+  }
 };
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const player = new Component(30, 30, 'red', 0, 110);
