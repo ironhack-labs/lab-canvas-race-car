1cb72a99f86e53b3ac529c97cd71f924ff01de28
churrundo <churrundo@gmail.com>
2023-05-06 17:48:33 -0600

    finish iteration 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
-
-  function startGame() {}
 };
+
+class Background {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.img = new Image();
+    this.img.src = "../images/road.png";
+    this.img.onload = () => {
+      this.draw();
+    };
+  }
+
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+}
+
+function startGame() {
+  new Background
+}
