a0474ca73958842072d0cba09c3312b7d1dea219
churrundo <churrundo@gmail.com>
2023-05-07 19:03:48 -0600

    add movement to the car

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let gameFrames=0
 
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
@@ -41,9 +42,40 @@ class Car {
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
+
+  moveLeft() {
+    if(this.x>45){
+    this.x -= 7;
+    }
+  }
+  moveRight() {
+    if(this.x<400){
+    this.x += 7;
+    }
+  }
 }
 
 function startGame() {
-  new Background;
-  new Car;
+  gameArea = new Background();
+  playerCar = new Car();
+  gameLoop()
 }
+
+function gameLoop() {
+  gameFrames++;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  gameArea.draw();
+  playerCar.draw();
+  playerCar.x
+  console.log(playerCar.x)
+  requestAnimationFrame(gameLoop)
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key == "ArrowLeft") {
+    playerCar.moveLeft();
+  }
+  if (e.key == "ArrowRight") {
+    playerCar.moveRight();
+  }
+});
