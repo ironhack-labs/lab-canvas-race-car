a5ea33f4c28ebae1c663251637e6afb73ee4c084
daliaelena <dalia.geo89@gmail.com>
2023-05-08 11:45:55 -0600

    ya se hace el movimiento del road

--- js/index.js
+++ js/index.js
@@ -4,30 +4,69 @@ window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
+
+     
 
   function startGame() {
 
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
-    const roadImage = new Image();
+    const roadImage = new Image();{}
     roadImage.src = 'images/road.png';
+
+    
 
     const carImage = new Image();
     carImage.src = 'images/car.png';
 
-      roadImage.onload = function() {
-      carImage.onload = function() {
+    //   roadImage.onload = function() {
+    //   carImage.onload = function() {
 
-        ctx.drawImage(roadImage, 0, 0, canvas.width, canvas.height);
+    //     ctx.drawImage(roadImage, 0, 0, canvas.width, canvas.height);
   
-        const carWidth = 60;
-        const carHeight = 120;
-        const carX = canvas.width / 2 - carWidth / 2;
-        const carY = canvas.height - carHeight;
+    //     const carWidth = 60;
+    //     const carHeight = 120;
+    //     const carX = canvas.width / 2 - carWidth / 2;
+    //     const carY = canvas.height - carHeight;
+
+    //     ctx.drawImage(carImage, carX, carY, carWidth, carHeight);
+
+    //   }
+    // }
+
+    const roadImageMove = {
+      img:roadImage,
+      y:0,
+      speed: -1,
+      draw: function(){
+          this.y--; //Esto le resta un pixel cada segundo 
+          ctx.drawImage(this.img, 0, this.y, canvas.width, canvas.height) //Aqui pintamos la img que se sale 
+          ctx.drawImage(this.img, 0, this.y + canvas.width, canvas.width, canvas.height)            //Acaba en el min 1:07                                                     //aquí pintamos la 'posición de una segunda imagen, que entra conforme sale la priemra
   
-        ctx.drawImage(carImage, carX, carY, carWidth, carHeight);
-      }
-    }
+          }
+        }
+  
+    function updateCanvas(){
+            //limpiar el canvas
+      ctx.clearRect(0,0,canvas.width, canvas.height)
+      //Pintar otra vez el camino en la nueva posición
+        roadImageMove.draw();
+      // roadImageMove.draw();                                                         //min25  && en el 54 otra vez
+      //Ahora hay que hacer el "loop infinito"                                  // minuto 1:02
+      //movemos la imagen a la izquierda, restándole a x(y) un px cada invocación de draw. 
+      //   roadImageMove.draw()
+      // })
+  
+            }
+
+
+        setInterval(updateCanvas, 1000/60)
+    
   }
+
+
+
+
 }
