74e77ee086e07857fe77d5b1921ec1508ad8f261
Luis <luisguile495@gmail.com>
2023-05-13 12:05:23 -0400

    First y Second iteration solved

--- js/index.js
+++ js/index.js
@@ -3,5 +3,22 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const roadImg = new Image();
+    roadImg.src = "./images/road.png"
+    roadImg.onload = function(){
+      ctx.drawImage(roadImg, 0, 0, canvas.width, canvas.height);  
+    }
+  
+    const carImg = new Image();
+    carImg.src = "./images/car.png";
+    const carX = canvas.width / 2 - 25;
+    const carY = canvas.height - 100;
+    carImg.onload = function(){
+      ctx.drawImage(carImg,carX,carY,50,100);
+    }
+  }
 };
