dc9e26d2b47c1edfa36785855160f77c211d4d51
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-06-01 02:26:26 +0200

    New changes

--- js/game.js
+++ js/game.js
@@ -1,30 +1,59 @@
 class Game {
-    constructor(ctx) {
-      this.ctx = ctx;
-      this.background = new Background(this.ctx);
-      this.player = new Player(this.ctx);
-  
-      this.intervalId = undefined;
-    }
-  
-    start() {
-       this.intervalId = setInterval(() => {
-         this.clear();
-         this.draw();
-         this.move();
-       }, 1000 / 60);
-    }
-  
-    draw() {
-      this.background.draw();
-      this.player.draw();
-    }
-  
-    move() {
-    //   this.background.move();
-    }
-  
-    clear() {
-    //   this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.background = new Background(this.ctx);
+    this.player = new Player(this.ctx);
+
+    this.intervalId = null;
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      this.clear();
+      this.draw();
+      this.move();
+    }, 1000 / 60);
+  }
+
+  draw() {
+    this.background.draw();
+    this.player.draw();
+  }
+
+  move() {
+    //this.background.move();
+    this.player.move();
+  }
+
+  clear() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
+  handleKeyDown(event) {
+    if (!event.repeat) {
+      switch (event.code) {
+        case "ArrowLeft":
+          this.player.speedX = -5;
+          break;
+        case "ArrowRight":
+          this.player.speedX = 5;
+          break;
+        // case "ArrowUp":
+        //   this.player.speedY = -5;
+        //   break;
+        // case "ArrowDown":
+        //   this.player.speedY = 5;
+        //   break;
+      }
     }
   }
+
+  handleKeyUp(event) {
+    if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
+      this.player.speedX = 0;
+    }
+    // if (event.code === "ArrowUp" || event.code === "ArrowDown") {
+    //   this.player.speedY = 0;
+    // }
+  }
+}
--- js/player.js
+++ js/player.js
@@ -1,24 +1,43 @@
 class Player {
-    constructor(ctx) {
-      this.ctx = ctx;
-      this.image = new Image();
-      this.image.src = 'images/car.png';
-      this.width = 35;
-      this.height = this.width * this.image.height / this.image.width;
-      this.image.onload = () => {
-        this.isReady = true;
-      };
-    }
-  
-    draw() {
-      if (this.isReady) {
-        this.ctx.drawImage(
-          this.image,
-          232,
-          600,
-          this.width,
-          this.height
-        );
-      }
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.image = new Image();
+    this.image.src = "images/car.png";
+    this.width = 35;
+    this.height = (this.width * this.image.height) / this.image.width;
+    this.image.onload = () => {
+      this.isReady = true;
+    };
+
+    this.speedX = 232;
+    //this.speedY = 0;
+  }
+
+  draw() {
+    if (this.isReady) {
+      this.ctx.drawImage(this.image, 232, 600, this.width, this.height);
     }
   }
+
+  move() {
+    this.x += this.speedX;
+    //this.y += this.speedY;
+
+    if (this.x <= 0) {
+      this.x = 0;
+    }
+
+    if (this.x >= this.ctx.canvas.width - this.width) {
+      this.x = this.ctx.canvas.width - this.width;
+    }
+
+//      if (this.y <= 0) {
+//        this.y = 0;
+//      }
+
+//      if (this.y >= this.ctx.canvas.height - this.height) {
+//        this.y = this.ctx.canvas.height - this.height;
+//      }
+    }
+}
+
