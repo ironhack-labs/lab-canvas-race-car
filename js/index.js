0a99465b704e5ddef45d44bca6cf67d8517aa450
9sheila <shey.hdez1999@gmail.com>
2023-06-03 14:20:42 +0200

    casi terminado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const game = new Game(ctx);
+const startButton = document.querySelector('#start-button');
+
+game.start();
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
+    startButton.setAttribute('disabled', true);
   };
 
-  function startGame() {}
+document.addEventListener("keydown", event => {
+  if (event.code === "ArrowRight") {
+    game.player.speedX += 5;
+ }
+ if (event.code === "ArrowLeft"){
+  game.player.speedX -= 5;
+ }
+})
+document.addEventListener('keyup', event => {
+  if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+    game.player.speedX = 0;
+  }
+})
+  function startGame() {
+    game.start();
+  }
 };
